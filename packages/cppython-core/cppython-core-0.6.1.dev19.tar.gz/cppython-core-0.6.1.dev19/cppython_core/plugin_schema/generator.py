54601c65bca87caaa90403b4e8ed2918d97b0876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:19 +0000

    Add package cppython-core/0.6.1.dev19/cppython-core-0.6.1.dev19.tar.gz

---
+++ packages/cppython-core/cppython-core-0.6.1.dev19.tar.gz/cppython-core-0.6.1.dev19/cppython_core/plugin_schema/generator.py
@@ -0,0 +1,40 @@
+"""Generator data plugin definitions"""
+from __future__ import annotations
+
+from abc import abstractmethod
+from typing import Any, TypeVar
+
+from pydantic import Field
+from pydantic.types import DirectoryPath
+
+from cppython_core.schema import DataPlugin, PluginGroupData
+
+
+class GeneratorData(PluginGroupData):
+    """Base class for the configuration data that is set by the project for the generator"""
+
+    root_directory: DirectoryPath = Field(description="The directory where the pyproject.toml lives")
+
+
+class Generator(DataPlugin[GeneratorData]):
+    """Abstract type to be inherited by CPPython Generator plugins"""
+
+    @staticmethod
+    def group() -> str:
+        """The plugin group name as used by 'setuptools'summary
+
+        Returns:
+            The group name
+        """
+        return "generator"
+
+    @abstractmethod
+    def sync(self, results: list[Any]) -> None:
+        """Synchronizes generator files and state with the generators input
+
+        Args:
+            results: List of information gathered from providers
+        """
+
+
+GeneratorT = TypeVar("GeneratorT", bound=Generator)
