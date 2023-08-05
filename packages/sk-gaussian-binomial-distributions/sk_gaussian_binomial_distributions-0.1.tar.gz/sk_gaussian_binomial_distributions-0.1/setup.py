581f4855940d859c39c86d239cedf02237782b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:16 +0000

    Add package sk-gaussian-binomial-distributions/0.1/sk_gaussian_binomial_distributions-0.1.tar.gz

---
+++ packages/sk-gaussian-binomial-distributions/sk_gaussian_binomial_distributions-0.1.tar.gz/sk_gaussian_binomial_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sk_gaussian_binomial_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sk_gaussian_binomial_distributions'],
+      author='Spyros Karydis',
+      zip_safe=False)
