768cb0f5edb8e5bbd85e5e6028c78fd349464fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:05 +0000

    Add package strings-from-file/0.1/strings_from_file-0.1.tar.gz

---
+++ packages/strings-from-file/strings_from_file-0.1.tar.gz/strings_from_file-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='strings_from_file',
+      version='0.1',
+      description='Get strings from file and rewrite file',
+      packages=['strings_from_file'],
+      author_email='nazarkalashnikovkalashnikov@yandex.ru',
+      zip_safe=False)
