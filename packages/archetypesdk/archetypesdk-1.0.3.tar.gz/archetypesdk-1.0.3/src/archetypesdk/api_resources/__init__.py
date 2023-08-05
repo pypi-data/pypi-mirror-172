b30e2b265d2663125afc67be68d50049d243078f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:28 +0000

    Add package archetypesdk/1.0.3/archetypesdk-1.0.3.tar.gz

---
+++ packages/archetypesdk/archetypesdk-1.0.3.tar.gz/archetypesdk-1.0.3/src/archetypesdk/api_resources/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+
+from archetypewsgi.api_resources.product import Product
+from archetypewsgi.api_resources.endpoint import Endpoint
+from archetypewsgi.api_resources.billable_metric import BillableMetric
+from archetypewsgi.api_resources.customer import Customer
+from archetypewsgi.api_resources.error import APIRequestError
+from archetypewsgi.api_resources.auth import Auth
