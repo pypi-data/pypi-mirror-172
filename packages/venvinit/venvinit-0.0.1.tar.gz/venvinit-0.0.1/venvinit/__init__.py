bcea09ef46bb059ae50f1ef3368e3a082fbea2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:18 +0000

    Add package venvinit/0.0.1/venvinit-0.0.1.tar.gz

---
+++ packages/venvinit/venvinit-0.0.1.tar.gz/venvinit-0.0.1/venvinit/__init__.py
@@ -0,0 +1,15 @@
+"""
+venvinit
+~~~~~~~~
+venvinit is a tool to create a virtual environment and install packages
+from a requirements file.
+
+:copyright (c) 2022 LuxLuth
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'venvinit'
+__author__ = 'LuxLuth'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022 LuxLuth'
+__version__ = '0.0.1'
