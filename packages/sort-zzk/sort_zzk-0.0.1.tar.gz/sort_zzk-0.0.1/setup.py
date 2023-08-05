988e91b913ea514e03f0b19d2f2dd8d551bbd104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:12 +0000

    Add package sort-zzk/0.0.1/sort_zzk-0.0.1.tar.gz

---
+++ packages/sort-zzk/sort_zzk-0.0.1.tar.gz/sort_zzk-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sort_zzk",
+    version="0.0.1",
+    author="ZhangZhikun",
+    author_email="1304944085@qq.com",
+    description="This is a package that sorts by the key of the dictionary in the list using sorted function",
+    # 项目主页
+    # url="",
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+)
