a63f2546a5ebea26f641018b7312068dc2c29bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package DuckBuBi/0.0.1/DuckBuBi-0.0.1.tar.gz

---
+++ packages/DuckBuBi/DuckBuBi-0.0.1.tar.gz/DuckBuBi-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DuckBuBi',
+    version='0.0.1',
+    auther='SunBo',
+    auther_email='sun18238136554@163.com',
+    packages=find_packages()
+)
