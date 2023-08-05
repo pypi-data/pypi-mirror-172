ddc8eb0f8d555e0e6a5a4f8ecac281dcf054447a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:35 +0000

    Add package thefirstock/2.5/thefirstock-2.5-py3-none-any.whl

---
+++ packages/thefirstock/thefirstock-2.5-py3-none-any.whl/thefirstock/thefirstock.py
@@ -0,0 +1,45 @@
+from thefirstock.firstockModules.loginFirstock import firstock_login
+
+from thefirstock.firstockModules.logoutFirstock import firstock_logout
+
+from thefirstock.firstockModules.userDetailsFirstock import firstock_userDetails
+
+from thefirstock.firstockModules.getIndexListFirstock import firstock_getIndexList
+
+from thefirstock.firstockModules.getOptionChainFirstock import firstock_OptionChain
+
+from thefirstock.firstockModules.getQuotesFirstock import firstock_getQuotes
+
+from thefirstock.firstockModules.getSecurityInfoFirstock import firstock_getSecurityInfo
+
+from thefirstock.firstockModules.searchScripsFirstock import firstock_SearchScrips
+
+from thefirstock.firstockModules.spanCalculatorFirstock import firstock_SpanCalculator
+
+from thefirstock.firstockModules.timePriceSeriesFirstock import firstock_TimePriceSeries
+
+from thefirstock.firstockModules.cancelOrderFirstock import firstock_cancelOrder
+
+from thefirstock.firstockModules.holdingFirstock import firstock_Holding
+
+from thefirstock.firstockModules.limitFirstock import firstock_Limits
+
+from thefirstock.firstockModules.modifyOrderFirstock import firstock_ModifyOrder
+
+from thefirstock.firstockModules.orderBookFirstock import firstock_orderBook
+
+from thefirstock.firstockModules.orderMarginFirstock import firstock_orderMargin
+
+from thefirstock.firstockModules.placeOrderFirstock import firstock_placeOrder
+
+from thefirstock.firstockModules.positionBookFirstock import firstock_PositionBook
+
+from thefirstock.firstockModules.productConversionFirstock import firstock_ConvertProduct
+
+from thefirstock.firstockModules.singleOrderHistoryFirstock import firstock_SingleOrderHistory
+
+from thefirstock.firstockModules.tradeBookFirstock import firstock_TradeBook
+
+from thefirstock.firstockModules.basketMarginFirstock import firstock_BasketMargin
+
+from thefirstock.firstockModules.optionGreekFirstock import firstock_OptionGreek
