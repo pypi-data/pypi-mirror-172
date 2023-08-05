b7f3aef29a85303e78ecc998742128bab5bb7034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package akasj/0.0.1/akasj-0.0.2.tar.gz

---
+++ packages/akasj/akasj-0.0.2.tar.gz/akasj-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="akasj",
+    version="0.0.1",
+    author="ShiLuYao",
+    description="Hello Word",
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+)
