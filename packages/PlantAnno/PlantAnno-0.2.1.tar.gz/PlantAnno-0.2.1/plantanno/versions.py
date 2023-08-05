5c199a036c9267f250ae29778d744e0428d84422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:15 +0000

    Add package PlantAnno/0.2.1/PlantAnno-0.2.1.tar.gz

---
+++ packages/PlantAnno/PlantAnno-0.2.1.tar.gz/PlantAnno-0.2.1/plantanno/versions.py
@@ -0,0 +1,24 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.2.1",
+        "features": [
+            "1. update setup.py",
+        ],
+    },
+    {
+        "number": "0.2.0",
+        "features": [
+            "1. add orthoDB mode",
+        ],
+    },
+    {
+        "number": "0.1.0",
+        "features": [
+            "1. PlantAnno init",
+        ],
+    },
+]
