12ef4b2fc9c60c3bb116ffeb47186b97f0b911cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:53 +0000

    Add package arucodetect/1.1/arucodetect-1.1.tar.gz

---
+++ packages/arucodetect/arucodetect-1.1.tar.gz/arucodetect-1.1/setup.py
@@ -0,0 +1,13 @@
+from io import open
+from setuptools import setup
+
+
+setup(
+    name='arucodetect',
+    version='1.1',
+    author='myortf_inside',
+    author_email='faridgarifullin66@gmail.com',
+
+    packages=['arucodetect'],
+    install_requires = ['opencv-python']
+)
