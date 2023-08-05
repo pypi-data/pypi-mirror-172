180a1a43b35debda25de6ebbe38b8c53cce78634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:50 +0000

    Add package RelevanceAI-Workflows-Core/0.1.4/RelevanceAI%20Workflows%20Core-0.1.4.tar.gz

---
+++ packages/RelevanceAI-Workflows-Core/RelevanceAI%20Workflows%20Core-0.1.4.tar.gz/RelevanceAI Workflows Core-0.1.4/workflows_core/engine/stable_engine.py
@@ -0,0 +1,31 @@
+import logging
+from typing import Any
+
+from workflows_core.engine.abstract_engine import AbstractEngine
+
+from tqdm.auto import tqdm
+
+logger = logging.getLogger(__file__)
+
+
+class StableEngine(AbstractEngine):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._show_progress_bar = kwargs.pop("show_progress_bar", True)
+
+    def apply(self) -> Any:
+
+        iterator = self.iterate()
+
+        for chunk in tqdm(
+            iterator,
+            desc=repr(self.operator),
+            disable=(not self._show_progress_bar),
+            total=self.num_chunks,
+        ):
+            new_batch = self.operator(chunk)
+            result = self.update_chunk(new_batch)
+            logging.debug(result)
+
+        return
