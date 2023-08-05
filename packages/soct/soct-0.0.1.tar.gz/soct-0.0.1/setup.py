84ebc40086e3457cf0a16d78d469fa7b9bee8656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package soct/0.0.1/soct-0.0.1.tar.gz

---
+++ packages/soct/soct-0.0.1.tar.gz/soct-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='soct',
+    version='0.0.1',
+    author='XiaoShuang',
+    author_email='3214311865@qq.com',
+    description='This project provides a way to sort',
+    packages=find_packages()
+)
