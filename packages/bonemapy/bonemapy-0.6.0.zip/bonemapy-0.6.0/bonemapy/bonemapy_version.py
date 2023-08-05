817e090dfc50e7c832e0d9647f8b2144c27cf085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:54 +0000

    Add package bonemapy/0.6.0/bonemapy-0.6.0.zip

---
+++ packages/bonemapy/bonemapy-0.6.0.zip/bonemapy-0.6.0/bonemapy/bonemapy_version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2022 Michael Hogg
+
+# This file is part of bonemapy - See LICENSE.txt for information on usage and redistribution
+
+# Version and date
+version     = '0.6.0'
+releasedate = '2022-10-17'
