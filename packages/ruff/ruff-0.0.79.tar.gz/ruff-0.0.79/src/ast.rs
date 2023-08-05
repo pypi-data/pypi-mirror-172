30b8245918211393506e66998e3a7d03af3b0665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:06 +0000

    Add package ruff/0.0.79/ruff-0.0.79.tar.gz

---
+++ packages/ruff/ruff-0.0.79.tar.gz/ruff-0.0.79/src/ast.rs
@@ -0,0 +1,6 @@
+pub mod checkers;
+pub mod helpers;
+pub mod operations;
+pub mod relocate;
+pub mod types;
+pub mod visitor;
---
+++ packages/ruff/ruff-0.0.79.tar.gz/ruff-0.0.79/src/docstrings.rs
@@ -0,0 +1,8 @@
+pub mod definition;
+pub mod extraction;
+mod google;
+mod helpers;
+mod numpy;
+pub mod plugins;
+pub mod sections;
+mod styles;
