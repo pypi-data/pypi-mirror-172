ec279d2d8d4d9efc8c84549f4dea1deca5b89582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:10 +0000

    Add package kalabash-radicale/1.5.7/kalabash_radicale-1.5.7-py2.py3-none-any.whl

---
+++ packages/kalabash-radicale/kalabash_radicale-1.5.7-py2.py3-none-any.whl/kalabash_radicale/static/kalabash_radicale/js/chunk-2d0bdf0b.5010fad0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bdf0b"],{"2dad":function(n,e){FullCalendar.globalLocales.push(function(){"use strict";var n={code:"en-nz",week:{dow:1,doy:4},buttonHints:{prev:"Previous $0",next:"Next $0",today:"This $0"},viewHint:"$0 view",navLinkHint:"Go to $0",moreLinkHint(n){return`Show ${n} more event${1===n?"":"s"}`}};return n}())}}]);
+//# sourceMappingURL=chunk-2d0bdf0b.5010fad0.js.map
