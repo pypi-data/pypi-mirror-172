9e0762d20de08173e2401e229d2bc402992ce9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:06 +0000

    Add package osalgopackage-utsav/1.1.0/osalgopackage_utsav-1.1.0.tar.gz

---
+++ packages/osalgopackage-utsav/osalgopackage_utsav-1.1.0.tar.gz/osalgopackage_utsav-1.1.0/Readme.md
@@ -0,0 +1,10 @@
+## Installation
+
+    `pip install os_shedulingAlgo_utsav`
+
+## USE
+
+    from osalgopackage_utsav import BestShedulingAlgo
+
+    instance=BestShedulingAlgo(pass the size of set of processes)
+    instance.RunAndCompare()
---
+++ packages/osalgopackage-utsav/osalgopackage_utsav-1.1.0.tar.gz/osalgopackage_utsav-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="osalgopackage_utsav",
+    version="1.1.0",
+    description="Get an ides how OS Sheduling Algorithms Work on a set of Process",
+    author="Utsav",
+    packages=['osalgopackage_utsav'],
+    install_requires=[]
+)
