8d0e75292e359997d2b73525518fe6d423292dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:24 +0000

    Add package ezloay/0.0.1/ezloay-0.0.1.tar.gz

---
+++ packages/ezloay/ezloay-0.0.1.tar.gz/ezloay-0.0.1/ezloay/andeng.py
@@ -0,0 +1,12 @@
+"""
+@Project:ezly
+@File:andeng.py
+@Author:邓建祥
+@Date:10:52
+"""
+def grate():
+    print("Msdeng牛逼！")
+
+
+if __name__ == "__main__":
+    grate()
---
+++ packages/ezloay/ezloay-0.0.1.tar.gz/ezloay-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+name="ezloay",
+version="0.0.1",
+author="DengJianxiang",
+author_email="2262532979@qq.com",
+description="This project provides an easy way to recognize faces usingAI",
+# 项目主页
+url="https://github.com/sunjian-github/ezface",
+# 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+packages=find_packages()
+)
