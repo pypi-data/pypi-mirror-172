95a5fd465323328ae0b7e1d70b41a80da2c8c947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:35 +0000

    Add package nmdmail/0.2.0/nmdmail-0.2.0-py3-none-any.whl

---
+++ packages/nmdmail/nmdmail-0.2.0-py3-none-any.whl/nmdmail/helpers.py
@@ -0,0 +1,31 @@
+import re
+
+
+def sanitize_email_address(address):
+    if address is None:
+        return None
+
+    if isinstance(address, (list, tuple)):
+        return [sanitize_email_address(a) for a in address]
+
+    if "," in address:
+        return sanitize_email_address(address.split(","))
+
+    address = address.strip()
+
+    match = re.match("^(.*)<(.*)>$", address)
+    if match:
+        name, address = match.groups()
+        return name.strip(), address.strip()
+    else:
+        return address
+
+
+def is_string(val):
+    return isinstance(val, str)
+
+
+def to_bool(val):
+    if is_string(val):
+        return val.lower() in ("1", "true", "yes", "y")
+    return bool(val)
