5a71d375d256ab2f78611471513eb8b795d952a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:44 +0000

    Add package filter-pyspark/0.2.2/filter_pyspark-0.2.2.tar.gz

---
+++ packages/filter-pyspark/filter_pyspark-0.2.2.tar.gz/filter_pyspark-0.2.2/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='filter_pyspark',
+        version='0.2.2',
+        description='filter rename  in pyspark dataframe',
+        packages=['filter_pyspark'],
+        author_email='ranjitmaity95@gmail.com',
+        zip_safe=False,
+        long_description_content_type="text/markdown",
+        long_description=open('README.md').read(),
+        )
