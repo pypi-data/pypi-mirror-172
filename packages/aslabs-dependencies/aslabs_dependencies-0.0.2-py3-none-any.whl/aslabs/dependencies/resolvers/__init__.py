2a20a7b251a29484dc8a6ed61f0994269e8051b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:19 +0000

    Add package aslabs-dependencies/0.0.2/aslabs_dependencies-0.0.2-py3-none-any.whl

---
+++ packages/aslabs-dependencies/aslabs_dependencies-0.0.2-py3-none-any.whl/aslabs/dependencies/resolvers/__init__.py
@@ -0,0 +1,5 @@
+from .direct_resolver import DirectResolver
+from .instance_resolver import InstanceResolver
+from .hollow_resolver import HollowResolver
+from .resolver_abc import ResolverABC
+from .singleton_resolver_wrapper import SingletonResolverWrapper
