8b907d7e8cb5758fdbca77ef4ab2677d3160fd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:56 +0000

    Add package serpyco-rs/0.1.3/serpyco_rs-0.1.3.tar.gz

---
+++ packages/serpyco-rs/serpyco_rs-0.1.3.tar.gz/serpyco_rs-0.1.3/src/serializer/py.rs
@@ -0,0 +1,206 @@
+use crate::serializer::macros::{call_method, ffi};
+use crate::serializer::types::ITEMS_STR;
+use pyo3::once_cell::GILOnceCell;
+use pyo3::types::PyTuple;
+use pyo3::{ffi, AsPyPointer, Py, PyAny, PyErr, PyObject, PyResult, Python, ToPyObject};
+use pyo3_ffi::Py_ssize_t;
+use std::ffi::CString;
+use std::os::raw::{c_char, c_int};
+use std::ptr::NonNull;
+
+static DECIMAL: GILOnceCell<PyObject> = GILOnceCell::new();
+static BUILTINS: GILOnceCell<PyObject> = GILOnceCell::new();
+static NOT_SET: GILOnceCell<PyObject> = GILOnceCell::new();
+static OBJECT_NEW: GILOnceCell<PyObject> = GILOnceCell::new();
+
+fn _decimal_cls() -> Py<PyAny> {
+    Python::with_gil(|py| {
+        // todo: refactor
+        DECIMAL
+            .get_or_init(py, || {
+                py.import("decimal")
+                    .expect("Error when importing decimal.Decimal")
+                    .getattr("Decimal")
+                    .expect("Error when importing decimal.Decimal")
+                    .to_object(py)
+            })
+            .to_object(py)
+    })
+}
+
+pub fn to_decimal(value: *mut ffi::PyObject) -> PyResult<*mut ffi::PyObject> {
+    let decimal = _decimal_cls();
+    py_object_call1_make_tuple_or_err(decimal.as_ptr(), value)
+}
+
+fn builtins(py: Python) -> &PyAny {
+    BUILTINS
+        .get_or_init(py, || {
+            py.import("builtins")
+                .expect("Error when importing builtins")
+                .into()
+        })
+        .as_ref(py)
+}
+
+pub fn py_len(obj: *mut ffi::PyObject) -> PyResult<Py_ssize_t> {
+    let v = ffi!(PyObject_Size(obj));
+    if v == -1 {
+        Err(Python::with_gil(|py| PyErr::fetch(py)))
+    } else {
+        Ok(v)
+    }
+}
+
+pub fn is_not_set(obj: &PyAny) -> PyResult<bool> {
+    let py = obj.py();
+    let not_set = NOT_SET
+        .get_or_init(py, || {
+            py.import("serpyco_rs")
+                .expect("Error when importing serpyco_rs")
+                .getattr("_describe")
+                .expect("Error when importing serpyco_rs._describe")
+                .getattr("NOT_SET")
+                .expect("Error when importing serpyco_rs._describe.NOT_SET")
+                .into()
+        })
+        .as_ref(py);
+    Ok(not_set.is(obj))
+}
+
+pub fn create_new_object(cls: &PyTuple) -> PyResult<*mut ffi::PyObject> {
+    let py = cls.py();
+    let __new__ = OBJECT_NEW
+        .get_or_init(py, || {
+            let builtins = builtins(py);
+            builtins
+                .getattr("object")
+                .expect("Error when importing builtins.object")
+                .getattr("__new__")
+                .expect("Error getattr builtins.object.__new__")
+                .into()
+        })
+        .as_ref(py);
+    py_object_call1_or_err(__new__.as_ptr(), cls.as_ptr())
+}
+
+pub fn iter_over_dict_items(obj: *mut ffi::PyObject) -> PyResult<PyObjectIterator> {
+    let items = call_method!(obj, ITEMS_STR)?;
+    to_iter(items)
+}
+
+pub fn obj_to_str(obj: *mut ffi::PyObject) -> PyResult<*mut ffi::PyObject> {
+    from_ptr_or_err(ffi!(PyObject_Str(obj)))
+}
+
+pub fn to_py_string(s: &str) -> *mut ffi::PyObject {
+    let c_str = CString::new(s).unwrap();
+    let c_world: *const c_char = c_str.as_ptr() as *const c_char;
+    ffi!(PyUnicode_InternFromString(c_world))
+}
+
+fn py_object_call1_or_err(
+    obj: *mut ffi::PyObject,
+    args: *mut ffi::PyObject,
+) -> PyResult<*mut ffi::PyObject> {
+    from_ptr_or_err(ffi!(PyObject_CallObject(obj, args)))
+}
+
+pub fn py_object_call1_make_tuple_or_err(
+    obj: *mut ffi::PyObject,
+    arg: *mut ffi::PyObject,
+) -> PyResult<*mut ffi::PyObject> {
+    let tuple_arg = from_ptr_or_err(ffi!(PyTuple_Pack(1, arg)))?;
+    let result = py_object_call1_or_err(obj, tuple_arg)?;
+    ffi!(Py_DECREF(tuple_arg));
+    Ok(result)
+}
+
+pub fn py_object_set_attr(
+    obj: *mut ffi::PyObject,
+    attr_name: *mut ffi::PyObject,
+    value: *mut ffi::PyObject,
+) -> PyResult<()> {
+    let ret = ffi!(PyObject_SetAttr(obj, attr_name, value));
+    error_on_minusone(ret)
+}
+
+#[inline]
+pub fn py_str_to_str(obj: *mut ffi::PyObject) -> PyResult<&'static str> {
+    let utf8_slice = {
+        cfg_if::cfg_if! {
+            if #[cfg(any(Py_3_10, not(Py_LIMITED_API)))] {
+                // PyUnicode_AsUTF8AndSize only available on limited API starting with 3.10.
+                let mut size: ffi::Py_ssize_t = 0;
+                let data = ffi!(PyUnicode_AsUTF8AndSize(obj, &mut size));
+                if data.is_null() {
+                    return Err(Python::with_gil(|py| PyErr::fetch(py)));
+                } else {
+                    unsafe { std::slice::from_raw_parts(data as *const u8, size as usize) }
+                }
+            } else {
+                let ptr = from_ptr_or_err(ffi!(PyUnicode_AsUTF8String(obj)))?;
+                unsafe {
+                    let buffer = ffi!(PyBytes_AsString(ptr)) as *const u8;
+                    let length = ffi!(PyBytes_Size(ptr)) as usize;
+                    debug_assert!(!buffer.is_null());
+                    std::slice::from_raw_parts(buffer, length)
+                }
+            }
+        }
+    };
+    Ok(unsafe { std::str::from_utf8_unchecked(utf8_slice) })
+}
+
+pub fn py_object_get_attr(
+    obj: *mut ffi::PyObject,
+    attr_name: *mut ffi::PyObject,
+) -> PyResult<*mut ffi::PyObject> {
+    from_ptr_or_err(ffi!(PyObject_GetAttr(obj, attr_name)))
+}
+
+pub fn py_tuple_get_item(obj: *mut ffi::PyObject, index: usize) -> PyResult<*mut ffi::PyObject> {
+    from_ptr_or_err(ffi!(PyTuple_GetItem(obj, index as Py_ssize_t)))
+}
+
+pub fn py_object_get_item(
+    obj: *mut ffi::PyObject,
+    key: *mut ffi::PyObject,
+) -> PyResult<*mut ffi::PyObject> {
+    from_ptr_or_err(ffi!(PyObject_GetItem(obj, key)))
+}
+
+fn from_ptr_or_opt(ptr: *mut ffi::PyObject) -> Option<*mut ffi::PyObject> {
+    NonNull::new(ptr).map(|p| p.as_ptr())
+}
+
+pub fn from_ptr_or_err(ptr: *mut ffi::PyObject) -> PyResult<*mut ffi::PyObject> {
+    from_ptr_or_opt(ptr).ok_or_else(|| Python::with_gil(|py| PyErr::fetch(py)))
+}
+
+#[inline]
+pub fn error_on_minusone(result: c_int) -> PyResult<()> {
+    if result != -1 {
+        Ok(())
+    } else {
+        Err(Python::with_gil(|py| PyErr::fetch(py)))
+    }
+}
+
+fn to_iter(obj: *mut ffi::PyObject) -> PyResult<PyObjectIterator> {
+    let internal = PyObjectIterator(from_ptr_or_err(ffi!(PyObject_GetIter(obj)))?);
+    Ok(internal)
+}
+
+pub struct PyObjectIterator(*mut ffi::PyObject);
+
+impl Iterator for PyObjectIterator {
+    type Item = PyResult<*mut ffi::PyObject>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match from_ptr_or_opt(ffi!(PyIter_Next(self.0))) {
+            Some(item) => Some(Ok(item)),
+            None => Python::with_gil(|py| PyErr::take(py).map(Err)),
+        }
+    }
+}
---
+++ packages/serpyco-rs/serpyco_rs-0.1.3.tar.gz/serpyco_rs-0.1.3/src/serializer/types.rs
@@ -0,0 +1,145 @@
+use pyo3::ffi::PyObject;
+use pyo3::types::PyModule;
+use pyo3::Python;
+use pyo3::{AsPyPointer, Py, PyAny, PyResult};
+use std::sync::Once;
+
+pub static mut INTEGER_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut STRING_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut BYTES_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut FLOAT_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut DECIMAL_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut BOOLEAN_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut UUID_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut TIME_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut DATETIME_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut DATE_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut ENUM_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut ENTITY_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut OPTIONAL_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut ARRAY_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut DICTIONARY_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut TUPLE_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut ANY_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut ITEMS_STR: *mut PyObject = 0 as *mut PyObject;
+pub static mut ISOFORMAT_STR: *mut PyObject = 0 as *mut PyObject;
+pub static mut VALUE_STR: *mut PyObject = 0 as *mut PyObject;
+pub static mut UUID_PY_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut NONE_PY_TYPE: *mut PyObject = 0 as *mut PyObject;
+pub static mut PY_TUPLE_0: *mut PyObject = 0 as *mut PyObject;
+
+static INIT: Once = Once::new();
+
+#[derive(Clone, Debug)]
+pub enum Type {
+    IntegerType(Py<PyAny>),
+    StringType(Py<PyAny>),
+    BytesType(Py<PyAny>),
+    FloatType(Py<PyAny>),
+    DecimalType(Py<PyAny>),
+    BooleanType(Py<PyAny>),
+    UUIDType(Py<PyAny>),
+    TimeType(Py<PyAny>),
+    DateTimeType(Py<PyAny>),
+    DateType(Py<PyAny>),
+    EnumType(Py<PyAny>),
+    EntityType(Py<PyAny>),
+    OptionalType(Py<PyAny>),
+    ArrayType(Py<PyAny>),
+    DictionaryType(Py<PyAny>),
+    TupleType(Py<PyAny>),
+    AnyType(Py<PyAny>),
+}
+
+pub fn get_object_type(type_info: &PyAny) -> PyResult<Type> {
+    if check_type!(type_info, INTEGER_TYPE) {
+        Ok(Type::IntegerType(type_info.into()))
+    } else if check_type!(type_info, STRING_TYPE) {
+        Ok(Type::StringType(type_info.into()))
+    } else if check_type!(type_info, BYTES_TYPE) {
+        Ok(Type::BytesType(type_info.into()))
+    } else if check_type!(type_info, FLOAT_TYPE) {
+        Ok(Type::FloatType(type_info.into()))
+    } else if check_type!(type_info, DECIMAL_TYPE) {
+        Ok(Type::DecimalType(type_info.into()))
+    } else if check_type!(type_info, BOOLEAN_TYPE) {
+        Ok(Type::BooleanType(type_info.into()))
+    } else if check_type!(type_info, UUID_TYPE) {
+        Ok(Type::UUIDType(type_info.into()))
+    } else if check_type!(type_info, TIME_TYPE) {
+        Ok(Type::TimeType(type_info.into()))
+    } else if check_type!(type_info, DATETIME_TYPE) {
+        Ok(Type::DateTimeType(type_info.into()))
+    } else if check_type!(type_info, DATE_TYPE) {
+        Ok(Type::DateType(type_info.into()))
+    } else if check_type!(type_info, ENUM_TYPE) {
+        Ok(Type::EnumType(type_info.into()))
+    } else if check_type!(type_info, ENTITY_TYPE) {
+        Ok(Type::EntityType(type_info.into()))
+    } else if check_type!(type_info, OPTIONAL_TYPE) {
+        Ok(Type::OptionalType(type_info.into()))
+    } else if check_type!(type_info, ARRAY_TYPE) {
+        Ok(Type::ArrayType(type_info.into()))
+    } else if check_type!(type_info, DICTIONARY_TYPE) {
+        Ok(Type::DictionaryType(type_info.into()))
+    } else if check_type!(type_info, TUPLE_TYPE) {
+        Ok(Type::TupleType(type_info.into()))
+    } else if check_type!(type_info, ANY_TYPE) {
+        Ok(Type::AnyType(type_info.into()))
+    } else {
+        todo!("py Error 'Unsupported type'")
+    }
+}
+
+pub fn init(py: Python<'_>) {
+    INIT.call_once(|| unsafe {
+        let describe = PyModule::import(py, "serpyco_rs._describe").unwrap();
+        INTEGER_TYPE = get_attr_ptr!(describe, "IntegerType");
+        STRING_TYPE = get_attr_ptr!(describe, "StringType");
+        BYTES_TYPE = get_attr_ptr!(describe, "BytesType");
+        FLOAT_TYPE = get_attr_ptr!(describe, "FloatType");
+        DECIMAL_TYPE = get_attr_ptr!(describe, "DecimalType");
+        BOOLEAN_TYPE = get_attr_ptr!(describe, "BooleanType");
+        UUID_TYPE = get_attr_ptr!(describe, "UUIDType");
+        TIME_TYPE = get_attr_ptr!(describe, "TimeType");
+        DATETIME_TYPE = get_attr_ptr!(describe, "DateTimeType");
+        DATE_TYPE = get_attr_ptr!(describe, "DateType");
+        ENUM_TYPE = get_attr_ptr!(describe, "EnumType");
+        ENTITY_TYPE = get_attr_ptr!(describe, "EntityType");
+        OPTIONAL_TYPE = get_attr_ptr!(describe, "OptionalType");
+        ARRAY_TYPE = get_attr_ptr!(describe, "ArrayType");
+        DICTIONARY_TYPE = get_attr_ptr!(describe, "DictionaryType");
+        TUPLE_TYPE = get_attr_ptr!(describe, "TupleType");
+        ANY_TYPE = get_attr_ptr!(describe, "AnyType");
+
+        let uuid = PyModule::import(py, "uuid").unwrap();
+        UUID_PY_TYPE = get_attr_ptr!(uuid, "UUID");
+
+        let builtins = PyModule::import(py, "builtins").unwrap();
+        NONE_PY_TYPE = get_attr_ptr!(builtins, "None");
+
+        ITEMS_STR = to_py_string("items");
+        VALUE_STR = to_py_string("value");
+        ISOFORMAT_STR = to_py_string("isoformat");
+
+        PY_TUPLE_0 = ffi!(PyTuple_New(0));
+    });
+}
+
+macro_rules! check_type {
+    ($py_obj:ident, $type:expr) => {
+        $py_obj.get_type().as_ptr() == unsafe { $type }
+    };
+}
+
+macro_rules! get_attr_ptr {
+    ($mod:expr, $type:expr) => {
+        $mod.getattr($type).unwrap().as_ptr()
+    };
+}
+
+use crate::serializer::py::to_py_string;
+pub(crate) use check_type;
+pub(crate) use get_attr_ptr;
+
+use super::macros::ffi;
