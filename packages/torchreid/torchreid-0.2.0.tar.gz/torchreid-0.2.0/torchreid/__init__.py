25faeb808666268c9e49d733851c8a9bd70a6102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:15 +0000

    Add package torchreid/0.2.0/torchreid-0.2.0.tar.gz

---
+++ packages/torchreid/torchreid-0.2.0.tar.gz/torchreid-0.2.0/torchreid/__init__.py
@@ -0,0 +1,3 @@
+from torchreid.reid import data, engine, losses, metrics, models, optim, utils
+
+__version__ = '0.2.0'
---
+++ packages/torchreid/torchreid-0.2.0.tar.gz/torchreid-0.2.0/torchreid/reid/engine/image/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+from torchreid.reid.engine.image.softmax import ImageSoftmaxEngine
+from torchreid.reid.engine.image.triplet import ImageTripletEngine
