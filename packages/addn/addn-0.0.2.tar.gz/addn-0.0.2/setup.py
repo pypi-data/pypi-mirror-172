f713b72716785da5d550cba8bad7dea29f3bd9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:44 +0000

    Add package addn/0.0.2/addn-0.0.2.tar.gz

---
+++ packages/addn/addn-0.0.2.tar.gz/addn-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+packages = ['addn']
+
+setup(
+    name='addn',                            # pip list 时显示的包名
+    version='0.0.2',                        # 版本号
+    author='mirschao',                      # 作者名
+    packages=packages,                      # import 时写的名字
+)
