61ee739f1b6e728fec55408f1b86c34550ceabfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:30 +0000

    Add package panel-highcharts/20221016.1/panel_highcharts-20221016.1-py3-none-any.whl

---
+++ packages/panel-highcharts/panel_highcharts-20221016.1-py3-none-any.whl/panel_highcharts/__init__.py
@@ -0,0 +1,9 @@
+"""Use
+
+- panel_highcharts.config.js_files to define which Highcharts js files to include
+- the HighChart pane to use the Highcharts Chart in Panel.
+"""
+from . import config
+from .charts import HighChart, HighGantt, HighMap, HighStock
+
+VERSION = "20221016.1"
---
+++ packages/panel-highcharts/panel_highcharts-20221016.1-py3-none-any.whl/panel_highcharts/dist/lib/models/index.js
@@ -0,0 +1,5 @@
+export { HighChart } from "./highchart";
+export { HighStock } from "./highstock";
+export { HighMap } from "./highmap";
+export { HighGantt } from "./highgantt";
+//# sourceMappingURL=index.js.map
