90f353ccb41db3ee88a1dc25895e577fbde8bf60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:29 +0000

    Add package gqylpy-dict/1.1/gqylpy_dict-1.1.tar.gz

---
+++ packages/gqylpy-dict/gqylpy_dict-1.1.tar.gz/gqylpy_dict-1.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import gqylpy_dict as g
+
+setuptools.setup(
+    name=g.__name__,
+    version='.'.join(str(n) for n in g.__version__),
+    author=g.__author__.split()[0],
+    author_email=g.__author__.split()[1][1:-1],
+    license='WTFPL,Apache-2.0',
+    url='http://gqylpy.com',
+    project_urls={'Source': g.__source__},
+    description='''
+        基于内置 dict，它是对内置 dict 的增强。dict 能做的它能做，dict 不能做的它更能做。
+    '''.strip(),
+    long_description=open('README.md', encoding='utf8').read(),
+    long_description_content_type='text/markdown',
+    packages=[g.__name__],
+    python_requires='>=3.6',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Utilities',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ]
+)
