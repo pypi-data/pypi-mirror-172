eb362db1aa0da46fa943c38c2cd8ee8d2db67f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:36 +0000

    Add package thefirstock/2.5/thefirstock-2.5.tar.gz

---
+++ packages/thefirstock/thefirstock-2.5.tar.gz/thefirstock-2.5/src/thefirstock/Variables/enums.py
@@ -0,0 +1,50 @@
+"""
+URLS for FIRSTOCK apiV2
+"""
+
+"""Login"""
+LOGIN = 'https://connect.thefirstock.com/apiV2/login'
+
+USERDETAILS = "https://connect.thefirstock.com/apiV2/userDetails"
+
+LOGOUT = "https://connect.thefirstock.com/apiV2/logout"
+
+PLACEORDER = "https://connect.thefirstock.com/apiV2/placeOrder"
+
+ORDERMARGIN = "https://connect.thefirstock.com/apiV2/orderMargin"
+
+ORDERBOOK = "https://connect.thefirstock.com/apiV2/orderBook"
+
+CANCELORDER = "https://connect.thefirstock.com/apiV2/cancelOrder"
+
+MODIFYORDER = "https://connect.thefirstock.com/apiV2/modifyOrder"
+
+SINGLEORDERHISTORY = "https://connect.thefirstock.com/apiV2/singleOrderHistory"
+
+TRADEBOOK = "https://connect.thefirstock.com/apiV2/tradeBook"
+
+POSITIONBOOK = "https://connect.thefirstock.com/apiV2/positionBook"
+
+PRODUCTCONVERSION = "https://connect.thefirstock.com/apiV2/productConversion"
+
+HOLDINGS = "https://connect.thefirstock.com/apiV2/holdings"
+
+LIMITS = "https://connect.thefirstock.com/apiV2/limit"
+
+GETQUOTES = "https://connect.thefirstock.com/apiV2/getQuotes"
+
+SEARCHSCRIPS = "https://connect.thefirstock.com/apiV2/searchScrips"
+
+GETSECURITYINFO = "https://connect.thefirstock.com/apiV2/securityInfo"
+
+GETINDEXLIST = "https://connect.thefirstock.com/apiV2/indexList"
+
+GETOPTIONCHAIN = "https://connect.thefirstock.com/apiV2/optionChain"
+
+SPANCALCULATOR = "https://connect.thefirstock.com/apiV2/spanCalculators"
+
+TIMEPRICESERIES = "https://connect.thefirstock.com/apiV2/timePriceSeries"
+
+OPTIONGREEK = "https://connect.thefirstock.com/apiV2/optionGreek"
+
+BASKETMARGIN = "https://connect.thefirstock.com/apiV2/basketMargin"
