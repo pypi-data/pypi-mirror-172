632105cc461ee504ea360849a7d62cb6bbfa10eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:07 +0000

    Add package ms2ml/0.0.4/ms2ml-0.0.4.tar.gz

---
+++ packages/ms2ml/ms2ml-0.0.4.tar.gz/ms2ml-0.0.4/ms2ml/metrics/__init__.py
@@ -0,0 +1,15 @@
+from .metrics import (
+    CosineLoss,
+    CosineSimilarity,
+    PearsonCorrelation,
+    SpectralAngle,
+    SpectralAngleLoss,
+)
+
+__all__ = [
+    "CosineLoss",
+    "CosineSimilarity",
+    "PearsonCorrelation",
+    "SpectralAngle",
+    "SpectralAngleLoss",
+]
