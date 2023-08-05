be38890cbd5bdc239430fa4b7dbb35e042ac035c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:15 +0000

    Add package torchreid/0.2.3/torchreid-0.2.3.tar.gz

---
+++ packages/torchreid/torchreid-0.2.3.tar.gz/torchreid-0.2.3/torchreid/reid/data/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import print_function, absolute_import
+
+from torchreid.reid.data.datasets.dataset import (
+    Dataset, ImageDataset, VideoDataset, register_image_dataset,
+    register_video_dataset
+)
+from torchreid.reid.data.datamanager import ImageDataManager, VideoDataManager
