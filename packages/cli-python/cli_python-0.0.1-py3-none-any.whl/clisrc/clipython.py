aca743400eb2f5fda89da21fcfe01bfb04fe7309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:21 +0000

    Add package cli-python/0.0.1/cli_python-0.0.1-py3-none-any.whl

---
+++ packages/cli-python/cli_python-0.0.1-py3-none-any.whl/clisrc/clipython.py
@@ -0,0 +1,7 @@
+class cli:
+    def __init__(self, cmdline):
+        ''' A command line interface in python created from the command line interface in linux.
+        '''
+        
+        self.cmd = cmdline
+        
