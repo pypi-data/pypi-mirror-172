657f9ce882d418c9a21d991c51476451414e5288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:38 +0000

    Add package woocommerce-prefect-tasks/0.0.4/woocommerce_prefect_tasks-0.0.4-py3-none-any.whl

---
+++ packages/woocommerce-prefect-tasks/woocommerce_prefect_tasks-0.0.4-py3-none-any.whl/woocommerce_prefect_tasks/tasks/update.py
@@ -0,0 +1,32 @@
+from ..api import Api
+
+from prefect import Task
+from prefect.utilities.tasks import defaults_from_attrs
+
+from typing import Any
+
+
+class Update(Task):
+    def __init__(self, aws_secret_name: str = None, aws_secret_region: str = None, event: dict = None, **kwargs: Any):
+        self.aws_secret_region = aws_secret_region
+        self.aws_secret_name = aws_secret_name
+        self.event = event
+        super().__init__(**kwargs)
+
+    @defaults_from_attrs("event", "aws_secret_region", "aws_secret_name")
+    def run(self, aws_secret_name: str = None, aws_secret_region: str = None, event: dict = None) -> dict:
+
+        if not event:
+            raise ValueError('An event must be provided.')
+
+        try:
+            wc_api = Api(aws_secret_name=aws_secret_name, aws_secret_region=aws_secret_region)
+            resource = wc_api.api_client.put(endpoint=event.get('endpoint', None),
+                                             data=event.get('data', None) or dict(),
+                                             **event.get('kwargs', None) or dict())
+
+        except Exception as e:
+            print(e)
+            raise e
+
+        return resource.json()
