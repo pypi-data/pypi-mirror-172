1eb4cd695d43ff1f1d747f89112c740f5cb4a219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:13 +0000

    Add package tlaze/0.0.1/tlaze-0.0.1.tar.gz

---
+++ packages/tlaze/tlaze-0.0.1.tar.gz/tlaze-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+name="tlaze",
+version="0.0.1",
+author="DengJianXiang",
+author_email="18625828597@163.com",
+description="This project provides an easy way to recognize faces usingAI",
+# 项目主页
+url="https://github.com/sunjian-github/ezface",
+# 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+packages=find_packages()
+)
