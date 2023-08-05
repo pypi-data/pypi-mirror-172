043e80430e6eaf59262aec6703b559a88f7706db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:08 +0000

    Add package wangzhenbin/0.0.2/wangzhenbin-0.0.2.tar.gz

---
+++ packages/wangzhenbin/wangzhenbin-0.0.2.tar.gz/wangzhenbin-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+name="wangzhenbin",
+version="0.0.2",
+author="wangzhenbin",
+author_email="2175206532@qq.com",
+description="Give dedicated students and teachers time reminders",
+# 项目主页
+# url="https://github.com/sunjian-github/ezface",
+# 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+packages=find_packages()
+)
