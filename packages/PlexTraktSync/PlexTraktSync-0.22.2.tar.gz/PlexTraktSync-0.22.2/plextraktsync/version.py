8589ca36d3b288543f3155bd85a01bf7a1b3bc59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:47 +0000

    Add package PlexTraktSync/0.22.2/PlexTraktSync-0.22.2.tar.gz

---
+++ packages/PlexTraktSync/PlexTraktSync-0.22.2.tar.gz/PlexTraktSync-0.22.2/plextraktsync/version.py
@@ -0,0 +1,26 @@
+def git_version_info():
+    try:
+        from gitinfo import get_git_info
+    except (ImportError, TypeError):
+        return None
+
+    commit = get_git_info()
+    if not commit:
+        return None
+
+    message = commit["message"].split("\n")[0]
+
+    return f"{commit['commit'][0:8]}: {message} @{commit['author_date']}"
+
+
+def version():
+    from plextraktsync import __version__
+
+    if __version__[-1] != "x":
+        return __version__
+
+    gv = git_version_info()
+    if gv:
+        return f"{__version__}: {gv}"
+
+    return __version__
