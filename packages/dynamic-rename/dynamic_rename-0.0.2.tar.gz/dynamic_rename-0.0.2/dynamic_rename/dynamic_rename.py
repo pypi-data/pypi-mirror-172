e474cc9ac74ec63f0bcbe1597d2fe5acb85fa1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:36 +0000

    Add package dynamic-rename/0.0.2/dynamic_rename-0.0.2.tar.gz

---
+++ packages/dynamic-rename/dynamic_rename-0.0.2.tar.gz/dynamic_rename-0.0.2/dynamic_rename/dynamic_rename.py
@@ -0,0 +1,13 @@
+class rename:
+    def  __init__(self):
+        self.self=self
+    def dynamic_rename(self,df):
+        df_col=[]
+        for i in df.columns:
+            df_col.append(i)
+        for i in range(len(df_col)):
+            print("current columns Name:" ,df_col[i])
+            new_col_name=input('Kindly Enter renamed column name :')
+            df=df.withColumnRenamed(df_col[i],new_col_name)
+            print('column renamed to sucessfully to',new_col_name,'\n')
+        return df
