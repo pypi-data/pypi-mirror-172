19abea4d4f0673835d6882eccfb194c7c38534b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:47 +0000

    Add package AbsBox/0.1.2.6/AbsBox-0.1.2.6.tar.gz

---
+++ packages/AbsBox/AbsBox-0.1.2.6.tar.gz/AbsBox-0.1.2.6/absbox/local/util.py
@@ -0,0 +1,52 @@
+import pandas as pd 
+import functools
+from enum import Enum
+
+def mkTag(x):
+    match x:
+        case (tagName, tagValue):
+            return {"tag": tagName, "contents": tagValue}
+        case (tagName):
+            return {"tag": tagName}
+
+#"{\"tag\":\"PrepaymentFactors\"
+#  ,\"contents\":{\"tag\":\"FactorCurveClosed\"
+#                ,\"contents\":[[\"2022-01-01\",{\"numerator\":33,\"denominator\":25}]]}}"
+
+def mkTs(n,vs):
+    return mkTag((n,vs))
+    
+def unify(xs,ns):
+    index_name = xs[0].index.name
+    dfs = []
+    for x,n in zip(xs,ns):
+        dfs.append(pd.concat([x],keys=[n],axis=1))
+    r = functools.reduce(lambda acc,x: acc.merge(x
+                                                ,how='outer'
+                                                ,on=[index_name])
+                        ,dfs)
+    return r.sort_index()
+
+def bondView(r,flow=None):
+    bnds = r['bonds']
+    bndNames = list(bnds.keys())
+    bndVals = list(bnds.values())
+    if flow is None:
+        return unify(bndVals,bndNames)
+    else:
+        newBnds = [ _b[flow] for _b in bndVals] 
+        return unify(newBnds,bndNames)
+
+
+class DC(Enum):  # TODO need to check with HS code
+    DC_30E_360 = "DC_30E_360"
+    DC_30Ep_360 ="DC_30Ep_360"
+    DC_ACT_360  = "DC_ACT_360"
+    DC_ACT_365A = "DC_ACT_365A"
+    DC_ACT_365L = "DC_ACT_365L"
+    DC_NL_365 = "DC_NL_365"
+    DC_ACT_365F = "DC_ACT_365F"
+    DC_ACT_ACT = "DC_ACT_ACT"
+    DC_30_360_ISDA = "DC_30_360_ISDA"
+    DC_30_360_German = "DC_30_360_German"
+    DC_30_360_US  = "DC_30_360_US"
