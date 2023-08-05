ba02647d8fe13d7d1ff036a626d0a3e6de748073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:59 +0000

    Add package pyrekordbox/0.1.0/pyrekordbox-0.1.0-py3-none-any.whl

---
+++ packages/pyrekordbox/pyrekordbox-0.1.0-py3-none-any.whl/pyrekordbox/db6/database.py
@@ -0,0 +1,353 @@
+# coding: utf-8
+#
+# This code is part of pyrekordbox.
+#
+# Copyright (c) 2022, Dylan Jones
+
+import os
+import re
+import base64
+import warnings
+import blowfish
+import logging
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from ..config import get_config
+from ..utils import read_rekordbox6_asar
+from ..anlz import get_anlz_paths, read_anlz_files
+from . import tables
+
+try:
+    from pysqlcipher3 import dbapi2 as sqlite3  # noqa
+except ImportError:
+    import sqlite3
+
+logger = logging.getLogger(__name__)
+
+rb6_config = get_config("rekordbox6")
+
+
+def _get_masterdb_key():
+    # See https://www.reddit.com/r/Rekordbox/comments/qou6nm/key_to_open_masterdb_file/
+
+    # Read password key from app.asar file
+    asar_data = read_rekordbox6_asar(rb6_config["install_dir"])
+    match = re.search('pass: ".(.*?)"', asar_data).group(0)
+    password = match.replace("pass: ", "").strip('"')
+    password_bytes = password.encode()
+
+    # Decode database key data
+    encoded_key_data = rb6_config["dp"]  # from 'options.json'
+    decoded_key_data = base64.standard_b64decode(encoded_key_data)
+
+    # Decrypt database key
+    cipher = blowfish.Cipher(password_bytes)
+    decrypted_bytes = b"".join(cipher.decrypt_ecb(decoded_key_data))
+    database_key = decrypted_bytes.decode()
+
+    return database_key
+
+
+def create_rekordbox_engine(path="", unlock=True, sql_driver=None, echo=None):
+    if not path:
+        path = rb6_config["db_path"]
+
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"File '{path}' does not exist!")
+    logger.info("Opening %s", path)
+
+    # Open database
+    if unlock:
+        key = _get_masterdb_key()
+        logger.info("Key: %s", key)
+        if sql_driver is None:
+            # Use default sqlite3 package
+            # This requires that the 'sqlite3.dll' was replaced by
+            # the 'sqlcipher.dll' (renamed to 'sqlite3.dll')
+            sql_driver = sqlite3
+        url = f"sqlite+pysqlcipher://:{key}@/{path}?"
+        engine = create_engine(url, module=sql_driver, echo=echo)
+    else:
+        engine = create_engine(f"sqlite:///{path}", echo=echo)
+
+    return engine
+
+
+def open_rekordbox_database(path="", unlock=True, sql_driver=None):
+    warnings.warn(
+        "This method is deprecated and will be removed in a future version!"
+        "Use the `Rekordbox6Database` class instead!",
+        DeprecationWarning,
+    )
+    if not path:
+        path = rb6_config["db_path"]
+
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"File '{path}' does not exist!")
+    logger.info("Opening %s", path)
+
+    # Open database
+    if sql_driver is None:
+        # Use default sqlite3 package
+        # This requires that the 'sqlite3.dll' was replaced by
+        # the 'sqlcipher.dll' (renamed to 'sqlite3.dll')
+        sql_driver = sqlite3
+    con = sql_driver.connect(path)
+
+    if unlock:
+        # Read and decode master.db key
+        key = _get_masterdb_key()
+        logger.info("Key: %s", key)
+        # Unlock database
+        con.execute(f"PRAGMA key='{key}'")
+
+    # Check connection
+    try:
+        con.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    except sqlite3.DatabaseError as e:
+        msg = f"Opening database failed: '{e}'. Check if the database key is correct!"
+        raise sqlite3.DatabaseError(msg)
+    else:
+        logger.info("Database unlocked!")
+
+    return con
+
+
+def _parse_query_result(result, kwargs):
+    if "ID" in kwargs:
+        result = result.one()
+    return result
+
+
+class Rekordbox6Database:
+    def __init__(self, path="", unlock=True):
+        self.engine = create_rekordbox_engine(path, unlock=unlock)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+
+        self._db_dir = os.path.normpath(rb6_config["db_dir"])
+        self._anlz_root = os.path.join(self._db_dir, "share")
+
+    def open(self):
+        self.session = self.Session()
+
+    def close(self):
+        self.session.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def query(self, *entities, **kwargs):
+        return self.session.query(*entities, **kwargs)
+
+    def commit(self):
+        self.session.commit()
+
+    # -- Djmd Tables -------------------------------------------------------------------
+
+    def get_active_censor(self, **kwargs):
+        res = self.query(tables.DjmdActiveCensor).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_album(self, **kwargs):
+        res = self.query(tables.DjmdAlbum).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_artist(self, **kwargs):
+        res = self.query(tables.DjmdArtist).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_category(self, **kwargs):
+        res = self.query(tables.DjmdCategory).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_color(self, **kwargs):
+        res = self.query(tables.DjmdColor).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_content(self, **kwargs):
+        res = self.query(tables.DjmdContent).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def search_content(self, search):
+
+        columns = ["Title", "Commnt", "SearchStr"]
+
+        results = list()
+        for col in columns:
+            obj = getattr(tables.DjmdContent, col)
+            res = self.query(tables.DjmdContent).filter(obj.contains(search))
+            results.extend(res.all())
+
+        rel_columns = {
+            "Album": (tables.DjmdAlbum, "Name"),
+            "Artist": (tables.DjmdArtist, "Name"),
+            "Composer": (tables.DjmdArtist, "Name"),
+            "Remixer": (tables.DjmdArtist, "Name"),
+            "Genre": (tables.DjmdGenre, "Name"),
+            "Key": (tables.DjmdKey, "ScaleName"),
+        }
+        for col, (table, attr) in rel_columns.items():
+            obj = getattr(tables.DjmdContent, col)
+            item = getattr(table, attr)
+            res = self.query(tables.DjmdContent).join(obj).filter(item.contains(search))
+            results.extend(res.all())
+
+        return results
+
+    def get_cue(self, **kwargs):
+        res = self.query(tables.DjmdCue).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_device(self, **kwargs):
+        res = self.query(tables.DjmdDevice).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_genre(self, **kwargs):
+        res = self.query(tables.DjmdGenre).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_history(self, **kwargs):
+        res = self.query(tables.DjmdHistory).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_history_songs(self, id_):
+        res = self.query(tables.DjmdSongHistory).filter_by(HistoryID=id_)
+        return res
+
+    def get_hot_cue_banklist(self, **kwargs):
+        res = self.query(tables.DjmdHotCueBanklist).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_hot_cue_banklist_songs(self, id_):
+        res = self.query(tables.DjmdSongHotCueBanklist).filter_by(HotCueBanklistID=id_)
+        return res
+
+    def get_key(self, **kwargs):
+        res = self.query(tables.DjmdKey).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_label(self, **kwargs):
+        res = self.query(tables.DjmdLabel).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_menu_items(self, **kwargs):
+        res = self.query(tables.DjmdMenuItems).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_mixer_param(self, **kwargs):
+        res = self.query(tables.DjmdMixerParam).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_my_tag(self, **kwargs):
+        res = self.query(tables.DjmdMyTag).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_my_tag_songs(self, id_):
+        res = self.query(tables.DjmdSongMyTag).filter_by(MyTagID=id_)
+        return res
+
+    def get_playlist(self, **kwargs):
+        res = self.query(tables.DjmdPlaylist).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_playlist_songs(self, id_):
+        res = self.query(tables.DjmdSongPlaylist).filter_by(PlaylistID=id_)
+        return res
+
+    def get_property(self, **kwargs):
+        res = self.query(tables.DjmdProperty).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_related_tracks(self, **kwargs):
+        res = self.query(tables.DjmdRelatedTracks).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_related_tracks_songs(self, id_):
+        res = self.query(tables.DjmdSongRelatedTracks).filter_by(RelatedTracksID=id_)
+        return res
+
+    def get_sampler(self, **kwargs):
+        res = self.query(tables.DjmdSampler).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_sampler_songs(self, id_):
+        res = self.query(tables.DjmdSongSampler).filter_by(SamplerID=id_)
+        return res
+
+    def get_tag_list_songs(self, id_):
+        res = self.query(tables.DjmdSongTagList).filter_by(ID=id_)
+        return res
+
+    def get_sort(self, **kwargs):
+        res = self.query(tables.DjmdSort).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    # -- Other tables ------------------------------------------------------------------
+
+    def get_agent_registry(self, **kwargs):
+        res = self.query(tables.AgentRegistry).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_cloud_agent_registry(self, **kwargs):
+        res = self.query(tables.CloudAgentRegistry).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_content_active_censor(self, **kwargs):
+        res = self.query(tables.ContentActiveCensor).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_content_cue(self, **kwargs):
+        res = self.query(tables.ContentCue).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_content_file(self, **kwargs):
+        res = self.query(tables.ContentFile).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_hot_cue_banklist_cue(self, **kwargs):
+        res = self.query(tables.HotCueBanklistCue).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_image_file(self, **kwargs):
+        res = self.query(tables.ImageFile).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_setting_file(self, **kwargs):
+        res = self.query(tables.SettingFile).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    def get_uuid_map(self, **kwargs):
+        res = self.query(tables.UuidIDMap).filter_by(**kwargs)
+        return _parse_query_result(res, kwargs)
+
+    # ==================================================================================
+
+    def get_mysetting_paths(self):
+        paths = list()
+        for item in self.get_setting_file():
+            paths.append(os.path.join(self._db_dir, item.Path.lstrip("/\\")))
+        return paths
+
+    def get_anlz_dir(self, content_id):
+        item = self.get_content(ID=content_id)
+        dat_path = os.path.normpath(item.AnalysisDataPath).strip("\\/")
+        path = os.path.join(self._anlz_root, os.path.dirname(dat_path))
+        assert os.path.exists(path)
+        return path
+
+    def get_anlz_paths(self, content_id):
+        root = self.get_anlz_dir(content_id)
+        return get_anlz_paths(root)
+
+    def read_anlz_files(self, content_id):
+        root = self.get_anlz_dir(content_id)
+        return read_anlz_files(root)
+
+    def set_content_path(self, content_id, path):
+        content = self.get_content(ID=content_id)
+        path = path.replace("\\", "/")
+        content.FolderPath = path.replace("\\", "/")
---
+++ packages/pyrekordbox/pyrekordbox-0.1.0-py3-none-any.whl/pyrekordbox/mysettings/file.py
@@ -0,0 +1,230 @@
+# coding: utf-8
+#
+# This code is part of pyrekordbox.
+#
+# Copyright (c) 2022, Dylan Jones
+
+import re
+from construct import Struct
+from collections.abc import MutableMapping
+from . import structs
+from ..utils import crc16xmodem
+
+RE_INVALID_KEY = re.compile("[_u][0-9]?", flags=re.IGNORECASE)
+
+
+def compute_checksum(data, struct):
+    """Computes the CRC16 XMODEM checksum for MySetting files.
+
+    The checksum is calculated over the contents of the `data` field,
+    except for `DJMSETTING.DAT` files where the checksum is calculated over all
+    preceding bytes including the length fields.
+
+    References
+    ----------
+    https://reveng.sourceforge.io/crc-catalogue/all.htm#crc.cat.crc-16-xmodem
+    """
+    start = 0 if struct == structs.DjmMySetting else 104
+    return crc16xmodem(data[start:-4])
+
+
+def _is_valid_key(k: str):
+    return not RE_INVALID_KEY.match(k)
+
+
+class SettingsFile(MutableMapping):
+    """Abtract base class of Rekordbox settings files."""
+
+    struct: Struct
+    defaults: dict
+    version: str = ""  # only used by DEVSETTING
+
+    def __init__(self):
+        super().__init__()
+        self.parsed = None
+        self._items = dict()
+
+    @classmethod
+    def parse(cls, data: bytes):
+        """Parses the in-memory data of a Rekordbox settings binary file.
+
+        Parameters
+        ----------
+        data : bytes
+            The in-memory binary contents of a Rekordbox settings file.
+
+        Returns
+        -------
+        self : SettingsFile
+            The new instance with the parsed file content.
+        """
+        self = cls()
+        self._parse(data)
+        return self
+
+    @classmethod
+    def parse_file(cls, path: str):
+        """Reads and parses a Rekordbox settings binary file.
+
+        Parameters
+        ----------
+        path : str
+            The path of a Rekordbox settings file which is used to read
+            the file contents before parsing the binary data.
+
+        Returns
+        -------
+        self : AnlzFile
+            The new instance with the parsed file content.
+
+        See Also
+        --------
+        SettingsFile.parse: Parses the data of a Rekordbox settings file.
+        """
+        with open(path, "rb") as fh:
+            data = fh.read()
+        return cls.parse(data)
+
+    def _parse(self, data):
+        parsed = self.struct.parse(data)
+        keys = filter(_is_valid_key, parsed.data.keys())
+        items = dict()
+        for key in keys:
+            items[key] = str(parsed.data[key])
+
+        self.parsed = parsed
+        self._items.update(items)
+
+    def build(self):
+        # Copy defaults and update with cuirrent data
+        items = self.defaults.copy()
+        items.update(self._items)
+
+        # Create file data
+        file_items = {"data": items, "checksum": 0}
+        if self.version:
+            file_items["version"] = self.version
+
+        # Compute and update checksum. For this the data has to be serialized twice:
+        # Once for generating the checksum and another time for writing the data
+        # with the updated checksum
+        data = self.struct.build(file_items)
+        checksum = compute_checksum(data, self.struct)
+        file_items["checksum"] = checksum
+
+        # Write data with updated checksum
+        return self.struct.build(file_items)
+
+    def save(self, path):
+        data = self.build()
+        with open(path, "wb") as fh:
+            fh.write(data)
+
+    def __len__(self):
+        return len(self.defaults.keys())
+
+    def __iter__(self):
+        return iter(self.defaults)
+
+    def __getitem__(self, key):
+        try:
+            return self._items[key]
+        except KeyError:
+            return self.defaults[key]
+
+    def __setitem__(self, key, value):
+        if key not in self.defaults.keys():
+            raise KeyError(f"Key {key} not a valid field of {self.__class__.__name__}")
+        self._items[key] = value
+
+    def __delitem__(self, key):
+        del self._items[key]
+
+    def get(self, key, default=None):
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return default
+
+    def set(self, key, value):
+        self.__setitem__(key, value)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}()"
+
+
+class DevSettingFile(SettingsFile):
+
+    struct = structs.DevSetting
+    defaults = dict(entries="")
+
+
+class DjmMySettingFile(SettingsFile):
+
+    struct = structs.DjmMySetting
+    defaults = {
+        "channel_fader_curve": structs.ChannelFaderCurve.linear,
+        "cross_fader_curve": structs.CrossfaderCurve.fast_cut,
+        "headphones_pre_eq": structs.HeadphonesPreEQ.post_eq,
+        "headphones_mono_split": structs.HeadphonesMonoSplit.stereo,
+        "beat_fx_quantize": structs.BeatFXQuantize.on,
+        "mic_low_cut": structs.MicLowCut.on,
+        "talk_over_mode": structs.TalkOverMode.advanced,
+        "talk_over_level": structs.TalkOverLevel.minus_18db,
+        "midi_channel": structs.MidiChannel.one,
+        "midi_button_type": structs.MidiButtonType.toggle,
+        "display_brightness": structs.MixerDisplayBrightness.five,
+        "indicator_brightness": structs.MixerIndicatorBrightness.three,
+        "channel_fader_curve_long": structs.ChannelFaderCurveLong.exponential,
+    }
+
+
+class MySettingFile(SettingsFile):
+
+    struct = structs.MySetting
+    defaults = {
+        "auto_cue": structs.AutoCue.on,
+        "auto_cue_level": structs.AutoCueLevel.memory,
+        "disc_slot_illumination": structs.DiscSlotIllumination.bright,
+        "eject_lock": structs.EjectLock.unlock,
+        "hotcue_autoload": structs.HotCueAutoLoad.on,
+        "hotcue_color": structs.HotCueColor.off,
+        "jog_mode": structs.JogMode.vinyl,
+        "jog_ring_brightness": structs.JogRingBrightness.bright,
+        "jog_ring_indicator": structs.JogRingIndicator.on,
+        "language": structs.Language.english,
+        "lcd_brightness": structs.LCDBrightness.three,
+        "master_tempo": structs.MasterTempo.off,
+        "needle_lock": structs.NeedleLock.lock,
+        "on_air_display": structs.OnAirDisplay.on,
+        "phase_meter": structs.PhaseMeter.type1,
+        "play_mode": structs.PlayMode.single,
+        "quantize": structs.Quantize.on,
+        "quantize_beat_value": structs.QuantizeBeatValue.one,
+        "slip_flashing": structs.SlipFlashing.on,
+        "sync": structs.Sync.off,
+        "tempo_range": structs.TempoRange.ten,
+        "time_mode": structs.TimeMode.remain,
+    }
+
+
+class MySetting2File(SettingsFile):
+
+    struct = structs.MySetting2
+    defaults = {
+        "vinyl_speed_adjust": structs.VinylSpeedAdjust.touch,
+        "jog_display_mode": structs.JogDisplayMode.auto,
+        "pad_button_brightness": structs.PadButtonBrightness.three,
+        "jog_lcd_brightness": structs.JogLCDBrightness.three,
+        "waveform_divisions": structs.WaveformDivisions.phrase,
+        "waveform": structs.Waveform.waveform,
+        "beat_jump_beat_value": structs.BeatJumpBeatValue.sixteen,
+    }
+
+
+FILES = {
+    "DEVSETTING.DAT": DevSettingFile,
+    "DJMMYSETTING.DAT": DjmMySettingFile,
+    "MYSETTING.DAT": MySettingFile,
+    "MYSETTING2.DAT": MySetting2File,
+}
