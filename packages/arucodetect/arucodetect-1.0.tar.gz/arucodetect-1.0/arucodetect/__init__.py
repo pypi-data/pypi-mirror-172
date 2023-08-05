a9c9e7446c69071b29cb83f7ac9ffe0879852a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:37 +0000

    Add package arucodetect/1.0/arucodetect-1.0.tar.gz

---
+++ packages/arucodetect/arucodetect-1.0.tar.gz/arucodetect-1.0/arucodetect/__init__.py
@@ -0,0 +1,6 @@
+from .arucodetect import findArucoMarkers
+
+
+__author__ = "myortf_inside"
+__version__ = "1.0.0"
+__email__ = "faridgarifullin66@gmail.com"
---
+++ packages/arucodetect/arucodetect-1.0.tar.gz/arucodetect-1.0/setup.py
@@ -0,0 +1,13 @@
+from io import open
+from setuptools import setup
+
+
+setup(
+    name='arucodetect',
+    version='1.0',
+    author='myortf_inside',
+    author_email='faridgarifullin66@gmail.com',
+
+    packages=['arucodetect'],
+    install_requires = ['opencv','cv2', 'math']
+)
