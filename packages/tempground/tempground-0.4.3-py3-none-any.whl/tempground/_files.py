e7109b479161f7cd3256a297e721b8e43b38c065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:18 +0000

    Add package tempground/0.4.3/tempground-0.4.3-py3-none-any.whl

---
+++ packages/tempground/tempground-0.4.3-py3-none-any.whl/tempground/_files.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: (c) 2022 Artsiom iG (rtmigo.github.io)
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import os
+import shutil
+from pathlib import Path
+
+
+def _rmtree_skipping_permission_errors(path: Path):
+    # TODO test
+    if path.is_dir():
+        try:
+            shutil.rmtree(path)
+        except PermissionError:
+            # Happens on Windows, when deleting .git subdir.
+            # We cannot delete the whole `path`, but we can try to delete
+            # something inside of it.
+            for sub in path.glob("*"):
+                _rmtree_skipping_permission_errors(sub)
+    else:
+        try:
+            os.remove(path)
+        except PermissionError:
+            pass
