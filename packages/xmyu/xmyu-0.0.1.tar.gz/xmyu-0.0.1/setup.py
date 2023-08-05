383e24f4bf1041dcfa9679d5f2ad97f7bc8c9a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:49 +0000

    Add package xmyu/0.0.1/xmyu-0.0.1.tar.gz

---
+++ packages/xmyu/xmyu-0.0.1.tar.gz/xmyu-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+name="xmyu",
+version="0.0.1",
+author="LuoTing",
+author_email="977935501@qq.com",
+description="咸鱼只想占个名字罢辽",
+# 项目主页
+url="https://gitee.com/ltfd/a-salted-fish-package.git",
+# 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+packages=find_packages()
+
+)
+
+
+
+
