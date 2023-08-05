373145687b6282a80ff74bd17b07ace9a0289150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:18 +0000

    Add package tempground/0.4.3/tempground-0.4.3.tar.gz

---
+++ packages/tempground/tempground-0.4.3.tar.gz/tempground-0.4.3/setup.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+name = "tempground"
+
+setup(
+    name=name,
+    version="0.4.3",
+
+    author="Artsiom iG",
+    author_email="ortemeo@gmail.com",
+
+    packages=find_packages(include=['tempground', 'tempground.*']),
+    package_data={'tempground': ['py.typed']},
+
+    python_requires='>=3.10, <4',  # 3.10 for `match`
+    install_requires=[],
+
+    long_description=(Path(__file__).parent / 'README.md') \
+        .read_text(encoding="utf-8"),
+    long_description_content_type='text/markdown',
+
+    license="MIT",
+
+    keywords="temp temporary project sandbox "
+             "library unit testing integration".split(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows"
+    ],
+)
---
+++ packages/tempground/tempground-0.4.3.tar.gz/tempground-0.4.3/tempground/_errors.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: (c) 2022 Artsiom iG (rtmigo.github.io)
+# SPDX-License-Identifier: MIT
+
+import warnings
+
+
+class GradleRunFailed (Exception):
+    def __init__(self, msg):
+        warnings.warn("Obsolete 2022-09", DeprecationWarning)
+        super().__init__(msg)
+
+
+class UnexpectedOutput (Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
