7124f0e4a76931fd0dbd61e05466a4648775fd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:28 +0000

    Add package calcipy/0.20.0/calcipy-0.20.0-py3-none-any.whl

---
+++ packages/calcipy/calcipy-0.20.0-py3-none-any.whl/calcipy/dot_dict.py
@@ -0,0 +1,26 @@
+"""Dotted dictionary for consistent interface."""
+
+from beartype import beartype
+from beartype.typing import Any, Dict, Union
+from box import Box
+
+DDICT_TYPE = Union[Dict[str, Any], Box]
+"""Return type from `ddict()`."""
+
+
+@beartype
+def ddict(**kwargs: Dict[str, Any]) -> DDICT_TYPE:
+    """Return a dotted dictionary that can also be accessed normally.
+
+    Currently uses `python-box` because there is a more recent release, but could also use `munch`
+
+    Other variations are no longer supported, such as `bunch` and `ddict` among others
+
+    Args:
+        **kwargs: keyword arguments formatted into dictionary
+
+    Returns:
+        DDICT_TYPE: dotted dictionary
+
+    """
+    return Box(kwargs)
