09da04e620df6f5639688c3bd3d742b9eb290aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:23 +0000

    Add package sqaureandcube/0.0.4/sqaureandcube-0.0.4-py3-none-any.whl

---
+++ packages/sqaureandcube/sqaureandcube-0.0.4-py3-none-any.whl/squareofnumber/square.py
@@ -0,0 +1,6 @@
+def square():
+    n=int(input("Enter the Number!!!!!\n"))
+    print("The square of {} is {}".format(n,n*n))
+
+if __name__=="__main__":
+    square()
---
+++ packages/sqaureandcube/sqaureandcube-0.0.4-py3-none-any.whl/tests/cubetest.py
@@ -0,0 +1,6 @@
+# import unittest
+# from squareandcube import cube as c
+# class TestSquare(unittest.TestCase):
+#     def test_square(self):
+#         self.assertEqual(c.cube(4),64)
+# unittest.main()
