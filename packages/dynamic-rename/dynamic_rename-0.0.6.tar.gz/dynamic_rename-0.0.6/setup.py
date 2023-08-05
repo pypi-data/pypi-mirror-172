182b709277f10ded58a312d2f581345d6ddb4235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:21 +0000

    Add package dynamic-rename/0.0.6/dynamic_rename-0.0.6.tar.gz

---
+++ packages/dynamic-rename/dynamic_rename-0.0.6.tar.gz/dynamic_rename-0.0.6/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='dynamic_rename',
+        version='0.0.6',
+        description='Column rename  in pyspark dataframe',
+        packages=['dynamic_rename'],
+        author_email='ranjitmaity95@gmail.com',
+        zip_safe=False,
+        long_description_content_type="text/markdown",
+        long_description=open('README.md').read(),
+        )
