453944487617e3d04a6c9656c6466f84fa796dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:19 +0000

    Add package ezface/0.0.1/ezface-0.0.1.tar.gz

---
+++ packages/ezface/ezface-0.0.1.tar.gz/ezface-0.0.1/README.md
@@ -0,0 +1,7 @@
+# ezface
+This project provides an easy way to recognize faces using AI
+# 安装
+pip install ezface
+
+# 准备工作
+准备待识别的人脸照片
---
+++ packages/ezface/ezface-0.0.1.tar.gz/ezface-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ezface",
+    version="0.0.1",
+    author="SunJian",
+    author_email="330160114@qq.com",
+
+    description="This project provides an easy way to recognize faces using AI",
+
+    # 项目主页
+    url="https://github.com/sunjian-github/ezface",
+
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+)
