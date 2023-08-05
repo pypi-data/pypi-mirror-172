6c06d284e41f3ba11a5f3bd44edfff68b01747c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:03 +0000

    Add package nPerlinNoise/0.1.3a1/nPerlinNoise-0.1.3a1.tar.gz

---
+++ packages/nPerlinNoise/nPerlinNoise-0.1.3a1.tar.gz/nPerlinNoise-0.1.3a1/docs/CHANGELOG.md
@@ -0,0 +1,18 @@
+## v0.1.0-alpha:
+- first public release
+
+## v0.1.[1,2]-alpha:
+- bugs fixed
+- docs implemented
+- readability improved
+
+## v0.1.2-alpha[x]:
+- packaging trails
+
+## v0.1.3-alpha:
+- ready for public PyPI release
+- wheels supported now
+
+## v0.1.3-alpha1:
+- improved docs, appended usage section
+- renamed import directory `NPerlinNoise` to `nPerlinNoise`
---
+++ packages/nPerlinNoise/nPerlinNoise-0.1.3a1.tar.gz/nPerlinNoise-0.1.3a1/tests/main.py
@@ -0,0 +1,59 @@
+# main.py
+
+# todo: versions auto in [package.json, __init__.py, pyproject.toml, README.md]
+
+from nPerlinNoise import *
+
+noise = Noise(
+    seed=None,
+    frequency=8,
+    waveLength=128,
+    warp=None,
+    _range=None,
+    octaves=8,
+    persistence=0.5,
+    lacunarity=2
+)
+
+
+def main():
+    plot = 1
+    mul, res = 1, 4
+    colorMap = (
+        "#00f",
+        "#0f0",
+        "#f00",
+    )
+    gradients = Gradient.none()
+    colorGradient = LinearColorGradient(*colorMap, grad='s').earth(grad='i') or LinearColorGradient.none()
+    h, coordsMesh = perlinGenerator(noise,
+                                    (0, noise.waveLength[0] * mul, noise.waveLength[0] * res),
+                                    (0, noise.waveLength[1] * mul, noise.waveLength[0] * res))
+    g = applyGrads(h, coordsMesh, gradients)
+    c = colorGradient(g)
+
+    if plot == 1:
+        from matplotlib import pyplot
+        # ---matplotlib---
+        fig, ax = pyplot.subplots()
+        ax.imshow(c, cmap="gray")
+        pyplot.show()
+    elif plot == 2:
+        import plotly.graph_objects as go
+        from plotly.offline import offline
+        # -----plotly-----
+        marker_data = go.Surface(x=coordsMesh[0], y=coordsMesh[1], z=g)
+        fig = go.Figure(data=marker_data)
+        fig.update_layout(
+            scene=dict(zaxis=dict(nticks=4, range=[0, 2])),
+            width=700,
+            margin=dict(r=20, l=10, b=10, t=10))
+        fig.update_traces(contours_z=dict(
+            show=True, usecolormap=True,
+            highlightcolor='limegreen',
+            project_z=True))
+        offline.plot(fig)
+
+
+if __name__ == '__main__':
+    main()
