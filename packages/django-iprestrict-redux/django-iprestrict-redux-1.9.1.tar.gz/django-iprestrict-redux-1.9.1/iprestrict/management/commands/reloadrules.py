43691e7df13071ebe006b871fd8f4b400b8593a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:56 +0000

    Add package django-iprestrict-redux/1.9.1/django-iprestrict-redux-1.9.1.tar.gz

---
+++ packages/django-iprestrict-redux/django-iprestrict-redux-1.9.1.tar.gz/django-iprestrict-redux-1.9.1/iprestrict/management/commands/reloadrules.py
@@ -0,0 +1,8 @@
+from . import reload_rules
+from ._utils import warn_about_renamed_command
+
+
+class Command(reload_rules.Command):
+    def handle(self, *args, **options):
+        warn_about_renamed_command("reloadrules", "reload_rules")
+        super(Command, self).handle(*args, **options)
