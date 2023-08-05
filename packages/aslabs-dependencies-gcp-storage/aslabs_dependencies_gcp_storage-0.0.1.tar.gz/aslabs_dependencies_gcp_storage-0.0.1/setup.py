f3066ef659640d0cbafa566e6ccbcc7f84ff332e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:11 +0000

    Add package aslabs-dependencies-gcp-storage/0.0.1/aslabs_dependencies_gcp_storage-0.0.1.tar.gz

---
+++ packages/aslabs-dependencies-gcp-storage/aslabs_dependencies_gcp_storage-0.0.1.tar.gz/aslabs_dependencies_gcp_storage-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aslabs', 'aslabs.dependencies.gcp.storage']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aslabs-dependencies>=0.0.3,<0.0.4', 'google-cloud-storage>=2.4.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'aslabs-dependencies-gcp-storage',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': '',
+    'author': 'Titusz Ban',
+    'author_email': 'tituszban@antisociallabs.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
