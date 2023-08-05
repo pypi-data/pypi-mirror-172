d29c3d887e19bd5fe0a8a90f350e850823d730b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:40 +0000

    Add package chalkpy/1.6.3/chalkpy-1.6.3.tar.gz

---
+++ packages/chalkpy/chalkpy-1.6.3.tar.gz/chalkpy-1.6.3/chalk/streams/__init__.py
@@ -0,0 +1,11 @@
+from chalk.streams.KafkaSource import KafkaSource
+from chalk.streams.stream import State, stream
+from chalk.streams.windows import Windowed, windowed
+
+__all__ = [
+    "KafkaSource",
+    "stream",
+    "State",
+    "Windowed",
+    "windowed",
+]
