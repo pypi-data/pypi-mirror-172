a6bff838eec8dd3698a23d03df213c5189bfcd07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:48 +0000

    Add package Bloxflip.py/1.0.6/Bloxflip.py-1.0.6.tar.gz

---
+++ packages/Bloxflip.py/Bloxflip.py-1.0.6.tar.gz/Bloxflip.py-1.0.6/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="Bloxflip.py",
+	version = "1.0.6",
+	author = "TheOnlyIceBear",
+	description = "An api wrapper for https://bloxflip.com",
+	packages=["bloxflip"]
+)
