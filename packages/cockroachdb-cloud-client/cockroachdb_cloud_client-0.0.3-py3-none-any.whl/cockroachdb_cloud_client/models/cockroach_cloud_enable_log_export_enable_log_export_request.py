f70f2c16d2baa88f7c578ceb8c86ded5049a7bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:44 +0000

    Add package cockroachdb-cloud-client/0.0.3/cockroachdb_cloud_client-0.0.3-py3-none-any.whl

---
+++ packages/cockroachdb-cloud-client/cockroachdb_cloud_client-0.0.3-py3-none-any.whl/cockroachdb_cloud_client/models/cockroach_cloud_enable_log_export_enable_log_export_request.py
@@ -0,0 +1,88 @@
+from typing import Any, Dict, List, Type, TypeVar, Union
+
+import attr
+
+from ..models.log_export_type import LogExportType
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="CockroachCloudEnableLogExportEnableLogExportRequest")
+
+
+@attr.s(auto_attribs=True)
+class CockroachCloudEnableLogExportEnableLogExportRequest:
+    """
+    Attributes:
+        auth_principal (Union[Unset, str]): auth_principal is either the AWS Role ARN that identifies a role
+            that the cluster account can assume to write to CloudWatch or the
+            GCP Project ID that the cluster service account has permissions to
+            write to for cloud logging.
+        log_name (Union[Unset, str]): log_name is an identifier for the logs in the customer's log sink.
+        type (Union[Unset, LogExportType]): LogExportType encodes the cloud selection that we're exporting to
+            along with the cloud logging platform. Currently, each cloud has a
+            single logging platform.
+    """
+
+    auth_principal: Union[Unset, str] = UNSET
+    log_name: Union[Unset, str] = UNSET
+    type: Union[Unset, LogExportType] = UNSET
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        auth_principal = self.auth_principal
+        log_name = self.log_name
+        type: Union[Unset, str] = UNSET
+        if not isinstance(self.type, Unset):
+            type = self.type.value
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({})
+        if auth_principal is not UNSET:
+            field_dict["auth_principal"] = auth_principal
+        if log_name is not UNSET:
+            field_dict["log_name"] = log_name
+        if type is not UNSET:
+            field_dict["type"] = type
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        auth_principal = d.pop("auth_principal", UNSET)
+
+        log_name = d.pop("log_name", UNSET)
+
+        _type = d.pop("type", UNSET)
+        type: Union[Unset, LogExportType]
+        if _type is None:
+            type = None
+        elif isinstance(_type, Unset):
+            type = UNSET
+        else:
+            type = LogExportType(_type)
+
+        cockroach_cloud_enable_log_export_enable_log_export_request = cls(
+            auth_principal=auth_principal,
+            log_name=log_name,
+            type=type,
+        )
+
+        cockroach_cloud_enable_log_export_enable_log_export_request.additional_properties = d
+        return cockroach_cloud_enable_log_export_enable_log_export_request
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
