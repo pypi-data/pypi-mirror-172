c76e0fcc0149dcd1d52c25bfd69d6f0c5a790bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:49 +0000

    Add package retico-core/0.2.10/retico-core-0.2.10.tar.gz

---
+++ packages/retico-core/retico-core-0.2.10.tar.gz/retico-core-0.2.10/retico_core/__init__.py
@@ -0,0 +1,8 @@
+from retico_core.abstract import *
+from retico_core import audio
+from retico_core import debug
+from retico_core import network
+from retico_core import text
+from retico_core import dialogue
+
+from retico_core.version import __version__
