a2d4cf93700dbd0199d6fc5ac407a7c4a1b824e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:06 +0000

    Add package cornflakes/2.4.8/cornflakes-2.4.8-cp38-cp38-manylinux_2_35_x86_64.whl

---
+++ packages/cornflakes/cornflakes-2.4.8-cp38-cp38-manylinux_2_35_x86_64.whl/cornflakes/__init__.py
@@ -0,0 +1,38 @@
+"""cornflakes (Top-level package).
+__________________________________
+
+.. currentmodule:: cornflakes
+
+.. autosummary::
+   :toctree: _generate
+
+    ini_load
+    eval_type
+    eval_datetime
+    eval_csv
+    extract_between
+    apply_match
+    ini_config
+    generate_config_group
+"""  # noqa: RST303 D205
+from _cornflakes import apply_match, eval_csv, eval_datetime, eval_type, extract_between, ini_load
+from cornflakes.decorator import ini_config, ini_group
+from cornflakes.config import generate_ini_group_module
+from cornflakes.click import make_cli
+
+__author__ = "Semjon Geist"
+__email__ = "semjon.geist@ionos.com"
+__version__ = "2.4.8"
+
+__all__ = [
+    "ini_load",
+    "eval_type",
+    "eval_datetime",
+    "eval_csv",
+    "extract_between",
+    "apply_match",
+    "ini_config",
+    "ini_group",
+    "generate_ini_group_module",
+    "make_cli",
+]
