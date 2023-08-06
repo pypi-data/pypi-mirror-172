29c796776a435a25645b103700b1d11a3c71378c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:00 +0000

    Add package lnschema_wetlab/0.7.2/lnschema_wetlab-0.7.2-py2.py3-none-any.whl

---
+++ packages/lnschema_wetlab/lnschema_wetlab-0.7.2-py2.py3-none-any.whl/lnschema_wetlab/_biosample.py
@@ -0,0 +1,31 @@
+from datetime import datetime as datetime
+from typing import Optional
+
+from lnschema_core._timestamps import CreatedAt, UpdatedAt
+from sqlmodel import Field, SQLModel
+
+from . import id as idg
+
+
+class biosample(SQLModel, table=True):  # type: ignore
+    """Biological samples that are registered in experiments."""
+
+    id: str = Field(default_factory=idg.biosample, primary_key=True)
+    external_id: Optional[str] = Field(default=None, index=True, unique=True)
+    name: Optional[str] = Field(default=None, index=True)
+    batch: Optional[str] = None
+    species_id: Optional[str] = Field(
+        default=None, foreign_key="species.id", index=True
+    )
+    tissue_id: Optional[str] = Field(default=None, foreign_key="tissue.id", index=True)
+    cell_type_id: Optional[str] = Field(
+        default=None, foreign_key="cell_type.id", index=True
+    )
+    disease_id: Optional[str] = Field(
+        default=None, foreign_key="disease.id", index=True
+    )
+    treatment_id: Optional[str] = Field(
+        default=None, foreign_key="treatment.id", index=True
+    )
+    created_at: datetime = CreatedAt
+    updated_at: Optional[datetime] = UpdatedAt
