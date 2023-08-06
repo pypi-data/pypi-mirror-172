d9c74820203a02124f2a7c2e072070be350b6dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:30 +0000

    Add package flybirds/0.4.6/flybirds-0.4.6.tar.gz

---
+++ packages/flybirds/flybirds-0.4.6.tar.gz/flybirds-0.4.6/flybirds/core/plugin/plugins/default/ui_driver/paddleocr/ocr_manage.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Poco manage api
+"""
+import flybirds.core.global_resource as gr
+
+
+def ocr_init(lang=None):
+    """
+    Initialize the paddleocr object
+     :return:
+    """
+    if lang is None:
+        ocr_lang = gr.get_app_config_value("ocr_lang")
+    else:
+        ocr_lang = lang
+
+    if ocr_lang != "":
+        from paddleocr import PaddleOCR
+        # Paddleocr support languages
+        # example`ch`, `en`, `fr`, `german`, `korean`, `japan`
+        ocr = PaddleOCR(use_angle_cls=True,
+                        lang=ocr_lang)  # need to run only once to download and load model into memory
+
+        return ocr
+
