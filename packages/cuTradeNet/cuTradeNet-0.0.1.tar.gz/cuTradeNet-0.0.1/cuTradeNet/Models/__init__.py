295eae27908dae7f9269d89e8f2d961ea205ca2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:47 +0000

    Add package cuTradeNet/0.0.1/cuTradeNet-0.0.1.tar.gz

---
+++ packages/cuTradeNet/cuTradeNet-0.0.1.tar.gz/cuTradeNet-0.0.1/cuTradeNet/Models/__init__.py
@@ -0,0 +1,5 @@
+from .YardSale import YSNetModel as YardSale
+from .MergerSpinoff import MSNetModel as MergerSpinoff
+#from .Constant import CNetModel as Constant
+#from .AllIn import AINetModel as AllIn
+
