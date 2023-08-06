eb1238e8fe90d6fc53dcfdc786fd5a8cb44017e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:54 +0000

    Add package assembly-ai-demo/0.0.5/assembly_ai_demo-0.0.5.tar.gz

---
+++ packages/assembly-ai-demo/assembly_ai_demo-0.0.5.tar.gz/assembly_ai_demo-0.0.5/src/assembly_ai/__init__.py
@@ -0,0 +1,10 @@
+import requests
+
+from assembly_ai.walkthroughs import (submit_url_for_transcription, 
+                                      get_status_of_transcription, 
+                                      get_transcription_results)
+
+api_key = None
+audio_url = None
+
+endpoint = "https://api.assemblyai.com/v2/transcript"
