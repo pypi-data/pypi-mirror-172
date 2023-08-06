cc82466100d9a1bfd917b9f59e00be8cc08e4af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:43 +0000

    Add package hecalib/1.0.1/hecalib-1.0.1.tar.gz

---
+++ packages/hecalib/hecalib-1.0.1.tar.gz/hecalib-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "hecalib",
+    version = "1.0.1",
+    author = "Ikenna Enebuse",
+    author_email = "i.enebuse@gmail.com",
+    description = "A python library for different implementations of handeye calibration for vision guided robots",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ienebuse/hecalib",
+    packages = ["hecalib"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe = False
+)
