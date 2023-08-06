d29bc051e0738af3744042a8708b9b9c9bf345ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:23 +0000

    Add package sematic/0.18.1/sematic-0.18.1-py3-none-any.whl

---
+++ packages/sematic/sematic-0.18.1-py3-none-any.whl/sematic/examples/add/add_cloud.py
@@ -0,0 +1,17 @@
+# Standard Library
+import logging
+
+# Sematic
+from sematic import CloudResolver
+from sematic.examples.add.pipeline import pipeline
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    future = pipeline(1, 2, 3).set(
+        name="Basic add example pipeline", tags=["example", "basic", "final"]
+    )
+    # result = future.resolve(CloudResolver(detach=False, max_parallelism=2))
+    result = future.resolve(CloudResolver(detach=False))
+
+    logging.info(result)
