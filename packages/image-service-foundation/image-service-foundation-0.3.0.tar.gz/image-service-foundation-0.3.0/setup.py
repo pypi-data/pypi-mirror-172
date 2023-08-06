519a4f80c00b925313dbff0cf3aa7df9d9185e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:28 +0000

    Add package image-service-foundation/0.3.0/image-service-foundation-0.3.0.tar.gz

---
+++ packages/image-service-foundation/image-service-foundation-0.3.0.tar.gz/image-service-foundation-0.3.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['image_service_foundation',
+ 'image_service_foundation.config',
+ 'image_service_foundation.storage']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=5.1b3',
+ 'dropbox==11.35.0',
+ 'google-cloud-storage>=1.35,<2',
+ 'requests>=2.21,<3.0']
+
+setup_kwargs = {
+    'name': 'image-service-foundation',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Anustup Das',
+    'author_email': 'anustup@mediadistillery.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
