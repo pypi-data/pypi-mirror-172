146a86c06fd48c1fc621ed7de3a7db256869e8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:53 +0000

    Add package calorine/1.3/calorine-1.3.tar.gz

---
+++ packages/calorine/calorine-1.3.tar.gz/calorine-1.3/calorine/tools/__init__.py
@@ -0,0 +1,9 @@
+from .analysis import (analyze_data,
+                       get_autocorrelation_function,
+                       get_correlation_length,
+                       get_error_estimate)
+
+__all__ = ['analyze_data',
+           'get_autocorrelation_function',
+           'get_correlation_length',
+           'get_error_estimate']
