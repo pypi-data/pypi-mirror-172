b953ff17a1513482e33f8b7447ece95b3fd4f3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:08 +0000

    Add package django-api-forms/1.0.0rc5/django_api_forms-1.0.0rc5-py3-none-any.whl

---
+++ packages/django-api-forms/django_api_forms-1.0.0rc5-py3-none-any.whl/django_api_forms/__init__.py
@@ -0,0 +1,29 @@
+from .exceptions import DetailValidationError
+from .fields import BooleanField
+from .fields import FieldList
+from .fields import FormField
+from .fields import FormFieldList
+from .fields import EnumField
+from .fields import DictionaryField
+from .fields import AnyField
+from .fields import FileField
+from .fields import ImageField
+from .forms import Form
+from .forms import ModelForm
+from .version import __version__
+
+__all__ = [
+    'DetailValidationError',
+    'BooleanField',
+    'FieldList',
+    'FormField',
+    'FormFieldList',
+    'EnumField',
+    'DictionaryField',
+    'AnyField',
+    'FileField',
+    'ImageField',
+    'Form',
+    'ModelForm',
+    '__version__'
+]
