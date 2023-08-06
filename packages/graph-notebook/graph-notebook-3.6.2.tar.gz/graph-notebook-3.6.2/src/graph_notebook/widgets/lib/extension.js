3cbe606bc27b5a0d4fda9217db81d81b3db77532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:09 +0000

    Add package graph-notebook/3.6.2/graph-notebook-3.6.2.tar.gz

---
+++ packages/graph-notebook/graph-notebook-3.6.2.tar.gz/graph-notebook-3.6.2/src/graph_notebook/widgets/lib/extension.js
@@ -0,0 +1,12 @@
+"use strict";
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+// eslint-disable-next-line @typescript-eslint/camelcase
+window.__webpack_public_path__ =
+    document.body.getAttribute("data-base-url") +
+        "nbextensions/graph_notebook_widgets";
+__export(require("./index"));
---
+++ packages/graph-notebook/graph-notebook-3.6.2.tar.gz/graph-notebook-3.6.2/src/graph_notebook/widgets/lib/index.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+ */
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./version"));
+__export(require("./force_widget"));
