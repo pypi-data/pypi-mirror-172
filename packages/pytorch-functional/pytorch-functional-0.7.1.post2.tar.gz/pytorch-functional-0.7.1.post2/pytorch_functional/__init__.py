460b6763ac0fef8630ffc53934c84482f424fd50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:39 +0000

    Add package pytorch-functional/0.7.1.post2/pytorch-functional-0.7.1.post2.tar.gz

---
+++ packages/pytorch-functional/pytorch-functional-0.7.1.post2.tar.gz/pytorch-functional-0.7.1.post2/pytorch_functional/__init__.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 2022 Szymon Mikler
+
+from pytorch_symbolic import (
+    Input,
+    SymbolicModel as FunctionalModel,
+    add_to_model,
+    useful_layers,
+    graph_algorithms,
+    config,
+)
