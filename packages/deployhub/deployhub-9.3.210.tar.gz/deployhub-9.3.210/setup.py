18bc7f48b2cad642b9810ced7d5fff3bb1d43e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:12 +0000

    Add package deployhub/9.3.210/deployhub-9.3.210.tar.gz

---
+++ packages/deployhub/deployhub-9.3.210.tar.gz/deployhub-9.3.210/setup.py
@@ -0,0 +1,38 @@
+"""Setup config for the module."""
+
+import setuptools
+
+from distutils.core import setup
+
+setup(
+    setup_requires=['wheel'],
+    url='https://ortelius.io',
+    project_urls={ 
+        'Project Repo': 'https://github.com/ortelius/compupdate',
+        'Issues': 'https://github.com/ortelius/ortelius/issues',
+        'CLI Documentation': 'https://github.com/ortelius/compupdate/blob/main/doc/dh.md',
+        'Python API Documentation': 'https://github.com/ortelius/compupdate/blob/main/doc/deployhub.md',
+        'Jenkinsfile Example': 'https://github.com/ortelius/compupdate/blob/main/Jenkinsfile'
+        }, 
+    author='Steve Taylor',
+    author_email='steve@deployhub.com',
+    name='deployhub',
+    version='9.3.210',
+    packages=['deployhub',],
+    scripts=['bin/dh', 'bin/helminfo.sh'],
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
+    long_description=open('doc/dh.md').read(),
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+     install_requires=[
+        'click',
+        'qtoml',
+        'PyYAML',
+        'requests',
+        'configobj',
+        'flatten_dict',
+        'pydantic',
+        'certifi'
+        ],
+    include_package_data=True
+)
