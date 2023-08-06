562006449b533600af064b23f668986cf898ddd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:19 +0000

    Add package build-django/3.1.0/build_django-3.1.0-py3-none-any.whl

---
+++ packages/build-django/build_django-3.1.0-py3-none-any.whl/build_django/const.py
@@ -0,0 +1,27 @@
+GITIGNORE: str = '/venv\n/static\n/media\n__pycache__\n*.sqlite*\n*.log\n.env'
+
+ENV_EXAMPLE: str = '''
+SECRET_KEY=
+
+# List of comma separated values
+ALLOWED_HOSTS=
+
+# Remove if production
+DEBUG=
+
+# Sets SECURE_PROXY_SSL_HEAD to ('HTTP_X_FORWARDED_PROTO', 'https')
+# Remove if not needed
+USE_SSL=
+'''
+
+DEFAULT_ENV = '''
+    DEBUG=(bool, False),
+    USE_SSL=(bool, False)
+'''
+
+REQUIRED_PACKAGES: list[str] = (
+    'wheel',
+    'django',
+    'django-environ',
+    'gunicorn'
+)
