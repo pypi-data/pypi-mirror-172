ece560a2be3d6518a7a8b2b3dcec78dbccb9d409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:37 +0000

    Add package aidev/1.4/aidev-1.4.tar.gz

---
+++ packages/aidev/aidev-1.4.tar.gz/aidev-1.4/aidev/blocks/optimization.py
@@ -0,0 +1,91 @@
+from typing import Callable, List
+from aidev.utilities.types import Optimization
+from pymoo.algorithms.moo.unsga3 import NSGA3
+from pymoo.algorithms.soo.nonconvex.ga import GA
+from pymoo.algorithms.soo.nonconvex.nelder import NelderMead
+from pymoo.algorithms.soo.nonconvex.pso import PSO
+from pymoo.factory import get_reference_directions
+
+
+class NelderMeadSearch(Optimization):
+    def __init__(
+        self, objective: Callable[..., List[float]], features, popsize
+    ) -> None:
+        """
+
+        :param features: Collective targets and parameters information in the form of FeatureDefinition class
+        :type features: FeatureDefinition
+        :param popsize: population size to use in evolutionary algorithms (global optimization)
+        :type popsize: int
+        """
+        super().__init__(objective, features, popsize)
+
+    def _algorithm(self):
+        return NelderMead()
+
+
+class GeneticAlgorithm(Optimization):
+    def __init__(
+        self, objective: Callable[..., List[float]], features, popsize
+    ) -> None:
+        """
+
+        :param features: Collective targets and parameters information in the form of FeatureDefinition class
+        :type features: FeatureDefinition
+        :param popsize: population size to use in evolutionary algorithms (global optimization)
+        :type popsize: int
+        """
+        super().__init__(objective, features, popsize)
+
+    def _algorithm(self):
+        return GA(pop_size=self.popsize, eliminate_duplicates=True)
+
+
+class ParticleSwarm(Optimization):
+    def __init__(
+        self, objective: Callable[..., List[float]], features, popsize
+    ) -> None:
+        """
+
+        :param features: Collective targets and parameters information in the form of FeatureDefinition class
+        :type features: FeatureDefinition
+        :param popsize: population size to use in evolutionary algorithms (global optimization)
+        :type popsize: int
+        """
+        super().__init__(objective, features, popsize)
+
+    def _algorithm(self):
+        return PSO(pop_size=self.popsize)
+
+
+class NSGA_III(Optimization):
+    def __init__(
+        self, objective: Callable[..., List[float]], features, popsize
+    ) -> None:
+        """
+
+        :param features: Collective targets and parameters information in the form of FeatureDefinition class
+        :type features: FeatureDefinition
+        :param popsize: population size to use in evolutionary algorithms (global optimization)
+        :type popsize: int
+        """
+        super().__init__(objective, features, popsize)
+
+    def _algorithm(self):
+        """
+        Method defining the optimization algorithm.
+        For multi-objective problems NSGA-III is used.
+
+        :return: The algorithm object.
+        :rtype:
+
+        See https://pymoo.org/algorithms/moo/nsga3.html for more information.
+        """
+        ref_dirs = get_reference_directions(
+            "energy", self.features.nobjs, n_points=10, seed=1
+        )
+        algorithm = NSGA3(
+            pop_size=self.popsize, ref_dirs=ref_dirs, eliminate_duplicates=True
+        )
+
+        return algorithm
---
+++ packages/aidev/aidev-1.4.tar.gz/aidev-1.4/aidev/interfaces/nx_interface.py
@@ -0,0 +1,121 @@
+import NXOpen
+import NXOpen.CAE
+import NXOpen.MenuBar
+from typing import List
+
+from aidev.utilities.types import FeatureDefinition
+
+
+class NXInterface:
+    def __init__(
+        self,
+        features: FeatureDefinition,
+        sol_name: str,
+        files: dict,
+        ncpus: int = 1,
+    ) -> None:
+        self.parameters = features.pnames
+        self.targets = features.tnames
+        self.sol_name = sol_name
+        self.files = files
+        self.ncpus = ncpus
+
+    def objective(self, x: list) -> List[float]:
+        x = [str(p) for p in x]
+        theSession = NXOpen.Session.GetSession()
+        workSimPart = theSession.Parts.BaseWork
+        displaySimPart = theSession.Parts.BaseDisplay
+
+        # CAD
+
+        part1 = theSession.Parts.FindObject(self.files["part"])
+        status1, partLoadStatus1 = theSession.Parts.SetActiveDisplay(
+            part1,
+            NXOpen.DisplayPartOption.AllowAdditional,
+            NXOpen.PartDisplayPartWorkPartOption.SameAsDisplay,
+        )
+
+        workSimPart = NXOpen.BasePart.Null
+        workPart = theSession.Parts.Work
+        displaySimPart = NXOpen.BasePart.Null
+        displayPart = theSession.Parts.Display
+        partLoadStatus1.Dispose()
+
+        objects1 = [NXOpen.NXObject.Null] * len(self.parameters)
+        i = 0
+        for name, value in zip(self.parameters, x):
+            expression_part = workPart.Expressions.FindObject(name)
+            workPart.Expressions.EditExpressionWithUnits(
+                expression_part, expression_part.Units, value
+            )
+            objects1[i] = expression_part
+            i += 1
+
+        # measures = []
+        # for measurement in measurements:
+        #     measure = workPart.Expressions.FindObject(measurement)
+        #     measures.append(measure.GetValueUsingUnits(measure.UnitsOption.Expression))
+
+        # FEM
+
+        femPart1 = theSession.Parts.FindObject(self.files["fem"])
+        status2, partLoadStatus2 = theSession.Parts.SetActiveDisplay(
+            femPart1,
+            NXOpen.DisplayPartOption.AllowAdditional,
+            NXOpen.PartDisplayPartWorkPartOption.SameAsDisplay,
+        )
+
+        workPart = NXOpen.Part.Null
+        workFemPart = theSession.Parts.BaseWork
+        displayPart = NXOpen.Part.Null
+        displayFemPart = theSession.Parts.BaseDisplay
+        partLoadStatus2.Dispose()
+
+        fEModel1 = workFemPart.FindObject("FEModel")
+        fEModel1.UpdateFemodel()
+
+        # SIM
+
+        simPart1 = theSession.Parts.FindObject(self.files["sim"])
+        status3, partLoadStatus3 = theSession.Parts.SetActiveDisplay(
+            simPart1,
+            NXOpen.DisplayPartOption.AllowAdditional,
+            NXOpen.PartDisplayPartWorkPartOption.SameAsDisplay,
+        )
+
+        workSimPart = theSession.Parts.BaseWork
+        displaySimPart = theSession.Parts.BaseDisplay
+        partLoadStatus3.Dispose()
+
+        theCAESimSolveManager = NXOpen.CAE.SimSolveManager.GetSimSolveManager(
+            theSession
+        )
+
+        psolutions1 = [NXOpen.CAE.SimSolution.Null] * 1
+        simSimulation1 = workSimPart.FindObject("Simulation")
+        simSolution1 = simSimulation1.FindObject(f"Solution[{self.sol_name}]")
+        propertyTable1 = simSolution1.SolverOptionsPropertyTable
+        propertyTable1.SetBooleanPropertyValue("solution monitor", False)
+        propertyTable1.SetIntegerPropertyValue("parallel", self.ncpus)
+
+        psolutions1[0] = simSolution1
+        (
+            numsolutionssolved1,
+            numsolutionsfailed1,
+            numsolutionsskipped1,
+        ) = theCAESimSolveManager.SolveChainOfSolutions(
+            psolutions1,
+            NXOpen.CAE.SimSolution.SolveOption.Solve,
+            NXOpen.CAE.SimSolution.SetupCheckOption.DoNotCheck,
+            NXOpen.CAE.SimSolution.SolveMode.Foreground,
+        )
+
+        objects3 = [NXOpen.CAE.ResultMeasure.Null] * len(self.targets)
+        for c, target in enumerate(self.targets):
+            resultMeasure = simSimulation1.ResultMeasures.Find(
+                f"NXOpen.CAE.ResultMeasure[{target}]"
+            )
+            objects3[c] = resultMeasure
+        simSimulation1.ResultMeasures.UpdateMeasures(objects3)
+
+        return [object3.Result for object3 in objects3]
