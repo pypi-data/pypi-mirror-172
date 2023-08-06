e00c4b7386014ef9d256db1011596460ec9419a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:20 +0000

    Add package fishfish-gg/0.1.0/fishfish_gg-0.1.0-py3-none-any.whl

---
+++ packages/fishfish-gg/fishfish_gg-0.1.0-py3-none-any.whl/fishfish/__init__.py
@@ -0,0 +1,15 @@
+"""
+fishfish-py
+-----------
+FishFish API library for Python
+:copyright: (c) 2022 fishfish.gg
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "fishfish"
+__author__ = "fishfish-gg"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 fishfish.gg"
+__version__ = "0.1.0"
+
+print("FISHFISH: THIS LIBRARY HAS NOT YET BEEN RELEASED")
