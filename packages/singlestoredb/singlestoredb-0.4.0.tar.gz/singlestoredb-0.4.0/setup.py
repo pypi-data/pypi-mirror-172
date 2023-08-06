f75b5d2a078d23376d88bd83e4dd0fee21bb29f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:10 +0000

    Add package singlestoredb/0.4.0/singlestoredb-0.4.0.tar.gz

---
+++ packages/singlestoredb/singlestoredb-0.4.0.tar.gz/singlestoredb-0.4.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""SingleStoreDB package installer."""
+# from setuptools import Extension
+from setuptools import setup
+
+setup(
+    #     ext_modules=[
+    #         Extension(
+    #             '_pymysqlsv',
+    #             ['singlestoredb/clients/pymysqlsv/accel.c'],
+    #             py_limited_api=True,
+    #         ),
+    #     ],
+)
