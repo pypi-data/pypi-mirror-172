86a322009516388c8d9d288799f68498018737a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:43 +0000

    Add package smon-report/0.0.1/smon_report-0.0.1.tar.gz

---
+++ packages/smon-report/smon_report-0.0.1.tar.gz/smon_report-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='smon_report',
+    version="0.0.1",
+    author='Seryogin Vyacheslav',
+    author_email='vyacheslavseregin21@gmail.com',
+    description='An application that takes a data and returns the statistic for driver.',
+    packages=['source'],
+    license='MIT License',
+    readme="README.md",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.9',
+    ],
+    url='https://git.foxminded.com.ua/foxstudent102022/task-6-report-of-monaco-2018-racing/',
+
+)
