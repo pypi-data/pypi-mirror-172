824cd8e65f82add5dfb56407068fd828f8b90b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:02 +0000

    Add package boxtec/2.7/boxtec-2.7.tar.gz

---
+++ packages/boxtec/boxtec-2.7.tar.gz/boxtec-2.7/boxtec/scraper.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from random import choices
+
+def user_agents(cur):
+    sql = f"""
+            SELECT
+                percent, user_agent
+            FROM
+                user_agents
+            WHERE
+                ts = (
+                    SELECT
+                        MAX(ts)
+                    FROM
+                        user_agents)
+            """
+
+    cur.execute(sql)
+    return cur.fetchall()
+
+def choose_random(user_agents):
+    return choices(population=[ua['user_agent'] for ua in user_agents],
+                    weights=[ua['percent'] for ua in user_agents])[0]
