30fd5875d279a63cef987e4c9bcf1ace3257add1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:11 +0000

    Add package deep-utils/0.10.5/deep_utils-0.10.5-py3-none-any.whl

---
+++ packages/deep-utils/deep_utils-0.10.5-py3-none-any.whl/deep_utils/utils/json_utils/json_utils.py
@@ -0,0 +1,31 @@
+def load_json(json_path: str, encoding="utf-8") -> dict:
+    """
+    :param json_path: Path to json file to load
+    :param encoding: encoding format
+    :return: returns a json file
+    """
+    import json
+
+    with open(json_path, encoding=encoding) as f:
+        json_data = json.load(f)
+    return json_data
+
+
+def dump_json(
+    json_path, dict_object: dict, encoding="utf-8", ensure_ascii=True
+) -> None:
+    """
+    dumps a json file
+    Args:
+        dict_object:
+        json_path: path to json file
+        encoding: encoding format
+        ensure_ascii: set to False for persian characters
+
+    Returns:
+
+    """
+    import json
+
+    with open(json_path, mode="w", encoding=encoding) as f:
+        json.dump(dict_object, f, ensure_ascii=ensure_ascii)
---
+++ packages/deep-utils/deep_utils-0.10.5-py3-none-any.whl/deep_utils/vision/object_detection/__init__.py
@@ -0,0 +1,21 @@
+from deep_utils.utils.lib_utils.main_utils import list_utils, loader
+
+from .main import ObjectDetector
+
+try:
+    from deep_utils.dummy_objects.vision.object_detection import (
+        YOLOV5TorchObjectDetector,
+    )
+    from deep_utils.vision.object_detection.yolo.v5.torch.yolo_v5_torch_object_detection import (
+        YOLOV5TorchObjectDetector,
+    )
+except ModuleNotFoundError:
+    pass
+
+Object_Detection_Models = {"YOLOV5TorchObjectDetector": YOLOV5TorchObjectDetector}
+
+list_object_detection_models = list_utils(Object_Detection_Models)
+
+
+def object_detector_loader(name, **kwargs) -> ObjectDetector:
+    return loader(Object_Detection_Models, list_object_detection_models)(name, **kwargs)
