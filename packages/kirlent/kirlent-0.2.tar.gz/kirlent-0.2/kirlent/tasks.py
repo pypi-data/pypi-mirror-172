6c87f6fe0677434395f1179bf8f8965470fc2cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:49 +0000

    Add package kirlent/0.2/kirlent-0.2.tar.gz

---
+++ packages/kirlent/kirlent-0.2.tar.gz/kirlent-0.2/kirlent/tasks.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2022 H. Turgut Uyar <uyar@tekir.org>
+#
+# Kırlent is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Kırlent is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Kırlent.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+from invoke import Collection, task
+
+from . import pdf, slides
+
+
+@task
+def setup(c):
+    c.run("python -m pip --require-virtualenv install -U kirlent_docutils")
+    c.run("npm install decktape")
+
+    env = Path(__file__).parent / "env"
+    c.run(f"cp {env} .env")
+
+
+namespace = Collection(setup, slides, pdf)
