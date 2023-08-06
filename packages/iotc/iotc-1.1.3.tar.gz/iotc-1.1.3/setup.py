d1a20637be82d3cdf90171b70d7e4ba12b3ffd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:10 +0000

    Add package iotc/1.1.3/iotc-1.1.3.tar.gz

---
+++ packages/iotc/iotc-1.1.3.tar.gz/iotc-1.1.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import sys
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = "1.1.3"
+
+setuptools.setup(
+    name='iotc',
+    version=version,
+    author="Luca Druda",
+    author_email="ludruda@microsoft.com",
+    description="Azure IoT Central device client for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iot-for-all/iotc-python-client",
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    license="MIT",
+    platform="OS Independent",
+    keywords="iot,azure,iotcentral",
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.9'
+    ],
+    include_package_data=True,
+    install_requires=["azure-iot-device"]
+)
---
+++ packages/iotc/iotc-1.1.3.tar.gz/iotc-1.1.3/src/iotc/models.py
@@ -0,0 +1,125 @@
+import abc
+
+
+class GracefulExit(SystemExit):
+    code = 1
+
+
+class CredentialsCache(object):
+    def __init__(self, hub_name, device_id, device_key=None, certificate=None):
+        self._hub_name = hub_name
+        self._device_id = device_id
+        self._device_key = device_key
+        self._certificate = certificate
+
+    @property
+    def hub_name(self):
+        return self._hub_name
+
+    @property
+    def device_id(self):
+        return self._device_id
+
+    @property
+    def device_key(self):
+        return self._device_key
+
+    @property
+    def certificate(self):
+        return self._certificate
+
+    @property
+    def connection_string(self):
+        if self._hub_name is None or self._device_id is None:
+            return None
+        elif self._device_key is not None:
+            return "HostName={};DeviceId={};SharedAccessKey={}".format(
+                self._hub_name, self._device_id, self._device_key
+            )
+        elif self._certificate is not None:
+            return "HostName={};DeviceId={};x509=true".format(
+                self._hub_name, self._device_id
+            )
+
+    def todict(self):
+        return {
+            "device_id": self.device_id,
+            "hub_name": self.hub_name,
+            "connection_string": self.connection_string,
+            "device_key": self.device_key,
+        }
+
+    @classmethod
+    def from_dict(classref, data: dict):
+        return CredentialsCache(data["hub_name"], data["device_id"], data["device_key"])
+
+
+class Storage(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def persist(self, credentials):
+        pass
+
+    @abc.abstractmethod
+    def retrieve(self):
+        pass
+
+
+class Command(object):
+    def __init__(self, command_name, command_value, component_name=None):
+        self._command_name = command_name
+        self._command_value = command_value
+        if component_name is not None:
+            self._component_name = component_name
+        else:
+            self._component_name = None
+        self.reply = None
+
+    @property
+    def name(self):
+        return self._command_name
+
+    @property
+    def value(self):
+        return self._command_value
+
+    @property
+    def component_name(self):
+        return self._component_name
+
+    def __eq__(self, o: object) -> bool:
+        return (
+            self.name == o.name
+            and self.value == o.value
+            and self.component_name == o.component_name
+        )
+
+
+class Property(object):
+    def __init__(self, property_name, property_value, component_name=None):
+        self._property_name = property_name
+        self._property_value = property_value
+        if component_name is not None:
+            self._component_name = component_name
+        else:
+            self._component_name = None
+
+    @property
+    def name(self):
+        return self._property_name
+
+    @property
+    def value(self):
+        return self._property_value
+
+    @property
+    def component_name(self):
+        return self._component_name
+
+    def __eq__(self, o: object) -> bool:
+        return (
+            self.name == o.name
+            and self.value == o.value
+            and self.component_name == o.component_name
+        )
