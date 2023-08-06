85ad53b9d6db462994bb45c38c078da72315e9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:26 +0000

    Add package sunrpc/1.1.0/sunrpc-1.1.0.tar.gz

---
+++ packages/sunrpc/sunrpc-1.1.0.tar.gz/sunrpc-1.1.0/sunrpc/__init__.py
@@ -0,0 +1,12 @@
+from sunrpc.sunrpc import *
+
+import sunrpc.client
+import sunrpc.server
+import sunrpc.proxy
+import sunrpc.portmapper
+import sunrpc.types
+import sunrpc.utils
+import sunrpc.services
+
+name = 'sunrpc'
+sunrpc.services.update_service_names()
