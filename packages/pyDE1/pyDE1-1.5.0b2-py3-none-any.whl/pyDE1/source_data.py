2778b60f2e8499424d1afb9a27b0d2f4eb0142b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:14 +0000

    Add package pyDE1/1.5.0b2/pyDE1-1.5.0b2-py3-none-any.whl

---
+++ packages/pyDE1/pyDE1-1.5.0b2-py3-none-any.whl/pyDE1/source_data.py
@@ -0,0 +1,11 @@
+# DO NOT EDIT -- This file automatically generated -- DO NOT EDIT
+git_data = {
+    "hash": "3414226",
+    "subject": "scanner: Fix error on null/None in scan_from_api",
+    "ref_names": "HEAD -> devel-2022-10-scale",
+    "author_date": "Sat Oct 22 08:27:23 2022 -0700",
+    "commit_date": "Sat Oct 22 08:27:23 2022 -0700",
+}
+source_data = {
+    "git": git_data,
+}
