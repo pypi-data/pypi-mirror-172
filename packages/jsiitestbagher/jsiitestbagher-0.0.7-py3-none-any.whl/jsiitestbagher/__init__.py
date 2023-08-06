8e43aee80af282c28b97e8e994cbb6e12f55a7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:45 +0000

    Add package jsiitestbagher/0.0.7/jsiitestbagher-0.0.7-py3-none-any.whl

---
+++ packages/jsiitestbagher/jsiitestbagher-0.0.7-py3-none-any.whl/jsiitestbagher/__init__.py
@@ -0,0 +1,32 @@
+'''
+# replace this
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+from ._jsii import *
+
+
+class Hello(metaclass=jsii.JSIIMeta, jsii_type="jsiitestbagher.Hello"):
+    def __init__(self) -> None:
+        jsii.create(self.__class__, self, [])
+
+    @jsii.member(jsii_name="sayHello")
+    def say_hello(self) -> builtins.str:
+        return typing.cast(builtins.str, jsii.invoke(self, "sayHello", []))
+
+
+__all__ = [
+    "Hello",
+]
+
+publication.publish()
