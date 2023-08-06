817adbf8cfdafc027b7bb98e612337361945ebb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:37 +0000

    Add package zpretty/2.4.0/zpretty-2.4.0.tar.gz

---
+++ packages/zpretty/zpretty-2.4.0.tar.gz/zpretty-2.4.0/zpretty/tests/test_xml.py
@@ -0,0 +1,25 @@
+from pkg_resources import resource_filename
+from unittest import TestCase
+from zpretty.xml import XMLPrettifier
+
+
+class TestZpretty(TestCase):
+    """Test zpretty"""
+
+    maxDiff = None
+
+    def prettify(self, filename):
+        """Run prettify on filename and check that the output is equal to
+        the file content itself
+        """
+        resolved_filename = resource_filename("zpretty.tests", "original/%s" % filename)
+        prettifier = XMLPrettifier(resolved_filename)
+        observed = prettifier()
+        expected = open(resolved_filename).read()
+        self.assertListEqual(observed.splitlines(), expected.splitlines())
+
+    def test_zcml(self):
+        self.prettify("sample_xml.xml")
+
+    def test_sample_dtml(self):
+        self.prettify("sample_dtml.dtml")
