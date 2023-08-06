5f54088525a45891b003c2d8d9c5b38c90cfc830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:40 +0000

    Add package Mlconst/0.0.1/Mlconst-0.0.1.tar.gz

---
+++ packages/Mlconst/Mlconst-0.0.1.tar.gz/Mlconst-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+setuptools.setup(
+    name="Mlconst",
+    version="0.0.1",
+    author="Prajjawal Mishra",
+    author_email="prajjawalmishra.t101@gmail.com",
+    description="A simple library of maths algorithms",
+    long_description="""A library that will help you make your ml models at ease.
+Start your journey in machine learning making datasets and models with Mlconst.
+Mlconst's fast library modulation will make dynamic data handling fast and efficient.""",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mishra-developer/mlconst",
+    packages=setuptools.find_packages(),
+    classifiers=[   
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
