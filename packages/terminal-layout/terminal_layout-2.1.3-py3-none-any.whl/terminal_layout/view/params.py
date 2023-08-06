3a5aa5df392145f1ab7da586b0894b7e30a042a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:14 +0000

    Add package terminal-layout/2.1.3/terminal_layout-2.1.3-py3-none-any.whl

---
+++ packages/terminal-layout/terminal_layout-2.1.3-py3-none-any.whl/terminal_layout/view/params.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+
+class Width(object):
+    wrap = 'wrap'
+    fill = 'fill'
+
+
+class Visibility(object):
+    visible = 'visible'
+    invisible = 'invisible'
+    gone = 'gone'
+
+
+class Gravity(object):
+    left = 'left'
+    center = 'center'
+    right = 'right'
+
+
+class Overflow(object):
+    hidden_left = 'hidden_left'
+    hidden_right = 'hidden_right'
+
+
+################ linearlayout ###############3
+
+class Orientation(object):
+    # 水平
+    horizon = 'horizon'
+    # 垂直
+    vertical = 'vertical'
