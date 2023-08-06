568f570770cfaf492c1a8d05e146f257b180de28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:51 +0000

    Add package sph/0.2.7/sph-0.2.7-py3-none-any.whl

---
+++ packages/sph/sph-0.2.7-py3-none-any.whl/sph/sph.py
@@ -0,0 +1,22 @@
+import os
+import re
+import subprocess
+import shutil
+
+from configparser import ConfigParser
+from pathlib import Path
+
+import click
+
+from sph.check import check
+from sph.editable import create_editable_from_workspace
+from sph.workflow import workflow_group
+from sph.cleanup import cleanup
+
+@click.group()
+def be_helpful():
+    pass
+
+be_helpful.add_command(cleanup)
+be_helpful.add_command(workflow_group)
+be_helpful.add_command(check)
