f37e2dafe898f41b15c9223f16c80327acbf5d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:46:43 +0000

    Add package django-mailersend-backend/0.1.0/django_mailersend_backend-0.1.0.tar.gz

---
+++ packages/django-mailersend-backend/django_mailersend_backend-0.1.0.tar.gz/django_mailersend_backend-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['django_mailersend_backend']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Django>=1.4', 'mailersend>=0.5.0,<0.6.0']
+
+setup_kwargs = {
+    'name': 'django-mailersend-backend',
+    'version': '0.1.0',
+    'description': 'Email backend for Django which sends email via the Mailersend API',
+    'long_description': 'None',
+    'author': 'Adem Gaygusuz',
+    'author_email': 'adem@ardweb.co.uk',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
