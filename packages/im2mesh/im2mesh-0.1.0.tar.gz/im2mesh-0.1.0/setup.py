71242c2adf223abfea33b9b9f95514bf73c9169c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:56 +0000

    Add package im2mesh/0.1.0/im2mesh-0.1.0.tar.gz

---
+++ packages/im2mesh/im2mesh-0.1.0.tar.gz/im2mesh-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'im2mesh',
+    packages = find_packages(include=['im2mesh']),
+    version = '0.1.0',
+    description = 'Python library to create Finite Element meshes from segmented image stacks',
+    author = 'Diego Sainz-DeMena & Carlos Borau-Zamora (UNIZAR)',
+    author_email = "cborau@unizar.es",
+    license = 'MIT',
+    url = 'https://github.com/cborau/im2mesh',
+    install_requires = ['easygui==0.98.3','pymeshlab==2022.2.post2','gmsh==4.10.5',
+     'open3d==0.15.1', 'nibabel==4.0.2', 'numpy==1.23.2', 'opencv-python==4.6.0.*',
+     'pydicom==2.3.0', 'pydicom-seg==0.4.0', 'scikit-image==0.19.3', 'scipy==1.9.1'],
+     classifiers=[
+     'Programming Language :: Python :: 3.9']
+)
