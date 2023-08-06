15e1752ea16caef079adaf121f2224080fa21bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:33 +0000

    Add package annotate/1.0.19/annotate-1.0.19.zip

---
+++ packages/annotate/annotate-1.0.19.zip/annotate-1.0.19/src/annotate/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2012-2022 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/licenses/Zlib
+
+from .__about__ import * ; del __about__  # noqa
+
+from ._annotate import * ; del _annotate  # noqa
