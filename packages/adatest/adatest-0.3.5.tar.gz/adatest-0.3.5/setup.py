d80aa98d9707b1fce0f7c36eecd7c6358d815372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:44 +0000

    Add package adatest/0.3.5/adatest-0.3.5.tar.gz

---
+++ packages/adatest/adatest-0.3.5.tar.gz/adatest-0.3.5/setup.py
@@ -0,0 +1,57 @@
+import os
+import re
+import codecs
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="adatest",
+    version=find_version("adatest", "__init__.py"),
+    url="https://github.com/microsoft/adatest.git",
+    author="Scott Lundberg and Marco Tulio Ribeiro",
+    author_email="scott.lundberg@microsoft.com",
+    description="Adaptively test and debug any natural language machine learning model.",
+    packages=find_packages(exclude=["user_studies", "notebooks", "client"]),
+    package_data={"adatest": ["resources/*"]},
+    install_requires=[
+        "aiohttp",
+        "aiohttp_security",
+        "aiohttp_session",
+        "appdirs",
+        "cryptography",
+        "diskcache",
+        "nest_asyncio",
+        "numpy",
+        "pandas",
+        "profanity",
+        "scikit-learn",
+        "shap",
+    ],
+    extras_require={
+        "dev": [
+            "black",
+            "flake8",
+            "openai",
+            "datasets",
+            "transformers",
+            "pytest",
+            "pytest-mock",
+            "torch",
+        ]
+    },
+)
