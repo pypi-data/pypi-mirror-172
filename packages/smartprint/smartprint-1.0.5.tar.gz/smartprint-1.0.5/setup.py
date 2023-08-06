abf23cbd7d4cb4e19e11de61d20fd7d289992713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:50 +0000

    Add package smartprint/1.0.5/smartprint-1.0.5.tar.gz

---
+++ packages/smartprint/smartprint-1.0.5.tar.gz/smartprint-1.0.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+import os
+
+print("Path: ", os.getcwd())
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smartprint",
+    version="1.0.5",
+    author="abcnishant007",
+    author_email="abc.nishant007@gmail.com",
+    description="Include the variable name in print statements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abcnishant007/smartprint",
+    project_urls={
+        "Bug Tracker": "https://github.com/abcnishant007/smartprint/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=["rich==12.6.0"]
+)
