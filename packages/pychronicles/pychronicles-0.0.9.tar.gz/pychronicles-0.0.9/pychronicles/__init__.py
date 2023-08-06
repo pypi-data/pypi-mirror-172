ea1bf42e9e9d9c0d780657c3d2dc0d87372e0d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:37 +0000

    Add package pychronicles/0.0.9/pychronicles-0.0.9.tar.gz

---
+++ packages/pychronicles/pychronicles-0.0.9.tar.gz/pychronicles-0.0.9/pychronicles/__init__.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Chronicles
+
+@author: Thomas Guyet
+@date: 08/2022
+@institution: Inria
+"""
+
+name = "pychronicles"
+
+from pychronicles.timedsequence import TimedSequence
+from pychronicles.chronicle import Chronicle
+from pychronicles.fchronicle import FuzzyChronicle
+import pychronicles.pandas_tpatterns
