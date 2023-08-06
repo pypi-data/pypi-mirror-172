e9912d6af9d7d855040e7a77b9c12107e29566a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:54 +0000

    Add package python-aisweb/1.2.0/python-aisweb-1.2.0.tar.gz

---
+++ packages/python-aisweb/python-aisweb-1.2.0.tar.gz/python-aisweb-1.2.0/python_aisweb/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Carlos Damázio'
+__email__ = 'carlos.damazio@damazio.dev'
+__version__ = '1.2.0'
+
+
+from .python_aisweb import AISWEB
+
+__all__ = [
+    'AISWEB'
+]
