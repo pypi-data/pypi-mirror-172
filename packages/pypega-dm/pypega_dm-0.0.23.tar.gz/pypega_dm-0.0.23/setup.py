554801a933c56e0985b42740c1fce842e756a79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:47 +0000

    Add package pypega-dm/0.0.23/pypega_dm-0.0.23.tar.gz

---
+++ packages/pypega-dm/pypega_dm-0.0.23.tar.gz/pypega_dm-0.0.23/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypega_dm']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pypega>=0.0.47']
+
+setup_kwargs = {
+    'name': 'pypega-dm',
+    'version': '0.0.23',
+    'description': 'Python package for orchestrating Pega environments',
+    'long_description': '# pyPega Deployment Manager (Python)\n\nPython package for orchestrating Pega Deployment Manager - details coming soon',
+    'author': 'Rob Smart',
+    'author_email': 'rob.smart@pega.com',
+    'maintainer': 'Rob Smart',
+    'maintainer_email': 'rob.smart@pega.com',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+}
+
+
+setup(**setup_kwargs)
