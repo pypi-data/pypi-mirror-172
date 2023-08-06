9826d296d631adf50c31aa8e47490afaf8d68bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:04 +0000

    Add package nanocetpy/1.0.1/nanocetpy-1.0.1.tar.gz

---
+++ packages/nanocetpy/nanocetpy-1.0.1.tar.gz/nanocetpy-1.0.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('NanoCETPy/__init__.py', 'r') as f:
+    version_line = f.readline()
+
+version = version_line.split('=')[1].strip().replace("'", "")
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='nanocetpy',
+    version=version,
+    description='NanoCET Control and Analysis Software',
+    packages=find_packages(''),
+    url='https://github.com/Dispertech/NanoCETPy',
+    license='GPLv3',
+    author='Dispertech and Contributors',
+    author_email='info@dispertech.com',
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Development Status :: 4 - Beta',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+    ],
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "nanocet=NanoCETPy.__main__:main"
+        ]
+    }
+)
