aeda0d37019d9632cb9f5e7869854904969cc079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:23 +0000

    Add package nops-metadata/0.4.6/nops-metadata-0.4.6.tar.gz

---
+++ packages/nops-metadata/nops-metadata-0.4.6.tar.gz/nops-metadata-0.4.6/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nops_metadata', 'nops_metadata.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.17.102', 'pydantic>=1.8.0', 'pyrsistent>=0.17.3']
+
+setup_kwargs = {
+    'name': 'nops-metadata',
+    'version': '0.4.6',
+    'description': 'Metadata producer tooling used in nOps.io',
+    'long_description': None,
+    'author': 'nOps Engineers',
+    'author_email': 'eng@nops.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+}
+
+
+setup(**setup_kwargs)
