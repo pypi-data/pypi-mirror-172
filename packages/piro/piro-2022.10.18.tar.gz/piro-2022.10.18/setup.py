eae60931fd892f883b6e4162d9f84b58e9741c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:49 +0000

    Add package piro/2022.10.18/piro-2022.10.18.tar.gz

---
+++ packages/piro/piro-2022.10.18.tar.gz/piro-2022.10.18/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = "piro is software designed to assist in planning of synthesis pathways for inorganics"
+
+LONG_DESCRIPTION = """
+The piro software package enables prediction and planning of synthesis pathways
+and determination of pareto-optimal frontiers for synthesis of
+specific inorganic materials.
+"""
+
+setup(
+    name="piro",
+    version="2022.10.18",
+    packages=find_packages(),
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    install_requires=["matminer==0.7.8",
+                      "plotly==5.6.0",
+                      "pymongo==4.1.1",
+                      ],
+    classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+    ],
+    extras_require={
+        "tests": ["pytest",
+                  "pytest-cov",
+                  "coveralls"
+                  ]
+    },
+    package_data={
+        "piro": ["*.pickle", "*.json"]
+    },
+    include_package_data=True,
+    author="AMDD - Toyota Research Institute",
+    author_email="murat.aykol@tri.global",
+    maintainer="Joseph Montoya",
+    maintainer_email="joseph.montoya@tri.global",
+    # license="Apache",
+    keywords=[
+        "materials", "battery", "chemistry", "science",
+        "density functional theory", "energy", "AI", "artificial intelligence",
+        "sequential learning", "active learning"
+    ],
+    )
