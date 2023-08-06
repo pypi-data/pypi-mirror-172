fae0b82d3ca1ff2673a9dd62313a0d1b41539b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:36 +0000

    Add package BioResource/0.1.1/BioResource-0.1.1-py3-none-any.whl

---
+++ packages/BioResource/BioResource-0.1.1-py3-none-any.whl/bioresource/versions.py
@@ -0,0 +1,18 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.1.1",
+        "features": [
+            "1. add ncbi_genome_chooser;",
+        ],
+    },
+    {
+        "number": "0.1.0",
+        "features": [
+            "1. ncbi genome source;",
+        ],
+    },
+]
