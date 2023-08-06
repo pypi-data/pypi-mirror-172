3f0a16e97e810b207d588f9afabcb578d8c794d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:04 +0000

    Add package azs/1.0.3/azs-1.0.3.tar.gz

---
+++ packages/azs/azs-1.0.3.tar.gz/azs-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="azs",
+    version="1.0.3",
+    author="Harkame",
+    description="Scraper for Amazon",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Harkame/AmaZonScraper",
+    install_requires=[
+        "BeautifulSoup4",
+        "lxml",
+        "selenium"
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+)
