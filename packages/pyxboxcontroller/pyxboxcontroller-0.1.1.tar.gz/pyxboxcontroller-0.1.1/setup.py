d1b62dc5b5f44e08535d82558876085d64c0d2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:40 +0000

    Add package pyxboxcontroller/0.1.1/pyxboxcontroller-0.1.1.tar.gz

---
+++ packages/pyxboxcontroller/pyxboxcontroller-0.1.1.tar.gz/pyxboxcontroller-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = "0.1.1"
+DESCRIPTION = 'This module allows for accessing the current state of a connected Xbox controller on Windows via the XInput library(https://learn.microsoft.com/en-gb/windows/win32/xinput/getting-started-with-xinput?redirectedfrom=MSDN#getting-controller-state).'
+
+setup(name='pyxboxcontroller',
+    version = VERSION,
+    description = DESCRIPTION,
+    license = "MIT",
+    author = 'Dan Forbes',
+    author_email = 'danielforbes.123412@gmai.com',
+    url = 'https://github.com/SimpleHydrogen/pyxboxcontroller',
+    packages = find_packages(),
+    keywords = ["xbox controller", "XInput", "xbox", "controller"],
+    long_description = open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: Microsoft :: Windows :: Windows 11",
+    ]
+)
