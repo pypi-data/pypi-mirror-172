f784f5dea3d38ce58c4fb0902720444eeba4cecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:27 +0000

    Add package hackcheck/1.1.0/hackcheck-1.1.0.tar.gz

---
+++ packages/hackcheck/hackcheck-1.1.0.tar.gz/hackcheck-1.1.0/hackcheck/types.py
@@ -0,0 +1,16 @@
+from typing import NamedTuple, Optional
+
+
+class Source(NamedTuple):
+    name: Optional[str]
+    date: Optional[str]
+    description: Optional[str]
+
+
+class Result(NamedTuple):
+    email: Optional[str]
+    username: Optional[str]
+    password: Optional[str]
+    ip: Optional[str]
+    phone: Optional[str]
+    source: Optional[Source]
