c03fac6d0db6dd6443091bc104cd854fef56e8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:18 +0000

    Add package didimo-cli/2.7.0/didimo-cli-2.7.0.tar.gz

---
+++ packages/didimo-cli/didimo-cli-2.7.0.tar.gz/didimo-cli-2.7.0/cli/network.py
@@ -0,0 +1,211 @@
+import click
+import requests
+import hmac
+import time
+import sys
+import platform
+from hashlib import sha256
+
+from ._version import __version__
+
+import pickle
+import os
+import shutil
+from datetime import datetime
+from ._version import __version__
+
+class DidimoAuth(requests.auth.AuthBase):
+    def __init__(self, config, path):
+        self.config = config
+        self.path = path
+
+    def __call__(self, r):
+        access_key = self.config.access_key
+        r.headers["didimo-api-key"] = access_key
+        r.headers["User-Agent"] = "didimo-cli/%s (%s, %s)" % (__version__,
+                                                              platform.python_version(),
+                                                              platform.system())
+        r.headers["Didimo-Platform"] = "CLI"
+        r.headers["Didimo-Platform-Version"] = __version__
+        return r
+
+
+def http_get(url, **kwargs):
+    try:
+        r = requests.get(url, **kwargs)
+        if r.status_code == 200:
+            return r
+        else:
+            click.secho('Error %d' % r.status_code, err=True, fg='red')
+            click.echo(r.text)
+            return r
+    except:
+        click.echo("A Network Error Has Occured")
+        sys.exit(1)
+
+def http_get_no_error(url, **kwargs):
+    try:
+        r = requests.get(url, **kwargs)
+        return r
+    except:
+        click.echo("A Network Error Has Occured")
+        sys.exit(1)
+
+def http_delete(url, **kwargs):
+    try:
+        r = requests.delete(url, **kwargs)
+        if r.status_code == 204:
+            return r
+        else:
+            click.secho('Error %d' % r.status_code, err=True, fg='red')
+            click.echo(r.text)
+            #sys.exit(1)
+            return r
+    except:
+        click.echo("A Network Error Has Occured")
+        sys.exit(1)
+
+def http_put(url, **kwargs):
+    r = requests.put(url, **kwargs)
+    if r.status_code == 200:
+        return r
+    else:
+        click.secho('Error %d' % r.status_code, err=True, fg='red')
+        click.echo(r.text)
+        return r
+
+def http_post(url, **kwargs):
+    r = requests.post(url, **kwargs)
+    if r.status_code == 200:
+        return r
+    else:
+        click.secho('Error %d' % r.status_code, err=True, fg='red')
+        click.echo(r.text)
+        sys.exit(1)
+
+def http_post_no_break(url, **kwargs): 
+    r = requests.post(url, **kwargs)
+    if r.status_code == 200 or r.status_code == 201:
+        return r
+    else:
+        click.secho('Error %d' % r.status_code, err=True, fg='red')
+        click.echo(r.text)
+        return r
+
+def http_post_withphoto(url, access_key, payload, photo, photo_depth, photos_archive = None, check_status_code = True):
+
+    if photos_archive != None:
+        files = [('photos', (photos_archive, open(photos_archive, 'rb'), 'image/jpeg'))]
+    elif photo_depth != None:
+        files = [
+            ('photo', (photo, open(photo, 'rb'), 'image/jpeg')),
+            ('depth', (photo_depth, open(photo_depth, 'rb'), 'image/png'))
+        ]
+    else:
+        files = [('photo', (photo, open(photo, 'rb'), 'image/jpeg'))]
+
+    headers = {
+        'DIDIMO-API-KEY': access_key,
+        'Didimo-Platform': "CLI",
+        'Didimo-Platform-Version':__version__,
+        'User-Agent': "didimo-cli/%s (%s, %s)" % (__version__,
+                                                              platform.python_version(),
+                                                              platform.system())
+    }
+
+    r = requests.request("POST", url, headers=headers,
+                         data=payload, files=files)
+
+    if check_status_code:
+        if r.status_code == 200 or r.status_code == 201:
+            return r
+        else:
+            click.secho('Error %d' % r.status_code, err=True, fg='red')
+            click.echo(r.text)
+            #click.echo("An error has occured. Please check your API key")
+            sys.exit(1)
+    else:
+        return r
+
+def http_request_json(url, method, access_key, payload, check_status_code = True):
+
+    headers = {
+        'DIDIMO-API-KEY': access_key,
+        'Didimo-Platform': "CLI",
+        'Didimo-Platform-Version':__version__,
+        'User-Agent': "didimo-cli/%s (%s, %s)" % (__version__,
+                                                              platform.python_version(),
+                                                              platform.system()),
+        'Content-Type': "application/json"
+    }
+
+    if method != "POST" and method != "PUT" and method != "PATCH":
+        raise Exception("unknown method")
+
+    r = requests.request(method, url, headers=headers,
+                         data=payload)
+
+    if check_status_code:
+        if r.status_code == 200 or r.status_code == 201:
+            return r
+        else:
+            click.secho('Error %d' % r.status_code, err=True, fg='red')
+            click.echo(r.text)
+            #click.echo("An error has occured. Please check your API key")
+            sys.exit(1)
+    else:
+        return r
+
+#cache calls in the same day, and only if the call returns a 200 http status
+def cache_this_call(url, access_key, **kwargs):
+    curr_date = datetime.today().strftime('%Y-%m-%d')
+
+    root_temp_dir_path = "temp/"
+    root_cache_dir_path = root_temp_dir_path+"simple_cache/"
+    try:
+        directories = [(f.name, f.path)  for f in os.scandir(root_cache_dir_path) if f.is_dir()]
+        for (directory_name,directory_path) in directories:
+            if not directory_name == curr_date:
+                #print("deleting "+directory_name + " at "+directory_path)
+                shutil.rmtree(directory_path)
+    except OSError as e:
+        #print("Error: %s : %s" % (root_cache_dir_path, e.strerror))
+        pass
+
+    try: 
+        os.mkdir(root_temp_dir_path)
+    except OSError as error: 
+        pass
+
+    try: 
+        os.mkdir(root_cache_dir_path)
+    except OSError as error: 
+        pass
+
+    try: 
+        os.mkdir(root_cache_dir_path+curr_date)
+    except OSError as error: 
+        pass
+
+    filename = root_cache_dir_path+curr_date+"/"+str(sha256((url+"/"+access_key).encode('utf-8')).hexdigest()) 
+
+    if not os.path.exists(filename):
+        #print("no cache")
+        data = http_get(url, **kwargs)
+        if data.status_code == 200:
+            with open(filename, 'wb') as f:
+                pickle.dump(data, f)
+    else:
+        #print("fetching from cache")
+        with open(filename, 'rb') as f:
+            data = pickle.load(f)
+    return data
+
+
+def clear_network_cache():
+    root_cache_dir_path = "temp/simple_cache/"
+    try:
+        shutil.rmtree(root_cache_dir_path)
+    except OSError as e:
+        print("Error: %s : %s" % (root_cache_dir_path, e.strerror))
+
---
+++ packages/didimo-cli/didimo-cli-2.7.0.tar.gz/didimo-cli-2.7.0/cli/utils.py
@@ -0,0 +1,160 @@
+import click
+from datetime import datetime
+
+
+def print_key_value(key, value, fg="white"):
+    click.secho("%s: " % key, bold=True, nl=False, fg=fg, err=True)
+    click.secho(str(value), err=True)
+
+
+def print_status_header():
+    click.secho("{:<18} │ {:<10} │ {:<7} │ {:^4} │ {:^4}  "
+                .format(
+                    'didimo ID', 'Type', 'Percent', 'Status', 'Created At'),
+                bold=True, err=True)
+    click.secho("─" * 112)
+
+
+def print_status_row(didimo):
+
+    # Remove when /status endpoint is consistent with /list
+    if didimo.get('status') == "error":
+        click.secho("{:<18} │ {:<10} │ {:<7} │ {:<6} │ {:^4}  "
+                    .format(didimo['key'], "-", "-",
+                             didimo['status'].title(), "-"), fg="red")
+        return
+
+    color = 'white'
+    if didimo['status'] == "error":
+        color = 'red'
+    elif didimo['status'] == "done":
+        color = 'green'
+    elif didimo['status'] == "processing":
+        color = 'yellow'
+
+    didimo_type = didimo['input_type']
+    if didimo['input_type'] == "lofimesh_texture":
+        didimo_type = 'sony'
+    elif didimo['input_type'] == "hifimesh_texture_photo":
+        didimo_type = 'scan'
+    elif didimo['input_type'] == "blendshapes":
+        didimo_type = 'blend'
+    elif didimo['input_type'] == "vertexdeform":
+        didimo_type = 'vertx'
+
+    #created_at = datetime.strptime(didimo['created_at'], "%Y-%m-%d %H:%M:%S")
+    click.secho("{:<18} │ {:<10} │ {:<7} │ {:^7}│ {:^7}"
+                .format(
+                    didimo['key'], didimo_type.title(),
+                    didimo['percent'],
+                    didimo['status'].title(),
+                    didimo['created_at']),
+                fg=color)
+
+def create_set(ids):
+    return set(ids)
+
+#Didimo Generation Templates
+def print_didimo_generation_template_header():
+    click.secho("{:<32} │ {:<10} │ {:<6} │ {:<25} │ {:<42} │ {:^7}  "
+                .format(
+                    'DGT codename', 'Created At', 'Scope', 'Name', 'Description', 'Settings'),
+                bold=True, err=False)
+    click.secho("─" * 232)
+
+
+def print_didimo_generation_template_row(dgt):
+
+    if 'profile_id' in dgt and 'account_id' in dgt:
+        scope = "user"
+    else:
+        scope = "system"
+
+    if scope == "system":
+        color = 'yellow'
+    else:
+        color = 'white'
+
+    truncated_name = dgt['template_name'][0:20]
+    if len(dgt['template_name']) != len (truncated_name):
+        truncated_name = truncated_name + "(...)"
+
+    if 'description' in dgt and dgt['description'] != None:
+        truncated_description = dgt['description'][0:37]
+        if len(dgt['description']) != len (truncated_description):
+            truncated_description = truncated_description + "(...)"
+    else:
+        truncated_description = ""
+
+    created_at_datetime = datetime.strptime(dgt['created_at'], "%Y-%m-%dT%H:%M:%S")
+    created_at = created_at_datetime.strftime("%Y-%m-%d")
+
+    
+
+    click.secho("{:<32} │ {:<10} │ {:<6} │ {:<25} │ {:<43}│ {:^7}"
+                .format(
+                    dgt['codename'],
+                    created_at,
+                    scope,
+                    truncated_name,
+                    truncated_description,
+                    str(dgt['settings'])),
+                fg=color)
+
+
+#Bulk requests
+def print_bulk_requests_header():
+    click.secho("{:<36} │ {:<10} │ {:<10} │ {:<7}"
+                .format(
+                    'Bulk UUID', 'Created At', 'Status', '#Items'),
+                bold=True, err=False)
+    click.secho("─" * 72)
+
+
+def print_bulk_requests_row(bulk_request):
+
+    if 'status' in bulk_request and bulk_request['status']=='completed':
+        color = 'green'
+    elif 'status' in bulk_request and bulk_request['status']=='error':
+        color = 'red'
+    else:
+        color = 'white'
+
+    created_at_datetime = datetime.strptime(bulk_request['created_at'], "%Y-%m-%dT%H:%M:%S")
+    created_at = created_at_datetime.strftime("%Y-%m-%d")
+
+    items_count = len(bulk_request['items'])
+
+    click.secho("{:<36} │ {:<10} │ {:<10} │ {:<7}"
+                .format(
+                    bulk_request['uuid'],
+                    created_at,
+                    bulk_request['status'],
+                    str(items_count)),
+                fg=color)
+
+def print_bulk_request_item_header():
+    click.secho("{:<36} │ {:<10} │ {:<18} │ {:<7}"
+                .format(
+                    'Item UUID', 'Status', 'Key', 'File'),
+                bold=True, err=False)
+    click.secho("─" * 92)
+
+def print_bulk_request_item_row(bulk_request_item):
+
+    if 'status' in bulk_request_item and bulk_request_item['status']=='completed':
+        color = 'green'
+    elif 'status' in bulk_request_item and bulk_request_item['status']=='error':
+        color = 'red'
+    else:
+        color = 'white'
+
+    click.secho("{:<36} │ {:<10} │ {:<18} │ {:<7}"
+                .format(
+                    bulk_request_item['uuid'],
+                    bulk_request_item['status'],
+                    bulk_request_item['request_key'],
+                    bulk_request_item['file']),
+                fg=color)
+
+
