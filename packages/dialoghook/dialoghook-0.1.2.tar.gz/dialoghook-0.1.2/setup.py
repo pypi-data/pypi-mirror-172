4e6913685a8fad1fe4c417cb6c594b9a6a0f238c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:37 +0000

    Add package dialoghook/0.1.2/dialoghook-0.1.2.tar.gz

---
+++ packages/dialoghook/dialoghook-0.1.2.tar.gz/dialoghook-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dialoghook']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.10.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'dialoghook',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': 'Dialogflow Request and Response Constructs and validators for Webhooks in Dialogflow ES V2 API\n\nDev Instructions\nRun poetry install to install the env.\nRun poetry run pre-commit install to initialize the git hooks.\nRun poetry run pre-commit run --all-files if there are file that were committed before adding the git hooks.\nActivate the shell with: poetry shell\nLint with: poetry run pylint poetrypackagetemplate/ tests/\nTest with: poetry run pytest --cov=poetrypackagetemplate\n',
+    'author': 'aHardReset',
+    'author_email': 'ing.aarongaribay@outlook.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/aHardReset/dialoghook',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
