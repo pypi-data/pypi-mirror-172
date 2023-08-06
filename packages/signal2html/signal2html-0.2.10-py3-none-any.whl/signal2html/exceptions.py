842a69208b839a89fd0cd4f0ad4c4756f9872074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:15 +0000

    Add package signal2html/0.2.10/signal2html-0.2.10-py3-none-any.whl

---
+++ packages/signal2html/signal2html-0.2.10-py3-none-any.whl/signal2html/exceptions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Various exceptions
+
+License: See LICENSE file.
+
+"""
+
+
+class DatabaseNotFound(Exception):
+    pass
+
+
+class DatabaseVersionNotFound(Exception):
+    pass
