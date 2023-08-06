c8a05c3c44255cf1705a758cb9825cb9112f4429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:13 +0000

    Add package mysql-mimic/1.1.1/mysql-mimic-1.1.1.tar.gz

---
+++ packages/mysql-mimic/mysql-mimic-1.1.1.tar.gz/mysql-mimic-1.1.1/README.md
@@ -0,0 +1,59 @@
+# MySQL-Mimic
+
+[![Tests](https://github.com/kelsin/mysql-mimic/actions/workflows/tests.yml/badge.svg)](https://github.com/kelsin/mysql-mimic/actions/workflows/tests.yml)
+
+Pure-python implementation of the MySQL server [wire protocol](https://dev.mysql.com/doc/internals/en/client-server-protocol.html).
+
+This can be used to create applications that act as a MySQL server.
+
+MySQL-Mimic doesn't parse SQL - it only handles the wire protocol. For parsing, check out [sqlglot](https://github.com/tobymao/sqlglot). 
+
+## Installation
+
+```shell
+pip install mysql-mimic
+```
+
+# Usage
+
+This library is meant to be used as the basis for a proxy SQL service. A minimal
+use case might look like this:
+
+```python
+import asyncio
+
+from mysql_mimic import MysqlServer, Session
+
+
+class MySession(Session):
+    async def init(self, connection):
+        print(f"new session: {connection}")
+  
+    async def query(self, sql, attrs):
+        print(f"received query: {sql}")
+        return [("a", 1), ("b", 2)], ["col1", "col2"]
+  
+    async def close(self):
+        print("session closed")
+
+
+if __name__ == "__main__":
+    server = MysqlServer(session_factory=MySession)
+    asyncio.run(server.serve_forever())
+```
+
+See [examples](./examples) for more examples.
+
+## Todo
+
+- Add support for (at least) the `mysql_native_password` authentication method
+  with another callback.
+- *Eventually* Compression support
+- *Eventually* SSL support
+
+## Development
+
+You can install dependencies with `make deps`. You can format your code with
+`make format`. You can lint with `make lint`. You can run tests with `make
+test`. This will build a coverage report in `./htmlcov/index.html`. You can
+build a pip package with `make build`.
---
+++ packages/mysql-mimic/mysql-mimic-1.1.1.tar.gz/mysql-mimic-1.1.1/mysql_mimic/stream.py
@@ -0,0 +1,63 @@
+import asyncio
+import struct
+
+from mysql_mimic.errors import MysqlError, ErrorCode
+from mysql_mimic.types import uint_3, uint_1
+from mysql_mimic.utils import seq
+
+
+class ConnectionClosed(Exception):
+    pass
+
+
+class MysqlStream:
+    def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        self.reader = reader
+        self.writer = writer
+        self.seq = seq(256)
+
+    async def read(self) -> bytes:
+        data = b""
+        while True:
+            header = await self.reader.read(4)
+
+            if not header:
+                raise ConnectionClosed()
+
+            i = struct.unpack("<I", header)[0]
+            payload_length = i & 0x00FFFFFF
+            sequence_id = (i & 0xFF000000) >> 24
+
+            expected = next(self.seq)
+            if sequence_id != expected:
+                raise MysqlError(
+                    f"Expected seq({expected}) got seq({sequence_id})",
+                    ErrorCode.MALFORMED_PACKET,
+                )
+
+            if payload_length == 0:
+                return data
+
+            data += await self.reader.read(payload_length)
+
+            if payload_length < 0xFFFFFF:
+                return data
+
+    async def write(self, data: bytes) -> None:
+        while True:
+            # Grab first 0xFFFFFF bytes to send
+            payload = data[:0xFFFFFF]
+            data = data[0xFFFFFF:]
+
+            payload_length = uint_3(len(payload))
+            sequence_id = uint_1(next(self.seq))
+
+            self.writer.write(payload_length + sequence_id + payload)
+            await self.writer.drain()
+
+            # We are done unless len(send) == 0xFFFFFF
+            if len(payload) != 0xFFFFFF:
+                return
+
+    def reset_seq(self) -> None:
+        self.seq.reset()
