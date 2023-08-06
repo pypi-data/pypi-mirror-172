96a22bf28976d8e3f8867f3ae330c38edf6ee4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:27 +0000

    Add package nonebot-plugin-novelai/0.4.6/nonebot-plugin-novelai-0.4.6.tar.gz

---
+++ packages/nonebot-plugin-novelai/nonebot-plugin-novelai-0.4.6.tar.gz/nonebot-plugin-novelai-0.4.6/nonebot-plugin-novelai/requests.py
@@ -0,0 +1,69 @@
+import urllib.parse
+from .config import config
+lowQuality = "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry,"
+htags = ["nsfw", "nude","裸"]
+basetag = "{masterpiece}, extremely detailed 8k wallpaper, best quality, an extremely delicate and beautiful,"
+token = config.novelai_token
+header = {
+    "authorization": "Bearer " + token,
+    ":authority": urllib.parse.urlparse(config.novelai_api_domain).hostname,
+    ":path": "/ai/generate-image",
+    "content-type": "application/json",
+    "referer": config.novelai_site_domain,
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+}
+
+
+def txt2img_body(seed, input, width, height):
+    if config.novelai_h:
+        uc = lowQuality
+    else:
+        htagstr = ""
+        for i in htags:
+            htagstr = htagstr+i+","
+        uc = lowQuality+htagstr
+    return {
+        "input": basetag + config.novelai_tag + "," + input,
+        "model": "safe-diffusion",
+        "parameters": {
+            "width": width,
+            "height": height,
+            "scale": 11,
+            "sampler": "k_euler_ancestral",
+            "qualityToggle": True,
+            "steps": 28,
+            "seed": seed,
+            "n_samples": 1,
+            "ucPreset": 0,
+            "uc": uc,
+        },
+    }
+
+
+def img2img_body(seed, input, width, height, image):
+    if config.novelai_h:
+        uc = lowQuality
+    else:
+        htagstr = ""
+        for i in htags:
+            htagstr = htagstr+i+","
+        uc = lowQuality+htagstr
+    return {
+        "input": basetag + config.novelai_tag + "," + input,
+        "model": "safe-diffusion",
+        "parameters": {
+            "width": width,
+            "image": image,
+            "noise": 0.2,
+            "strength": 0.7,
+            "height": height,
+            "qualityToggle": True,
+            "scale": 11,
+            "sampler": "k_euler_ancestral",
+            "steps": 50,
+            "seed": seed,
+            "n_samples": 1,
+            "ucPreset": 0,
+            "uc": uc,
+        },
+    }
