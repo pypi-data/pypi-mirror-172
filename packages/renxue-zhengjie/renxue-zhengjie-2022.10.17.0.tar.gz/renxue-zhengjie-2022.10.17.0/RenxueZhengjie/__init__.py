23561e6dbe2259b34ff5ce9b121b211634b0c090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:05 +0000

    Add package renxue-zhengjie/2022.10.17.0/renxue-zhengjie-2022.10.17.0.tar.gz

---
+++ packages/renxue-zhengjie/renxue-zhengjie-2022.10.17.0.tar.gz/renxue-zhengjie-2022.10.17.0/RenxueZhengjie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""renxue-zhengjie
+https://github.com/apachecn/renxue-zhengjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.17.0"
