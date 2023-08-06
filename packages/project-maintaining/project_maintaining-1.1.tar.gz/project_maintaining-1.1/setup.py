866fc4d409877a14a4161ebd2e26baba2bdd191e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:59 +0000

    Add package project-maintaining/1.1/project_maintaining-1.1.tar.gz

---
+++ packages/project-maintaining/project_maintaining-1.1.tar.gz/project_maintaining-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='project_maintaining',
+    version='1.1',
+    packages=['project_maintaining'],
+    url='https://github.com/nawinds/remote_project_deletion',
+    license='MIT Licence',
+    author='nawinds',
+    author_email='me@nawinds.top',
+    description='Remote deletion of your project once run',
+    install_requires=["requests"]
+)
