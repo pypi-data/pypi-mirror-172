3300194785307c2eefbbcec63884339f2c90dd73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:19 +0000

    Add package receipt-scanner/0.3.0/receipt_scanner-0.3.0-py3-none-any.whl

---
+++ packages/receipt-scanner/receipt_scanner-0.3.0-py3-none-any.whl/receipt_scanner/image/filters/dilate.py
@@ -0,0 +1,21 @@
+from logging import getLogger
+
+import cv2
+import numpy as np
+
+from receipt_scanner.image.debug import debug_show
+from receipt_scanner.image.filters.base_filter import Filter
+
+logger = getLogger(__name__)
+
+
+class DilateFilter(Filter):
+    def __init__(self, debug: bool = False) -> None:
+        self.debug = debug
+
+    def eval(self, image: np.ndarray) -> np.ndarray:
+        logger.debug("Applying 'DilateFilter'...")
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        dilated_image = cv2.dilate(image, rect_kernel)
+        debug_show(dilated_image, debug=self.debug)
+        return dilated_image
