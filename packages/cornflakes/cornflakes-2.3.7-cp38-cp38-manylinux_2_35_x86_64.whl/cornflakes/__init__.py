07e3c4598bcd0ee05243774575d7e9284e5663c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:11 +0000

    Add package cornflakes/2.3.7/cornflakes-2.3.7-cp38-cp38-manylinux_2_35_x86_64.whl

---
+++ packages/cornflakes/cornflakes-2.3.7-cp38-cp38-manylinux_2_35_x86_64.whl/cornflakes/__init__.py
@@ -0,0 +1,45 @@
+"""cornflakes (Top-level package).
+__________________________________
+
+.. currentmodule:: cornflakes
+
+.. autosummary::
+   :toctree: _generate
+
+    default_ca_path
+    datetime_ms
+    ini_load
+    eval_type
+    eval_datetime
+    eval_csv
+    extract_between
+    apply_match
+    add_slots
+    generate_default_config_py
+    ini_config
+"""  # noqa: RST303 D205
+from _cornflakes import apply_match, eval_csv, eval_datetime, eval_type, extract_between, ini_load
+from cornflakes._default_ca_path import default_ca_path
+from cornflakes._types import DatetimeMS, datetime_ms
+from cornflakes._add_dataclass_slots import add_slots
+from cornflakes.config import generate_default_config_py
+from cornflakes.decorator import ini_config
+
+__author__ = "Semjon Geist"
+__email__ = "semjon.geist@ionos.com"
+__version__ = "2.3.7"
+
+__all__ = [
+    "datetime_ms",
+    "DatetimeMS",
+    "default_ca_path",
+    "ini_load",
+    "eval_type",
+    "eval_datetime",
+    "eval_csv",
+    "extract_between",
+    "apply_match",
+    "add_slots",
+    "ini_config",
+    "generate_default_config_py",
+]
