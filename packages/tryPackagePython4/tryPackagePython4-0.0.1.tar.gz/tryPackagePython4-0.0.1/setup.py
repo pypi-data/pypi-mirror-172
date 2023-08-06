e3ef66587fd3ac13f704c8239bca2bdc4297514d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:03 +0000

    Add package tryPackagePython4/0.0.1/tryPackagePython4-0.0.1.tar.gz

---
+++ packages/tryPackagePython4/tryPackagePython4-0.0.1.tar.gz/tryPackagePython4-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name = 'tryPackagePython4',
+    version='0.0.1',
+    description='My own first package',
+    long_description='this would be a long description',
+    url='',
+    author='Alex Pacheco',
+    author_email='<alex.pacheco.reynado@gmail.com>',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='',
+    packages=find_packages(),
+    install_requires=['numpy', 'numba']
+)
