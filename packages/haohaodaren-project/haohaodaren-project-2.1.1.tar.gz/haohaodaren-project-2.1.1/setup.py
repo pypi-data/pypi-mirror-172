8e457182f6f8a1bd5cf3b131b2f9bdbb6489788f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:05 +0000

    Add package haohaodaren-project/2.1.1/haohaodaren-project-2.1.1.tar.gz

---
+++ packages/haohaodaren-project/haohaodaren-project-2.1.1.tar.gz/haohaodaren-project-2.1.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  name="haohaodaren-project",
+  version="2.1.1",
+  author="kaizheng&&haoyuanli",
+  author_email="a1242884508@gmail.com",
+  description="A tool for prediction of molecules associations",
+  license = "MIT Licence",
+  url = "https://github.com/fengmm521/pipProject",
+  packages=setuptools.find_packages(),
+  include_package_data = True,
+  platforms = "any",
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
