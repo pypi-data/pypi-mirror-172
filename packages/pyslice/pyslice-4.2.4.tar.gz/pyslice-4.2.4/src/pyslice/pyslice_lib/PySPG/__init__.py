c3c0a9b1e40496907f3aafcb583b4fd0a4ab7681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:18 +0000

    Add package pyslice/4.2.4/pyslice-4.2.4.tar.gz

---
+++ packages/pyslice/pyslice-4.2.4.tar.gz/pyslice-4.2.4/src/pyslice/pyslice_lib/PySPG/__init__.py
@@ -0,0 +1,18 @@
+version_number = "1.9.2"
+release_date = "Wed Sep 30 2003"
+#
+#
+# :::~ Author: Claudio Juan Tessone <tessonec@imedea.uib.es> (c) 2002-2003
+#
+# Distributed According to GNU Generic Purpose License (GPL)
+# Please visit www.gnu.org
+#
+
+
+from .Agrizer import *
+from .Executor import *
+from .MatrixPlotter import *
+from .MeanCalculation import *
+from .MultiAgrizer import *
+from .ParamParser import *
+from .TeXParser import *
