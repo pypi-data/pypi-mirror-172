8f817ea2b37e28fc1038cfd7df01c9dbff1b3286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:25 +0000

    Add package zhijiang/0.0.3/zhijiang-0.0.3-py3-none-any.whl

---
+++ packages/zhijiang/zhijiang-0.0.3-py3-none-any.whl/zhijiang/scripts/install_useful_tools.sh
@@ -0,0 +1,9 @@
+sudo apt install -y tldr tmux
+sudo apt install autojump; wget -O ~/.autojump.bash "https://raw.githubusercontent.com/wting/autojump/master/bin/autojump.bash"; echo source ~/.autojump.bash > ~/.bashrc
+
+sudo apt install -y gdb cgdb 
+
+
+sudo pip install  py-spy viztracer
+
+sudo apt install -y icdiff
