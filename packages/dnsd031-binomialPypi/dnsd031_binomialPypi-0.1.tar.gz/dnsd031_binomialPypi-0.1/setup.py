295ded0410d14f28c9f5e1580f73af86832fa6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:20 +0000

    Add package dnsd031-binomialPypi/0.1/dnsd031_binomialPypi-0.1.tar.gz

---
+++ packages/dnsd031-binomialPypi/dnsd031_binomialPypi-0.1.tar.gz/dnsd031_binomialPypi-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dnsd031_binomialPypi',
+      version='0.1',
+      description='Gaussian and Binomial distribution',
+      packages=['dnsd031_binomialPypi'],
+      author= 'Rakoto031',
+      zip_safe=False)
+
+
