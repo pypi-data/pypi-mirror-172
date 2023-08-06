c57860936649933db3b584282591cc7524d0c92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:40 +0000

    Add package SigProfilerAssignment/0.0.15/SigProfilerAssignment-0.0.15-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.15-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.15'
+version = '0.0.15'
+Update = '1. Add subgroups for DBS and ID COSMIC signatures. 2. Add export_probabilities parameter .'
+    
+    
