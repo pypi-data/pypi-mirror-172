e7909599044bb61e91105206fdebc4ca7a68504c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:53 +0000

    Add package lumen/0.5.0b2/lumen-0.5.0b2-py2.py3-none-any.whl

---
+++ packages/lumen/lumen-0.5.0b2-py2.py3-none-any.whl/lumen/tests/views/test_base.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+
+import pandas as pd
+
+from lumen.panel import DownloadButton
+from lumen.sources import FileSource
+from lumen.state import state
+from lumen.variables import Variables
+from lumen.views import View, hvPlotView
+
+
+def test_resolve_module_type():
+    assert View._get_type('lumen.views.base.View') is View
+
+
+def test_view_hvplot_basis(set_root):
+    set_root(str(Path(__file__).parent.parent))
+    source = FileSource(tables={'test': 'sources/test.csv'})
+    view = {
+        'type': 'hvplot',
+        'table': 'test',
+        'x': 'A',
+        'y': 'B',
+        'kind': 'scatter',
+    }
+
+    view = View.from_spec(view, source, [])
+
+    assert isinstance(view, hvPlotView)
+    assert view.kind == 'scatter'
+    assert view.pipeline.source is source
+    assert view.pipeline.table == 'test'
+    assert view.pipeline.filters == []
+    assert view.pipeline.transforms == []
+    assert view.controls == []
+
+    df = view.get_data()
+
+    assert isinstance(df, pd.DataFrame)
+    assert df.shape == (5, 4)
+
+    plot = view.get_plot(df)
+
+    assert plot.kdims == ['A']
+    assert plot.vdims == ['B']
+
+
+def test_view_hvplot_variable(set_root):
+    set_root(str(Path(__file__).parent.parent))
+    source = FileSource(tables={'test': 'sources/test.csv'})
+    state._variables[None] = vars = Variables.from_spec({'y': 'B'})
+    view = {
+        'type': 'hvplot',
+        'table': 'test',
+        'x': 'A',
+        'y': '$variables.y',
+        'kind': 'scatter',
+    }
+
+    view = View.from_spec(view, source, [])
+
+    assert view.y == 'B'
+
+    df = view.get_data()
+    plot = view.get_plot(df)
+
+    assert plot.vdims == ['B']
+
+    vars._vars['y'].value = 'C'
+
+    plot = view.get_plot(df)
+
+    assert plot.vdims == ['C']
+
+
+def test_view_hvplot_download(set_root):
+    set_root(str(Path(__file__).parent.parent))
+    source = FileSource(tables={'test': 'sources/test.csv'})
+    view = {
+        'type': 'hvplot',
+        'table': 'test',
+        'x': 'A',
+        'y': 'B',
+        'kind': 'scatter',
+        'download': 'csv'
+    }
+
+    view = View.from_spec(view, source)
+
+    button = view.panel[0]
+    assert isinstance(button, DownloadButton)
+
+    button._on_click()
+    assert button.data.startswith('data:text/plain;charset=UTF-8;base64')
