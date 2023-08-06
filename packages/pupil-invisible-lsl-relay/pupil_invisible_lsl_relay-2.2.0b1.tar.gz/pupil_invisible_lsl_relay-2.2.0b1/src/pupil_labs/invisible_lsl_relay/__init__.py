654c0d0e8259e77975c7d8efde3c4b751025092d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:06 +0000

    Add package pupil-invisible-lsl-relay/2.2.0b1/pupil_invisible_lsl_relay-2.2.0b1.tar.gz

---
+++ packages/pupil-invisible-lsl-relay/pupil_invisible_lsl_relay-2.2.0b1.tar.gz/pupil_invisible_lsl_relay-2.2.0b1/src/pupil_labs/invisible_lsl_relay/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level entry-point for the <project_name> package"""
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pupil-invisible-lsl-relay")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+__all__ = ["__version__"]
---
+++ packages/pupil-invisible-lsl-relay/pupil_invisible_lsl_relay-2.2.0b1.tar.gz/pupil_invisible_lsl_relay-2.2.0b1/tests/test_api.py
@@ -0,0 +1,5 @@
+import pupil_labs.invisible_lsl_relay as this_project
+
+
+def test_package_metadata() -> None:
+    assert hasattr(this_project, "__version__")
