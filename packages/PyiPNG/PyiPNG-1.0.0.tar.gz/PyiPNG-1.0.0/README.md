5549909f15f5c698d165513d2118d6b4ec6d7bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:47 +0000

    Add package PyiPNG/1.0.0/PyiPNG-1.0.0.tar.gz

---
+++ packages/PyiPNG/PyiPNG-1.0.0.tar.gz/PyiPNG-1.0.0/README.md
@@ -0,0 +1,15 @@
+# PyiPNG
+convert apple fake png(CgBI) to real png
+
+# Requirements
+None
+# Installation
+Package is uploaded on [PyPI](https://pypi.org/project/pyipng).
+You can install it with pip:
+```
+$ python3 -m pip install PyiPNG
+```
+
+# Example
+
+see example.py
---
+++ packages/PyiPNG/PyiPNG-1.0.0.tar.gz/PyiPNG-1.0.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup,find_packages
+
+version = '1.0.0'
+
+with open('./README.md',encoding='utf-8') as f:
+    readme = f.read()
+
+
+setup(
+    name="PyiPNG",
+    version=version,
+    author="venjye",
+    author_email="venjye@gmail.com",
+    url="https://github.com/venjye/PyiPNG",
+    project_urls={
+        "Documentation": "https://github.com/venjye/PyiPNG",
+    },
+    description="Convert CgBI to PNG with Python",
+    long_description=readme,
+    packages=find_packages(exclude=["tests*"]),
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Image Processing",
+    ],
+    keywords="CgBI",
+)
