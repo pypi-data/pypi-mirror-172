9746627c7c56b1b3dbd21134b41cb58ce123d7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:31 +0000

    Add package tryPackagePython8/0.0.1/tryPackagePython8-0.0.1.tar.gz

---
+++ packages/tryPackagePython8/tryPackagePython8-0.0.1.tar.gz/tryPackagePython8-0.0.1/tryPackagePython8/__init__.py
@@ -0,0 +1,5 @@
+from myFunctions import IAR_OBR_modified
+from myFunctions import IAR_ONS_modified
+from myFunctions import IAR_OGD_modified
+from myFunctions import IARg_OGD
+from myFunctions import IARg_ONS
