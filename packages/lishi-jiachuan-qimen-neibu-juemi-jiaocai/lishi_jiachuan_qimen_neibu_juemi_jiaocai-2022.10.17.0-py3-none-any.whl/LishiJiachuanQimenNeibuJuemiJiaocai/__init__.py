015b5bd530833109ee7082be0fa80810c3935399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:12 +0000

    Add package lishi-jiachuan-qimen-neibu-juemi-jiaocai/2022.10.17.0/lishi_jiachuan_qimen_neibu_juemi_jiaocai-2022.10.17.0-py3-none-any.whl

---
+++ packages/lishi-jiachuan-qimen-neibu-juemi-jiaocai/lishi_jiachuan_qimen_neibu_juemi_jiaocai-2022.10.17.0-py3-none-any.whl/LishiJiachuanQimenNeibuJuemiJiaocai/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lishi-jiachuan-qimen-neibu-juemi-jiaocai
+https://github.com/apachecn/lishi-jiachuan-qimen-neibu-juemi-jiaocai"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.17.0"
