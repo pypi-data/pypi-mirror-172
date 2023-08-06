cc2752ce6de9fcee3f0423ff7658a2c120c6ed8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:35 +0000

    Add package graph-notebook/3.6.1/graph-notebook-3.6.1.tar.gz

---
+++ packages/graph-notebook/graph-notebook-3.6.1.tar.gz/graph-notebook-3.6.1/src/graph_notebook/widgets/lib/version.js
@@ -0,0 +1,18 @@
+"use strict";
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const data = require("../package.json");
+/**
+ * The _model_module_version/_view_module_version this package implements.
+ *
+ * The html widget manager assumes that this is the same as the npm package
+ * version number.
+ */
+exports.MODULE_VERSION = data.version;
+/*
+ * The current package name.
+ */
+exports.MODULE_NAME = data.name;
