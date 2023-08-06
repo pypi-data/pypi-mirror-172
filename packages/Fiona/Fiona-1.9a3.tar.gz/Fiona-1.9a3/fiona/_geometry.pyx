1922c54b812f5ff9bdf6068dd704a3c7c686c3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:58 +0000

    Add package Fiona/1.9a3/Fiona-1.9a3.tar.gz

---
+++ packages/Fiona/Fiona-1.9a3.tar.gz/Fiona-1.9a3/fiona/_geometry.pyx
@@ -0,0 +1,276 @@
+# Coordinate and geometry transformations.
+
+from __future__ import absolute_import
+
+import logging
+
+from fiona.errors import UnsupportedGeometryTypeError
+from fiona.model import _guard_model_object, GEOMETRY_TYPES, Geometry
+
+from fiona._err cimport exc_wrap_int
+
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+log = logging.getLogger(__name__)
+log.addHandler(NullHandler())
+
+
+# mapping of GeoJSON type names to OGR integer geometry types
+GEOJSON2OGR_GEOMETRY_TYPES = dict((v, k) for k, v in GEOMETRY_TYPES.iteritems())
+
+
+cdef unsigned int geometry_type_code(name) except? 9999:
+    """Map OGC geometry type names to integer codes."""
+    offset = 0
+    if name.endswith('ZM'):
+        offset = 3000
+    elif name.endswith('M'):
+        offset = 2000
+    elif name.endswith('Z'):
+        offset = 1000
+
+    normalized_name = name.rstrip('ZM')
+    if normalized_name not in GEOJSON2OGR_GEOMETRY_TYPES:
+        raise UnsupportedGeometryTypeError(name)
+
+    return offset + <unsigned int>GEOJSON2OGR_GEOMETRY_TYPES[normalized_name]
+
+
+cdef object normalize_geometry_type_code(unsigned int code):
+    """Normalize M geometry type codes."""
+    # Normalize 'M' types to 2D types.
+    if 2000 <= code < 3000:
+        code = code % 1000
+    elif code == 3000:
+        code = 0
+    # Normalize 'ZM' types to 3D types.
+    elif 3000 < code < 4000:
+        code = (code % 1000) | 0x80000000
+    if code not in GEOMETRY_TYPES:
+        raise UnsupportedGeometryTypeError(code)
+
+    return code
+
+
+cdef inline unsigned int base_geometry_type_code(unsigned int code):
+    """ Returns base geometry code without Z, M and ZM types """
+    # Remove 2.5D flag.
+    code = code & (~0x80000000)
+
+    # Normalize Z, M, and ZM types. Fiona 1.x does not support M
+    # and doesn't treat OGC 'Z' variants as special types of their
+    # own.
+    return code % 1000
+
+
+# Geometry related functions and classes follow.
+cdef void * _createOgrGeomFromWKB(object wkb) except NULL:
+    """Make an OGR geometry from a WKB string"""
+    wkbtype = bytearray(wkb)[1]
+    cdef unsigned char *buffer = wkb
+    cdef void *cogr_geometry = OGR_G_CreateGeometry(<OGRwkbGeometryType>wkbtype)
+    if cogr_geometry is not NULL:
+        exc_wrap_int(OGR_G_ImportFromWkb(cogr_geometry, buffer, len(wkb)))
+    return cogr_geometry
+
+
+cdef _deleteOgrGeom(void *cogr_geometry):
+    """Delete an OGR geometry"""
+    if cogr_geometry is not NULL:
+        OGR_G_DestroyGeometry(cogr_geometry)
+    cogr_geometry = NULL
+
+
+cdef class GeomBuilder:
+    """Builds Fiona (GeoJSON) geometries from an OGR geometry handle.
+    """
+    cdef _buildCoords(self, void *geom):
+        # Build a coordinate sequence
+        cdef int i
+        if geom == NULL:
+            raise ValueError("Null geom")
+        npoints = OGR_G_GetPointCount(geom)
+        coords = []
+        for i in range(npoints):
+            values = [OGR_G_GetX(geom, i), OGR_G_GetY(geom, i)]
+            if self.ndims > 2:
+                values.append(OGR_G_GetZ(geom, i))
+            coords.append(tuple(values))
+        return coords
+
+    cpdef _buildPoint(self):
+        return {'type': 'Point', 'coordinates': self._buildCoords(self.geom)[0]}
+
+    cpdef _buildLineString(self):
+        return {'type': 'LineString', 'coordinates': self._buildCoords(self.geom)}
+
+    cpdef _buildLinearRing(self):
+        return {'type': 'LinearRing', 'coordinates': self._buildCoords(self.geom)}
+
+    cdef _buildParts(self, void *geom):
+        cdef int j
+        cdef void *part
+        if geom == NULL:
+            raise ValueError("Null geom")
+        parts = []
+        for j in range(OGR_G_GetGeometryCount(geom)):
+            part = OGR_G_GetGeometryRef(geom, j)
+            parts.append(GeomBuilder().build(part))
+        return parts
+
+    cpdef _buildPolygon(self):
+        coordinates = [p['coordinates'] for p in self._buildParts(self.geom)]
+        return {'type': 'Polygon', 'coordinates': coordinates}
+
+    cpdef _buildMultiPoint(self):
+        coordinates = [p['coordinates'] for p in self._buildParts(self.geom)]
+        return {'type': 'MultiPoint', 'coordinates': coordinates}
+
+    cpdef _buildMultiLineString(self):
+        coordinates = [p['coordinates'] for p in self._buildParts(self.geom)]
+        return {'type': 'MultiLineString', 'coordinates': coordinates}
+
+    cpdef _buildMultiPolygon(self):
+        coordinates = [p['coordinates'] for p in self._buildParts(self.geom)]
+        return {'type': 'MultiPolygon', 'coordinates': coordinates}
+
+    cpdef _buildGeometryCollection(self):
+        parts = self._buildParts(self.geom)
+        return {'type': 'GeometryCollection', 'geometries': parts}
+
+    cdef build(self, void *geom):
+        # The only method anyone needs to call
+        if geom == NULL:
+            raise ValueError("Null geom")
+
+        cdef unsigned int etype = OGR_G_GetGeometryType(geom)
+
+        self.code = base_geometry_type_code(etype)
+
+        if self.code not in GEOMETRY_TYPES:
+            raise UnsupportedGeometryTypeError(self.code)
+
+        self.geomtypename = GEOMETRY_TYPES[self.code]
+        self.ndims = OGR_G_GetCoordinateDimension(geom)
+        self.geom = geom
+        built = getattr(self, '_build' + self.geomtypename)()
+        return Geometry.from_dict(**built)
+
+    cpdef build_wkb(self, object wkb):
+        # The only other method anyone needs to call
+        cdef object data = wkb
+        cdef void *cogr_geometry = _createOgrGeomFromWKB(data)
+        result = self.build(cogr_geometry)
+        _deleteOgrGeom(cogr_geometry)
+        return result
+
+
+cdef class OGRGeomBuilder:
+    """Builds OGR geometries from Fiona geometries.
+    """
+    cdef void * _createOgrGeometry(self, int geom_type) except NULL:
+        cdef void *cogr_geometry = OGR_G_CreateGeometry(<OGRwkbGeometryType>geom_type)
+        if cogr_geometry == NULL:
+            raise Exception("Could not create OGR Geometry of type: %i" % geom_type)
+        return cogr_geometry
+
+    cdef _addPointToGeometry(self, void *cogr_geometry, object coordinate):
+        if len(coordinate) == 2:
+            x, y = coordinate
+            OGR_G_AddPoint_2D(cogr_geometry, x, y)
+        else:
+            x, y, z = coordinate[:3]
+            OGR_G_AddPoint(cogr_geometry, x, y, z)
+
+    cdef void * _buildPoint(self, object coordinates) except NULL:
+        cdef void *cogr_geometry = self._createOgrGeometry(GEOJSON2OGR_GEOMETRY_TYPES['Point'])
+        self._addPointToGeometry(cogr_geometry, coordinates)
+        return cogr_geometry
+
+    cdef void * _buildLineString(self, object coordinates) except NULL:
+        cdef void *cogr_geometry = self._createOgrGeometry(GEOJSON2OGR_GEOMETRY_TYPES['LineString'])
+        for coordinate in coordinates:
+            self._addPointToGeometry(cogr_geometry, coordinate)
+        return cogr_geometry
+
+    cdef void * _buildLinearRing(self, object coordinates) except NULL:
+        cdef void *cogr_geometry = self._createOgrGeometry(GEOJSON2OGR_GEOMETRY_TYPES['LinearRing'])
+        for coordinate in coordinates:
+            self._addPointToGeometry(cogr_geometry, coordinate)
+        OGR_G_CloseRings(cogr_geometry)
+        return cogr_geometry
+
+    cdef void * _buildPolygon(self, object coordinates) except NULL:
+        cdef void *cogr_ring
+        cdef void *cogr_geometry = self._createOgrGeometry(GEOJSON2OGR_GEOMETRY_TYPES['Polygon'])
+        for ring in coordinates:
+            cogr_ring = self._buildLinearRing(ring)
+            exc_wrap_int(OGR_G_AddGeometryDirectly(cogr_geometry, cogr_ring))
+        return cogr_geometry
+
+    cdef void * _buildMultiPoint(self, object coordinates) except NULL:
+        cdef void *cogr_part
+        cdef void *cogr_geometry = self._createOgrGeometry(GEOJSON2OGR_GEOMETRY_TYPES['MultiPoint'])
+        for coordinate in coordinates:
+            cogr_part = self._buildPoint(coordinate)
+            exc_wrap_int(OGR_G_AddGeometryDirectly(cogr_geometry, cogr_part))
+        return cogr_geometry
+
+    cdef void * _buildMultiLineString(self, object coordinates) except NULL:
+        cdef void *cogr_part
+        cdef void *cogr_geometry = self._createOgrGeometry(GEOJSON2OGR_GEOMETRY_TYPES['MultiLineString'])
+        for line in coordinates:
+            cogr_part = self._buildLineString(line)
+            exc_wrap_int(OGR_G_AddGeometryDirectly(cogr_geometry, cogr_part))
+        return cogr_geometry
+
+    cdef void * _buildMultiPolygon(self, object coordinates) except NULL:
+        cdef void *cogr_part
+        cdef void *cogr_geometry = self._createOgrGeometry(GEOJSON2OGR_GEOMETRY_TYPES['MultiPolygon'])
+        for part in coordinates:
+            cogr_part = self._buildPolygon(part)
+            exc_wrap_int(OGR_G_AddGeometryDirectly(cogr_geometry, cogr_part))
+        return cogr_geometry
+
+    cdef void * _buildGeometryCollection(self, object geometries) except NULL:
+        cdef void *cogr_part
+        cdef void *cogr_geometry = self._createOgrGeometry(GEOJSON2OGR_GEOMETRY_TYPES['GeometryCollection'])
+        for part in geometries:
+            cogr_part = OGRGeomBuilder().build(part)
+            exc_wrap_int(OGR_G_AddGeometryDirectly(cogr_geometry, cogr_part))
+        return cogr_geometry
+
+    cdef void * build(self, object geometry) except NULL:
+        cdef object typename = geometry.type
+        cdef object coordinates = geometry.coordinates
+        cdef object geometries = geometry.geometries
+        if typename == 'Point':
+            return self._buildPoint(coordinates)
+        elif typename == 'LineString':
+            return self._buildLineString(coordinates)
+        elif typename == 'LinearRing':
+            return self._buildLinearRing(coordinates)
+        elif typename == 'Polygon':
+            return self._buildPolygon(coordinates)
+        elif typename == 'MultiPoint':
+            return self._buildMultiPoint(coordinates)
+        elif typename == 'MultiLineString':
+            return self._buildMultiLineString(coordinates)
+        elif typename == 'MultiPolygon':
+            return self._buildMultiPolygon(coordinates)
+        elif typename == 'GeometryCollection':
+            return self._buildGeometryCollection(geometries)
+        else:
+            raise UnsupportedGeometryTypeError("Unsupported geometry type %s" % typename)
+
+
+def geometryRT(geom):
+    # For testing purposes only, leaks the JSON data
+    geometry = _guard_model_object(geom)
+    cdef void *cogr_geometry = OGRGeomBuilder().build(geometry)
+    result = GeomBuilder().build(cogr_geometry)
+    _deleteOgrGeom(cogr_geometry)
+    return result
---
+++ packages/Fiona/Fiona-1.9a3.tar.gz/Fiona-1.9a3/setup.py
@@ -0,0 +1,291 @@
+from distutils.command.sdist import sdist
+from distutils import log
+import itertools as it
+import os
+import shutil
+import subprocess
+import sys
+from setuptools import setup
+from setuptools.extension import Extension
+
+
+# Use Cython if available.
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+
+def check_output(cmd):
+    return subprocess.check_output(cmd).decode('utf')
+
+def copy_data_tree(datadir, destdir):
+    try:
+        shutil.rmtree(destdir)
+    except OSError:
+        pass
+    shutil.copytree(datadir, destdir)
+
+
+# Parse the version from the fiona module.
+with open('fiona/__init__.py', 'r') as f:
+    for line in f:
+        if line.find("__version__") >= 0:
+            version = line.split("=")[1].strip().strip('"').strip("'")
+            break
+
+# Fiona's auxiliary files are UTF-8 encoded
+open_kwds = {'encoding': 'utf-8'}
+
+with open('VERSION.txt', 'w', **open_kwds) as f:
+    f.write(version)
+
+with open('README.rst', **open_kwds) as f:
+    readme = f.read()
+
+with open('CREDITS.txt', **open_kwds) as f:
+    credits = f.read()
+
+with open('CHANGES.txt', **open_kwds) as f:
+    changes = f.read()
+
+# Building Fiona requires options that can be obtained from GDAL's gdal-config
+# program or can be specified using setup arguments. The latter override the
+# former.
+#
+# A GDAL API version is strictly required. Without this the setup script
+# cannot know whether to use the GDAL version 1 or 2 source files. The GDAL
+# API version can be specified in 2 ways.
+#
+# 1. By the gdal-config program, optionally pointed to by GDAL_CONFIG
+# 2. By a GDAL_VERSION environment variable. This overrides number 1.
+
+
+include_dirs = []
+library_dirs = []
+libraries = []
+extra_link_args = []
+gdal_output = [None for i in range(4)]
+gdalversion = None
+language = None
+gdal_major_version = 0
+gdal_minor_version = 0
+
+if 'clean' not in sys.argv:
+    try:
+        gdal_config = os.environ.get('GDAL_CONFIG', 'gdal-config')
+        for i, flag in enumerate(
+                ["--cflags", "--libs", "--datadir", "--version"]):
+            gdal_output[i] = check_output([gdal_config, flag]).strip()
+        for item in gdal_output[0].split():
+            if item.startswith("-I"):
+                include_dirs.extend(item[2:].split(":"))
+        for item in gdal_output[1].split():
+            if item.startswith("-L"):
+                library_dirs.extend(item[2:].split(":"))
+            elif item.startswith("-l"):
+                libraries.append(item[2:])
+            else:
+                # e.g. -framework GDAL
+                extra_link_args.append(item)
+        gdalversion = gdal_output[3]
+        if gdalversion:
+            log.info("GDAL API version obtained from gdal-config: %s",
+                     gdalversion)
+
+    except Exception as e:
+        if os.name == "nt":
+            log.info("Building on Windows requires extra options to setup.py "
+                     "to locate needed GDAL files.\nMore information is "
+                     "available in the README.")
+        else:
+            log.warn("Failed to get options via gdal-config: %s", str(e))
+
+    # Get GDAL API version from environment variable.
+    if 'GDAL_VERSION' in os.environ:
+        gdalversion = os.environ['GDAL_VERSION']
+        log.info("GDAL API version obtained from environment: %s", gdalversion)
+
+    # Get GDAL API version from the command line if specified there.
+    if '--gdalversion' in sys.argv:
+        index = sys.argv.index('--gdalversion')
+        sys.argv.pop(index)
+        gdalversion = sys.argv.pop(index)
+        log.info("GDAL API version obtained from command line option: %s",
+                 gdalversion)
+
+    if not gdalversion:
+        log.fatal("A GDAL API version must be specified. Provide a path "
+                  "to gdal-config using a GDAL_CONFIG environment variable "
+                  "or use a GDAL_VERSION environment variable.")
+        sys.exit(1)
+
+    if os.environ.get('PACKAGE_DATA'):
+        destdir = 'fiona/gdal_data'
+        if gdal_output[2]:
+            log.info("Copying gdal data from %s" % gdal_output[2])
+            copy_data_tree(gdal_output[2], destdir)
+        else:
+            # check to see if GDAL_DATA is defined
+            gdal_data = os.environ.get('GDAL_DATA', None)
+            if gdal_data:
+                log.info("Copying gdal data from %s" % gdal_data)
+                copy_data_tree(gdal_data, destdir)
+
+        # Conditionally copy PROJ DATA.
+        projdatadir = os.environ.get('PROJ_DATA', os.environ.get('PROJ_LIB', '/usr/local/share/proj'))
+        if os.path.exists(projdatadir):
+            log.info("Copying proj data from %s" % projdatadir)
+            copy_data_tree(projdatadir, 'fiona/proj_data')
+
+    if "--cython-language" in sys.argv:
+        index = sys.argv.index("--cython-language")
+        sys.argv.pop(index)
+        language = sys.argv.pop(index).lower()
+
+    gdal_version_parts = gdalversion.split('.')
+    gdal_major_version = int(gdal_version_parts[0])
+    gdal_minor_version = int(gdal_version_parts[1])
+
+    if (gdal_major_version, gdal_minor_version) < (3, 1):
+        raise SystemExit(
+            "ERROR: GDAL >= 3.1 is required for fiona. "
+            "Please upgrade GDAL."
+        )
+
+    log.info("GDAL version major=%r minor=%r", gdal_major_version, gdal_minor_version)
+
+compile_time_env = {
+    "CTE_GDAL_MAJOR_VERSION": gdal_major_version,
+    "CTE_GDAL_MINOR_VERSION": gdal_minor_version,
+}
+
+ext_options = dict(
+    include_dirs=include_dirs,
+    library_dirs=library_dirs,
+    libraries=libraries,
+    extra_link_args=extra_link_args,
+    cython_compile_time_env=compile_time_env,
+)
+
+# Enable coverage for cython pyx files.
+if os.environ.get('CYTHON_COVERAGE'):
+    from Cython.Compiler.Options import get_directive_defaults
+    directive_defaults = get_directive_defaults()
+    directive_defaults['linetrace'] = True
+    directive_defaults['binding'] = True
+
+    ext_options.update(dict(
+        define_macros=[("CYTHON_TRACE_NOGIL", "1")]))
+
+# GDAL 2.3+ requires C++11
+
+if language == "c++":
+    ext_options["language"] = "c++"
+    if sys.platform != "win32":
+        ext_options["extra_compile_args"] = ["-std=c++11"]
+
+ext_options_cpp = ext_options.copy()
+if sys.platform != "win32":
+    ext_options_cpp["extra_compile_args"] = ["-std=c++11"]
+
+
+# Define the extension modules.
+ext_modules = []
+
+if "clean" not in sys.argv:
+    # When building from a repo, Cython is required.
+    log.info("MANIFEST.in found, presume a repo, cythonizing...")
+    if not cythonize:
+        raise SystemExit(
+            "Cython.Build.cythonize not found. "
+            "Cython is required to build fiona."
+        )
+
+    ext_modules = cythonize(
+        [
+            Extension("fiona._geometry", ["fiona/_geometry.pyx"], **ext_options),
+            Extension("fiona.schema", ["fiona/schema.pyx"], **ext_options),
+            Extension("fiona._transform", ["fiona/_transform.pyx"], **ext_options_cpp),
+            Extension("fiona.crs", ["fiona/crs.pyx"], **ext_options),
+            Extension("fiona._env", ["fiona/_env.pyx"], **ext_options),
+            Extension("fiona._err", ["fiona/_err.pyx"], **ext_options),
+            Extension("fiona.ogrext", ["fiona/ogrext.pyx"], **ext_options),
+        ],
+        compiler_directives={"language_level": "3"},
+        compile_time_env=compile_time_env,
+    )
+
+requirements = [
+    'attrs>=17',
+    'certifi',
+    'click>=4.0',
+    'cligj>=0.5',
+    'click-plugins>=1.0',
+    'munch',
+]
+
+extras_require = {
+    'calc': ['shapely'],
+    's3': ['boto3>=1.2.4'],
+    'test': ['pytest>=3', 'pytest-cov', 'boto3>=1.2.4']
+}
+
+extras_require['all'] = list(set(it.chain(*extras_require.values())))
+
+
+setup_args = dict(
+    metadata_version='1.2',
+    name='Fiona',
+    version=version,
+    python_requires='>=3.7',
+    requires_external='GDAL (>=3.1)',
+    description="Fiona reads and writes spatial data files",
+    license='BSD',
+    keywords='gis vector feature data',
+    author='Sean Gillies',
+    author_email='sean.gillies@gmail.com',
+    maintainer='Sean Gillies',
+    maintainer_email='sean.gillies@gmail.com',
+    url='https://github.com/Toblerity/Fiona',
+    long_description=readme + "\n" + changes + "\n" + credits,
+    package_dir={'': '.'},
+    packages=['fiona', 'fiona.fio'],
+    entry_points='''
+        [console_scripts]
+        fio=fiona.fio.main:main_group
+
+        [fiona.fio_commands]
+        bounds=fiona.fio.bounds:bounds
+        calc=fiona.fio.calc:calc
+        cat=fiona.fio.cat:cat
+        collect=fiona.fio.collect:collect
+        distrib=fiona.fio.distrib:distrib
+        dump=fiona.fio.dump:dump
+        env=fiona.fio.env:env
+        filter=fiona.fio.filter:filter
+        info=fiona.fio.info:info
+        insp=fiona.fio.insp:insp
+        load=fiona.fio.load:load
+        ls=fiona.fio.ls:ls
+        rm=fiona.fio.rm:rm
+        ''',
+    install_requires=requirements,
+    extras_require=extras_require,
+    ext_modules=ext_modules,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: GIS'])
+
+if os.environ.get('PACKAGE_DATA'):
+    setup_args['package_data'] = {'fiona': ['gdal_data/*', 'proj_data/*', '.libs/*', '.libs/licenses/*']}
+
+setup(**setup_args)
