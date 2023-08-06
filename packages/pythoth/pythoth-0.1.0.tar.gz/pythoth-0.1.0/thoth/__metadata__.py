ec75fd3f6f3d55b109ef9c425de78bc65fc1351a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:14 +0000

    Add package pythoth/0.1.0/pythoth-0.1.0.tar.gz

---
+++ packages/pythoth/pythoth-0.1.0.tar.gz/pythoth-0.1.0/thoth/__metadata__.py
@@ -0,0 +1,12 @@
+"""Metadata module."""
+
+__title__: str = "thoth"
+__package_title__: str = "pythoth"
+__description__: str = (
+    "Python tool for profiling-based anomaly monitoring on ETL data pipelines "
+    "leveraging ML and Apache Spark."
+)
+__version__: str = "0.1.0"
+__url__: str = "https://github.com/rafaelleinio/thoth"
+__author__: str = "Rafael Leiniö"
+__license__: str = "MIT"
