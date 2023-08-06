d4abff403f705c3933354b52755fdd823c28352f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:16 +0000

    Add package fractif-http/0.0.4/fractif-http-0.0.4.tar.gz

---
+++ packages/fractif-http/fractif-http-0.0.4.tar.gz/fractif-http-0.0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = '''Simple, yet elegant, HTTP library. As like as Requests.
+Actually, it is based on requests package...'''
+
+
+def read(file_name: str):
+    with open(file_name) as fi:
+        return fi.read()
+
+
+setup(
+    name="fractif-http",
+    version=VERSION,
+    description=DESCRIPTION,
+    author="Tom LARGE",
+    author_email="tom@fractif.com",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'bs4',
+        'user_agent',
+        'coloredlogs',
+    ],
+    keywords='http',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ],
+    url="https://github.com/Fractif/fractif-http",
+)
