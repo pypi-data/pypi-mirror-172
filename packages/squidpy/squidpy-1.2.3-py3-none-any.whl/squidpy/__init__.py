cfe8b76a4edba3e63967ec32e5fc566eb11661bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:02 +0000

    Add package squidpy/1.2.3/squidpy-1.2.3-py3-none-any.whl

---
+++ packages/squidpy/squidpy-1.2.3-py3-none-any.whl/squidpy/__init__.py
@@ -0,0 +1,25 @@
+from squidpy import gr, im, pl, read, datasets
+
+__author__ = __maintainer__ = "Theislab"
+__email__ = ", ".join(
+    [
+        "giovanni.palla@helmholtz-muenchen.de",
+        "hannah.spitzer@helmholtz-muenchen.de",
+    ]
+)
+__version__ = "1.2.3"
+
+try:
+    from importlib_metadata import version  # Python < 3.8
+except ImportError:
+    from importlib.metadata import version  # Python = 3.8
+
+from packaging.version import parse
+
+try:
+    __full_version__ = parse(version(__name__))
+    __full_version__ = f"{__version__}+{__full_version__.local}" if __full_version__.local else __version__
+except ImportError:
+    __full_version__ = __version__
+
+del version, parse
