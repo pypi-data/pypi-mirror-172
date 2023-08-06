2f6ba0e433dbda1fb1e5a9a8a4e68a1a4d53bb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:49 +0000

    Add package hexacolors/0.4.0/hexacolors-0.4.0.tar.gz

---
+++ packages/hexacolors/hexacolors-0.4.0.tar.gz/hexacolors-0.4.0/hexacolors/__init__.py
@@ -0,0 +1,83 @@
+"""
+hexacolors
+~~~~~~~~~~~~~~~~~~~
+
+A simple library that converts string to hexadecimal understandable by python
+
+A simple library that converts RGB to hexadecimal understandable by python
+
+A simple library that converts CMYK to hexadecimal understandable by python
+
+A simple library that converts hexadecimal understandable by python
+
+:(c) 2022-present Marciel404
+:license: MIT, see LICENSE for more details.
+
+I created this library more to use as pycord integration
+
+Using StringColor:
+
+    >>> import hexacolors
+    >>>
+    >>> hexacolors.stringcolor('Blue')
+    >>>
+    >>> print(hexacolors.listall) #List all colors availables
+
+Using HexaColor:
+
+    >>> import hexacolors
+    >>>
+    >>> hexacolors.hexacolor('#0000FF') #Convert Hexadecimal Color for Python understand
+
+Using rgb:
+
+    >>> import hexacolors
+    >>>
+    >>> hexacolors.rgb('255,255,255')
+
+Using cmyk:
+
+    >>> import hexacolors
+    >>>
+    >>> hexacolors.cmyk('423,522,4,244')
+
+Using AutoDetect:
+
+    >>> import hexacolors
+    >>>
+    >>> hexacolors.autodetect('Blue') #Identify string
+    >>>
+    >>> hexacolors.autodetect('#fff000') #Identify Hexadecimal
+    >>>
+    >>> hexacolors.autodetect('255,255,255') #Identify RGB
+    >>>
+    >>> hexacolors.autodetect('423,522,4,244') #Identify CMYK
+
+"""
+
+__title__ = "Hexacolors"
+__author__ = "Marciel404"
+__license__ = "MIT"
+__copyright__ = "2022-present Marciel404"
+__version__ = "0.4.0"
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+from .str import stringcolor
+from .api import hexacolor, rgb, cmyk, hsl
+from .autodetect import autodetect
+
+from .listcolors import (
+    listblack,
+    listblue,
+    listcyan,
+    listgreen,
+    listbrown,
+    listpurple,
+    listpink,
+    listred,
+    listorange,
+    listyellow,
+    listlight,
+    listall
+    )
