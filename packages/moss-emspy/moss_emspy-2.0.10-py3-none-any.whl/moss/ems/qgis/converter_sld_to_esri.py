dc9d4db340b60c5bf2fccd4dd2883dda242fbeb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:22 +0000

    Add package moss-emspy/2.0.10/moss_emspy-2.0.10-py3-none-any.whl

---
+++ packages/moss-emspy/moss_emspy-2.0.10-py3-none-any.whl/moss/ems/qgis/converter_sld_to_esri.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+import xml.etree.ElementTree as ET
+
+
+class PolygonSymbolizer:
+    def __init__(self, xml_block: str) -> dict:
+        """ """
+
+    
+
+
+@dataclass
+class ConverterSldToEsri:
+    def generate_esri(self, input_sld: str) -> dict:
+        """
+        Generate an ESRI Style starting from SLD
+        """
+        pass
