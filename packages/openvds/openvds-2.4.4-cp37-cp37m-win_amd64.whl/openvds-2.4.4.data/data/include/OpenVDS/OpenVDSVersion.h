8b5c5fd412ec88850ac3b46d3a2296064709a54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:33 +0000

    Add package openvds/2.4.4/openvds-2.4.4-cp37-cp37m-win_amd64.whl

---
+++ packages/openvds/openvds-2.4.4-cp37-cp37m-win_amd64.whl/openvds-2.4.4.data/data/include/OpenVDS/OpenVDSVersion.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+** Copyright 2019 The Open Group
+** Copyright 2019 Bluware, Inc.
+** Copyright 2020 Microsoft Corp.
+** Copyright 2020 Google, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+****************************************************************************/
+
+#ifndef OPENVDSVERSION_H
+#define OPENVDSVERSION_H
+
+#define OPENVDS_PROJECT_NAME "OpenVDS+"
+#define OPENVDS_VERSION "2.4.4"
+#define OPENVDS_MAJOR_VERSION 2
+#define OPENVDS_MINOR_VERSION 4
+#define OPENVDS_PATCH_VERSION 4
+#define OPENVDS_REVISION "97c057ef8f2ae6fbe4d164d12376561c03de687e"
+
+#endif
