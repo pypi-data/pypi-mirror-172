21e256d5edd4075519a742380c28a99e0e7a0199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:43 +0000

    Add package dvclive/0.12.1/dvclive-0.12.1-py3-none-any.whl

---
+++ packages/dvclive/dvclive-0.12.1-py3-none-any.whl/dvclive/catalyst.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from catalyst import utils
+from catalyst.core.callback import Callback, CallbackOrder
+
+from dvclive import Live
+
+
+class DvcLiveCallback(Callback):
+    def __init__(
+        self, model_file=None, dvclive: Optional[Live] = None, **kwargs
+    ):
+        super().__init__(order=CallbackOrder.external)
+        self.model_file = model_file
+        self.dvclive = dvclive if dvclive is not None else Live(**kwargs)
+
+    def on_epoch_end(self, runner) -> None:
+        for loader_key, per_loader_metrics in runner.epoch_metrics.items():
+            for key, value in per_loader_metrics.items():
+                key = key.replace("/", "_")
+                self.dvclive.log(f"{loader_key}/{key}", float(value))
+
+        if self.model_file:
+            checkpoint = utils.pack_checkpoint(
+                model=runner.model,
+                criterion=runner.criterion,
+                optimizer=runner.optimizer,
+                scheduler=runner.scheduler,
+            )
+            utils.save_checkpoint(checkpoint, self.model_file)
+        self.dvclive.next_step()
