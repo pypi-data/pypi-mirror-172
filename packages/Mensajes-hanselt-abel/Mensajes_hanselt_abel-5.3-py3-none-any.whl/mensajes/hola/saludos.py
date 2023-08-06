ecff17a8fdc7748730eb818c9be7778eddd5bdc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:27 +0000

    Add package Mensajes-hanselt-abel/5.3/Mensajes_hanselt_abel-5.3-py3-none-any.whl

---
+++ packages/Mensajes-hanselt-abel/Mensajes_hanselt_abel-5.3-py3-none-any.whl/mensajes/hola/saludos.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def saludar():
+    print("Hola, te saludo desde saludos.saludar()")
+
+
+def prueba():
+    print("Esto es una prueba de la nueva versión.")
+
+
+def generar_array(numeros):
+    print("Esta linea viene con una actualización")
+    return np.arange(numeros)
+
+
+class Saludo:
+    def __init__(self):
+        print("Hola, te saludo desde Saludo.__init__()")
+
+
+if __name__ == '__main__':
+    print(generar_array(5))
