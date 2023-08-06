ac511e34a060f16322aa60320b79999b453d7ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:19 +0000

    Add package packj/0.11/packj-0.11.tar.gz

---
+++ packages/packj/packj-0.11.tar.gz/packj-0.11/packj/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import os
+
+# sys.version_info[0] is the major version number. sys.version_info[1] is minor
+if sys.version_info[0] != 3:
+	print('\n*** WARNING *** Please use Python 3! Exiting.')
+	exit(1)
+
+def main(config:str='.packj.yaml'):
+	try:
+		# parse command line args
+		from packj.options import Options
+		opts = Options(sys.argv[1:])
+		assert opts, 'Failed to parse cmdline args!'
+
+		args = opts.args()
+		assert args, 'Failed to get cmdline args!'
+
+		# configuration file
+		if not os.path.exists(config):
+			config = os.path.expanduser(os.path.join('~', f'{config}'))
+		assert os.path.exists(config), f'No {config} file found'
+
+		# audit request
+		if args.cmd == 'audit':
+			from packj.audit.main import main
+			main(args, config)
+
+		# sandbox install
+		elif args.cmd == 'sandbox':
+			from packj.sandbox.main import main
+			main(args, config)
+
+	except Exception as e:
+		print(str(e))
+		exit(1)
