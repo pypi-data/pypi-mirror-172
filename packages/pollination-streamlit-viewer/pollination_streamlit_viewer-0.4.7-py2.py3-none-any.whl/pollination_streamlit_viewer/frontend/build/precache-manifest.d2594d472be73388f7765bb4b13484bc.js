23ce1ba2b4a1ffdd2f137a9b6aa7db0dcd210cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:56:29 +0000

    Add package pollination-streamlit-viewer/0.4.7/pollination_streamlit_viewer-0.4.7-py2.py3-none-any.whl

---
+++ packages/pollination-streamlit-viewer/pollination_streamlit_viewer-0.4.7-py2.py3-none-any.whl/pollination_streamlit_viewer/frontend/build/precache-manifest.d2594d472be73388f7765bb4b13484bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3611b4d90430dafcc2625af590ba049",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c471b4b05039b1cd3c8",
+    "url": "./static/css/2.fc555424.chunk.css"
+  },
+  {
+    "revision": "ff1b919afdfbb7f59619",
+    "url": "./static/css/main.6f2db4ab.chunk.css"
+  },
+  {
+    "revision": "5c471b4b05039b1cd3c8",
+    "url": "./static/js/2.ba3b52f2.chunk.js"
+  },
+  {
+    "revision": "2a1769dcd839a974a894b4105ed17497",
+    "url": "./static/js/2.ba3b52f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1b919afdfbb7f59619",
+    "url": "./static/js/main.b692d6a1.chunk.js"
+  },
+  {
+    "revision": "b3d2ba5e760084bc1c89",
+    "url": "./static/js/runtime-main.fd5618dd.js"
+  }
+]);
