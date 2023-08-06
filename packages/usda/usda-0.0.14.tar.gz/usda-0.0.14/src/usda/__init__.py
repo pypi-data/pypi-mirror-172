b7c25c18660cd23283cfd4e60c674b6fc59f3094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:59 +0000

    Add package usda/0.0.14/usda-0.0.14.tar.gz

---
+++ packages/usda/usda-0.0.14.tar.gz/usda-0.0.14/src/usda/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 21 15:11:00 2022
+
+@author: Richie Bao-caDesign设计(cadesign.cn)
+"""
+
+__version__ = "0.0.14"
+
+__all__ = [
+    "datasets",
+    "database",
+    "utils",
+    "charts",
+    "stats",
+    ]
---
+++ packages/usda/usda-0.0.14.tar.gz/usda-0.0.14/src/usda/utils/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 21 15:11:00 2022
+
+@author: Richie Bao-caDesign设计(cadesign.cn)
+"""
+from ._bunch import Bunch
+from ._DisplayablePath import DisplayablePath
+from ._file_structure import filePath_extraction
+
+__all__=[
+    "Bunch",
+    "DisplayablePath",
+    "filePath_extraction",
+    ]
