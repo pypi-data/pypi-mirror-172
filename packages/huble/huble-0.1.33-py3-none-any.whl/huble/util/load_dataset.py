3a3cd485b8f4f1abd693c2360e6589b86f543268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:31 +0000

    Add package huble/0.1.33/huble-0.1.33-py3-none-any.whl

---
+++ packages/huble/huble-0.1.33-py3-none-any.whl/huble/util/load_dataset.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import requests
+
+
+def load_dataset(url) -> pd.DataFrame:
+    local_filename = "test.csv"  # url.split('/')[-1]
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return pd.read_csv(local_filename)
