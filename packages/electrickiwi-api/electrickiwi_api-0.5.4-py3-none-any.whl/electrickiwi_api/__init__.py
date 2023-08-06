6993baf779d110fbe2f3144e7d9839d44d01b341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:12 +0000

    Add package electrickiwi-api/0.5.4/electrickiwi_api-0.5.4-py3-none-any.whl

---
+++ packages/electrickiwi-api/electrickiwi_api-0.5.4-py3-none-any.whl/electrickiwi_api/__init__.py
@@ -0,0 +1,21 @@
+# __init__.py
+
+# version of ElectricKiwiApi for Python
+__version__ = "0.5.4"
+
+from electrickiwi_api.api import (
+    ElectricKiwiEndpoint,
+    ElectricKiwiApi
+)
+
+from apiclient import (
+    BasicAuthentication,
+    HeaderAuthentication,
+    NoAuthentication,
+    QueryParameterAuthentication,
+)
+
+# remove dev for production
+# Authorization URL 	https://welcome-dev.electrickiwi.co.nz/oauth/authorize
+# Token URL 	https://welcome-dev.electrickiwi.co.nz/oauth/token
+# API 	https://api-dev.electrickiwi.co.nz
