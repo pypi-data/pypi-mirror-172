c86b75918d1100336a1588df7f6f69084924af7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:48 +0000

    Add package pysqlx-core/0.1.1/pysqlx_core-0.1.1.tar.gz

---
+++ packages/pysqlx-core/pysqlx_core-0.1.1.tar.gz/pysqlx_core-0.1.1/src/lib.rs
@@ -0,0 +1,29 @@
+pub mod base;
+pub mod db;
+pub mod record;
+pub mod value;
+
+use base::error::PysqlxDBError;
+use base::types::PysqlxRows;
+use db::Connection;
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+#[pyfunction]
+fn new<'a>(py: Python<'a>, uri: String) -> Result<&'a PyAny, pyo3::PyErr> {
+    pyo3_asyncio::tokio::future_into_py(py, async move {
+        match Connection::_new(uri).await {
+            Ok(r) => Ok(r),
+            Err(e) => return Err(PyErr::from(PysqlxDBError::from(e))),
+        }
+    })
+}
+
+#[pymodule]
+fn pysqlx_core(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(new, m)?)?;
+    m.add_class::<Connection>()?;
+    m.add_class::<PysqlxRows>()?;
+    m.add_class::<PysqlxDBError>()?;
+    Ok(())
+}
