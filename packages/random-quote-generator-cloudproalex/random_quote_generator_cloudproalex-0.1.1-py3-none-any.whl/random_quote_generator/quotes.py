9303aa302b477948bf3f6c6a176dd6f5e70f3a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:49 +0000

    Add package random-quote-generator-cloudproalex/0.1.1/random_quote_generator_cloudproalex-0.1.1-py3-none-any.whl

---
+++ packages/random-quote-generator-cloudproalex/random_quote_generator_cloudproalex-0.1.1-py3-none-any.whl/random_quote_generator/quotes.py
@@ -0,0 +1,57 @@
+quotes = [
+    {
+        "quote": "Strong minds discuss ideas,"
+        " average minds discuss events,"
+        " weak minds discuss people.",
+        "author": "Socrates",
+    },
+    {
+        "quote": "Moral excellence comes about as a result of habit."
+        " We become just by doing just acts,"
+        " temperate by doing temperate acts,"
+        " brave by doing brave acts.",
+        "author": "Aristotle",
+    },
+    {
+        "quote": "Knowing what's right doesn't mean much"
+        " unless you do what's right.",
+        "author": "Theordore Roosevelt",
+    },
+    {
+        "quote": "The greatest glory in living lies not in never falling,"
+        " but in rising every time we fall.",
+        "author": "Nelson Mandela",
+    },
+    {
+        "quote": "The best and most beautiful things in the world"
+        " cannot be seen or even touched"
+        " — they must be felt with the heart.",
+        "author": "Helen Keller",
+    },
+    {
+        "quote": "Tell me and I forget."
+        " Teach me and I remember."
+        " Involve me and I learn.",
+        "author": "Benjamin Franklin",
+    },
+    {
+        "quote": "You will face many defeats in life,"
+        " but never let yourself be defeated.",
+        "author": "Maya Angelou",
+    },
+    {
+        "quote": "Spread love everywhere you go. Let no"
+        " one ever come to you without leaving happier.",
+        "author": "Mother Teresa",
+    },
+    {
+        "quote": "Do not go where the path may lead,"
+        " go instead where there is no path"
+        " and leave a trail.",
+        "author": "Ralph Waldo Emerson",
+    },
+    {
+        "quote": "Whoever is happy will make others happy too.",
+        "author": "Anne Frank",
+    },
+]
