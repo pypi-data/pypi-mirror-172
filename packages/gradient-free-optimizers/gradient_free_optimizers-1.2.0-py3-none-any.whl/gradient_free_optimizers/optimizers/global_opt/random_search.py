515662e501883775353652d197e122f9db223d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:57 +0000

    Add package gradient-free-optimizers/1.2.0/gradient_free_optimizers-1.2.0-py3-none-any.whl

---
+++ packages/gradient-free-optimizers/gradient_free_optimizers-1.2.0-py3-none-any.whl/gradient_free_optimizers/optimizers/global_opt/random_search.py
@@ -0,0 +1,23 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+from ..base_optimizer import BaseOptimizer
+from ...search import Search
+
+
+class RandomSearchOptimizer(BaseOptimizer, Search):
+    name = "Random Search"
+    _name_ = "random_search"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @BaseOptimizer.track_new_pos
+    def iterate(self):
+        return self.move_random()
+    
+    @BaseOptimizer.track_new_score
+    def evaluate(self, score_new):
+        return super().evaluate(score_new)
