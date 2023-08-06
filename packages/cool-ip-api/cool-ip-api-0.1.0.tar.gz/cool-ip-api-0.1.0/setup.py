cecc7180d23e4d421fa69d4132eb0c68b68e0cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:16 +0000

    Add package cool-ip-api/0.1.0/cool-ip-api-0.1.0.tar.gz

---
+++ packages/cool-ip-api/cool-ip-api-0.1.0.tar.gz/cool-ip-api-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cool_ip_api', 'cool_ip_api.provider', 'cool_ip_api.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.23.0,<0.24.0', 'pydantic>=1.10.2,<2.0.0', 'requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'cool-ip-api',
+    'version': '0.1.0',
+    'description': 'A collections of wrapper for different IP info APIs',
+    'long_description': '# cool-ip-api\nA package to get information from an IP using a collection of IP info apis.\n',
+    'author': 'sokripon',
+    'author_email': 'sokripon@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/sokirpon/cool-ip-api',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
