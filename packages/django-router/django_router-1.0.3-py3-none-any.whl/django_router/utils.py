ccd0199ae1a064ecfee9f14613bb0a452b99843a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:28 +0000

    Add package django-router/1.0.3/django_router-1.0.3-py3-none-any.whl

---
+++ packages/django-router/django_router-1.0.3-py3-none-any.whl/django_router/utils.py
@@ -0,0 +1,28 @@
+import re
+
+from django.views import generic
+
+from django_router.settings import ROUTER_SETTINGS
+
+CAMEL_PATTERN = re.compile(r"(?<!^)(?=[A-Z])")
+
+
+def from_camel(string: str, separator=ROUTER_SETTINGS.NAME_WORDS_SEPARATOR):
+    return CAMEL_PATTERN.sub(separator, string).lower()
+
+
+DJANGO_ROUTER_MAP = {
+    generic.ListView: ("list", ""),
+    generic.CreateView: ("create", "create/"),
+    generic.DetailView: ("detail", ""),
+    generic.UpdateView: ("update", "update/"),
+    generic.DeleteView: ("delete", "delete/"),
+}
+
+DJANGO_ADMIN_LIKE_MAP = {
+    generic.ListView: ("changelist", ""),
+    generic.CreateView: ("add", "add/"),
+    generic.DetailView: ("detail", ""),
+    generic.UpdateView: ("change", "change/"),
+    generic.DeleteView: ("delete", "delete/"),
+}
