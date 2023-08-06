45a20ff320d9796c49871a1f17b86065d7d7b5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:36 +0000

    Add package fractal-tasks-core/0.2.6/fractal-tasks-core-0.2.6.tar.gz

---
+++ packages/fractal-tasks-core/fractal-tasks-core-0.2.6.tar.gz/fractal-tasks-core-0.2.6/fractal_tasks_core/lib_upscale_array.py
@@ -0,0 +1,90 @@
+"""
+Copyright 2022 (C)
+    Friedrich Miescher Institute for Biomedical Research and
+    University of Zurich
+
+    Original authors:
+    Tommaso Comparin <tommaso.comparin@exact-lab.it>
+
+    This file is part of Fractal and was originally developed by eXact lab
+    S.r.l.  <exact-lab.it> under contract with Liberali Lab from the Friedrich
+    Miescher Institute for Biomedical Research and Pelkmans Lab from the
+    University of Zurich.
+
+Function to increase the shape of an array by replicating it
+"""
+import warnings
+from typing import Iterable
+
+import numpy as np
+
+
+def upscale_array(
+    *,
+    target_shape: Iterable[int] = None,
+    array=None,
+    axis: Iterable[int] = None,
+) -> np.ndarray:
+    """
+    Upscale array along given axis, to match a
+    target shape. Upscaling is based on np.repeat.
+
+    :param dummy: this is just a placeholder
+    :type dummy: int
+    """
+
+    array_shape = array.shape
+    info = (
+        f"Trying to upscale from {array_shape=} to {target_shape=}, "
+        f"acting on {axis=}."
+    )
+
+    # Default behavior: use all axis
+    if axis is None:
+        axis = list(range(len(target_shape)))
+
+    if len(array_shape) != len(target_shape):
+        raise ValueError(f"{info} Dimensions-number mismatch.")
+    if min(axis) < 0:
+        raise ValueError(f"{info} Negative axis specification not allowed.")
+
+    # Check that upscale is doable
+    for ind, dim in enumerate(array_shape):
+        # Check that array is not larger than target (downscaling)
+        if dim > target_shape[ind]:
+            raise ValueError(
+                f"{info} {ind}-th array dimension is larger than target."
+            )
+        # Check that all relevant axis are included in axis
+        if dim != target_shape[ind] and ind not in axis:
+            raise ValueError(
+                f"{info} {ind}-th array dimension differs from "
+                f"target, but {ind} is not included in "
+                f"{axis=}."
+            )
+
+    # Compute upscaling factors
+    upscale_factors = {}
+    for ax in axis:
+        upscale_factors[ax] = target_shape[ax] // array_shape[ax]
+        # Check that this is not downscaling
+        if upscale_factors[ax] < 1:
+            raise ValueError(info)
+    info = f"{info} Upscale factors: {upscale_factors}"
+
+    # Raise a warning if upscaling is non-homogeneous across all axis
+    if len(set(upscale_factors)) > 1:
+        warnings.warn(info)
+
+    # Upscale array, via np.repeat
+    upscaled_array = array
+    for ax in axis:
+        upscaled_array = np.repeat(
+            upscaled_array, upscale_factors[ax], axis=ax
+        )
+
+    # Check that final shape is correct
+    if not upscaled_array.shape == target_shape:
+        raise Exception(f"{info} {upscaled_array.shape=}.")
+
+    return upscaled_array
