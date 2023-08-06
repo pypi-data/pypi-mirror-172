9ad226a41934f0451c0ff802da0f5195533397b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:57 +0000

    Add package tvb-ext-xircuits/0.1a1/tvb-ext-xircuits-0.1a1.tar.gz

---
+++ packages/tvb-ext-xircuits/tvb-ext-xircuits-0.1a1.tar.gz/tvb-ext-xircuits-0.1a1/xai_components/xai_tvb_simulator/simulator.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+#
+# "TheVirtualBrain - Widgets" package
+#
+# (c) 2022-2023, TVB Widgets Team
+#
+from tvb.simulator.backend.templates import MakoUtilMix
+from tvb.datatypes.connectivity import Connectivity
+from tvb.datatypes.cortex import Cortex
+from tvb.datatypes.patterns import SpatioTemporalPattern
+from tvb.simulator.coupling import Coupling
+from tvb.simulator.integrators import Integrator
+from tvb.simulator.models.base import Model
+
+from xai_components.base import InArg, OutArg, Component, xai_component
+from xai_components.utils import print_component_summary, set_defaults, set_values
+
+
+@xai_component(color='rgb(220, 5, 45)')
+class Simulator(Component):
+    from tvb.simulator.simulator import Simulator
+    connectivity: InArg[Connectivity]
+    conduction_speed: InArg[float]
+    coupling: InArg[Coupling]
+    surface: InArg[Cortex]
+    stimulus: InArg[SpatioTemporalPattern]
+    model: InArg[Model]
+    integrator: InArg[Integrator]
+    initial_conditions: InArg[list]
+    monitors: InArg[list]
+    simulation_length: InArg[float]
+    backend: InArg[MakoUtilMix]
+
+    time_series_list: OutArg[list]
+
+    def __init__(self):
+        set_defaults(self, self.Simulator)
+        self.backend = InArg(None)
+        self.time_series = OutArg(None)
+
+    def execute(self, ctx) -> None:
+        # imports
+        from tvb.simulator.backend.nb_mpr import NbMPRBackend
+
+        simulator = self.Simulator()
+        set_values(self, simulator)
+        simulator.configure()
+
+        print_component_summary(simulator)
+
+        # run simulation
+        backend = self.backend.value
+        if isinstance(backend, NbMPRBackend):
+            result = backend.run_sim(simulator, simulation_length=simulator.simulation_length)
+        else:
+            result = simulator.run()
+
+        # create TS
+        ts_list = []
+        for i in range(len(simulator.monitors)):
+            monitor = simulator.monitors[i]
+            time, data = result[i]
+            ts = monitor.create_time_series(connectivity=simulator.connectivity)
+            ts.data = data
+            ts.time = time
+            ts.configure()
+            ts_list.append(ts)
+
+        for ts in ts_list:
+            print_component_summary(ts)
+
