a578170e3e44203e5e06ae745789d25acb1b014a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:25 +0000

    Add package stag/0.2.2/stag-0.2.2.tar.gz

---
+++ packages/stag/stag-0.2.2.tar.gz/stag-0.2.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from distutils.core import setup, Extension
+
+VERSION = '0.2.2'
+DESCRIPTION = 'STAG: Spectral Toolkit of Algorithms for Graphs'
+LONG_DESCRIPTION =\
+    "This library provides several methods and algorithms relating to spectral graph theory in python."
+URL = "https://staglibrary.io"
+
+# specify the name of the extension and source files
+# required to compile this
+ext_modules = [Extension(name='stag._stag_internal',
+                         sources=["stag/stag_internal_wrap.cxx",
+                                  "stag/stag_lib/graph.cpp",
+                                  "stag/stag_lib/random.cpp",
+                                  "stag/stag_lib/graphio.cpp",
+                                  "stag/stag_lib/cluster.cpp",
+                                  "stag/stag_lib/utility.cpp"],
+                         include_dirs=["stag/eigen-3.3.9", "stag/stag_lib"],
+                         extra_compile_args=['-std=c++2a'])]
+
+# Setting up
+setup(
+    name="stag",
+    ext_modules=ext_modules,
+    version=VERSION,
+    author="Peter Macgregor",
+    author_email="<macgregor.pr@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["scipy", "networkx", "matplotlib"],
+    long_description_content_type='text/markdown',
+    url=URL,
+    include_package_data=True,
+
+    keywords=['python', 'spectral', 'graph', 'algorithms', 'clustering', 'cheeger'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        # "Operating System :: MacOS :: MacOS X",
+        # "Operating System :: Microsoft :: Windows",
+        'Operating System :: POSIX :: Linux'
+    ]
+)
