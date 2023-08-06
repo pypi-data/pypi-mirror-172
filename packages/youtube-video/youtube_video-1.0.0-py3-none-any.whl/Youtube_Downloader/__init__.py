09f560ae0afd77265df1aa01c85453415d156e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:26 +0000

    Add package youtube-video/1.0.0/youtube_video-1.0.0-py3-none-any.whl

---
+++ packages/youtube-video/youtube_video-1.0.0-py3-none-any.whl/Youtube_Downloader/__init__.py
@@ -0,0 +1,8 @@
+# from Youtube_Downloader.streaming import StreamingServer
+# from Youtube_Downloader.streaming import CameraClient
+# from Youtube_Downloader.streaming import VideoClient
+# from Youtube_Downloader.streaming import ScreenShareClient
+# from Youtube_Downloader.youtube import AudioSender
+# from Youtube_Downloader.youtube import AudioReceiver
+
+from Youtube_Downloader.youtube import download
