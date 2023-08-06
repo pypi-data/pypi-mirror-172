e6196dc541e47140b49d116bf3b9295bab070779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:21 +0000

    Add package thoughtful/1.4.0/thoughtful-1.4.0.tar.gz

---
+++ packages/thoughtful/thoughtful-1.4.0.tar.gz/thoughtful-1.4.0/supervisor/__init__.py
@@ -0,0 +1,5 @@
+# isort: skip_file due to the importance of the python parsing order of these
+# modules
+
+from .__version__ import __version__ as __version__
+from supervisor.default_instances import *
