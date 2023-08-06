10793b1a9c5f9c69f4db53d4386d33fe38b4c6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:32 +0000

    Add package beyondml/0.1.2/beyondml-0.1.2-py3-none-any.whl

---
+++ packages/beyondml/beyondml-0.1.2-py3-none-any.whl/beyondml/pt/layers/MultiConv2D.py
@@ -0,0 +1,65 @@
+import torch
+
+
+class MultiConv2D(torch.nn.Module):
+    """
+    Multi- 2D Convolutional layer initialized with weights rather than hyperparameters
+    """
+
+    def __init__(
+            self,
+            kernel,
+            bias,
+            padding='same',
+            strides=1
+    ):
+        """
+        Parameters
+        ----------
+        kernel : torch.Tensor or Tensor-like
+            The kernel tensor to use
+        bias : torch.Tensor or Tensor-like
+            The bias matrix to use
+        padding : str or int (default 'same')
+            The padding to use
+        strides : int or tuple (default 1)
+            The strides to use
+        """
+        super().__init__()
+        self.w = torch.nn.Parameter(torch.Tensor(kernel))
+        self.b = torch.nn.Parameter(torch.Tensor(bias))
+        self.padding = padding
+        self.strides = strides
+
+    def forward(
+            self,
+            inputs
+    ):
+        """
+        Call the layer on input data
+
+        Parameters
+        ----------
+        inputs : torch.Tensor
+            Inputs to call the layer's logic on
+
+        Returns
+        -------
+        results : torch.Tensor
+            The results of the layer's logic
+        """
+        outputs = []
+        kernel = self.w
+        bias = self.b
+
+        for i in range(len(inputs)):
+            outputs.append(
+                torch.nn.functional.conv2d(
+                    inputs[i],
+                    kernel[i],
+                    bias[i],
+                    stride=self.strides,
+                    padding=self.padding
+                )
+            )
+        return outputs
---
+++ packages/beyondml/beyondml-0.1.2-py3-none-any.whl/beyondml/pt/layers/SparseMultiConv2D.py
@@ -0,0 +1,65 @@
+import torch
+
+
+class SparseMultiConv2D(torch.nn.Module):
+    """
+    Sparse implementation of a Multi 2D Convolutional layer
+    """
+
+    def __init__(
+            self,
+            kernel,
+            bias,
+            padding='same',
+            strides=1
+    ):
+        """
+        Parameters
+        ----------
+        kernel : torch.Tensor or Tensor-like
+            The kernel to use
+        bias : torch.Tensor or Tensor-like
+            The bias to use
+        padding : str or int (default 'same')
+            The padding to use
+        strides : int or tuple (default 1)
+            The padding to use
+        """
+        super().__init__()
+        self.w = torch.Tensor(kernel).to_sparse()
+        self.b = torch.Tensor(bias).to_sparse()
+        self.padding = padding
+        self.strides = strides
+
+    def forward(
+            self,
+            inputs
+    ):
+        """
+        Call the layer on input data
+
+        Parameters
+        ----------
+        inputs : torch.Tensor
+            Inputs to call the layer's logic on
+
+        Returns
+        -------
+        results : torch.Tensor
+            The results of the layer's logic
+        """
+        outputs = []
+        kernel = self.w.to_dense()
+        bias = self.b.to_dense()
+
+        for i in range(len(inputs)):
+            outputs.append(
+                torch.nn.functional.conv2d(
+                    inputs[i],
+                    kernel[i],
+                    bias[i],
+                    stride=self.strides,
+                    padding=self.padding
+                )
+            )
+        return outputs
