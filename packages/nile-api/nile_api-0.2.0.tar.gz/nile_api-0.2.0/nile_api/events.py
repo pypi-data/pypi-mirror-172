c573d3bd7da37acb74b26e856f7106cf475d1904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:00 +0000

    Add package nile-api/0.2.0/nile_api-0.2.0.tar.gz

---
+++ packages/nile-api/nile_api-0.2.0.tar.gz/nile_api-0.2.0/nile_api/events.py
@@ -0,0 +1,32 @@
+"""
+Support for the nile events API.
+
+This file is not automatically generated from the OpenAPI spec.
+"""
+
+import asyncio
+
+from nile_api.client import Client
+from nile_api.api.entities import instance_events
+
+
+async def on(
+    *,
+    client: Client,
+    workspace: str,
+    type: str,
+    seq: int,
+    refresh=5000,
+):
+    while True:
+        events = await instance_events.asyncio(
+            client=client,
+            type=type,
+            workspace=workspace,
+            seq=seq,
+        )
+        for event in events:
+            yield event
+            print(event.after)
+            seq = event.after.seq
+        await asyncio.sleep(refresh)
