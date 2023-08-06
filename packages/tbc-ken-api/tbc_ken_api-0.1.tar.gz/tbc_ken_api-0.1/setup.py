bf731f9f48b757a6a7f7b08364663fb82679a755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:20 +0000

    Add package tbc-ken-api/0.1/tbc_ken_api-0.1.tar.gz

---
+++ packages/tbc-ken-api/tbc_ken_api-0.1.tar.gz/tbc_ken_api-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="tbc_ken_api",
+version='0.1',
+description="Trading Bot Club alpaca api",
+author="Kris Luangpenthong",
+packages=['ken_api'],
+zip_safe=False)
