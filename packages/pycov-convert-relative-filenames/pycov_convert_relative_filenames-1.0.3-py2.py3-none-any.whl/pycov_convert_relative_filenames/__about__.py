c8c7f03f5303eef42fd93b8f5693507976ad7d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:36 +0000

    Add package pycov-convert-relative-filenames/1.0.3/pycov_convert_relative_filenames-1.0.3-py2.py3-none-any.whl

---
+++ packages/pycov-convert-relative-filenames/pycov_convert_relative_filenames-1.0.3-py2.py3-none-any.whl/pycov_convert_relative_filenames/__about__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "__author__",
+    "__copyright__",
+    "__email__",
+    "__license__",
+    "__summary__",
+    "__title__",
+    "__uri__",
+    "__version__",
+]
+
+__title__ = "pycov-convert-relative-filenames"
+__summary__ = "Quick and dirty coverage.xml file converter for GitLab CI"
+__uri__ = "https://gitlab.com/jlecomte/projects/python/pycov-convert-relative-filenames"
+
+__version__ = "1.0.3"
+
+__author__ = "Julien Lecomte"
+__email__ = "julien@lecomte.at"
+
+__license__ = "MIT"
+__copyright__ = f"2022-2022 {__author__}"
