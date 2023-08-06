6059551a1e0041dcd17acd902e9fc7f1fcd373b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:31 +0000

    Add package PyOfficeRobot/0.0.11/PyOfficeRobot-0.0.11-py3-none-any.whl

---
+++ packages/PyOfficeRobot/PyOfficeRobot-0.0.11-py3-none-any.whl/tests/test_file.py
@@ -0,0 +1,19 @@
+import unittest
+
+from PyOfficeRobot.api.chat import *
+from PyOfficeRobot.api.file import *
+
+keywords = {
+    '你好': "在干嘛？"
+}
+
+
+class TestFile(unittest.TestCase):
+    def test_send_file(self):
+        send_file(who='文件传输助手', file=r'./test_files/0816.jpg')
+
+    def test_chat_by_keyword(self):
+        chat_by_keywords(who='兆锋', keywords=keywords)
+
+    def test_receive_message(self):
+        receive_message(who='程序员晚枫')
