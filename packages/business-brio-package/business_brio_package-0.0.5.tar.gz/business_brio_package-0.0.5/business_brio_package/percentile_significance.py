991bbe09263f3ccc6c97f4dc049f61d8bd49fb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:32 +0000

    Add package business-brio-package/0.0.5/business_brio_package-0.0.5.tar.gz

---
+++ packages/business-brio-package/business_brio_package-0.0.5.tar.gz/business_brio_package-0.0.5/business_brio_package/percentile_significance.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+class test():
+  def __init__(self,x,y,percentile):
+    self.x=x
+    self.y=y
+    self.percentile=percentile
+    df1 = pd.DataFrame()
+    df2 = pd.DataFrame()
+    cutoff=self.y.quantile(self.percentile)
+    probs1=self.y
+    y_pred1=np.where(probs1>=cutoff,"greater_equal_percentile","smaller_percentile")
+    df2 = pd.DataFrame(y_pred1)
+    df1["y"]=df2[0]
+    df1["x"]=self.x
+    table=pd.crosstab(df1["y"],df1["x"]) 
+    self.table=pd.DataFrame(table)
+    odds,p=stats.fisher_exact(table)
+    self.odds_value=odds
+    self.p=p
+  def result(self):
+    return print(f"\ncontigency table:\n{self.table} \np value:{self.p} \nodds value:\n{self.odds}")
+  def pvalue(self):
+    return print(self.p)
+  def odds(self):
+    return print(self.odds_value)
+  def cont_table(self):
+    return print(self.table)
---
+++ packages/business-brio-package/business_brio_package-0.0.5.tar.gz/business_brio_package-0.0.5/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+
+long_description1 = """
+                      This package contains several submodules which can be directly used after installing the package.
+
+                      You can find the details of our package from:
+
+                      https://github.com/Business-Brio/MIT_Open_license.git
+
+
+                    """
+
+setup(name='business_brio_package',
+      packages = ['business_brio_package'],
+      version='0.0.5',
+      description='A package contains several submodules and functions',
+      long_description= long_description1,
+      url='https://github.com/Business-Brio/MIT_Open_license.git',
+      download_url = 'https://github.com/Business-Brio/MIT_Open_license/archive/0.0.1.tar.gz',
+      author='business_brio',
+      
+      keywords = ['chi_test',],
+      license='MIT',
+
+      install_requires=['pandas','numpy','scipy',],
+  
+
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License', # Your License Here  
+        'Programming Language :: Python :: 3',    # List Python versions that you support Here  
+        'Programming Language :: Python :: 3.4',
+        ],
+)
