1f9b1c0d944c3c96805900c380b858d7e2e8279a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:24 +0000

    Add package converter-example-by-thuongnn-v1/0.0.1/converter-example-by-thuongnn-v1-0.0.1.tar.gz

---
+++ packages/converter-example-by-thuongnn-v1/converter-example-by-thuongnn-v1-0.0.1.tar.gz/converter-example-by-thuongnn-v1-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "0.0.1"
+
+setuptools.setup(
+    name="converter-example-by-thuongnn-v1",
+    version=__version__,
+    author="Nguyen Nhu Thuong",
+    author_email="thuongnn@ssi.com.vn",
+    description="Sort description",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/thuongnn/github-actions-learning-python.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
