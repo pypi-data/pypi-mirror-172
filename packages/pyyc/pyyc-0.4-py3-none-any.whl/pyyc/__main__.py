dd64620d137ec21711ba60a030baf6274aacf37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:12 +0000

    Add package pyyc/0.4/pyyc-0.4-py3-none-any.whl

---
+++ packages/pyyc/pyyc-0.4-py3-none-any.whl/pyyc/__main__.py
@@ -0,0 +1,36 @@
+import sys
+
+def main():                 # Will be used as entry-point in setup.cfg
+    """
+    Main function of the package.
+
+    Read arguments from `sys.argv[1:]`.
+
+    .. Note:: Will be used as `pyyc` entry-point in :ref:`setup.cfg`,
+              and as the main script (`python -m pyyc`).
+    """
+
+    print("Command line arguments:", sys.argv[1:])
+
+
+def main_addition():
+    """
+    Another entry point of the package.
+
+    Read arguments from `sys.argv[1:]`.
+
+    .. Note:: Will be used as `pyyc_addition` entry-point in
+              :ref:`setup.cfg`.
+    """
+
+    try:
+        iargs = [ int(arg) for arg in sys.argv[1:] ]
+    except ValueError:
+        print("Only integers accepted as command-line arguments:", sys.argv[1:])
+
+    print(" + ".join([ str(arg) for arg in iargs ]), "=", sum(iargs))
+
+
+if __name__ == '__main__':  # Will be used by `python -m pyyc`
+
+    main()
