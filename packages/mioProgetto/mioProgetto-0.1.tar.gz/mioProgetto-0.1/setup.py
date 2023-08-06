a7c886a71b1698d06d00c3b1f27379bb0bef2c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:49 +0000

    Add package mioProgetto/0.1/mioProgetto-0.1.tar.gz

---
+++ packages/mioProgetto/mioProgetto-0.1.tar.gz/mioProgetto-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mioProgetto",
+    version="0.1",
+    description="mioProgetto",
+    long_description="mioProgetto",
+    long_description_content_type='text/x-rst',
+    url="https://gitlab.com/magni5/mioProgetto.git",
+    author="magni5",
+    packages=find_packages(),
+    python_requires='>=3',
+)
