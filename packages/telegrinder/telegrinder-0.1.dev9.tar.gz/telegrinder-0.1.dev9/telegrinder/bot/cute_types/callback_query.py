b5d302f75e37f0f86bda32ff79e84ad4ae9a8f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:57 +0000

    Add package telegrinder/0.1.dev9/telegrinder-0.1.dev9.tar.gz

---
+++ packages/telegrinder/telegrinder-0.1.dev9.tar.gz/telegrinder-0.1.dev9/telegrinder/bot/cute_types/callback_query.py
@@ -0,0 +1,28 @@
+from telegrinder.types import CallbackQuery, User
+from telegrinder.types.methods import APIMethods
+from telegrinder.api import API, APIError, Token
+from telegrinder.result import Result
+import typing
+
+
+class CallbackQueryCute(CallbackQuery):
+    api: API
+
+    @property
+    def from_user(self) -> User:
+        return self.from_
+
+    @property
+    def ctx_api(self) -> API:
+        return self.api
+
+    async def answer(
+        self,
+        text: typing.Optional[str] = None,
+        show_alert: typing.Optional[bool] = None,
+        url: typing.Optional[str] = None,
+        cache_time: typing.Optional[int] = None,
+        **other
+    ) -> Result[bool, APIError]:
+        params = APIMethods.get_params(locals())
+        return await self.ctx_api.answer_callback_query(self.id, **params)
