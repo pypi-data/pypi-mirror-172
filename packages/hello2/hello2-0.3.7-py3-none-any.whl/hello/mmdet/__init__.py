08a3da4069248140db23479757e51cc704ea9ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:42 +0000

    Add package hello2/0.3.7/hello2-0.3.7-py3-none-any.whl

---
+++ packages/hello2/hello2-0.3.7-py3-none-any.whl/hello/mmdet/__init__.py
@@ -0,0 +1,35 @@
+import sys
+
+help_doc_str = """usage: hello-mmdet <command> [options]
+
+Commands:
+    export
+    infer
+    log
+"""
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    if len(args) >= 1:
+        command, *args = args
+    else:
+        command, *args = ["--help"]
+
+    args = ["--help"] if len(args) == 0 else args
+
+    if command == "export":
+        from hello.mmdet.export import main as _main
+        _main(args)
+    elif command == "infer":
+        from hello.mmdet.infer import main as _main
+        _main(args)
+    elif command == "log":
+        from hello.mmdet.log import main as _main
+        _main(args)
+    else:
+        print(help_doc_str)
+
+    return 0
