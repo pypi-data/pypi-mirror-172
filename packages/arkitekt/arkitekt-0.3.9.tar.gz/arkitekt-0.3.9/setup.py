eae07125f5392ab5eaa445a0b709ae1d009030a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:35 +0000

    Add package arkitekt/0.3.9/arkitekt-0.3.9.tar.gz

---
+++ packages/arkitekt/arkitekt-0.3.9.tar.gz/arkitekt-0.3.9/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['arkitekt',
+ 'arkitekt.apps',
+ 'arkitekt.cli',
+ 'arkitekt.cli.dev',
+ 'arkitekt.cli.prod',
+ 'arkitekt.qt']
+
+package_data = \
+{'': ['*'], 'arkitekt.qt': ['assets/dark/*', 'assets/light/*']}
+
+install_requires = \
+['fakts>=0.2.11,<0.3.0',
+ 'herre>=0.2.7,<0.3.0',
+ 'mikro>=0.2.24,<0.3.0',
+ 'rekuest>=0.0.18,<0.0.19']
+
+entry_points = \
+{'console_scripts': ['arkitekt = arkitekt.cli.main:entrypoint']}
+
+setup_kwargs = {
+    'name': 'arkitekt',
+    'version': '0.3.9',
+    'description': 'rpc and node backbone',
+    'long_description': 'None',
+    'author': 'jhnnsrs',
+    'author_email': 'jhnnsrs@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
