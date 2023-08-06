c042db08f7bcbb6558a665b8693c5de8981e3236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:22 +0000

    Add package funkman/0.6.2/funkman-0.6.2.tar.gz

---
+++ packages/funkman/funkman-0.6.2.tar.gz/funkman-0.6.2/setup.py
@@ -0,0 +1,15 @@
+# Import
+from setuptools import setup, find_packages
+
+# Call setup funtion
+setup(
+    author="funkyfranky",
+    description="Interface between DCS/MOOSE and Discord via an UDP socket.",
+    name="funkman",
+    version="0.6.2",
+    packages=find_packages(include=["funkman", "funkman.*"]),
+    install_requires=[
+        'discord>=1.7.3',
+        'matplotlib',
+        'numpy']
+)
