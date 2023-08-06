a9a2465be9768789d628b1566ffd489477fd5f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:11 +0000

    Add package invagination/0.0.2/invagination-0.0.2-py3-none-any.whl

---
+++ packages/invagination/invagination-0.0.2-py3-none-any.whl/invagination/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM invagination SETUP.PY
+#
+short_version = '0.0.2'
+version = '0.0.2'
+full_version = '0.0.2'
+git_revision = '03d0a47035693f565ec91fa56a703ab4e38efdd5'
+release = True
+if not release:
+    version = full_version
