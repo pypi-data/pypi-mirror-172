42445dff6c6c2864b85169df7a4ca2cb3e05d96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:19 +0000

    Add package mypackageSAU1/1.0/mypackageSAU1-1.0.tar.gz

---
+++ packages/mypackageSAU1/mypackageSAU1-1.0.tar.gz/mypackageSAU1-1.0/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+name='mypackageSAU1', # name of the library
+version='1.0', 
+description='description', #
+packages = find_packages(),
+author='author', # 
+author_email='123@gmail.com',
+)
