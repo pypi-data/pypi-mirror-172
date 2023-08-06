8e31a46e941360bfcb5673c35938381a44bcdb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:11 +0000

    Add package investiny/0.7.2/investiny-0.7.2-py3-none-any.whl

---
+++ packages/investiny/investiny-0.7.2-py3-none-any.whl/investiny/__init__.py
@@ -0,0 +1,8 @@
+"""`investiny `: `investpy` but made tiny."""
+
+__author__ = "Alvaro Bartolome, alvarobartt @ GitHub"
+__version__ = "0.7.2"
+
+from investiny.historical import historical_data  # noqa: F401
+from investiny.info import info  # noqa: F401
+from investiny.search import search_assets  # noqa: F401
