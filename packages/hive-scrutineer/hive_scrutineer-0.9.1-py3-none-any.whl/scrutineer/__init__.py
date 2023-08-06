7b354f10c852b9a7f68a04e4b1f6d88e0891a52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:23 +0000

    Add package hive-scrutineer/0.9.1/hive_scrutineer-0.9.1-py3-none-any.whl

---
+++ packages/hive-scrutineer/hive_scrutineer-0.9.1-py3-none-any.whl/scrutineer/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+"""
+Hive Posts Scutineer
+~~~~~~~~~~~~~~~~
+
+Performance and quality analytics on Hive Posts.
+
+"""
+
+__version__ = "0.9.1"
+from .scrutineer import Scrutineer
+
+__all__ = ["scrutineer"]
