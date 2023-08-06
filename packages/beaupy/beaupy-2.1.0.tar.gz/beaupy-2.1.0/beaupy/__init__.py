71cae07ca8a0fac1a8bfb81ef574d668acbad45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:18 +0000

    Add package beaupy/2.1.0/beaupy-2.1.0.tar.gz

---
+++ packages/beaupy/beaupy-2.1.0.tar.gz/beaupy-2.1.0/beaupy/__init__.py
@@ -0,0 +1,13 @@
+import beaupy.key as key  # noqa
+import beaupy.spinners as spinners  # noqa
+from beaupy._beaupy import (  # noqa
+    Config,
+    ConversionError,
+    DefaultKeys,
+    ValidationError,
+    confirm,
+    console,
+    prompt,
+    select,
+    select_multiple,
+)
