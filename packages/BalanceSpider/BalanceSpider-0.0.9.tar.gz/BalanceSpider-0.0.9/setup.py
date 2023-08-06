3e712319355831a574c1d545007e7052f279bc4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package BalanceSpider/0.0.9/BalanceSpider-0.0.9.tar.gz

---
+++ packages/BalanceSpider/BalanceSpider-0.0.9.tar.gz/BalanceSpider-0.0.9/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BalanceSpider",
+    version="0.0.9",
+    author="vSir",
+    author_email="weiguo341@gmail.com",
+    description="simple tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nevquit/monitor_msg_tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    'lxml==4.5.0','requests'
+    ]
+)
