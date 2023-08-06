1a1270950d02d2395ae26a8d716cf77f8342e5c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:43 +0000

    Add package urmapi/1.0.2/urmapi-1.0.2.tar.gz

---
+++ packages/urmapi/urmapi-1.0.2.tar.gz/urmapi-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as r:
+    requirements = r.read().split("\n")
+    r.close()
+
+setup(
+   name='urmapi',
+   version='1.0.2',
+   description='A Decentralized Package Manager.',
+   license="MIT",
+   author='NoKodaAddictions',
+   author_email='nokodaaddictions@gmail.com',
+   url="https://nokodaaddictions.github.io",
+   packages=find_packages(),
+   install_requires=requirements
+)
+#2022 NoKodaAddictions
