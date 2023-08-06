88c8895203527c4258d6fbf2be596fc5f5990b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:42 +0000

    Add package wxc-sdk/1.7.1/wxc_sdk-1.7.1-py3-none-any.whl

---
+++ packages/wxc-sdk/wxc_sdk-1.7.1-py3-none-any.whl/wxc_sdk/person_settings/common.py
@@ -0,0 +1,43 @@
+from ..api_child import ApiChild
+from ..rest import RestSession
+
+__all__ = ['PersonSettingsApiChild']
+
+
+class PersonSettingsApiChild(ApiChild, base=''):
+    """
+    Base class for all classes implementing person settings APIs
+    """
+
+    feature = None
+
+    def __init__(self, *, session: RestSession,
+                 workspaces: bool = False, locations: bool = False):
+        self.feature_prefix = '/features/'
+        if workspaces:
+            self.selector = 'workspaces'
+        elif locations:
+            self.selector = 'telephony/config/locations'
+            self.feature_prefix = '/'
+        else:
+            self.selector = 'people'
+        super().__init__(session=session, base=self.selector)
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(base='')
+        if cls.feature is None:
+            raise TypeError('feature has to be defined')
+
+    def f_ep(self, person_id: str, path: str = None) -> str:
+        """
+        person specific feature endpoint like v1/people/{uid}/features/....
+
+        :param person_id: Unique identifier for the person.
+        :type person_id: str
+        :param path: path in the endpoint after the feature base URL
+        :type path: str
+        :return: full endpoint
+        :rtype: str
+        """
+        path = path and f'/{path}' or ''
+        return self.session.ep(f'{self.selector}/{person_id}{self.feature_prefix}{self.feature}{path}')
