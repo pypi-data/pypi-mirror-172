52d5b04d3f169ecc5fd3192f9a4184379e06d38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:35 +0000

    Add package dkist-processing-test/0.0.76/dkist-processing-test-0.0.76.tar.gz

---
+++ packages/dkist-processing-test/dkist-processing-test-0.0.76.tar.gz/dkist-processing-test-0.0.76/dkist_processing_test/tasks/quality.py
@@ -0,0 +1,18 @@
+from typing import List
+from typing import Optional
+
+from dkist_processing_common.models.tags import Tag
+from dkist_processing_common.tasks import QualityL0Metrics
+from dkist_processing_common.tasks import SubmitQuality
+
+
+class TestQualityL0Metrics(QualityL0Metrics):
+    def run(self) -> None:
+        paths = self.read(tags=[Tag.input()])
+        self.calculate_l0_metrics(paths=paths)
+
+
+class TestSubmitQuality(SubmitQuality):
+    @property
+    def polcal_label_list(self) -> Optional[List[str]]:
+        return ["Beam1"]
