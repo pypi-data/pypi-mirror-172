89886f37a66e8eb50c86eeb0b4d71e68eeb5b367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:47 +0000

    Add package searchin/0.1.6/searchin-0.1.6.tar.gz

---
+++ packages/searchin/searchin-0.1.6.tar.gz/searchin-0.1.6/setup.py
@@ -0,0 +1,35 @@
+"""Setup.py for Pypi.org"""
+from distutils.core import setup
+
+setup(
+    name='searchin',
+    packages=['searchin'],
+    version='0.1.6',
+    description='Search anything in a live python object, method or variable!',
+    author='Nicolas Micaux',  # Type in your name
+    # author_email='your.email@domain.com',  # Type in your E-Mail
+    url='https://github.com/NicolasMICAUX/searchin',
+    # download_url='https://github.com/NicolasMICAUX/searchin/archive/refs/tags/v0.1.5.tar.gz',
+    keywords=['SEARCH', 'DEBUG'],
+    install_requires=[],  # None
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+)
