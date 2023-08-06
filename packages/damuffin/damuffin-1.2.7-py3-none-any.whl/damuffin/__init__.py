b3cc0f486b2c07eaf5861e99eb9d76a4cbbe123d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:25 +0000

    Add package damuffin/1.2.7/damuffin-1.2.7-py3-none-any.whl

---
+++ packages/damuffin/damuffin-1.2.7-py3-none-any.whl/damuffin/__init__.py
@@ -0,0 +1,4 @@
+from .damuffin import TemporaryDirectory, CompressionObject, DaMuffinCompress, DaMuffinDecompress, DaMuffinZip
+from .sysinfo import sysinfo
+from .net import net
+from .hector import hector
