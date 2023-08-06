9e7c584e82d2e56dd2c08dd6a32a359278ae9506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:58 +0000

    Add package txmc/1.0.1/txmc-1.0.1.tar.gz

---
+++ packages/txmc/txmc-1.0.1.tar.gz/txmc-1.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="txmc",
+    version="1.0.1",
+    author="童心少年",
+    author_email="363488350@qq.com",
+    description="我的编程世界Python组件",
+    long_description='通过txmc实现在我的世界中畅游',
+    long_description_content_type="text/markdown",
+    url="https://github.com/tanxinyue/multable",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
