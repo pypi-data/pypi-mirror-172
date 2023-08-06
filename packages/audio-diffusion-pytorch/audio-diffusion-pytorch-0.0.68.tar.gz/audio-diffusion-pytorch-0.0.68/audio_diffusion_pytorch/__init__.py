2c7398b735e36c17717f9cc15ca91390e81ead7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:55 +0000

    Add package audio-diffusion-pytorch/0.0.68/audio-diffusion-pytorch-0.0.68.tar.gz

---
+++ packages/audio-diffusion-pytorch/audio-diffusion-pytorch-0.0.68.tar.gz/audio-diffusion-pytorch-0.0.68/audio_diffusion_pytorch/__init__.py
@@ -0,0 +1,38 @@
+from .diffusion import (
+    ADPM2Sampler,
+    AEulerSampler,
+    Diffusion,
+    DiffusionInpainter,
+    DiffusionSampler,
+    Distribution,
+    KarrasSampler,
+    KarrasSchedule,
+    KDiffusion,
+    LinearSchedule,
+    LogNormalDistribution,
+    Sampler,
+    Schedule,
+    SpanBySpanComposer,
+    UniformDistribution,
+    VDiffusion,
+    VKDiffusion,
+    VKDistribution,
+    VSampler,
+)
+from .model import (
+    AudioDiffusionAutoencoder,
+    AudioDiffusionConditional,
+    AudioDiffusionModel,
+    AudioDiffusionUpsampler,
+    DiffusionAutoencoder1d,
+    DiffusionUpsampler1d,
+    Model1d,
+)
+from .modules import (
+    AutoEncoder1d,
+    MultiEncoder1d,
+    T5Embedder,
+    UNet1d,
+    UNetConditional1d,
+    Variational,
+)
