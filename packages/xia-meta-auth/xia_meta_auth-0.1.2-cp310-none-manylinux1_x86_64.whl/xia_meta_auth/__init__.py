a9af00dc2c2efc08e0a8daa5156f872435132f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:55 +0000

    Add package xia-meta-auth/0.1.2/xia_meta_auth-0.1.2-cp310-none-manylinux1_x86_64.whl

---
+++ packages/xia-meta-auth/xia_meta_auth-0.1.2-cp310-none-manylinux1_x86_64.whl/xia_meta_auth/__init__.py
@@ -0,0 +1,12 @@
+from xia_meta_auth.acl import Acl, AclItem
+from xia_meta_auth.user import User
+from xia_meta_auth.permission import Permission
+
+
+__all__ = [
+    "Acl", "AclItem",
+    "User",
+    "Permission",
+]
+
+__version__ = "0.1.2"
