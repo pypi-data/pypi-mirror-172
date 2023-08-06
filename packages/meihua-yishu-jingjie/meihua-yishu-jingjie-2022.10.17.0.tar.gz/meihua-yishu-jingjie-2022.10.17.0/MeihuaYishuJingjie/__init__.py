87c7b3fafbbf34d702e5a4c13f7415caff717b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:34 +0000

    Add package meihua-yishu-jingjie/2022.10.17.0/meihua-yishu-jingjie-2022.10.17.0.tar.gz

---
+++ packages/meihua-yishu-jingjie/meihua-yishu-jingjie-2022.10.17.0.tar.gz/meihua-yishu-jingjie-2022.10.17.0/MeihuaYishuJingjie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""meihua-yishu-jingjie
+https://github.com/apachecn/meihua-yishu-jingjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.17.0"
