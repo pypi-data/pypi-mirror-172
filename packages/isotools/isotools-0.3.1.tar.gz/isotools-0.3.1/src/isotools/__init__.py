db43ec6b1bfffa7e215f61dae8967f1be014f014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:51 +0000

    Add package isotools/0.3.1/isotools-0.3.1.tar.gz

---
+++ packages/isotools/isotools-0.3.1.tar.gz/isotools-0.3.1/src/isotools/__init__.py
@@ -0,0 +1,16 @@
+
+try:
+    from importlib.metadata import distribution
+except ModuleNotFoundError:
+    from importlib_metadata import distribution  # py3.7
+__version__ = distribution('isotools').version
+from .gene import Gene
+from .transcriptome import Transcriptome
+from .splice_graph import SegmentGraph, SegGraphNode
+from ._transcriptome_stats import estimate_tpm_threshold
+
+from ._transcriptome_filter import DEFAULT_GENE_FILTER, DEFAULT_TRANSCRIPT_FILTER, DEFAULT_REF_TRANSCRIPT_FILTER, ANNOTATION_VOCABULARY
+
+
+__all__ = ['Transcriptome', 'Gene', 'SegmentGraph', 'SegGraphNode', 'estimate_tpm_threshold',
+           'DEFAULT_GENE_FILTER', 'DEFAULT_TRANSCRIPT_FILTER', 'DEFAULT_REF_TRANSCRIPT_FILTER', 'ANNOTATION_VOCABULARY']
