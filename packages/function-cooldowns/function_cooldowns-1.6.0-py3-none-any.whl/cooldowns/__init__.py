c6c0ac34d8ea236b0cc446aa1bedf292b8397662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:53 +0000

    Add package function-cooldowns/1.6.0/function_cooldowns-1.6.0-py3-none-any.whl

---
+++ packages/function-cooldowns/function_cooldowns-1.6.0-py3-none-any.whl/cooldowns/__init__.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+
+from .buckets import CooldownBucket, SlashBucket
+from .protocols import CooldownBucketProtocol
+from .cooldown import Cooldown, cooldown, shared_cooldown
+from .cooldown_times_per import CooldownTimesPer
+from .exceptions import (
+    CallableOnCooldown,
+    NoRegisteredCooldowns,
+    UnknownBucket,
+    CooldownAlreadyExists,
+)
+from .utils import (
+    get_remaining_calls,
+    reset_cooldown,
+    reset_cooldowns,
+    reset_bucket,
+    get_cooldown,
+    define_shared_cooldown,
+    get_shared_cooldown,
+)
+
+__all__ = (
+    "CooldownBucket",
+    "SlashBucket",
+    "Cooldown",
+    "cooldown",
+    "shared_cooldown",
+    "CooldownTimesPer",
+    "CooldownBucketProtocol",
+    "CallableOnCooldown",
+    "NoRegisteredCooldowns",
+    "CooldownAlreadyExists",
+    "get_remaining_calls",
+    "UnknownBucket",
+    "reset_cooldowns",
+    "reset_bucket",
+    "reset_cooldown",
+    "get_cooldown",
+    "define_shared_cooldown",
+    "get_shared_cooldown",
+)
+
+__version__ = "1.6.0"
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(major=1, minor=6, micro=0, releaselevel="final", serial=0)
