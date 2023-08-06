cb9ebc2652bdddfb4c809773198088834ad6b3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:04 +0000

    Add package tensor-annotations-tensorflow-stubs/2.0.2/tensor_annotations_tensorflow_stubs-2.0.2-py3-none-any.whl

---
+++ packages/tensor-annotations-tensorflow-stubs/tensor_annotations_tensorflow_stubs-2.0.2-py3-none-any.whl/tensorflow-stubs/train/__init__.pyi
@@ -0,0 +1,10 @@
+from typing import Any
+
+summary_iterator: Any
+AdamOptimizer: Any
+Coordinator: Any
+ExponentialMovingAverage: Any
+MomentumOptimizer: Any
+RMSPropOptimizer: Any
+QueueRunner: Any
+TrackableView: Any
