b5a8e0ee39e661cd24540b945001b1f4080b327d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:00 +0000

    Add package ovos-core/0.0.5a28/ovos_core-0.0.5a28-py3-none-any.whl

---
+++ packages/ovos-core/ovos_core-0.0.5a28-py3-none-any.whl/mycroft/res/ui/translations/customize_settings_de.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>customize_settings</name>
+    <message>
+        <location filename="customize_settings.qml" line="60"/>
+        <source>Customize Settings</source>
+        <translation>Einstellungen anpassen</translation>
+    </message>
+    <message>
+        <location filename="customize_settings.qml" line="264"/>
+        <source>Device Settings</source>
+        <translation>Geräteeinstellungen</translation>
+    </message>
+    <message>
+        <location filename="customize_settings.qml" line="294"/>
+        <source>Create Scheme</source>
+        <translation>Thema erstellen</translation>
+    </message>
+    <message>
+        <location filename="customize_settings.qml" line="399"/>
+        <source>Select Style</source>
+        <translation>Wählen Sie Stil</translation>
+    </message>
+    <message>
+        <location filename="customize_settings.qml" line="468"/>
+        <source>Cancel</source>
+        <translation>Absagen</translation>
+    </message>
+</context>
+</TS>
---
+++ packages/ovos-core/ovos_core-0.0.5a28-py3-none-any.whl/mycroft/res/ui/translations/settingspage_de.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>settingspage</name>
+    <message>
+        <location filename="settingspage.qml" line="19"/>
+        <source>Homescreen Settings</source>
+        <translation>Homescreen-Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="settingspage.qml" line="25"/>
+        <source>Customize</source>
+        <translation>Anpassen</translation>
+    </message>
+    <message>
+        <location filename="settingspage.qml" line="31"/>
+        <source>Display</source>
+        <translation>Anzeige</translation>
+    </message>
+    <message>
+        <location filename="settingspage.qml" line="37"/>
+        <source>Enable SSH</source>
+        <translation>SSH aktivieren</translation>
+    </message>
+    <message>
+        <location filename="settingspage.qml" line="43"/>
+        <source>Developer Settings</source>
+        <translation>Entwicklereinstellungen</translation>
+    </message>
+    <message>
+        <location filename="settingspage.qml" line="49"/>
+        <source>About</source>
+        <translation>Um</translation>
+    </message>
+    <message>
+        <location filename="settingspage.qml" line="68"/>
+        <source>Device Settings</source>
+        <translation>Geräteeinstellungen</translation>
+    </message>
+    <message>
+        <location filename="settingspage.qml" line="180"/>
+        <source>Home</source>
+        <translation>Heim</translation>
+    </message>
+</context>
+</TS>
