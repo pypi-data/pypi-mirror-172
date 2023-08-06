aa5a637d1246de9f4da18dc87b1692bae9c28520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:59 +0000

    Add package photoshow/1.0.7/photoshow-1.0.7-py3-none-any.whl

---
+++ packages/photoshow/photoshow-1.0.7-py3-none-any.whl/photoshow/__init__.py
@@ -0,0 +1,11 @@
+"""
+Package for displaying photos in a full screen slideshow
+"""
+# -----------------------------------------------
+
+from ._photoshow import present
+
+__version__ = '1.0.7'
+
+# -----------------------------------------------
+# End.
