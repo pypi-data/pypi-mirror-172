1c5cd2f51c24d41a2e74127f9874b283cf784c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:05 +0000

    Add package radar-mongodb/0.4.0/radar-mongodb-0.4.0.tar.gz

---
+++ packages/radar-mongodb/radar-mongodb-0.4.0.tar.gz/radar-mongodb-0.4.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['radar_mongodb', 'radar_mongodb.collections']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pymongo==4.2.0']
+
+setup_kwargs = {
+    'name': 'radar-mongodb',
+    'version': '0.4.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Ernesto Martinez del Pino',
+    'author_email': 'ernestomar1997@hotmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
