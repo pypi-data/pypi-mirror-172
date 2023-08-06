7abc2629e292e0eaf08454a6dacd15862d19dbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:58 +0000

    Add package tgcf/0.3/tgcf-0.3-py3-none-any.whl

---
+++ packages/tgcf/tgcf-0.3-py3-none-any.whl/tgcf/web_ui/run.py
@@ -0,0 +1,12 @@
+import os
+from importlib import resources
+
+import tgcf.web_ui as wu
+
+
+def main():
+    package_dir = resources.path(package=wu, resource="").__enter__()
+    print(package_dir)
+    path = os.path.join(package_dir, "0_👋_Hello.py")
+    os.environ["STREAMLIT_THEME_BASE"] = "light"
+    os.system(f"streamlit run {path}")
