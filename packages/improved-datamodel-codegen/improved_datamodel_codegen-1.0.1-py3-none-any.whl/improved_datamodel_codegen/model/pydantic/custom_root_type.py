78dff836164db4be7d24376f3adfb88ef6b702c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:14 +0000

    Add package improved-datamodel-codegen/1.0.1/improved_datamodel_codegen-1.0.1-py3-none-any.whl

---
+++ packages/improved-datamodel-codegen/improved_datamodel_codegen-1.0.1-py3-none-any.whl/improved_datamodel_codegen/model/pydantic/custom_root_type.py
@@ -0,0 +1,8 @@
+from typing import ClassVar
+
+from improved_datamodel_codegen.model.pydantic.base_model import BaseModel
+
+
+class CustomRootType(BaseModel):
+    TEMPLATE_FILE_PATH: ClassVar[str] = 'pydantic/BaseModel_root.jinja2'
+    BASE_CLASS: ClassVar[str] = 'pydantic.BaseModel'
