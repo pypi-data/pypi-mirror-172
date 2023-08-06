770841d1e92bceb2411327e2619d05f65f272b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:43 +0000

    Add package guarana/2.2.1/guarana-2.2.1.tar.gz

---
+++ packages/guarana/guarana-2.2.1.tar.gz/guarana-2.2.1/src/guarana/schemas/base.py
@@ -0,0 +1,11 @@
+from uuid import UUID
+
+from pydantic import BaseModel as PydanticBaseModel
+
+
+class BaseModel(PydanticBaseModel):
+    user_id: UUID
+
+    def __init__(self, **kwargs):
+        kwargs["user_id"] = kwargs["id_customer"]
+        super().__init__(**kwargs)
