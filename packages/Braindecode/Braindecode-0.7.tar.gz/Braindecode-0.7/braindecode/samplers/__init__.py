e85c90f96f98e176fceac6b6410a20e1c3367ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:11 +0000

    Add package Braindecode/0.7/Braindecode-0.7.tar.gz

---
+++ packages/Braindecode/Braindecode-0.7.tar.gz/Braindecode-0.7/braindecode/samplers/__init__.py
@@ -0,0 +1,5 @@
+"""Classes to sample examples.
+"""
+
+from .base import RecordingSampler, SequenceSampler, BalancedSequenceSampler
+from .ssl import RelativePositioningSampler
---
+++ packages/Braindecode/Braindecode-0.7.tar.gz/Braindecode-0.7/braindecode/visualization/__init__.py
@@ -0,0 +1,6 @@
+"""
+Functions for visualisations, especially of the ConvNets.
+"""
+
+from .gradients import compute_amplitude_gradients
+from .confusion_matrices import plot_confusion_matrix
