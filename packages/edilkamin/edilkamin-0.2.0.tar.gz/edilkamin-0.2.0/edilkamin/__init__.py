0babedd482aed1a43f2731b865cabb654e9f3b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:30 +0000

    Add package edilkamin/0.2.0/edilkamin-0.2.0.tar.gz

---
+++ packages/edilkamin/edilkamin-0.2.0.tar.gz/edilkamin-0.2.0/edilkamin/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "sign_in",
+    "device_info",
+    "mqtt_command",
+    "set_power",
+    "set_power_on",
+    "set_power_off",
+]
+from edilkamin.api import (
+    device_info,
+    mqtt_command,
+    set_power,
+    set_power_off,
+    set_power_on,
+    sign_in,
+)
