5a9b75f17f3d7b43da6209bcd5ceeaf896de7166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:25 +0000

    Add package luen-fuwen-zhanbu-zixue-shouce/2022.10.17.0/luen-fuwen-zhanbu-zixue-shouce-2022.10.17.0.tar.gz

---
+++ packages/luen-fuwen-zhanbu-zixue-shouce/luen-fuwen-zhanbu-zixue-shouce-2022.10.17.0.tar.gz/luen-fuwen-zhanbu-zixue-shouce-2022.10.17.0/LuenFuwenZhanbuZixueShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""luen-fuwen-zhanbu-zixue-shouce
+https://github.com/apachecn/luen-fuwen-zhanbu-zixue-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.17.0"
