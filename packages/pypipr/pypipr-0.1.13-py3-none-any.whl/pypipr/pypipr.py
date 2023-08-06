dec686382e1ec9de2c75d1417d0170b16c096a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:46 +0000

    Add package pypipr/0.1.13/pypipr-0.1.13-py3-none-any.whl

---
+++ packages/pypipr/pypipr-0.1.13-py3-none-any.whl/pypipr/pypipr.py
@@ -0,0 +1,14 @@
+PYPIPR = True
+
+"""PYPIPR Module"""
+
+"""PYTHON Standard Module"""
+
+"""PYPI Module"""
+
+
+class Pypipr:
+    @staticmethod
+    def test_print():
+        """Print simple text to test this module is working"""
+        print("Hello from PyPIPr")
