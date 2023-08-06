ebf3891fb9c3e908d4f58fbd2975ed51f7301532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:34 +0000

    Add package power-grid-model/1.4.12/power_grid_model-1.4.12-cp311-cp311-win_amd64.whl

---
+++ packages/power-grid-model/power_grid_model-1.4.12-cp311-cp311-win_amd64.whl/power_grid_model/enum.py
@@ -0,0 +1,78 @@
+# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+"""
+Common Enumerations
+
+Note: these enumeration match the C++ arithmetic core, so dont change the values unless you change thmn in C++ as well
+
+"""
+
+from enum import Enum, IntEnum
+
+# pylint: disable=invalid-name
+
+
+class LoadGenType(IntEnum):
+    """Load and Generator Types"""
+
+    const_power = 0
+    const_impedance = 1
+    const_current = 2
+
+
+class WindingType(IntEnum):
+    """Transformer Winding Types"""
+
+    wye = 0
+    wye_n = 1
+    delta = 2
+    zigzag = 3
+    zigzag_n = 4
+
+
+class BranchSide(IntEnum):
+    """Branch Sides"""
+
+    from_side = 0
+    to_side = 1
+
+
+class Branch3Side(IntEnum):
+    """Branch3 Sides"""
+
+    side_1 = 0
+    side_2 = 1
+    side_3 = 2
+
+
+class CalculationType(Enum):
+    """Calculation Types"""
+
+    power_flow = "power_flow"
+    state_estimation = "state_estimation"
+
+
+class CalculationMethod(IntEnum):
+    """Calculation Methods"""
+
+    linear = 0
+    newton_raphson = 1
+    iterative_linear = 2
+    iterative_current = 3
+    linear_current = 4
+
+
+class MeasuredTerminalType(IntEnum):
+    """The type of asset measured by a (power) sensor"""
+
+    branch_from = 0
+    branch_to = 1
+    source = 2
+    shunt = 3
+    load = 4
+    generator = 5
+    branch3_1 = 6
+    branch3_2 = 7
+    branch3_3 = 8
