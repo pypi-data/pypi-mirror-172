2eae4495cce285d83284a7642d944ef3d7364848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:21 +0000

    Add package CV-Robot/0.0.2/CV_Robot-0.0.2-py3-none-any.whl

---
+++ packages/CV-Robot/CV_Robot-0.0.2-py3-none-any.whl/CV_Robot/vision.py
@@ -0,0 +1,16 @@
+CV_BASIC_MODEL = '/some/model/file'
+
+class Objects:
+    STOP_SIGN = 'STOP_SIGN'
+
+def get_camera_image():
+    """
+    Retrieves current image from robot camera
+    """
+    pass
+
+def process_image(model, image):
+    """
+    Runs machine learning model on image specified, returns list of identified objects
+    """
+    pass
