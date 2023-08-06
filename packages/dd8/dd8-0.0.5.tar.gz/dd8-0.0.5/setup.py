2080564369f8c1b30f5339dc419af3a0905e151e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:34 +0000

    Add package dd8/0.0.5/dd8-0.0.5.tar.gz

---
+++ packages/dd8/dd8-0.0.5.tar.gz/dd8-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dd8',
+      version='0.0.5',
+      description='Package for Application of Data Science in Finance',
+      url='https://github.com/AiRiFiEd/dd8',
+      author='yqlim',
+      author_email='yuanqing87@gmail.com',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
