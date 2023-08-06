eff902cdd8fab49bc921bacd95753c5c2d098c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:40 +0000

    Add package didyoumean-nhannht/0.1.13/didyoumean_nhannht-0.1.13.tar.gz

---
+++ packages/didyoumean-nhannht/didyoumean_nhannht-0.1.13.tar.gz/didyoumean_nhannht-0.1.13/didyoumean_nhannht/main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+#%%
+from os.path import expanduser
+
+import requests
+import json
+from os import environ
+# Get key from json file or from env variable
+home = expanduser("~")
+with open(f'{home}/org-one/api_key.json') as f:
+    api_key = json.load(f)['apilayer_key']
+if environ.get('API_LAYER_TOKEN') is not None:
+    api_key = environ.get('API_LAYER_TOKEN')
+
+#%%
+payload = {}
+headers= {
+    "apikey": f"{api_key}"
+}
+#%% Do not run interactive
+# parse args
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("query", help="query to search")
+args = parser.parse_args()
+#%% scratch area
+q = "hello"
+url_didyoumean = f"https://api.apilayer.com/dymt/did_you_mean_this?q={q}"
+url_suggest = f"https://api.apilayer.com/spell/spellchecker?q={q}"
+suggest = requests.request("GET", url_suggest, headers=headers, data = payload)
+suggest.json()
+
+# didyoumean = requests.request("GET", url_didyoumean, headers=headers, data = payload)
+# didyoumean.json()
+#%%
+def main():
+    q = args.query
+    url_didyoumean = f"https://api.apilayer.com/dymt/did_you_mean_this?q={q}"
+    url_suggest = f"https://api.apilayer.com/spell/spellchecker?q={q}"
+    didyoumean = requests.request("GET", url_didyoumean, headers=headers, data = payload)
+    suggest = requests.request("GET", url_suggest, headers=headers, data = payload)
+    didyoumean_ismodified = didyoumean.json()['is_modified']
+    suggest_correction = suggest.json()['corrections']
+
+    if didyoumean_ismodified:
+        print(f"<div><b>Did you mean</b> \"{didyoumean.json()['result']}\"?</div>")
+    if len(suggest_correction) > 0:
+        suggest_candidates = suggest_correction[0]['candidates']
+        print("<div><b>Other candidate</b></div>")
+        print("<ul>")
+        for candidate in suggest_candidates:
+            print(f"<li>{candidate}</li>")
+        print("</ul>")
+    #%%
+if __name__ == '__main__':
+    main()
---
+++ packages/didyoumean-nhannht/didyoumean_nhannht-0.1.13.tar.gz/didyoumean_nhannht-0.1.13/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['didyoumean_nhannht']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.3,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['didyoumean = didyoumean_nhannht.main:main']}
+
+setup_kwargs = {
+    'name': 'didyoumean-nhannht',
+    'version': '0.1.13',
+    'description': '',
+    'long_description': '',
+    'author': 'nhanclassroom',
+    'author_email': 'nhanclassroom@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
