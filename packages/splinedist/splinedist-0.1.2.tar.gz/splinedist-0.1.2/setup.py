1752c181a0dfe21f4cc6e6b13dfb241b3542c4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:21 +0000

    Add package splinedist/0.1.2/splinedist-0.1.2.tar.gz

---
+++ packages/splinedist/splinedist-0.1.2.tar.gz/splinedist-0.1.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["stardist>=0.8.3", "tensorflow", "opencv-python-headless","csbdeep>=0.6.3"]
+
+setup(
+    author="uhlmanngroup",
+    author_email="uhlmann@ebi.ac.uk",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    description="splinedist",
+    install_requires=requirements,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="splinedist",
+    name="splinedist",
+    packages=find_packages(),
+    url="https://github.com/uhlmanngroup/splinedist",
+    version="0.1.2",
+    zip_safe=False,
+)
