8f7abceb61311e32b4c3049143b0a6553392cb1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:13 +0000

    Add package aiogram-di/0.1.0/aiogram-di-0.1.0.tar.gz

---
+++ packages/aiogram-di/aiogram-di-0.1.0.tar.gz/aiogram-di-0.1.0/setup.py
@@ -0,0 +1,41 @@
+import os
+
+from setuptools import find_packages, setup
+
+VERSION = "0.1.0"  # Version will be replaced by the CI/CD pipeline
+
+if VERSION.startswith("{{"):
+    VERSION = "0.0.0"
+
+
+def get_readme() -> str:
+    """Load the contents of the README file"""
+    readme_path = os.path.join(os.path.dirname(__file__), "README.md")
+    with open(readme_path, "r") as f:
+        return f.read()
+
+
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="aiogram-di",
+    description="",
+    long_description=get_readme(),
+    long_description_content_type="text/markdown",
+    version=VERSION,
+    packages=find_packages(include=["aiogram-di"]),
+    url="https://github.com/MaximZayats/aiogram-di",
+    author="Maxim",
+    author_email="maximzayats1@gmail.com",
+    install_requires=get_requirements(),
+    keywords=[
+        "python",
+        "aiogram",
+        "di",
+        "dependency injection",
+        "aiogram di",
+    ],
+)
