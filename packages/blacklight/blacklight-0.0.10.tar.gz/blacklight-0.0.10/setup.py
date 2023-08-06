c4d8839632ea81175c5d95559e2430d459610a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:21 +0000

    Add package blacklight/0.0.10/blacklight-0.0.10.tar.gz

---
+++ packages/blacklight/blacklight-0.0.10.tar.gz/blacklight-0.0.10/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['GDNN', 'Dataset', 'blacklightutils']
+install_requires = \
+['keras>=2.10.0,<3.0.0',
+ 'numpy>=1.23.4,<2.0.0',
+ 'pandas>=1.5.1,<2.0.0',
+ 'scikit-learn>=1.1.2,<2.0.0',
+ 'scipy>=1.9.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'blacklight',
+    'version': '0.0.10',
+    'description': 'Genetic Deep Neural Net Topology Optimization',
+    'long_description': '# GeneticDNN\nAn attempt to generalize neural network topology searches using genetic algorithms. \n',
+    'author': 'Cole Agard',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
