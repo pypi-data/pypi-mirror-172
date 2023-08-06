c6342bb256a8a6d2556728c7e089387c0942ad90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:07 +0000

    Add package rbwr/0.1.0/rbwr-0.1.0-py3-none-any.whl

---
+++ packages/rbwr/rbwr-0.1.0-py3-none-any.whl/rbwr/__init__.py
@@ -0,0 +1,18 @@
+"""
+Rather be writing Rust
+
+A small Python library providing sum types that play well with existing typechecking
+PEPs and should work out-of-the-box with any good typechecker, such as Pyright.
+"""
+
+from . import either, result
+from .either import Either
+from .result import Result
+
+# Keep this list sorted lexicographically
+__all__ = [
+    "Either",
+    "Result",
+    "either",
+    "result",
+]
