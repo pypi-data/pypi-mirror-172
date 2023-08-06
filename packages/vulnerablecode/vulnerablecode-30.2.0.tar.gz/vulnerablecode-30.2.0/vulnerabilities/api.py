1db1618049dd01940feee32e661b4ab7c7fd3e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:51 +0000

    Add package vulnerablecode/30.2.0/vulnerablecode-30.2.0.tar.gz

---
+++ packages/vulnerablecode/vulnerablecode-30.2.0.tar.gz/vulnerablecode-30.2.0/vulnerabilities/api.py
@@ -0,0 +1,359 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# VulnerableCode is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/nexB/vulnerablecode for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+from urllib.parse import unquote
+
+from django.db.models import Prefetch
+from django_filters import rest_framework as filters
+from packageurl import PackageURL
+from rest_framework import serializers
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from vulnerabilities.models import Alias
+from vulnerabilities.models import Package
+from vulnerabilities.models import Vulnerability
+from vulnerabilities.models import VulnerabilityReference
+from vulnerabilities.models import VulnerabilitySeverity
+
+
+class VulnerabilitySeveritySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VulnerabilitySeverity
+        fields = ["value", "scoring_system"]
+
+
+class VulnerabilityReferenceSerializer(serializers.ModelSerializer):
+    scores = VulnerabilitySeveritySerializer(many=True, source="vulnerabilityseverity_set")
+    reference_url = serializers.CharField(source="url")
+
+    class Meta:
+        model = VulnerabilityReference
+        fields = ["reference_url", "reference_id", "scores", "url"]
+
+
+class MinimalPackageSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Used for nesting inside vulnerability focused APIs.
+    """
+
+    purl = serializers.CharField(source="package_url")
+
+    class Meta:
+        model = Package
+        fields = ["url", "purl", "is_vulnerable"]
+
+
+class VulnSerializerRefsAndSummary(serializers.HyperlinkedModelSerializer):
+    """
+    Used for nesting inside package focused APIs.
+    """
+
+    fixed_packages = MinimalPackageSerializer(
+        many=True, source="filtered_fixed_packages", read_only=True
+    )
+
+    references = VulnerabilityReferenceSerializer(many=True, source="vulnerabilityreference_set")
+
+    class Meta:
+        model = Vulnerability
+        fields = ["url", "vulnerability_id", "summary", "references", "fixed_packages"]
+
+
+class MinimalVulnerabilitySerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Used for nesting inside package focused APIs.
+    """
+
+    class Meta:
+        model = Vulnerability
+        fields = ["url", "vulnerability_id"]
+
+
+class AliasSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Used for nesting inside package focused APIs.
+    """
+
+    class Meta:
+        model = Alias
+        fields = ["alias"]
+
+
+class VulnerabilitySerializer(serializers.HyperlinkedModelSerializer):
+
+    fixed_packages = MinimalPackageSerializer(
+        many=True, source="filtered_fixed_packages", read_only=True
+    )
+    affected_packages = MinimalPackageSerializer(many=True, source="vulnerable_to", read_only=True)
+
+    references = VulnerabilityReferenceSerializer(many=True, source="vulnerabilityreference_set")
+    aliases = AliasSerializer(many=True, source="alias")
+
+    class Meta:
+        model = Vulnerability
+        fields = [
+            "url",
+            "vulnerability_id",
+            "summary",
+            "aliases",
+            "fixed_packages",
+            "affected_packages",
+            "references",
+        ]
+
+
+class PackageSerializer(serializers.HyperlinkedModelSerializer):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["unresolved_vulnerabilities"] = data["affected_by_vulnerabilities"]
+        return data
+
+    purl = serializers.CharField(source="package_url")
+
+    affected_by_vulnerabilities = serializers.SerializerMethodField("get_affected_vulnerabilities")
+
+    fixing_vulnerabilities = serializers.SerializerMethodField("get_fixed_vulnerabilities")
+
+    def get_fixed_packages(self, package):
+        """
+        Return a queryset of all packages that fixes a vulnerability with
+        same type, namespace, name, subpath and qualifiers of the `package`
+        """
+        return Package.objects.filter(
+            name=package.name,
+            namespace=package.namespace,
+            type=package.type,
+            qualifiers=package.qualifiers,
+            subpath=package.subpath,
+            packagerelatedvulnerability__fix=True,
+        ).distinct()
+
+    def get_vulnerabilities_for_a_package(self, package, fix):
+        """
+        Return a queryset of vulnerabilities related to the given `package`.
+        Return vulnerabilities that affects the `package` if given `fix` flag is False,
+        otherwise return vulnerabilities fixed by the `package`.
+        """
+        fixed_packages = self.get_fixed_packages(package=package)
+        qs = package.vulnerabilities.filter(packagerelatedvulnerability__fix=fix)
+        qs = qs.prefetch_related(
+            Prefetch(
+                "packages",
+                queryset=fixed_packages,
+                to_attr="filtered_fixed_packages",
+            )
+        )
+        return VulnSerializerRefsAndSummary(
+            instance=qs,
+            many=True,
+            context={"request": self.context["request"]},
+        ).data
+
+    def get_fixed_vulnerabilities(self, package):
+        """
+        Return a queryset of vulnerabilities fixed in the given `package`.
+        """
+        return self.get_vulnerabilities_for_a_package(package=package, fix=True)
+
+    def get_affected_vulnerabilities(self, package):
+        """
+        Return a queryset of vulnerabilities that affects the given `package`.
+        """
+        return self.get_vulnerabilities_for_a_package(package=package, fix=False)
+
+    class Meta:
+        model = Package
+        fields = [
+            "url",
+            "purl",
+            "type",
+            "namespace",
+            "name",
+            "version",
+            "qualifiers",
+            "subpath",
+            "affected_by_vulnerabilities",
+            "fixing_vulnerabilities",
+        ]
+
+
+class PackageFilterSet(filters.FilterSet):
+    purl = filters.CharFilter(method="filter_purl")
+
+    class Meta:
+        model = Package
+        fields = [
+            "name",
+            "type",
+            "version",
+            "subpath",
+            "purl",
+            "namespace",
+            "packagerelatedvulnerability__fix",
+        ]
+
+    def filter_purl(self, queryset, name, value):
+        purl = unquote(value)
+        try:
+            purl = PackageURL.from_string(purl)
+
+        except ValueError as ve:
+            raise serializers.ValidationError(
+                detail={"error": f'"{purl}" is not a valid Package URL: {ve}'},
+            )
+
+        attrs = {k: v for k, v in purl.to_dict().items() if v}
+        return self.queryset.filter(**attrs)
+
+
+class PackageViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Package.objects.all()
+    serializer_class = PackageSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = PackageFilterSet
+
+    # TODO: Fix the swagger documentation for this endpoint
+    @action(detail=False, methods=["post"])
+    def bulk_search(self, request):
+        """
+        See https://github.com/nexB/vulnerablecode/pull/369#issuecomment-796877606 for docs
+        """
+        response = []
+        purls = request.data.get("purls", []) or []
+        if not purls or not isinstance(purls, list):
+            return Response(
+                status=400,
+                data={"Error": "A non-empty 'purls' list of package URLs is required."},
+            )
+        for purl in request.data["purls"]:
+            try:
+                purl_string = purl
+                purl = PackageURL.from_string(purl).to_dict()
+            except ValueError:
+                return Response(status=400, data={"Error": f"Invalid Package URL: {purl}"})
+            purl_data = Package.objects.filter(
+                **{key: value for key, value in purl.items() if value}
+            )
+            purl_response = {}
+            if purl_data:
+                purl_response = PackageSerializer(purl_data[0], context={"request": request}).data
+            else:
+                purl_response = purl
+                purl_response["unresolved_vulnerabilities"] = []
+                purl_response["resolved_vulnerabilities"] = []
+                purl_response["purl"] = purl_string
+            response.append(purl_response)
+
+        return Response(response)
+
+    @action(detail=False, methods=["get"])
+    def all(self, request):
+        """
+        Return all the vulnerable Package URLs.
+        """
+        vulnerable_packages = Package.objects.vulnerable().only(*PackageURL._fields).distinct()
+        vulnerable_purls = [str(package.purl) for package in vulnerable_packages]
+        return Response(vulnerable_purls)
+
+
+class VulnerabilityFilterSet(filters.FilterSet):
+    class Meta:
+        model = Vulnerability
+        fields = ["vulnerability_id"]
+
+
+class VulnerabilityViewSet(viewsets.ReadOnlyModelViewSet):
+    def get_fixed_packages_qs(self):
+        """
+        Filter the packages that fixes a vulnerability
+        on fields like name, namespace and type.
+        """
+        package_filter_data = {"packagerelatedvulnerability__fix": True}
+
+        query_params = self.request.query_params
+        for field_name in ["name", "namespace", "type"]:
+            value = query_params.get(field_name)
+            if value:
+                package_filter_data[field_name] = value
+
+        return PackageFilterSet(package_filter_data).qs
+
+    def get_queryset(self):
+        """
+        Assign filtered packages queryset from `get_fixed_packages_qs`
+        to a custom attribute `filtered_fixed_packages`
+        """
+        return Vulnerability.objects.prefetch_related(
+            Prefetch(
+                "packages",
+                queryset=self.get_fixed_packages_qs(),
+                to_attr="filtered_fixed_packages",
+            )
+        )
+
+    serializer_class = VulnerabilitySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = VulnerabilityFilterSet
+
+
+class CPEFilterSet(filters.FilterSet):
+    cpe = filters.CharFilter(method="filter_cpe")
+
+    def filter_cpe(self, queryset, name, value):
+        cpe = unquote(value)
+        return self.queryset.filter(vulnerabilityreference__reference_id__startswith=cpe).distinct()
+
+
+class CPEViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Vulnerability.objects.filter(
+        vulnerabilityreference__reference_id__startswith="cpe"
+    ).distinct()
+    serializer_class = VulnerabilitySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = CPEFilterSet
+
+    @action(detail=False, methods=["post"])
+    def bulk_search(self, request):
+        """
+        This endpoint is used to search for vulnerabilities by more than one CPE.
+        """
+        cpes = request.data.get("cpes", []) or []
+        if not cpes or not isinstance(cpes, list):
+            return Response(
+                status=400,
+                data={"Error": "A non-empty 'cpes' list of CPEs is required."},
+            )
+        for cpe in cpes:
+            if not cpe.startswith("cpe"):
+                return Response(status=400, data={"Error": f"Invalid CPE: {cpe}"})
+        vulnerabilitiesResponse = Vulnerability.objects.filter(
+            vulnerabilityreference__reference_id__in=cpes
+        ).distinct()
+        return Response(
+            VulnerabilitySerializer(
+                vulnerabilitiesResponse, many=True, context={"request": request}
+            ).data
+        )
+
+
+class AliasFilterSet(filters.FilterSet):
+    alias = filters.CharFilter(method="filter_alias")
+
+    def filter_alias(self, queryset, name, value):
+        alias = unquote(value)
+        return self.queryset.filter(aliases__alias__icontains=alias)
+
+
+class AliasViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Vulnerability.objects.all()
+    serializer_class = VulnerabilitySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AliasFilterSet
---
+++ packages/vulnerablecode/vulnerablecode-30.2.0.tar.gz/vulnerablecode-30.2.0/vulnerabilities/importers/nvd.py
@@ -0,0 +1,187 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# VulnerableCode is a trademark of nexB Inc.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/nexB/vulnerablecode for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+import gzip
+import json
+from datetime import date
+from typing import Iterable
+
+import requests
+from dateutil import parser as dateparser
+from django.db.models.query import QuerySet
+
+from vulnerabilities import severity_systems
+from vulnerabilities.importer import AdvisoryData
+from vulnerabilities.importer import Importer
+from vulnerabilities.importer import Reference
+from vulnerabilities.importer import VulnerabilitySeverity
+from vulnerabilities.improver import Improver
+from vulnerabilities.improver import Inference
+from vulnerabilities.models import Advisory
+from vulnerabilities.utils import get_item
+
+BASE_URL = "https://nvd.nist.gov/vuln/search/results"
+PARAMS = "?adv_search=true&isCpeNameSearch=true"
+
+
+class NVDImporter(Importer):
+    # See https://github.com/nexB/vulnerablecode/issues/665 for follow up
+    spdx_license_expression = "LicenseRef-scancode-unknown"
+
+    def advisory_data(self):
+        current_year = date.today().year
+        # NVD json feeds start from 2002.
+        for year in range(2002, current_year + 1):
+            download_url = f"https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-{year}.json.gz"
+            data = fetch(download_url)
+            yield from to_advisories(data)
+
+
+# Isolating network calls for simplicity of testing
+def fetch(url):
+    gz_file = requests.get(url)
+    data = gzip.decompress(gz_file.content)
+    return json.loads(data)
+
+
+def extract_summary(cve_item):
+    """
+    Return a summary for a given CVE item.
+    """
+    # In 99% of cases len(cve_item['cve']['description']['description_data']) == 1 , so
+    # this usually returns  cve_item['cve']['description']['description_data'][0]['value']
+    # In the remaining 1% cases this returns the longest summary.
+    summaries = []
+    for desc in get_item(cve_item, "cve", "description", "description_data") or []:
+        if desc.get("value"):
+            summaries.append(desc["value"])
+    return max(summaries, key=len) if summaries else None
+
+
+def to_advisories(nvd_data):
+    """
+    Yield AdvisoryData objects from a NVD json feed.
+    """
+    for cve_item in nvd_data.get("CVE_Items") or []:
+        cpes = extract_cpes(cve_item)
+        if related_to_hardware(cpes):
+            continue
+
+        aliases = []
+        cve_id = get_item(cve_item, "cve", "CVE_data_meta", "ID")
+        ref_urls = extract_reference_urls(cve_item)
+        references = []
+        severity_scores = list(extract_severity_scores(cve_item))
+        for cpe in cpes:
+            cpe_url = f"{BASE_URL}{PARAMS}&query={cpe}"
+            references.append(
+                Reference(
+                    reference_id=cpe,
+                    url=cpe_url,
+                )
+            )
+        references.append(
+            Reference(
+                url=f"https://nvd.nist.gov/vuln/detail/{cve_id}",
+                reference_id=cve_id,
+                severities=severity_scores,
+            )
+        )
+        if "https://nvd.nist.gov/vuln/detail/{cve_id}" in ref_urls:
+            ref_urls.remove(f"https://nvd.nist.gov/vuln/detail/{cve_id}")
+        references.extend([Reference(url=url) for url in ref_urls])
+        if cve_id:
+            aliases.append(cve_id)
+        summary = extract_summary(cve_item)
+        yield AdvisoryData(
+            aliases=aliases,
+            summary=summary,
+            references=sorted(references),
+            date_published=dateparser.parse(cve_item.get("publishedDate")),
+        )
+
+
+def extract_reference_urls(cve_item):
+    """
+    Return a list of reference URLs for a given CVE item.
+    """
+    urls = set()
+    for reference in get_item(cve_item, "cve", "references", "reference_data") or []:
+        ref_url = reference.get("url")
+
+        if not ref_url:
+            continue
+
+        if ref_url.startswith(
+            (
+                "http",
+                "ftp",
+            )
+        ):
+            urls.add(ref_url)
+
+    return urls
+
+
+def related_to_hardware(cpes):
+    """
+    Return True if the CVE item is related to hardware.
+    """
+    for cpe in cpes:
+        cpe_comps = cpe.split(":")
+        # CPE follow the format cpe:cpe_version:product_type:vendor:product
+        if len(cpe_comps) > 2 and cpe_comps[2] == "h":
+            return True
+
+    return False
+
+
+def extract_cpes(cve_item):
+    """
+    Return a list of CPEs for a given CVE item.
+    """
+    cpes = set()
+    for node in get_item(cve_item, "configurations", "nodes") or []:
+        for cpe_data in node.get("cpe_match") or []:
+            cpe23_uri = cpe_data.get("cpe23Uri")
+            if cpe23_uri:
+                cpes.add(cpe23_uri)
+    return cpes
+
+
+def extract_severity_scores(cve_item):
+    """
+    Yield a vulnerability severity for each `cve_item`.
+    """
+    if not isinstance(cve_item, dict):
+        return None
+    impact = cve_item.get("impact") or {}
+    base_metric_v3 = impact.get("baseMetricV3") or {}
+    if base_metric_v3:
+        cvss_v3 = get_item(base_metric_v3, "cvssV3")
+        yield VulnerabilitySeverity(
+            system=severity_systems.CVSSV3,
+            value=str(cvss_v3.get("baseScore") or ""),
+        )
+        yield VulnerabilitySeverity(
+            system=severity_systems.CVSSV3_VECTOR,
+            value=str(cvss_v3.get("vectorString") or ""),
+        )
+
+    base_metric_v2 = impact.get("baseMetricV2") or {}
+    if base_metric_v2:
+        cvss_v2 = base_metric_v2.get("cvssV2") or {}
+        yield VulnerabilitySeverity(
+            system=severity_systems.CVSSV2,
+            value=str(cvss_v2.get("baseScore") or ""),
+        )
+        yield VulnerabilitySeverity(
+            system=severity_systems.CVSSV2_VECTOR,
+            value=str(cvss_v2.get("vectorString") or ""),
+        )
