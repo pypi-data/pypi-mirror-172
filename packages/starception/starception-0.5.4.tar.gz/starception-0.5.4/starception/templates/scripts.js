f7817790422d9475c5152555a0b6d6246348d073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:15 +0000

    Add package starception/0.5.4/starception-0.5.4.tar.gz

---
+++ packages/starception/starception-0.5.4.tar.gz/starception-0.5.4/starception/templates/scripts.js
@@ -0,0 +1,52 @@
+Array.from(document.querySelectorAll('[id^="switch"]')).forEach(function (el) {
+    el.addEventListener('click', function () {
+        var traceIndex = el.dataset.traceTargetIndex;
+        var traceElement = document.querySelector('#trace-target-' + traceIndex);
+        var current = traceElement.querySelector('.snippet-wrapper.current');
+        if (current) {
+            current.classList.remove('current');
+        }
+
+        var frameIndex = el.dataset.frameIndex;
+        var target = traceElement.querySelector('#snippet-' + frameIndex);
+        if (target) {
+            target.classList.add('current');
+        }
+
+        var currentSwitch = traceElement.querySelector('.frame.current');
+        if (currentSwitch) {
+            currentSwitch.classList.remove('current');
+        }
+        el.classList.add('current');
+    });
+});
+
+
+Array.from(document.querySelectorAll('[data-reveal]')).forEach(function (el) {
+    el.addEventListener('click', function () {
+        el.innerText = el.dataset.reveal;
+    });
+});
+
+
+document.querySelector('#vendor-frames').addEventListener('click', function (e) {
+    Array.from(document.querySelectorAll('.frames .vendor')).forEach(function (frame) {
+        if (e.target.checked) {
+            frame.classList.add('hidden');
+        } else {
+            frame.classList.remove('hidden');
+        }
+    });
+});
+
+document.querySelectorAll('[data-toggle-trace]').forEach(el => {
+    el.addEventListener('click', () => {
+        const index = el.dataset.traceIndex;
+        const target = document.querySelector(`[data-trace-target="${index}"]`);
+        if (target.classList.contains('collapsed')) {
+            target.classList.remove('collapsed');
+        } else {
+            target.classList.add('collapsed');
+        }
+    });
+})
