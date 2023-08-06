f8c7f24990c46e3e74dd0e167c64a67f9e4c6585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:52 +0000

    Add package videosrc/0.1.1/videosrc-0.1.1-py3-none-any.whl

---
+++ packages/videosrc/videosrc-0.1.1-py3-none-any.whl/videosrc/crawl/__init__.py
@@ -0,0 +1,6 @@
+from videosrc.crawl.timcast import TimcastCrawler
+from videosrc.crawl.peertube import PeerTubeCrawler
+from videosrc.crawl.rumble import RumbleCrawler
+from videosrc.crawl.html import HTMLCrawler
+from videosrc.crawl.mrss import MRSSCrawler
+from videosrc.crawl.odysee import OdyseeCrawler
