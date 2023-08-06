6f15ab09899edc089f2d0fbee0685f303b3b36d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:57 +0000

    Add package metriql-tableau/0.7/metriql_tableau-0.7-py3-none-any.whl

---
+++ packages/metriql-tableau/metriql_tableau-0.7-py3-none-any.whl/metriql2tableau/__init__.py
@@ -0,0 +1,30 @@
+import argparse
+import json
+from .metadata import MetriqlMetadata
+from .generate import GenerateTDS
+import sys
+
+__version__ = "0.7"
+
+
+def main(args: list = None):
+    parser = argparse.ArgumentParser(description="Generates Tableau TDS files for metriql datasets")
+
+    parser.add_argument("command", choices=["create-tds"], help="command to execute")
+
+    parser.add_argument("--metriql-url", help="metriql URL", )
+
+    parser.add_argument("--dataset", help="dataset for the TDS file")
+
+    parser.add_argument("--file", help="source of the metadata file. if not set, the source is stdin")
+
+    parser.add_argument("--out", help="target location for TDS file. if not set, the source is stdout")
+
+    parsed = parser.parse_args(args=args)
+    if parsed.command == "create-tds":
+        if parsed.file is not None:
+            source = open(parsed.file).read()
+        else:
+            source = sys.stdin.readline()
+        metriql_metadata = MetriqlMetadata(parsed.metriql_url, json.loads(source))
+        GenerateTDS(metriql_metadata).generate(parsed.dataset, parsed.out)
