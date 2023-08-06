2c1dff8c8b4f34d3b823ef3c70ea9c8b74088ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:08 +0000

    Add package webknossos/0.10.19/webknossos-0.10.19-py3-none-any.whl

---
+++ packages/webknossos/webknossos-0.10.19-py3-none-any.whl/webknossos/annotation/__init__.py
@@ -0,0 +1,7 @@
+from webknossos.annotation.annotation import (
+    Annotation,
+    AnnotationState,
+    AnnotationType,
+    open_annotation,
+)
+from webknossos.annotation.annotation_info import AnnotationInfo
