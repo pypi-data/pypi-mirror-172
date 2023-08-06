e7e217fd276139efa2739c90605c2976c370a8ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:54 +0000

    Add package source-converter/0.1.8/source_converter-0.1.8.tar.gz

---
+++ packages/source-converter/source_converter-0.1.8.tar.gz/source_converter-0.1.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+packages = find_packages(exclude=['tests', 'tests.*'])
+packages.extend(['.'])
+setup(
+    name='source_converter',
+    version='0.1.8',
+    packages=packages,
+    install_requires=_requires_from_file('requirements.txt'),
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov"]
+)
