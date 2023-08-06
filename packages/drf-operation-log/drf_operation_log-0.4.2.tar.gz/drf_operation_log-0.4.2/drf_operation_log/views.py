25da735d7c17523671a60c48a355303259cf2a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:43 +0000

    Add package drf-operation-log/0.4.2/drf_operation_log-0.4.2.tar.gz

---
+++ packages/drf-operation-log/drf_operation_log-0.4.2.tar.gz/drf_operation_log-0.4.2/drf_operation_log/views.py
@@ -0,0 +1,19 @@
+from django.core.validators import EMPTY_VALUES
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAdminUser
+
+from .models import OperationLogEntry
+from .serializers import (
+    OperationLogEntrySerializer
+)
+
+
+class OperationlogViewSet(
+    ListModelMixin,
+    RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = OperationLogEntry.objects.select_related("user", "content_type", "domain_content_type")
+    permission_classes = (IsAdminUser,)
+    serializer_class = OperationLogEntrySerializer
