82ae5bba48daee3402a2f1bf4e31da90af722244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:38 +0000

    Add package pfutils/0.0.32/pfutils-0.0.32-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pfutils/pfutils-0.0.32-cp38-cp38-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/pfutils/commands/__init__.py
@@ -0,0 +1,6 @@
+from pfutils.commands.main import cli
+import pfutils.commands.cp
+import pfutils.commands.rm
+import pfutils.commands.scp
+import pfutils.commands.du
+import pfutils.commands.mdtest
