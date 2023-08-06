b2e7c58519192b6fa6700191a7e912b86b78a8cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:48 +0000

    Add package xia-cache/0.1.1/xia_cache-0.1.1-cp310-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-cache/xia_cache-0.1.1-cp310-none-macosx_11_0_x86_64.whl/xia_cache/__init__.py
@@ -0,0 +1,10 @@
+from xia_cache.base import BaseCache, Cache
+from xia_cache.cache import LocalCache, MultiCache
+
+
+__all__ = [
+    "BaseCache", "Cache",
+    "LocalCache", "MultiCache",
+]
+
+__version__ = "0.1.1"
