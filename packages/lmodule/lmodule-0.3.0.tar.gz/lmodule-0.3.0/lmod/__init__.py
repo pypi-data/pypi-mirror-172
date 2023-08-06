dc44c997abf6f3bd00ebc18338c346a431576198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:36 +0000

    Add package lmodule/0.3.0/lmodule-0.3.0.tar.gz

---
+++ packages/lmodule/lmodule-0.3.0.tar.gz/lmodule-0.3.0/lmod/__init__.py
@@ -0,0 +1,2 @@
+LMODULE_VERSION = "0.3.0"
+__version__ = LMODULE_VERSION
---
+++ packages/lmodule/lmodule-0.3.0.tar.gz/lmodule-0.3.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from lmod import LMODULE_VERSION
+
+
+setup(
+    name="lmodule",
+    version=LMODULE_VERSION,
+    author="Shahzeb Siddiqui",
+    author_email="shahzebmsiddiqui@gmail.com",
+    description="Lmod Module API",
+    long_description=open("README.rst").read(),
+    url="https://github.com/buildtesters/lmodule",
+    license="MIT",
+    python_requires=">=3.6, <4",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    project_urls={
+        "Source Code": "https://github.com/buildtesters/lmodule",
+        "Documentation": "https://lmodule.readthedocs.io/",
+        "Bug Report": "https://github.com/buildtesters/lmodule/issues",
+    },
+)
