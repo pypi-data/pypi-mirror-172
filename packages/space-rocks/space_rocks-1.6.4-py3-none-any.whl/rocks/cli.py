b9d38f68bf9b6ebc12f3857b5d4807bf8605aea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:25 +0000

    Add package space-rocks/1.6.4/space_rocks-1.6.4-py3-none-any.whl

---
+++ packages/space-rocks/space_rocks-1.6.4-py3-none-any.whl/rocks/cli.py
@@ -0,0 +1,407 @@
+#!/usr/bin/env python
+"""rocks command line suite."""
+
+import json
+import keyword
+import sys
+import subprocess
+import shutil
+import textwrap
+import time
+import webbrowser
+
+import click
+import numpy as np
+import rich
+from rich import prompt
+from rich.console import Console
+
+import rocks
+
+
+class AliasedGroup(click.Group):
+    """Click group with custom default mode implementation."""
+
+    def get_command(self, ctx, cmd_name):
+
+        # ------
+        # Resolve known commands
+
+        # Add command aliases
+        if cmd_name == "identify":
+            cmd_name = "id"
+        elif cmd_name == "aliases":
+            cmd_name = "ids"
+        elif cmd_name == "parameter":
+            cmd_name = "parameters"
+        elif cmd_name == "update":
+            cmd_name = "status"
+
+        # If it's a known subcommand, execute it
+        rv = click.Group.get_command(self, ctx, cmd_name)
+
+        if rv is not None:
+            return rv
+
+        # ------
+        # Unknown subcommand -> echo asteroid parameter
+        return echo()
+
+
+@click.group(cls=AliasedGroup)
+@click.version_option(version=rocks.__version__, message="%(version)s")
+def cli_rocks():
+    """CLI for minor body exploration."""
+    pass
+
+
+@cli_rocks.command()
+def docs():
+    """Open the rocks documentation in browser."""
+    webbrowser.open("https://rocks.readthedocs.io/en/latest/", new=2)
+
+
+@cli_rocks.command()
+@click.argument("name", nargs=1)
+def author(name):
+    """Check presence of peer-rievewed article based on first-author name."""
+
+    rocks.utils.check_datasets(name)
+
+
+@cli_rocks.command()
+@click.argument("id_", nargs=-1)
+def id(id_):
+    """Resolve the asteroid name and number from string input."""
+
+    if not id_:
+        id_ = rocks.resolve._interactive()
+    else:
+        id_ = id_[0]
+
+    name, number = rocks.identify(id_)  # type: ignore
+
+    if isinstance(name, (str)):
+        click.echo(f"({number}) {name}")
+    else:
+        rocks.utils.list_candidate_ssos(id_)
+
+
+@cli_rocks.command()
+@click.argument("id_", nargs=-1)
+def info(id_):
+    """Print the ssoCard of an asteroid."""
+
+    if not id_:
+        id_ = rocks.resolve._interactive()
+    else:
+        id_ = id_[0]
+
+    _, _, id_ = rocks.identify(id_, return_id=True)  # type: ignore
+
+    if not isinstance(id_, str):
+        sys.exit()
+
+    ssoCard = rocks.ssodnet.get_ssocard(id_)
+    rich.print(ssoCard)
+
+
+@cli_rocks.command()
+def parameters():
+    """Print the ssoCard structure and its description."""
+
+    if not rocks.PATH_MAPPING.is_file():
+        rocks.utils.retrieve_metadata("mappings")
+
+    with open(rocks.PATH_MAPPING, "r") as file_:
+        DESC = json.load(file_)
+
+    rich.print(DESC)
+
+
+@cli_rocks.command(hidden=True)
+@click.argument("id_", nargs=-1)
+def ids(id_):
+    """Echo the aliases of an asteroid."""
+
+    if not id_:
+        id_ = rocks.resolve._interactive()
+    else:
+        id_ = id_[0]
+
+    name, number, ssodnetid = rocks.identify(id_, return_id=True)  # type: ignore
+
+    if name is None:
+        sys.exit()
+
+    aliases = rocks.index.get_aliases(ssodnetid)
+
+    rich.print(f"({number}) {name}, aka \n {aliases}")
+
+
+@cli_rocks.command()
+def status():
+    """Echo the status of the ssoCards and datacloud catalogues."""
+
+    # ------
+    # Echo inventory
+
+    # Get set of ssoCards and datacloud catalogues in cache
+    cached_cards, cached_catalogues = rocks.utils.cache_inventory()
+
+    # Get the modification date of the index
+    date_index = (rocks.PATH_INDEX / "1.pkl").stat().st_mtime
+    date_index = time.strftime("%d %b %Y", time.localtime(date_index))
+
+    # Print the findings
+    rich.print(
+        f"""\nContents of {rocks.PATH_CACHE}:
+
+        {len(cached_cards)} ssoCards
+        {len(cached_catalogues)} datacloud catalogues\n
+        Asteroid name-number index updated on {date_index}\n"""
+    )
+
+    # ------
+    # Echo update recommendations
+    latest_rocks = rocks.utils.retrieve_rocks_version()
+
+    if latest_rocks and tuple(map(int, latest_rocks.split("."))) > tuple(
+        map(int, rocks.__version__.split("."))
+    ):
+        rich.print(
+            f"[red]The running [green]rocks[/green] version ({rocks.__version__}) is behind the "
+            f"latest version ({latest_rocks}). The ssoCard structure might have changed.[/red]\n"
+        )
+        rich.print(
+            "You should run [green]$ pip install -U space-rocks[/green] and clear the cache directory.\n"
+        )
+
+    # Update or clear
+    if cached_cards:
+
+        decision = prompt.Prompt.ask(
+            "Update or clear the cached ssoCards and datacloud catalogues?\n"
+            "[blue][0][/blue] Do nothing "
+            "[blue][1][/blue] Clear the cache "
+            "[blue][2][/blue] Update the data",
+            choices=["0", "1", "2", "3"],
+            show_choices=False,
+            default="1",
+        )
+
+        if decision == "1":
+            rich.print("\nClearing the cached ssoCards and datacloud catalogues..")
+            rocks.utils.clear_cache()
+
+        elif decision == "2":
+
+            # Update metadata
+            rocks.utils.retrieve_metadata("mappings")
+
+            # Update the cached data
+            ids = [ssodnet_id for ssodnet_id in cached_cards]
+
+            # Ensure that the IDs are current
+            rich.print("\n(1/3) Verifying the ID of the cached ssoCards..")
+            rocks.utils.confirm_identity(ids)
+
+            # Update ssoCards
+            rich.print("\n(2/3) Updating the cached ssoCards..")
+            rocks.ssodnet.get_ssocard(ids, progress=True, local=False)
+
+            # ------
+            # Update datacloud catalogues
+            rich.print("\n(3/3) Updating the cached datacloud catalogues..")
+            rocks.utils.update_datacloud_catalogues(cached_catalogues)
+
+        elif decision == "3":
+            with Console().status("Observing all asteroids.. [~11GB]", spinner="earth"):
+                rocks.utils.cache_all_ssocards()
+
+    # ------
+    # Update asteroid name-number index
+    response = prompt.Prompt.ask(
+        "\nUpdate the asteroid name-number index?\n"
+        "[blue][0][/blue] No "
+        "[blue][1][/blue] Yes",
+        choices=["0", "1"],
+        show_choices=False,
+        default="1",
+    )
+
+    if response == "1":
+        click.echo()
+        rocks.index._build_index()
+
+
+@cli_rocks.command()
+@click.argument("id_", nargs=-1)
+def who(id_):
+    """Get name citation of asteroid from MPC."""
+
+    if not id_:
+        id_ = rocks.resolve._interactive()
+    else:
+        id_ = id_[0]
+
+    name, number = rocks.identify(id_)
+
+    if name is None:
+        sys.exit()
+
+    if not np.isnan(number):
+        id_ = number
+    else:
+        id_ = name
+
+    citation = rocks.utils.get_citation_from_mpc(id_)
+
+    if citation is None:
+        rich.print(f"({number}) {name} has no citation attached to its name.")
+        sys.exit()
+
+    # Pretty-print citation
+    rich.print(f"({number}) {name}")
+    rich.print(
+        textwrap.fill(
+            citation,
+            width=shutil.get_terminal_size()[0] - 2,
+            initial_indent="  ",
+            subsequent_indent="  ",
+        ),
+    )
+
+
+def echo():
+    """Echos asteroid parameter to command line. Optionally opens plot."""
+
+    # Should we plot?
+    for arg in ["-p", "--plot"]:
+
+        if arg in sys.argv:
+            sys.argv.remove(arg)
+
+            plot = True
+            break
+    else:
+        plot = False
+
+    # Verbose output?
+    for arg in ["-v", "--verbose"]:
+
+        if arg in sys.argv:
+            sys.argv.remove(arg)
+
+            verbose = True
+            break
+    else:
+        verbose = False
+
+    # Are there enough arguments to continue?
+    if len(sys.argv) == 2:
+        print(f"\nUnknown command '{sys.argv[-1]}'.\n")
+        ctx = click.get_current_context()
+        click.echo(ctx.get_help())
+        ctx.exit()
+
+    # Get parameter and asteroid id
+    _, parameter, *id_ = sys.argv
+    id_ = " ".join(id_)
+
+    # Allow for comma-separated parameter chaining
+    parameter = parameter.split(",")
+
+    # Check if any of the parameters might be missing an underscore
+    parameter = [
+        f"{p}_" if keyword.iskeyword(p.split(".")[-1]) else p for p in parameter
+    ]
+
+    # Check what datacloud properties we need
+    datacloud = [
+        p.split(".")[0]
+        for p in parameter
+        if p.split(".")[0] in rocks.datacloud.CATALOGUES.keys()
+    ]
+
+    # And let's go
+    rock = rocks.Rock(id_, datacloud=datacloud, suppress_errors=not verbose)
+
+    # Identifier could not be resolved
+    if not rock.id_:
+        rocks.utils.list_candidate_ssos(id_)
+        sys.exit()
+
+    # Pretty-print the paramter
+    for param in parameter:
+        if param in datacloud:
+            rocks.datacloud.pretty_print(rock, rocks.utils.rgetattr(rock, param), param)
+        else:
+            value = rocks.utils.rgetattr(rock, param)
+
+            if isinstance(value, rocks.core.ListWithAttributes):
+                # if verbose:
+                #     for entry in value:
+                #         rich.print_json(entry.json(), sort_keys=True)
+                # else:
+                #     for entry in value:
+                #         rich.print(entry)
+                rich.print(value)
+
+            else:
+                if verbose:
+                    rich.print_json(value.json(), sort_keys=True)
+                else:
+                    print(value)
+
+        if plot:
+            if param not in datacloud:
+                print(
+                    "Only datacloud collections can be plotted. "
+                    f"Try the plural of {param}."
+                )
+                sys.exit()
+
+            rocks.utils.rgetattr(rock, param).plot(param)
+
+    # Avoid error message from click
+    sys.exit()
+
+
+def _interactive(LINES):
+    """Launch interactive selection using fzf."""
+
+    PATH_EXECUTABLE = shutil.which("fzf")
+
+    if PATH_EXECUTABLE is None:
+        rich.print(
+            "Interactive selection is not possible as the fzf tool is not installed.\n"
+            "Either install fzf or specify an asteroid identifier.\n"
+            "Refer to https://rocks.readthedocs.io/en/latest/getting_started.html#optional-interactive-search"
+        )
+        sys.exit()
+
+    FZF_OPTIONS = []
+
+    # Open fzf subprocess
+    process = subprocess.Popen(
+        [shutil.which("fzf"), *FZF_OPTIONS],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=None,
+    )
+
+    for line in LINES:
+        process.stdin.write(line)
+        process.stdin.flush()
+
+    # Run process and wait for user selection
+    process.stdin.close()
+    process.wait()
+
+    # Extract selected line
+    try:
+        choice = [line for line in process.stdout][0].decode()
+    except IndexError:  # no choice was made, c-c c-c
+        sys.exit()
+    return choice
---
+++ packages/space-rocks/space_rocks-1.6.4-py3-none-any.whl/rocks/index.py
@@ -0,0 +1,362 @@
+#!/usr/bin/env python
+"""Asteroid name-number index functions for rocks."""
+
+import pickle
+import pickletools
+import re
+import string
+import sys
+import typing
+
+import numpy as np
+import pandas as pd
+import rich
+from rich import progress
+
+import rocks
+
+# ------
+# Building the index
+def _build_index():
+    """Build asteroid name-number index from SsODNet sso_index."""
+
+    tasks = [
+        _build_index_of_aliases,
+        _build_number_index,
+        _build_name_index,
+        _build_designation_index,
+        _build_palomar_transit_index,
+        _build_fuzzy_searchable_index,
+    ]
+
+    with progress.Progress(
+        "[progress.description]{task.description}",
+        progress.BarColumn(),
+        "[progress.percentage]{task.percentage:>3.0f}%",
+    ) as progress_bar:
+
+        steps = progress_bar.add_task("Building index", total=len(tasks) + 1)
+
+        index = _retrieve_index_from_ssodnet()
+        progress_bar.update(steps, advance=1)
+
+        for task in tasks:
+            task(index)
+            progress_bar.update(steps, advance=1)
+
+
+def _build_index_of_aliases(index):
+    """Create the SsODNetID -> aliases index.
+
+    Parameters
+    ----------
+    index : pd.DataFrame
+        The formatted index from SsODNet.
+    """
+
+    aliases = dict(
+        zip(index.SsODNetID, [aliases.split(";") for aliases in index.Aliases])
+    )
+
+    _write_to_cache(aliases, "aliases.pkl")
+
+
+def _build_number_index(index):
+    """Build the number -> name,SsODNetID index parts.
+
+    Parameters
+    ----------
+    index : pd.DataFrame
+        The formatted index from SsODNet.
+    """
+
+    SIZE = 1e3  # Build chunks of 1k entries
+
+    # Find next 10,000 to largest number
+    parts = np.arange(1, np.ceil(index.Number.max() / SIZE) * SIZE, SIZE, dtype=int)
+
+    for part in parts:
+
+        part_index = index.loc[(part <= index.Number) & (index.Number < part + SIZE)]
+
+        part_index = dict(
+            zip(
+                part_index.Number,
+                [[sso.Name, sso.SsODNetID] for _, sso in part_index.iterrows()],
+            )
+        )
+
+        _write_to_cache(part_index, f"{part}.pkl")
+
+
+def _build_name_index(index):
+    """Build the reduced -> number,SsODNetID index.
+
+    Parameters
+    ----------
+    index : pd.DataFrame
+        The formatted index from SsODNet.
+    """
+
+    parts = string.ascii_lowercase  # first character of name
+
+    names = set(red for red in index.Reduced if re.match(r"^[a-z\'-]*$", red))
+    names.add(r"g!kun||'homdima")  # everyone's favourite shell injection
+
+    for part in parts:
+
+        names_subset = set(name for name in names if name.startswith(part))
+
+        if part == "a":
+            names_subset.add(
+                "'aylo'chaxnim"
+            )  # another edge case for the daughter of venus
+        part_index = index.loc[index.Reduced.isin(names_subset)]
+
+        part_index = dict(
+            zip(
+                part_index.Reduced,
+                [
+                    [sso.Name, sso.SsODNetID]
+                    if pd.isna(sso.Number)
+                    else [sso.Name, sso.Number, sso.SsODNetID]
+                    for _, sso in part_index.iterrows()
+                ],
+            )
+        )
+
+        _write_to_cache(part_index, f"{part}.pkl")
+
+
+def _build_designation_index(index):
+    """Build the designation -> name,number,SsODNetID index.
+
+    Parameters
+    ----------
+    index : pd.DataFrame
+        The formatted index from SsODNet.
+    """
+
+    designations = set(
+        red
+        for red in index.Reduced
+        if re.match(
+            r"(^([11][8-9][0-9]{2}[a-z]{2}[0-9]{0,3}$)|"
+            r"(^20[0-9]{2}[a-z]{2}[0-9]{0,3}$))",
+            red,
+        )
+    )
+    max_year = max([int(year[2:4]) for year in designations if year.startswith("20")])
+    parts = [
+        "18",
+        "19",
+        *[f"20{year:02}" for year in range(0, max_year + 1)],
+    ]
+
+    for part in parts:
+
+        part_designations = set(desi for desi in designations if desi.startswith(part))
+
+        # Asteroids in this chunk
+        part_index = index.loc[index.Reduced.isin(part_designations)]
+
+        part_index = dict(
+            zip(
+                part_index.Reduced,
+                [
+                    [sso.Name, sso.SsODNetID]
+                    if pd.isna(sso.Number)
+                    else [sso.Name, sso.Number, sso.SsODNetID]
+                    for _, sso in part_index.iterrows()
+                ],
+            )
+        )
+
+        _write_to_cache(part_index, f"d{part}.pkl")
+
+
+def _build_palomar_transit_index(index):
+    """Build the reduced -> name,number,SsODNetID index for anything that is not
+    a name and not a designation.
+
+    Parameters
+    ----------
+    index : pd.DataFrame
+        The formatted index from SsODNet.
+    """
+
+    rest = set(
+        red
+        for red in index.Reduced
+        if not re.match(
+            r"(^([11][8-9][0-9]{2}[a-z]{2}[0-9]{0,3}$)|"
+            r"(^20[0-9]{2}[a-z]{2}[0-9]{0,3}$))",
+            red,
+        )
+        and not re.match(r"^[a-z\'-]*$", red)
+    )
+
+    part_index = index.loc[index.Reduced.isin(rest)]
+
+    part_index = dict(
+        zip(
+            part_index.Reduced,
+            [
+                [sso.Name, sso.SsODNetID]
+                if pd.isna(sso.Number)
+                else [sso.Name, sso.Number, sso.SsODNetID]
+                for _, sso in part_index.iterrows()
+            ],
+        )
+    )
+
+    _write_to_cache(part_index, "PLT.pkl")
+
+
+def _build_fuzzy_searchable_index(index):
+    """Merge name, number and SsODNet ID of all entries to fuzzy-searchable lines.
+
+    Parameters
+    ----------
+    index : pd.DataFrame
+        The formatted index from SsODNet.
+    """
+
+    LINES = []
+
+    for _, row in index.sort_values(["Number", "Name"]).iterrows():
+
+        number = f"({int(row.Number)})" if not pd.isna(row.Number) else ""
+        name = row.Name
+
+        LINE = (
+            f"{number.ljust(9)} {name.ljust(20)}".encode(sys.getdefaultencoding())
+            + b"\n"
+        )
+
+        LINES.append(LINE)
+
+    _write_to_cache(LINES, "fuzzy_index.pkl")
+
+
+def _write_to_cache(obj, filename):
+    """Save the pickled object to the path in the rocks cache.
+
+    Parameters
+    ----------
+    obj : any
+        The python object to pickle.
+    filename : str
+        The output filename, relative to the index directory.
+    """
+
+    with open(rocks.PATH_INDEX / filename, "wb") as file_:
+        obj_pickled = pickle.dumps(obj, protocol=4)
+        file_.write(pickletools.optimize(obj_pickled))
+
+
+# ------
+# Retrieving the index
+def _retrieve_index_from_ssodnet():
+    """Download and format asteroid name-number index from SsODNet.
+
+    Returns
+    -------
+    pd.DataFrame
+        The formatted index.
+    """
+
+    # The gzipped index is exposed under this address
+    URL_INDEX = "https://asterm.imcce.fr/public/ssodnet/sso_index.csv.gz"
+    index = pd.read_csv(URL_INDEX)
+
+    # There are some spurious spaces in the column headers
+    index = index.rename(columns={c: c.replace(" ", "") for c in index.columns})
+
+    # We use NaNs instead of 0 for unnumbered objects
+    index.loc[index["Number"] == 0, "Number"] = np.nan
+    index["Number"] = index["Number"].astype("Int64")
+
+    return index
+
+
+def get_aliases(ssodnetid):
+    """Return the aliases of the passed reduced identifier.
+
+    Parameters
+    ----------
+    ssodnetid :  str
+        The SsODNetID of the asteroid to alias.
+
+    Returns
+    -------
+    list
+        The list of aliases of the asteroid.
+    """
+    return _load("aliases.pkl")[ssodnetid]
+
+
+def get_index_file(id_: typing.Union[int, str]) -> dict:
+    """Get absolute path to the index chunk where id_ is located.
+
+    Parameters
+    ----------
+    id_ : str, int
+        The asteroid identifier.
+
+    Returns
+    -------
+    str
+        The absolute path to the index chunk.
+    """
+
+    # Is it numeric?
+    if isinstance(id_, int):
+        index_range = np.arange(1, int(1e6), int(1e3))
+        try:
+            index_number = index_range[index_range <= id_][-1]
+        # The passed id_ is larger than 1e6
+        except IndexError:
+            index_number = 1
+        which = f"{index_number}.pkl"
+
+    # Is it a name?
+    elif re.match(r"^[a-z\'-]*$", id_) or id_ == r"g!kun||'homdima":
+
+        if id_[0] == "'":  # catch 'aylo'chaxnim
+            which = f"{id_[0]}.pkl"
+        else:
+            which = rocks.PATH_INDEX / f"{id_[0]}.pkl"
+
+    # Is it a designation?
+    elif re.match(
+        r"(^([11][8-9][0-9]{2}[a-z]{2}[0-9]{0,3}$)|"
+        r"(^20[0-9]{2}[a-z]{2}[0-9]{0,3}$))",
+        id_,
+    ):
+        if id_.startswith("20"):
+            year = f"20{id_[2:4]}"
+        else:
+            year = id_[:2]
+        which = f"d{year}.pkl"
+
+    # Should be in this one then
+    else:
+        which = "PLT.pkl"
+
+    if not which in rocks.INDEX:
+        rocks.INDEX[which] = _load(which)
+
+    return rocks.INDEX[which]
+
+
+def _load(which):
+    """Load a pickled index file."""
+    if not (rocks.PATH_INDEX / which).exists():
+        rich.print(
+            "The asteroid name-number index is malformed. Run '$ rocks status' to update it."
+        )
+        sys.exit()
+
+    with open(rocks.PATH_INDEX / which, "rb") as file_:
+        return pickle.load(file_)
