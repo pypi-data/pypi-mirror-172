ab811b7dd2a7e08fa5536285f4507eddbdb374c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:18 +0000

    Add package pandas-eda/0.9.0/pandas_eda-0.9.0-py3-none-any.whl

---
+++ packages/pandas-eda/pandas_eda-0.9.0-py3-none-any.whl/pandas_eda/__init__.py
@@ -0,0 +1,25 @@
+import sys
+import subprocess
+import tempfile
+import atexit
+
+import pandas as pd
+import pandas_eda.streamlit_app
+import pandas_eda.explore
+
+
+def eda(df):
+    fp = tempfile.TemporaryFile(suffix='.xlsx', delete=False)  # TODO - delete file at exit
+    df.to_excel(fp, index=None)
+    args = [sys.executable, "-m", "streamlit", "run", pandas_eda.streamlit_app.__file__,
+            "--", fp.name,
+            ]
+    process = subprocess.Popen(args)
+    atexit.register(eda_exit_handler, process)
+
+
+def eda_exit_handler(process):
+    process.kill()
+
+
+pd.core.frame.DataFrame.eda = lambda df: pandas_eda.eda(df)
