0a708fb7235968dbf6db6a9a82c949c40f4fab08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:14 +0000

    Add package castleamongthestars/1.0.15/castleamongthestars-1.0.15.tar.gz

---
+++ packages/castleamongthestars/castleamongthestars-1.0.15.tar.gz/castleamongthestars-1.0.15/castleamongthestars/__init__.py
@@ -0,0 +1,3 @@
+from castleamongthestars.plinko import autoplinko
+from castleamongthestars.mines import intpredict , indpredict
+from castleamongthestars.crash import crashpredict
---
+++ packages/castleamongthestars/castleamongthestars-1.0.15.tar.gz/castleamongthestars-1.0.15/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="castleamongthestars",
+	author = "Xtev",
+	version='1.0.15',
+	description = "A system",
+	packages=["castleamongthestars"]
+)
