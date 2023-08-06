2b7e394de6af3d3667d846d8208c1b2cfa55e938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:38 +0000

    Add package metacity/0.6.1/metacity-0.6.1-cp39-cp39-macosx_10_9_universal2.whl

---
+++ packages/metacity/metacity-0.6.1-cp39-cp39-macosx_10_9_universal2.whl/metacity/io/__init__.py
@@ -0,0 +1,30 @@
+from metacity.io.geojson import parse as parse_geojson
+from metacity.io.shapefile import parse as parse_shapefile
+
+import metacity.utils.filesystem as fs
+from metacity.geometry import Progress 
+
+__all__ = ["parse", "parse_recursively"]
+
+
+def parse(file: str, progress: Progress = None):
+    if progress is None:
+        progress = Progress("Loading Model")
+
+    if file.endswith('.shp'):
+        return parse_shapefile(file, progress)
+    elif file.endswith('.json'):
+        return parse_geojson(file, progress)
+
+
+
+def parse_recursively(directory: str):
+    models = []
+    progress = Progress("Loading Models")
+    for file in fs.list_files_recursive(directory):
+        submodels = parse(file, progress)
+        if submodels is not None:
+            models.extend(submodels)
+    return models
+
+        
