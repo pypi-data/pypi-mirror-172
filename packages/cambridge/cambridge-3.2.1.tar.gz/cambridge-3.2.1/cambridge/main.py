fe4e799fb99bfc58b77faa481422a8b7e9d881d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:07 +0000

    Add package cambridge/3.2.1/cambridge-3.2.1.tar.gz

---
+++ packages/cambridge/cambridge-3.2.1.tar.gz/cambridge-3.2.1/cambridge/main.py
@@ -0,0 +1,37 @@
+"""
+Cambridge is a terminal version of Cambridge Dictionary.
+The dictionary data comes from https://dictionary.cambridge.org
+If you're not satisfied with the result, you can try with "-w" flag to look up the word in Merriam-Webster Dictionary.
+"""
+
+import sqlite3
+
+# import sys
+# import os
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# from cambridge.args import parse_args
+# from cambridge.cache import DB
+
+from .args import parse_args
+from .cache import DB
+
+
+def main():
+    try:
+        con = sqlite3.connect(
+            DB, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+        )
+        cur = con.cursor()
+
+        args = parse_args()
+        args.func(args, con, cur)
+
+        cur.close()
+        con.close()
+
+    except KeyboardInterrupt:
+        print("\nStopped by user")
+
+
+# main()
