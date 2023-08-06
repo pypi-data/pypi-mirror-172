bbfb8fe86a405cca8dd7641e29f4dd57796c8b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:23 +0000

    Add package turtle2img/1.0.1/turtle2img-1.0.1.tar.gz

---
+++ packages/turtle2img/turtle2img-1.0.1.tar.gz/turtle2img-1.0.1/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+
+def get_readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="turtle2img",
+    version="1.0.1",
+    description="Save turtle images as jpg or png",
+    author="An-20",
+    url="https://github.com/An-20/turtle2img",
+    license="BSD (3 clauses)",
+    long_description=get_readme(),
+    packages=["canvasvg"],
+    package_dir={"canvasvg": "src"},
+    keywords=[
+        "turtle",
+        "image",
+        "png",
+        "jpg"
+    ],
+    install_requires=[
+        "CairoSVG",
+        "Pillow"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Multimedia :: Graphics :: Capture :: Screen Capture",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+    ],
+)
