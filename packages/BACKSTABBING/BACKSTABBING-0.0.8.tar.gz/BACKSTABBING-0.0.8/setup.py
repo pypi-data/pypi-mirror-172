71a12d90df2ddb883b6bf1efbad94620cfd0ca00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:52 +0000

    Add package BACKSTABBING/0.0.8/BACKSTABBING-0.0.8.tar.gz

---
+++ packages/BACKSTABBING/BACKSTABBING-0.0.8.tar.gz/BACKSTABBING-0.0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as fhand:
+#     long_description = fhand.read()
+
+setuptools.setup(
+    name="BACKSTABBING",
+    version="0.0.8",
+    author="Nitesh_Dohre/Akshay_Pratap/Anurag_krishna",
+    author_email="niteshdohre@gmail.com",
+    description="(CLI package to run the server and connect a client to play a multiplayer game!)",
+    url="https://github.com/momus-s/Real-time-network-multiplayer-game",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pygame"],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "bs-server = src.cli:run_server",
+            "bs-client = src.cli:run_client",
+        ]
+    }
+)
---
+++ packages/BACKSTABBING/BACKSTABBING-0.0.8.tar.gz/BACKSTABBING-0.0.8/src/__init__.py
@@ -0,0 +1,6 @@
+from src import Client
+from src import server_connection
+from src import server_constants
+from src import server_game
+from src import server_player
+from src import server_to_play
