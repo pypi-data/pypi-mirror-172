de17e58fa782868a7b5cea052f787b5f28184fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:13 +0000

    Add package django-outbox-menu/1.0.5/django_outbox_menu-1.0.5-py3-none-any.whl

---
+++ packages/django-outbox-menu/django_outbox_menu-1.0.5-py3-none-any.whl/menu/_version.py
@@ -0,0 +1,16 @@
+"""
+Provides menu version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update menu` to change this file.
+
+from incremental import Version
+
+__version__ = Version("menu", 1, 0, 5)
+__all__ = ["__version__"]
+
+
+
+# Update version using 
+# python -m incremental.update menu --newversion 1.0.5
