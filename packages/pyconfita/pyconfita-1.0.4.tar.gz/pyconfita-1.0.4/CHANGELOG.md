a1997ca59ec13c1af155cd94972c958ca3fc193d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:19 +0000

    Add package pyconfita/1.0.4/pyconfita-1.0.4.tar.gz

---
+++ packages/pyconfita/pyconfita-1.0.4.tar.gz/pyconfita-1.0.4/CHANGELOG.md
@@ -0,0 +1,25 @@
+## 1.0.4 (2022-10-21)
+
+- Updated value processing: empty string should not be dumped. Only dump None values.
+- Updated README
+
+## 1.0.3 (2022-10-21)
+
+[Missed CHANGELOG update]
+
+## 1.0.2 (2022-10-14)
+
+- Added case sensitivity option in Confita
+
+## 1.0.1 (2022-10-13)
+
+- Updated setup.py and tools
+
+## 1.0.0 (2022-10-13)
+
+- Initialized package
+- Added Vault backend
+- Added File backend (YAML format)
+- Added Dict backend
+- Added Environment backend
+- Added explicit type conversion support (str, bool, int, float)
---
+++ packages/pyconfita/pyconfita-1.0.4.tar.gz/pyconfita-1.0.4/src/pyconfita/pyconfita.py
@@ -0,0 +1,70 @@
+from typing import List, Optional, Any
+
+from pyconfita.backend.backend import Backend
+from pyconfita.logging_interface import LoggingInterface
+
+
+class Confita:
+    logger: LoggingInterface
+    backends: List[Backend]
+    case_sensitive: bool = True
+
+    def __init__(
+        self,
+        logger: LoggingInterface,
+        backends: List[Backend],
+        case_sensitive: bool = True,
+        *args,
+        **kwargs,
+    ):
+        """
+
+        :param logger:
+        :param backends: list of key-value backends. Order sets the
+        evaluation order for values.
+        :param args:
+        :param kwargs:
+        """
+        self.logger = logger
+        self.backends = backends
+        self.case_sensitive = case_sensitive
+
+    def get(self, key: str, **kwargs) -> Optional[Any]:
+        """
+        Read the value at key in all the backends.
+        Returns the last not None value found in order of the list of
+        backends. Returns None if not found.
+
+        :param key:
+        :param kwargs:
+        :return:
+        """
+        _value = None
+
+        for bk in self.backends:
+            if self.case_sensitive:
+                # Initial casing for key
+                tmp_value = bk.get(key, **kwargs)
+            else:
+                # Try reading with casing variations on key
+                tmp_value = (
+                    bk.get(key, **kwargs)
+                    or bk.get(key.upper(), **kwargs)
+                    or bk.get(key.lower(), **kwargs)
+                )
+            if tmp_value is not None:  # Override if defined only
+                _value = tmp_value
+                self.logger.zlog(
+                    **{
+                        "level": "debug",
+                        "message": {"message": f"{bk.name} reads {key} = {tmp_value}"},
+                    }
+                )
+
+        self.logger.zlog(
+            **{
+                "level": "debug",
+                "message": {"message": f"Final value read for {key} = {_value}"},
+            }
+        )
+        return _value
