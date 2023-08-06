0f3b435bebedeae204a2a32c46565fb79194c3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:31 +0000

    Add package platzky/0.1.10/platzky-0.1.10-py3-none-any.whl

---
+++ packages/platzky/platzky-0.1.10-py3-none-any.whl/platzky/db_loader.py
@@ -0,0 +1,13 @@
+from importlib.util import spec_from_file_location, module_from_spec
+import os
+import sys
+from os.path import dirname, abspath
+
+
+def load_db_driver(db_type):
+    db_dir = os.path.join(dirname(abspath(__file__)), 'db')
+    spec = spec_from_file_location(db_type, os.path.join(db_dir, db_type + ".py"))
+    db_driver = module_from_spec(spec)
+    sys.modules[db_type] = db_driver
+    spec.loader.exec_module(db_driver)
+    return db_driver
