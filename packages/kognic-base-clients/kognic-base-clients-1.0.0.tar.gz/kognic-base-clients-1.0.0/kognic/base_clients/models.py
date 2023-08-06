77e3392eedc1ca563fcd52fc1d6668629ecae00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:45 +0000

    Add package kognic-base-clients/1.0.0/kognic-base-clients-1.0.0.tar.gz

---
+++ packages/kognic-base-clients/kognic-base-clients-1.0.0.tar.gz/kognic-base-clients-1.0.0/kognic/base_clients/models.py
@@ -0,0 +1,32 @@
+from typing import Union, Optional, Dict
+
+from humps import camelize
+from pydantic import BaseModel
+
+
+def to_camel_case(string: str) -> str:
+    return camelize(string)
+
+
+class BaseSerializer(BaseModel):
+
+    @classmethod
+    def from_json(cls, js: Dict):
+        return cls.parse_obj(js)
+
+    def to_dict(self, by_alias=True) -> Dict:
+        return self.dict(exclude_none=True, by_alias=by_alias)
+
+    class Config:
+        alias_generator = to_camel_case
+        orm_mode = True
+        allow_population_by_field_name = True
+
+
+class PageMetadata(BaseSerializer):
+    next_cursor_id: Optional[int]
+
+
+class PaginatedResponse(BaseSerializer):
+    data: Union[dict, list]
+    metadata: PageMetadata
