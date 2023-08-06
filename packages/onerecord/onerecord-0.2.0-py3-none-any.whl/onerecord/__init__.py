02adbfed29f7e84d8e87fa8a23953d445ae7ca6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:22 +0000

    Add package onerecord/0.2.0/onerecord-0.2.0-py3-none-any.whl

---
+++ packages/onerecord/onerecord-0.2.0-py3-none-any.whl/onerecord/__init__.py
@@ -0,0 +1,15 @@
+import logging
+
+from onerecord.client import ONERecordClient
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s.%(funcName)s:%(lineno)d -  %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+__all__ = [
+    "ONERecordClient",
+]
+
+__version__ = "0.2.0"
