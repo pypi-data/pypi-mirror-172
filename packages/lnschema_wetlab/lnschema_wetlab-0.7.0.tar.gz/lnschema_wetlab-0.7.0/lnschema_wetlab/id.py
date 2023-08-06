cec59a77de351eab9f59a93a3c80093028444622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:34 +0000

    Add package lnschema_wetlab/0.7.0/lnschema_wetlab-0.7.0.tar.gz

---
+++ packages/lnschema_wetlab/lnschema_wetlab-0.7.0.tar.gz/lnschema_wetlab-0.7.0/lnschema_wetlab/id.py
@@ -0,0 +1,27 @@
+"""IDs.
+
+Entity-related generators:
+
+.. autosummary::
+   :toctree: .
+
+   biometa
+   techsample
+   biosample
+   readout
+   treatment
+   experiment
+   experiment_type
+   project
+
+"""
+from ._id import (  # noqa
+    biometa,
+    biosample,
+    experiment,
+    experiment_type,
+    project,
+    readout,
+    techsample,
+    treatment,
+)
