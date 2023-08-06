bdc158388fc40e2a145433576e07ee5f589ce5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:54 +0000

    Add package spimdisasm/1.7.0/spimdisasm-1.7.0.tar.gz

---
+++ packages/spimdisasm/spimdisasm-1.7.0.tar.gz/spimdisasm-1.7.0/spimdisasm/elf32/Elf32File.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2022 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from typing import Callable
+
+from .. import common
+
+from .Elf32Constants import Elf32HeaderIdentifier, Elf32HeaderFlag, Elf32SectionHeaderType
+from .Elf32Dyns import Elf32Dyns
+from .Elf32GlobalOffsetTable import Elf32GlobalOffsetTable
+from .Elf32Header import Elf32Header
+from .Elf32RegInfo import Elf32RegInfo
+from .Elf32SectionHeaders import Elf32SectionHeaders, Elf32SectionHeaderEntry
+from .Elf32StringTable import Elf32StringTable
+from .Elf32Syms import Elf32Syms
+from .Elf32Rels import Elf32Rels
+
+
+class Elf32File:
+    def __init__(self, array_of_bytes: bytearray):
+        self.header = Elf32Header.fromBytearray(array_of_bytes)
+        # print(self.header)
+
+        dataEncoding = self.header.ident.getDataEncoding()
+        if dataEncoding == Elf32HeaderIdentifier.DataEncoding.DATA2MSB:
+            common.GlobalConfig.ENDIAN = common.InputEndian.BIG
+        elif dataEncoding == Elf32HeaderIdentifier.DataEncoding.DATA2LSB:
+            common.GlobalConfig.ENDIAN = common.InputEndian.LITTLE
+
+        elfFlags, unknownElfFlags = Elf32HeaderFlag.parseFlags(self.header.flags)
+        self.elfFlags = elfFlags
+        self.unknownElfFlags = unknownElfFlags
+
+        if self.unknownElfFlags != 0:
+            common.Utils.eprint(f"Warning: Elf header has unknown flags: 0x{self.unknownElfFlags:X}")
+
+        self.strtab: Elf32StringTable | None = None
+        self.symtab: Elf32Syms | None = None
+
+        self.dynamic: Elf32Dyns | None = None
+        self.dynstr: Elf32StringTable | None = None
+        self.dynsym: Elf32Syms | None = None
+
+        self.progbits: dict[common.FileSectionType, Elf32SectionHeaderEntry] = dict()
+        self.nobits: Elf32SectionHeaderEntry | None = None
+
+        self.rel: dict[common.FileSectionType, Elf32Rels] = dict()
+
+        self.reginfo: Elf32RegInfo | None = None
+
+        self.sectionHeaders = Elf32SectionHeaders(array_of_bytes, self.header.shoff, self.header.shnum)
+
+        shstrtabSectionEntry = self.sectionHeaders.sections[self.header.shstrndx]
+        self.shstrtab = Elf32StringTable(array_of_bytes, shstrtabSectionEntry.offset, shstrtabSectionEntry.size)
+
+        self.got: Elf32GlobalOffsetTable | None = None
+
+        for entry in self.sectionHeaders.sections:
+            sectionEntryName = self.shstrtab[entry.name]
+            # print(sectionEntryName, end="\t ")
+            # print(entry)
+
+            callback = self._sectionProcessorCallbacks.get(entry.type)
+            if callback is not None:
+                callback(self, array_of_bytes, entry, sectionEntryName)
+            elif common.GlobalConfig.VERBOSE:
+                common.Utils.eprint("Unknown section header type found:", sectionEntryName, entry, "\n")
+
+        if self.got is not None and self.dynamic is not None and self.dynsym is not None:
+            self.got.initTables(self.dynamic, self.dynsym)
+
+
+    def _processSection_NULL(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        pass
+
+    def _processSection_PROGBITS(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        fileSecType = common.FileSectionType.fromStr(sectionEntryName)
+
+        if fileSecType != common.FileSectionType.Invalid:
+            self.progbits[fileSecType] = entry
+            if fileSecType == common.FileSectionType.Text:
+                self.sectionHeaders.mipsText = entry
+            elif fileSecType == common.FileSectionType.Data:
+                self.sectionHeaders.mipsData = entry
+        elif sectionEntryName == ".got":
+            self.got = Elf32GlobalOffsetTable(array_of_bytes, entry.offset, entry.size)
+        elif sectionEntryName == ".interp":
+            # strings with names of dynamic libraries
+            common.Utils.printVerbose(f"Unhandled SYMTAB found: '{sectionEntryName}'")
+        elif sectionEntryName == ".MIPS.stubs":
+            # ?
+            common.Utils.printVerbose(f"Unhandled SYMTAB found: '{sectionEntryName}'")
+        elif sectionEntryName == ".init":
+            # ?
+            common.Utils.printVerbose(f"Unhandled SYMTAB found: '{sectionEntryName}'")
+        elif common.GlobalConfig.VERBOSE:
+            common.Utils.eprint(f"Unhandled PROGBITS found: '{sectionEntryName}'", entry, "\n")
+
+    def _processSection_SYMTAB(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        if sectionEntryName == ".symtab":
+            self.symtab = Elf32Syms(array_of_bytes, entry.offset, entry.size)
+        elif common.GlobalConfig.VERBOSE:
+            common.Utils.eprint("Unhandled SYMTAB found: ", sectionEntryName, entry, "\n")
+
+    def _processSection_STRTAB(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        if sectionEntryName == ".strtab":
+            self.strtab = Elf32StringTable(array_of_bytes, entry.offset, entry.size)
+        elif sectionEntryName == ".dynstr":
+            self.dynstr = Elf32StringTable(array_of_bytes, entry.offset, entry.size)
+        elif sectionEntryName == ".shstrtab":
+            pass
+        elif common.GlobalConfig.VERBOSE:
+            common.Utils.eprint("Unhandled STRTAB found: ", sectionEntryName, entry, "\n")
+
+    def _processSection_RELA(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+    def _processSection_HASH(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+    def _processSection_DYNAMIC(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        if sectionEntryName == ".dynamic":
+            self.dynamic = Elf32Dyns(array_of_bytes, entry.offset, entry.size)
+        elif common.GlobalConfig.VERBOSE:
+            common.Utils.eprint("Unhandled DYNAMIC found: ", sectionEntryName, entry, "\n")
+
+    def _processSection_NOTE(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+    def _processSection_NOBITS(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        if sectionEntryName == ".bss":
+            self.nobits = entry
+        elif common.GlobalConfig.VERBOSE:
+            common.Utils.eprint("Unhandled NOBITS found: ", sectionEntryName, entry, "\n")
+
+    def _processSection_REL(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        if sectionEntryName.startswith(".rel."):
+            fileSecType = common.FileSectionType.fromStr(sectionEntryName[4:])
+            if fileSecType != common.FileSectionType.Invalid:
+                self.rel[fileSecType] = Elf32Rels(array_of_bytes, entry.offset, entry.size)
+            elif common.GlobalConfig.VERBOSE:
+                common.Utils.eprint("Unhandled REL subsection found: ", sectionEntryName, entry, "\n")
+        elif common.GlobalConfig.VERBOSE:
+            common.Utils.eprint("Unhandled REL found: ", sectionEntryName, entry, "\n")
+
+    def _processSection_DYNSYM(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        if sectionEntryName == ".dynsym":
+            self.dynsym = Elf32Syms(array_of_bytes, entry.offset, entry.size)
+        elif common.GlobalConfig.VERBOSE:
+            common.Utils.eprint("Unhandled DYNSYM found: ", sectionEntryName, entry, "\n")
+
+
+    def _processSection_MIPS_LIBLIST(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+    def _processSection_MIPS_MSYM(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+    def _processSection_MIPS_GPTAB(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+    def _processSection_MIPS_DEBUG(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+    def _processSection_MIPS_REGINFO(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        if sectionEntryName == ".reginfo":
+            self.reginfo = Elf32RegInfo.fromBytearray(array_of_bytes, entry.offset)
+        elif common.GlobalConfig.VERBOSE:
+            common.Utils.eprint("Unhandled MIPS_REGINFO found: ", sectionEntryName, entry, "\n")
+
+    def _processSection_MIPS_OPTIONS(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+    def _processSection_MIPS_SYMBOL_LIB(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+    def _processSection_MIPS_ABIFLAGS(self, array_of_bytes: bytearray, entry: Elf32SectionHeaderEntry, sectionEntryName: str) -> None:
+        # ?
+        pass
+
+
+    _sectionProcessorCallbacks: dict[int, Callable[[Elf32File, bytearray, Elf32SectionHeaderEntry, str], None]] = {
+        Elf32SectionHeaderType.NULL.value: _processSection_NULL,
+        Elf32SectionHeaderType.PROGBITS.value: _processSection_PROGBITS,
+        Elf32SectionHeaderType.SYMTAB.value: _processSection_SYMTAB,
+        Elf32SectionHeaderType.STRTAB.value: _processSection_STRTAB,
+        # Elf32SectionHeaderType.RELA.value: _processSection_RELA,
+        Elf32SectionHeaderType.HASH.value: _processSection_HASH,
+        Elf32SectionHeaderType.DYNAMIC.value: _processSection_DYNAMIC,
+        # Elf32SectionHeaderType.NOTE.value: _processSection_NOTE,
+        Elf32SectionHeaderType.NOBITS.value: _processSection_NOBITS,
+        Elf32SectionHeaderType.REL.value: _processSection_REL,
+        Elf32SectionHeaderType.DYNSYM.value: _processSection_DYNSYM,
+
+        Elf32SectionHeaderType.MIPS_LIBLIST.value: _processSection_MIPS_LIBLIST,
+        Elf32SectionHeaderType.MIPS_MSYM.value: _processSection_MIPS_MSYM,
+        Elf32SectionHeaderType.MIPS_GPTAB.value: _processSection_MIPS_GPTAB,
+        Elf32SectionHeaderType.MIPS_DEBUG.value: _processSection_MIPS_DEBUG,
+        Elf32SectionHeaderType.MIPS_REGINFO.value: _processSection_MIPS_REGINFO,
+        Elf32SectionHeaderType.MIPS_OPTIONS.value: _processSection_MIPS_OPTIONS,
+        Elf32SectionHeaderType.MIPS_SYMBOL_LIB.value: _processSection_MIPS_SYMBOL_LIB,
+        Elf32SectionHeaderType.MIPS_ABIFLAGS.value: _processSection_MIPS_ABIFLAGS,
+    }
---
+++ packages/spimdisasm/spimdisasm-1.7.0.tar.gz/spimdisasm-1.7.0/spimdisasm/elfObjDisasm/ElfObjDisasmInternals.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2022 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import argparse
+from pathlib import Path
+
+from .. import common
+from .. import elf32
+from .. import mips
+
+
+def getArgsParser() -> argparse.ArgumentParser:
+    # TODO
+    description = ""
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument("binary", help="Path to input elf binary file")
+    parser.add_argument("output", help="Path to output. Use '-' to print to stdout instead")
+
+    parser.add_argument("--data-output", help="Path to output the data and rodata disassembly")
+
+    parser.add_argument("--save-context", help="Saves the context to a file", metavar="FILENAME")
+
+    common.GlobalConfig.addParametersToArgParse(parser)
+
+    mips.InstructionConfig.addParametersToArgParse(parser)
+
+    return parser
+
+def applyArgs(args: argparse.Namespace) -> None:
+    mips.InstructionConfig.parseArgs(args)
+    common.GlobalConfig.parseArgs(args)
+
+def applyGlobalConfigurations() -> None:
+    common.GlobalConfig.REMOVE_POINTERS = False
+    common.GlobalConfig.IGNORE_BRANCHES = False
+    common.GlobalConfig.SYMBOL_FINDER_FILTER_LOW_ADDRESSES = False
+
+    common.GlobalConfig.ALLOW_UNKSEGMENT = False
+
+
+def getOutputPath(inputPath: Path, textOutput: Path, dataOutput: Path, sectionType: common.FileSectionType) -> Path:
+    outputPath = dataOutput
+    if sectionType == common.FileSectionType.Text:
+        outputPath = textOutput
+
+    outputFilePath = outputPath
+    if outputPath != "-":
+        outputFilePath = outputFilePath / inputPath.stem
+
+    return outputFilePath
+
+def getProcessedSections(context: common.Context, elfFile: elf32.Elf32File, array_of_bytes: bytearray, inputPath: Path, textOutput: Path, dataOutput: Path) -> tuple[dict[common.FileSectionType, mips.sections.SectionBase], dict[common.FileSectionType, Path]]:
+    processedSegments: dict[common.FileSectionType, mips.sections.SectionBase] = dict()
+    segmentPaths: dict[common.FileSectionType, Path] = dict()
+
+    for sectionType, sectionEntry in elfFile.progbits.items():
+        outputFilePath = getOutputPath(inputPath, textOutput, dataOutput, sectionType)
+
+        vromStart = sectionEntry.offset
+        vromEnd = vromStart + sectionEntry.size
+        vramStart = sectionEntry.addr
+
+        mipsSection: mips.sections.SectionBase
+        if sectionType == common.FileSectionType.Text:
+            mipsSection = mips.sections.SectionText(context, vromStart, vromEnd, vramStart, inputPath.stem, array_of_bytes, 0, None)
+        elif sectionType == common.FileSectionType.Data:
+            mipsSection = mips.sections.SectionData(context, vromStart, vromEnd, vramStart, inputPath.stem, array_of_bytes, 0, None)
+        elif sectionType == common.FileSectionType.Rodata:
+            mipsSection = mips.sections.SectionRodata(context, vromStart, vromEnd, vramStart, inputPath.stem, array_of_bytes, 0, None)
+        else:
+            common.Utils.eprint(f"Error! Invalid section type '{sectionType}'")
+            exit(-1)
+        mipsSection.setCommentOffset(vromStart)
+        processedSegments[sectionType] = mipsSection
+        segmentPaths[sectionType] = outputFilePath
+
+    if elfFile.nobits is not None:
+        outputFilePath = getOutputPath(inputPath, textOutput, dataOutput, common.FileSectionType.Bss)
+
+        vromStart = elfFile.nobits.offset
+        vromEnd = vromStart + elfFile.nobits.size
+        bssStart = elfFile.nobits.addr
+        bssEnd = bssStart + elfFile.nobits.size
+
+        mipsSection = mips.sections.SectionBss(context, vromStart, vromEnd, bssStart, bssEnd, inputPath.stem, 0, None)
+        mipsSection.setCommentOffset(vromStart)
+        processedSegments[common.FileSectionType.Bss] = mipsSection
+        segmentPaths[common.FileSectionType.Bss] = outputFilePath
+
+    return processedSegments, segmentPaths
+
+def changeGlobalSegmentRanges(context: common.Context, processedSegments: dict[common.FileSectionType, mips.sections.SectionBase]) -> None:
+    lowestVromStart = None
+    highestVromEnd = None
+    lowestVramStart = None
+    highestVramEnd = None
+
+    for subSegment in processedSegments.values():
+        if lowestVromStart is None or subSegment.vromStart < lowestVromStart:
+            lowestVromStart = subSegment.vromStart
+        if highestVromEnd is None or subSegment.vromEnd > highestVromEnd:
+            highestVromEnd = subSegment.vromEnd
+        if lowestVramStart is None or subSegment.vram < lowestVramStart:
+            lowestVramStart = subSegment.vram
+        if highestVramEnd is None or subSegment.vramEnd > highestVramEnd:
+            highestVramEnd = subSegment.vramEnd
+
+    if lowestVromStart is None:
+        lowestVromStart = 0x0
+    if highestVromEnd is None:
+        highestVromEnd = 0xFFFFFFFF
+    if lowestVramStart is None:
+        lowestVramStart = 0x0
+    if highestVramEnd is None:
+        highestVramEnd = 0xFFFFFFFF
+    context.globalSegment.changeRanges(lowestVromStart, highestVromEnd, lowestVramStart, highestVramEnd)
+    context.totalVramStart = lowestVramStart
+    context.totalVramEnd = highestVramEnd
+    return
+
+
+def addRelocatedSymbol(context: common.Context, symEntry: elf32.Elf32SymEntry, symName: str|None):
+    if symEntry.value == 0:
+        return
+
+    if symEntry.stType == elf32.Elf32SymbolTableType.FUNC.value:
+        contextSym = context.globalSegment.addFunction(symEntry.value)
+    elif symEntry.stType == elf32.Elf32SymbolTableType.OBJECT.value:
+        contextSym = context.globalSegment.addSymbol(symEntry.value)
+    elif symEntry.stType == elf32.Elf32SymbolTableType.SECTION.value:
+        # print(symEntry)
+        return
+    elif symEntry.stType == elf32.Elf32SymbolTableType.NOTYPE.value:
+        # Is ok to just ignore this?
+        return
+    else:
+        common.Utils.eprint(f"Warning: symbol '{symName}' has an unhandled stType: '{symEntry.stType}'")
+        contextSym = context.globalSegment.addSymbol(symEntry.value)
+    if symName is not None:
+        contextSym.name = symName
+    contextSym.isUserDeclared = True
+    contextSym.setSizeIfUnset(symEntry.size)
+
+def insertSymtabIntoContext(context: common.Context, symbolTable: elf32.Elf32Syms, stringTable: elf32.Elf32StringTable, elfFile: elf32.Elf32File, processedSegments: dict[common.FileSectionType, mips.sections.SectionBase]):
+    # Use the symbol table to replace symbol names present in disassembled sections
+    for i, symEntry in enumerate(symbolTable):
+        symName = stringTable[symEntry.name]
+
+        if symEntry.shndx == 0:
+            continue
+
+        sectHeaderEntry = elfFile.sectionHeaders[symEntry.shndx]
+        if sectHeaderEntry is None:
+            continue
+
+        if elfFile.header.type != elf32.Elf32ObjectFileType.REL.value:
+            addRelocatedSymbol(context, symEntry, symName)
+            continue
+
+        sectName = elfFile.shstrtab[sectHeaderEntry.name]
+        sectType = common.FileSectionType.fromStr(sectName)
+        if sectType != common.FileSectionType.Invalid:
+            subSegment = processedSegments[sectType]
+            symbolOffset = symEntry.value + subSegment.vromStart
+
+            contextOffsetSym = common.ContextOffsetSymbol(symbolOffset, symName, sectType)
+            contextOffsetSym.isUserDeclared = True
+            context.offsetSymbols[sectType][symbolOffset] = contextOffsetSym
+        else:
+            common.Utils.eprint(f"Warning: symbol {i} (name: '{symName}', value: 0x{symEntry.value:X}) is referencing invalid section '{sectName}'")
+
+def insertDynsymIntoContext(context: common.Context, symbolTable: elf32.Elf32Syms, stringTable: elf32.Elf32StringTable):
+    for symEntry in symbolTable:
+        symName = stringTable[symEntry.name]
+
+        addRelocatedSymbol(context, symEntry, symName)
+
+
+def injectAllElfSymbols(context: common.Context, elfFile: elf32.Elf32File, processedSegments: dict[common.FileSectionType, mips.sections.SectionBase]) -> None:
+    if elfFile.symtab is not None and elfFile.strtab is not None:
+        # Inject symbols from the reloc table referenced in each section
+        if elfFile.header.type == elf32.Elf32ObjectFileType.REL.value:
+            for sectType, relocs in elfFile.rel.items():
+                # subSection = processedFiles[sectType][1]
+                for rel in relocs:
+                    symbolEntry = elfFile.symtab[rel.rSym]
+                    symbolName = elfFile.strtab[symbolEntry.name]
+                    if symbolName == "":
+                        continue
+
+                    contextRelocSym = common.ContextRelocSymbol(rel.offset, symbolName, sectType)
+                    contextRelocSym.isDefined = True
+                    contextRelocSym.relocType = rel.rType
+                    context.relocSymbols[sectType][rel.offset] = contextRelocSym
+
+        # Use the symtab to replace symbol names present in disassembled sections
+        insertSymtabIntoContext(context, elfFile.symtab, elfFile.strtab, elfFile, processedSegments)
+
+    if elfFile.dynsym is not None and elfFile.dynstr is not None:
+        # Use the dynsym to replace symbol names present in disassembled sections
+        insertDynsymIntoContext(context, elfFile.dynsym, elfFile.dynstr)
+    return
+
+def processGlobalOffsetTable(context: common.Context, elfFile: elf32.Elf32File) -> None:
+    if elfFile.reginfo is not None:
+        common.GlobalConfig.GP_VALUE = elfFile.reginfo.gpValue
+
+    if elfFile.got is not None:
+        context.got.localsTable = elfFile.got.localsTable
+        context.got.globalsTable = elfFile.got.globalsTable
+
+        for address in context.got.globalsTable:
+            contextSym = context.globalSegment.getSymbol(address)
+            if contextSym is not None:
+                contextSym.isGotGlobal = True
+
+    if elfFile.dynamic is not None:
+        context.got.tableStart = elfFile.dynamic.pltGot
+    return
+
+
+def elfObjDisasmMain():
+    args = getArgsParser().parse_args()
+    applyArgs(args)
+
+    applyGlobalConfigurations()
+
+    context = common.Context()
+
+    inputPath = Path(args.binary)
+    array_of_bytes = common.Utils.readFileAsBytearray(inputPath)
+    elfFile = elf32.Elf32File(array_of_bytes)
+
+    if elf32.Elf32HeaderFlag.PIC in elfFile.elfFlags or elf32.Elf32HeaderFlag.CPIC in elfFile.elfFlags:
+        common.GlobalConfig.PIC = True
+
+    textOutput = Path(args.output)
+    if args.data_output is None:
+        dataOutput = textOutput
+    else:
+        dataOutput = Path(args.data_output)
+
+    processedSegments, segmentPaths = getProcessedSections(context, elfFile, array_of_bytes, inputPath, textOutput, dataOutput)
+
+    changeGlobalSegmentRanges(context, processedSegments)
+    injectAllElfSymbols(context, elfFile, processedSegments)
+    processGlobalOffsetTable(context, elfFile)
+
+    for subSegment in processedSegments.values():
+        subSegment.analyze()
+        subSegment.printAnalyzisResults()
+
+    for sectionType, subSegment in processedSegments.items():
+        outputFilePath = segmentPaths[sectionType]
+        mips.FilesHandlers.writeSection(outputFilePath, subSegment)
+
+    if args.save_context is not None:
+        contextPath = Path(args.save_context)
+        contextPath.parent.mkdir(parents=True, exist_ok=True)
+        context.saveContextToFile(contextPath)
