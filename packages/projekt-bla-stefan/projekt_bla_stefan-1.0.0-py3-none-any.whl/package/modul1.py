67350c5504eee1e562977b75dd5025e9fec4e690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:36 +0000

    Add package projekt-bla-stefan/1.0.0/projekt_bla_stefan-1.0.0-py3-none-any.whl

---
+++ packages/projekt-bla-stefan/projekt_bla_stefan-1.0.0-py3-none-any.whl/package/modul1.py
@@ -0,0 +1,5 @@
+from . import modul2
+
+def funktion1():
+    print("Grüße aus Modul 1")
+
---
+++ packages/projekt-bla-stefan/projekt_bla_stefan-1.0.0-py3-none-any.whl/package/modul2.py
@@ -0,0 +1,9 @@
+def funktion2():
+    print("Grüße aus Modul 2")
+
+
+def main():
+    funktion2()
+
+if __name__ == "__main__":
+    main()
