d8587c53042d0824aaaf609469b2dc52ea673fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:20 +0000

    Add package Mensajes-hanselt-abel/5.1/Mensajes-hanselt_abel-5.1.tar.gz

---
+++ packages/Mensajes-hanselt-abel/Mensajes-hanselt_abel-5.1.tar.gz/Mensajes-hanselt_abel-5.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Mensajes-hanselt_abel',
+    version='5.1',
+    description='Un paquete para saludar y despedir',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Hanselt Baca',
+    author_email='hanselt_a14@hotmail.com',
+    url='https://twitter.com/hanselt_abel',
+    license_files=['LICENSE'],
+    packages=find_packages(),    
+    scripts=[],
+    install_requires=[paquete.strip()   for paquete in open("requirements.txt").readlines()],
+    test_suite='tests',
+    classifiers=[
+        #Categorias
+        'Environment :: Console',
+        'Topic :: Artistic Software',
+        'License :: Free For Educational Use',
+        'Programming Language :: Python :: 3'
+    ]
+)
