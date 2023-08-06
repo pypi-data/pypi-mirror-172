3974b3c972ce5219849bcdea4e52ab912695dbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:03 +0000

    Add package allianceauth-wiki-js/0.0.10/allianceauth-wiki-js-0.0.10.tar.gz

---
+++ packages/allianceauth-wiki-js/allianceauth-wiki-js-0.0.10.tar.gz/allianceauth-wiki-js-0.0.10/wikijs/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, re_path
+
+from . import views
+
+app_name = 'wikijs'
+
+module_urls = [
+    re_path(r'^activate/$', views.activate_wikijs, name='activate'),
+    re_path(r'^deactivate/$', views.deactivate_wikijs, name='deactivate'),
+    re_path(r'^reset_password/$', views.reset_password, name='reset_password'),
+    re_path(r'^set_password/$', views.set_password, name='set_password'),
+
+]
+
+urlpatterns = [
+    re_path(r'^wikijs/', include((module_urls, app_name), namespace=app_name))
+]
