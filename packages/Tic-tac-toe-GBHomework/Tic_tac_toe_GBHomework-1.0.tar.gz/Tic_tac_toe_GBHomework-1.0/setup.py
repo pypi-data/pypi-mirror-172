a5d1d79337ef692c5726f201c410fa684f44fb70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:21 +0000

    Add package Tic-tac-toe-GBHomework/1.0/Tic_tac_toe_GBHomework-1.0.tar.gz

---
+++ packages/Tic-tac-toe-GBHomework/Tic_tac_toe_GBHomework-1.0.tar.gz/Tic_tac_toe_GBHomework-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='Tic_tac_toe_GBHomework',
+	version='1.0',
+	author='MrFlansDay',
+	author_email='lexa2004924@gmail.com',
+	description='GB Homework',
+	packages=['Tic_tac_toe_GBHomework'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
