900d8a901906db19fd96d4ae98a4cfd3ac035419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:22 +0000

    Add package woshinidie002/0.0.1/woshinidie002-0.0.1.tar.gz

---
+++ packages/woshinidie002/woshinidie002-0.0.1.tar.gz/woshinidie002-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+setuptools.setup(
+    name="woshinidie002",
+    version="0.0.1",
+    author="Sen",
+    description='A  module for test', # 简要描述
+    author_email='zhangsen2001@gmail.com',   # 作者邮件
+    url='https://github.com/SengeNB/OliverSen',
+    packages = setuptools.find_packages(),
+
+)
