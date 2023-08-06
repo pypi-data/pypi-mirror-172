a4ccff6b94a248ee28104ea871572082e2428c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:30 +0000

    Add package image-israel/0.0.2/image_israel-0.0.2.tar.gz

---
+++ packages/image-israel/image_israel-0.0.2.tar.gz/image_israel-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_israel",
+    version="0.0.2",
+    author="israelclaudio",
+    author_email="israelclaudio21@gmail.com",
+    description="Pacote criado para processamento de imagem, a partir do curso de criação de pacotes da DIO",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/israelclaudio/image_processing_package",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.5',
+)
