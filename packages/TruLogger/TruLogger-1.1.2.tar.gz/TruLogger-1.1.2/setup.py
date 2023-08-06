2f59e23ae4374e35c88e153c9441de3deb95a6a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:09 +0000

    Add package TruLogger/1.1.2/TruLogger-1.1.2.tar.gz

---
+++ packages/TruLogger/TruLogger-1.1.2.tar.gz/TruLogger-1.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='TruLogger',
+    version='1.1.2',
+    description='Logger package',
+    author='Jacob Truman',
+    author_email='jacob.truman@gmail.com',
+    url='https://github.com/jacobtruman/TruLogger',
+    packages=['trulogger'],
+    install_requires=[],
+    dependency_links=[]
+)
