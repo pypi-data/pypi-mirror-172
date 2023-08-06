33911360b0dbfdf9bd9141a9b8bc98d94ccc31ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:46 +0000

    Add package puresnmp/2.0.0a21/puresnmp-2.0.0a21-py3-none-any.whl

---
+++ packages/puresnmp/puresnmp-2.0.0a21-py3-none-any.whl/puresnmp/const.py
@@ -0,0 +1,18 @@
+"""
+This file contains various values used to avoid magic numbers and strings in
+the application.
+"""
+#: Maximum number of usable varbinds as defined in :rfc:`3416`
+MAX_VARBINDS = 2147483647
+
+#: A magic value used to detect strict error-handling
+ERRORS_STRICT = "strict"
+
+#: A magic value used to detect lenient error-handling
+ERRORS_WARN = "warn"
+
+#: TCP timeout which is used if not manually overridden
+DEFAULT_TIMEOUT = 6
+
+#: Number of retries we attempt when sending out packets before giving up
+DEFAULT_RETRIES = 10
