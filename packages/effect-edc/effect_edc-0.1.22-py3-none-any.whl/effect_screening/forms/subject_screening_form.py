53170e3f3457f7be9dc0a9dc53ab7bb3da91cd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:43 +0000

    Add package effect-edc/0.1.22/effect_edc-0.1.22-py3-none-any.whl

---
+++ packages/effect-edc/effect_edc-0.1.22-py3-none-any.whl/effect_screening/forms/subject_screening_form.py
@@ -0,0 +1,40 @@
+from typing import Any
+
+from django import forms
+from edc_form_validators import FormValidatorMixin
+from edc_screening.modelform_mixins import AlreadyConsentedFormMixin
+from effect_form_validators.effect_screening import SubjectScreeningFormValidator
+
+from ..models import SubjectScreening
+
+
+class SubjectScreeningForm(AlreadyConsentedFormMixin, FormValidatorMixin, forms.ModelForm):
+
+    form_validator_cls = SubjectScreeningFormValidator
+
+    screening_identifier = forms.CharField(
+        label="Screening Identifier",
+        required=False,
+        widget=forms.TextInput(attrs={"readonly": "readonly"}),
+    )
+
+    subject_identifier = forms.CharField(
+        label="Subject identifier",
+        required=False,
+        widget=forms.TextInput(attrs={"readonly": "readonly"}),
+    )
+
+    def clean(self: Any) -> dict:
+        cleaned_data = super().clean()
+        return cleaned_data
+
+    class Meta:
+        model = SubjectScreening
+        fields = "__all__"
+        labels = {
+            "gender": "Sex",
+            "unsuitable_agreed": (
+                "Does the study clinician agree that the patient is not "
+                "suitable for the study?"
+            ),
+        }
