658c5b23c6f5e39d6cda1cd77992c0bd91671200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:40 +0000

    Add package hesperos/0.1.32/hesperos-0.1.32.tar.gz

---
+++ packages/hesperos/hesperos-0.1.32.tar.gz/hesperos-0.1.32/src/hesperos/annotation/shoulder_bones.py
@@ -0,0 +1,16 @@
+# ============ List of structure to annotate ============
+
+LIST_STRUCTURES = [
+    "BONES",
+]
+
+DICT_SUB_STRUCTURES = {
+    "BONES":[
+        'Initial Results',
+        'Clavicle',
+        'Humerus',
+        'Ribs',
+        'Scapula',
+        'Sternum',
+        'Vertebrae']
+}
