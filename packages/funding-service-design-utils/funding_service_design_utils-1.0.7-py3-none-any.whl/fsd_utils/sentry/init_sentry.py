1f166d280421947cdae650304b61b7ce9d8457eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:43 +0000

    Add package funding-service-design-utils/1.0.7/funding_service_design_utils-1.0.7-py3-none-any.whl

---
+++ packages/funding-service-design-utils/funding_service_design_utils-1.0.7-py3-none-any.whl/fsd_utils/sentry/init_sentry.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+import sentry_sdk
+from fsd_utils import CommonConfig
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+
+def init_sentry():
+    if getenv("SENTRY_DSN"):
+        sentry_sdk.init(
+            environment=CommonConfig.FLASK_ENV,
+            integrations=[
+                FlaskIntegration(),
+            ],
+            traces_sample_rate=0.1,
+            release=getenv("GITHUB_SHA"),
+        )
