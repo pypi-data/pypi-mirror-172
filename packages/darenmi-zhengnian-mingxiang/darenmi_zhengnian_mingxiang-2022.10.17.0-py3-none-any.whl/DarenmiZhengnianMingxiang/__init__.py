49fafb0d32eee38dc23eb080cd442daf498b0116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:40 +0000

    Add package darenmi-zhengnian-mingxiang/2022.10.17.0/darenmi_zhengnian_mingxiang-2022.10.17.0-py3-none-any.whl

---
+++ packages/darenmi-zhengnian-mingxiang/darenmi_zhengnian_mingxiang-2022.10.17.0-py3-none-any.whl/DarenmiZhengnianMingxiang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""darenmi-zhengnian-mingxiang
+https://github.com/apachecn/darenmi-zhengnian-mingxiang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.17.0"
