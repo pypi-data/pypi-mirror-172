bf4f6213a75d535f6245f8e72387b196da1a3101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:34 +0000

    Add package eis1600/0.3.0/eis1600-0.3.0.tar.gz

---
+++ packages/eis1600/eis1600-0.3.0.tar.gz/eis1600-0.3.0/eis1600/bin/insert_uids.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+import sys
+import os
+from argparse import ArgumentParser, Action, RawDescriptionHelpFormatter
+from glob import glob
+from multiprocessing import Pool
+
+from eis1600.helper.repo import travers_eis1600_dir, get_files_from_eis1600_dir, write_to_readme, read_files_from_readme
+from eis1600.markdown.methods import insert_uids
+
+
+class CheckFileEndingAction(Action):
+    def __call__(self, parser, namespace, input_arg, option_string=None):
+        if input_arg and os.path.isfile(input_arg):
+            filepath, fileext = os.path.splitext(input_arg)
+            if fileext != '.EIS1600TMP':
+                parser.error('You need to input an EIS1600TMP file')
+            else:
+                setattr(namespace, self.dest, input_arg)
+        else:
+            setattr(namespace, self.dest, None)
+
+
+if __name__ == '__main__':
+
+    arg_parser = ArgumentParser(prog=sys.argv[0], formatter_class=RawDescriptionHelpFormatter,
+                                description='''Script to insert UIDs in EIS1600TMP file(s) and thereby converting them to final EIS1600 file(s).
+-----
+Give a single EIS1600TMP file as input
+or 
+Give an input AND an output directory for batch processing.
+
+Use -e <EIS1600_repo> to batch process all EIS1600TMP files in the EIS1600 directory which have not been processed yet.
+''')
+    arg_parser.add_argument('-v', '--verbose', action='store_true')
+    arg_parser.add_argument('-e', '--eis1600_repo', type=str,
+                            help='Takes a path to the EIS1600 file repo and batch processes all files which have not been processed yet')
+    arg_parser.add_argument('input', type=str, nargs='?',
+                            help='EIS1600TMP file to process or input directory with EIS1600TMP files to process if an output directory is also given',
+                            action=CheckFileEndingAction)
+    arg_parser.add_argument('output', type=str, nargs='?',
+                            help='Optional, if given batch processes all files from the input directory to the output directory')
+    args = arg_parser.parse_args()
+
+    verbose = args.verbose
+
+    if args.input and not args.output:
+        infile = './' + args.input
+        insert_uids(infile, None, verbose)
+
+    elif args.output:
+        input_dir = args.input
+        output_dir = args.output
+
+        print(f'Insert UIDs into {input_dir}, save resulting EIS1600 files to {output_dir}')
+
+        infiles = glob(input_dir + '/*.EIS1600TMP')
+        if not infiles:
+            print(
+                'The input directory does not contain any EIS1600TMP files to process')
+            sys.exit()
+
+        # Check if output directory exists else create that directory
+        Path(output_dir).mkdir(exist_ok=True, parents=True)
+
+        params = [(infile, output_dir, verbose) for infile in infiles]
+
+        with Pool() as p:
+            p.starmap_async(insert_uids, params).get()
+    elif args.eis1600_repo:
+        input_dir = args.eis1600_repo
+        if not input_dir[-1] == '/':
+            input_dir += '/'
+
+        print(f'Insert UIDs into files from the EIS1600 repo (only if there is not an EIS1600 file yet)')
+        files_list = read_files_from_readme(input_dir, '# Texts with fixed poetry\n')
+        infiles = get_files_from_eis1600_dir(input_dir, files_list, '*.EIS1600TMP', '*.EIS1600')
+        if not infiles:
+            print(
+                'There are no more EIS1600TMP files to process')
+            sys.exit()
+
+        params = [(infile, None, verbose) for infile in infiles]
+
+        with Pool() as p:
+            p.starmap_async(insert_uids, params).get()
+
+        write_to_readme(input_dir, infiles, '# Texts converted into `.EIS1600`\n', '.EIS1600')
+    else:
+        print(
+            'Pass in a <uri.EIS1600TMP> file to process a single file or use the -e option for batch processing'
+        )
+        sys.exit()
+
+    print('Done')
---
+++ packages/eis1600/eis1600-0.3.0.tar.gz/eis1600-0.3.0/eis1600/helper/repo.py
@@ -0,0 +1,122 @@
+from glob import glob
+from os.path import split, splitext
+
+
+def write_to_readme(path, files, which, ext):
+    file_list = []
+    try:
+        with open(path + 'README.md', 'r', encoding='utf8') as readme_h:
+            out_file_start = ''
+            out_file_end = ''
+            checked_boxes = False
+            line = next(readme_h)
+            while line != which:
+                out_file_start += line
+                line = next(readme_h)
+            out_file_start += line
+            out_file_start += next(readme_h)
+            line = next(readme_h)
+            while line and line != '\n':
+                if line.startswith('- ['):
+                    checked_boxes = True
+                    md, file = line.split('] ')
+                    file_list.append((file, md == '- [x'))
+                    line = next(readme_h, None)
+                else:
+                    file_list.append(line[2])
+                    line = next(readme_h, None)
+            while line:
+                out_file_end += line
+                line = next(readme_h, None)
+
+        for file in files:
+            file_path, uri = split(file)
+            uri, _ = splitext(uri)
+            if checked_boxes:
+                file_list.append((uri + ext + '\n', False))
+            else:
+                file_list.append(uri + ext + '\n')
+
+        file_list.sort()
+
+        def get_entry(file_name, checked):
+            x = 'x' if checked else ' '
+            return '- [' + x + '] ' + file_name
+
+        with open(path + 'README.md', 'w', encoding='utf8') as readme_h:
+            readme_h.write(out_file_start)
+            if checked_boxes:
+                readme_h.writelines([get_entry(file, checked) for file, checked in file_list])
+            else:
+                readme_h.writelines(['- ' + file for file in file_list])
+            readme_h.write(out_file_end)
+            
+    except StopIteration:
+        file_list = []
+        for file in files:
+            file_path, uri = split(file)
+            uri, ext = splitext(uri)
+            file_list.append(uri + '.EIS1600\n')
+        with open(path + 'FILE_LIST.log', 'w', encoding='utf8') as file_list_h:
+            file_list_h.writelines(file_list)
+
+        print(f'Could not write to the README file, check {path + "FILE_LIST.log"} for changed files')
+
+
+def read_files_from_readme(path, which):
+    file_list = []
+    try:
+        with open(path + 'README.md', 'r', encoding='utf8') as readme_h:
+            line = next(readme_h)
+            while line != which:
+                line = next(readme_h)
+            next(readme_h)
+            line = next(readme_h)
+            while line and line != '\n':
+                if line.startswith('- ['):
+                    md, file = line.split('] ')
+                    file_list.append((file[:-1], md == '- [x'))
+                    line = next(readme_h, None)
+                else:
+                    file_list.append(line[2:-1])
+                    line = next(readme_h, None)
+    except StopIteration:
+        print(f'The README.md file does not seem to contain a "{which[:-1]}" section')
+
+    return file_list
+
+
+def get_files_from_eis1600_dir(path, file_list, file_ext_from, file_ext_to=None):
+    path += 'data/'
+    files = []
+    for file in file_list:
+        author, work, text = file.split('.')[:3]
+        file_path = path + '/'.join([author, '.'.join([author, work]), '.'.join([author, work, text])])
+        if file_ext_to and not glob(file_path + file_ext_to):
+            if type(file_ext_from) == list:
+                for ext in file_ext_from:
+                    tmp = glob(file_path + ext)
+                    if tmp:
+                        files.extend(tmp)
+            else:
+                files.extend(glob(file_path + file_ext_from))
+        elif not file_ext_to:
+            files.extend(glob(file_path + file_ext_from))
+    return files
+
+
+def travers_eis1600_dir(path, file_ext_from, file_ext_to=None):
+    path += 'data/*/*/'
+    in_files = glob(path + file_ext_from)
+    if not file_ext_to:
+        return in_files
+    else:
+        exclude_files = glob(path + file_ext_to)
+        files = []
+
+        for file in in_files:
+            path, ext = splitext(file)
+            if not path + '.' + file_ext_to in exclude_files:
+                files.append(file)
+
+        return files
