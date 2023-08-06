12662bd3715f9d0f28fc0c0c7908429acb2317c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:39 +0000

    Add package cornflakes/2.2.7/cornflakes-2.2.7-cp38-cp38-manylinux_2_35_x86_64.whl

---
+++ packages/cornflakes/cornflakes-2.2.7-cp38-cp38-manylinux_2_35_x86_64.whl/cornflakes/cli/_create.py
@@ -0,0 +1,11 @@
+import logging
+
+from cornflakes import click
+from cornflakes.logging import logger
+
+
+@click.command("create")
+def create_new_config():
+    """Create config template."""  # noqa: D400, D401
+    logger.info("call create")
+    logger.debug("debug log?")
