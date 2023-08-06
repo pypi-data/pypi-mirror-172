b3f3e7ca6a93e1675dd2b8eadc2f00b7e363fcfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:30 +0000

    Add package cell-dynamics/0.0.0/cell-dynamics-0.0.0.tar.gz

---
+++ packages/cell-dynamics/cell-dynamics-0.0.0.tar.gz/cell-dynamics-0.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import re, os, sys
+
+setup(
+    name="cell-dynamics",
+    version="0.0.0",
+    python_requires=">3.6.0",
+    author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
+    author_email="mvinyard@broadinstitute.org",
+    url="https://github.com/mvinyard/cell_dynamics",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description="Cell dynamics library.",
+    packages=[
+        "cell_dynamics",
+    ],
+    install_requires=[
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    license="MIT",
+)
