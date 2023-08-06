fe719f245312427f66dfaa02b50bd338bd10abff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:23 +0000

    Add package yewtube/2.9.0/yewtube-2.9.0.tar.gz

---
+++ packages/yewtube/yewtube-2.9.0.tar.gz/yewtube-2.9.0/mps_youtube/test.py
@@ -0,0 +1,5 @@
+import pafy
+from youtubesearchpython import *
+playlist = pafy.all_playlists_from_channel('UC_aEa8K-EOJ3D6gOs7HcyNg')
+print(playlist)
+# https://www.youtube.com/watch?v=aNGKy1Czctc - video pops up for this
