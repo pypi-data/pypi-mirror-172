44cd9c9a17cf965c678a01688a3363fe4bea1d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:33 +0000

    Add package certora-cli/3.4.2/certora-cli-3.4.2.tar.gz

---
+++ packages/certora-cli/certora-cli-3.4.2.tar.gz/certora-cli-3.4.2/certora_cli/EVMVerifier/Compiler/__init__.py
@@ -0,0 +1,20 @@
+"""
+This package include all scripts that are used by the continuous integration tests.
+
+This package is NOT a part of the certora_cli customer-facing Python package.
+"""
+"""
+This package contains scripts that are used for running the Certora Prover.
+
+This package is a part of the certora_cli customer-facing Python package.
+"""
+"""
+This package has files that contain utility functions which are used by all the other packages.
+Specifically, if you have a functionality used by both CI_tests and EVMVerifier, it should probably belong to one of the
+files under the current package.
+
+This package is a part of the certora_cli customer-facing Python package.
+"""
+"""
+This package contains scripts that are used for interacting with the source code compiler.
+"""
