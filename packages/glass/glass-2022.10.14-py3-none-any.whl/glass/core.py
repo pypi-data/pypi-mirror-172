0e259bf5e75996eb81fa4a190c6050fc4b47a8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:26 +0000

    Add package glass/2022.10.14/glass-2022.10.14-py3-none-any.whl

---
+++ packages/glass/glass-2022.10.14-py3-none-any.whl/glass/core.py
@@ -0,0 +1,366 @@
+# author: Nicolas Tessore <n.tessore@ucl.ac.uk>
+# license: MIT
+'''module for core functionality'''
+
+import logging
+import time
+import os.path
+import pickle
+from functools import wraps
+from datetime import timedelta
+from collections import UserDict
+from collections.abc import Sequence, Mapping
+
+from .generator import generator
+
+
+log = logging.getLogger(__name__)
+
+
+# variable definitions
+ITER = 'iteration'
+'''The number of the current iteration, starting from 1.'''
+
+
+class GeneratorError(RuntimeError):
+    '''raised when an error occurred in a generator'''
+
+    def __init__(self, generator, state):
+        '''construct a GeneratorError for generator and state'''
+        self._generator = generator
+        self._state = state
+        g, n = generator.__name__, state[ITER]
+        super().__init__(f'iteration {n}: uncaught exception in {g}')
+
+    @property
+    def generator(self):
+        '''the generator that caused the exception'''
+        return self._generator
+
+    @property
+    def state(self):
+        '''the state of the simulation when the exception occurred'''
+        return self._state
+
+
+class State(UserDict):
+    '''simulation state with recursive lookups and context'''
+
+    Nothing = object()
+
+    def __init__(self, data=None, context=None, **kwargs):
+        super().__init__(data, **kwargs)
+        self.context = context
+        self.data['state'] = self
+
+    def __setitem__(self, key, value):
+        self.setall(key, value)
+
+    def __getitem__(self, key):
+        return self.getall(key)
+
+    def getcontext(self, key, default=Nothing):
+        '''return item from self or context'''
+        if key in self.data:
+            return self.data[key]
+        if self.context is not None:
+            try:
+                return self.context[key]
+            except KeyError:
+                pass
+        if default is not State.Nothing:
+            return default
+        raise KeyError(key)
+
+    def getall(self, key):
+        '''recursive dictionary getter'''
+        if key is None:
+            return None
+        elif isinstance(key, str):
+            if key.endswith('?'):
+                return self.getcontext(key[:-1], None)
+            else:
+                return self.getcontext(key)
+        elif isinstance(key, Sequence):
+            return type(key)(self.getall(k) for k in key)
+        elif isinstance(key, Mapping):
+            return type(key)((k, self.getall(key[k])) for k in key)
+        else:
+            return self.getcontext(key)
+
+    def setall(self, key, value):
+        '''recursive dictionary setter'''
+        if key is None:
+            pass
+        elif isinstance(key, str):
+            self.data[key] = value
+        elif isinstance(key, Sequence):
+            if not isinstance(value, Sequence):
+                raise TypeError('cannot set sequence of keys with non-sequence of values')
+            if len(key) != len(value):
+                raise ValueError(f'cannot set {len(key)} items with {len(value)} values')
+            for k, v in zip(key, value):
+                self.setall(k, v)
+        elif isinstance(key, Mapping):
+            if not isinstance(value, Mapping):
+                raise TypeError('cannot set mapping of keys with non-mapping of values')
+            for k in key:
+                self.setall(key[k], value[k])
+        else:
+            self.data[key] = value
+
+
+def _gencall(generator, state):
+    '''call a generator'''
+
+    t = time.monotonic()
+
+    log.info('--- %s ---', generator.__name__)
+
+    if state is GeneratorExit:
+        generator.close()
+    elif state is None:
+        generator.send(None)
+    else:
+        receives = getattr(generator, 'receives', None)
+        yields = getattr(generator, 'yields', None)
+        state[yields] = generator.send(state[receives])
+
+    log.info('>>> %s: %s <<<', generator.__name__, timedelta(seconds=time.monotonic()-t))
+
+
+def generate(generators, yields=None):
+    '''run generators
+
+    Raises
+    ------
+    :class:`GeneratorError`
+        If an uncaught exception is raised by a generator.  The original
+        exception is the ``__cause__`` of the :class:`GeneratorError`.
+
+    '''
+
+    log.info('=== initialize ===')
+
+    # prime all generators
+    for g in generators:
+        _gencall(g, None)
+
+    # simulation status
+    n = 0
+    t0 = time.monotonic()
+
+    # iterate simulation
+    # for each iteration, run all generators, then yield the results
+    # for each generator, collect its inputs and store its outputs
+    # stop if any of the generators stops by throwing StopIteration
+    # raise a GeneratorError if there is an uncaught exception
+    while True:
+        n += 1
+        ts = time.monotonic()
+
+        state = State()
+        state[ITER] = n
+
+        log.info('=== iteration %d ===', n)
+
+        for g in generators:
+            try:
+                _gencall(g, state)
+            except StopIteration:
+                log.info('>>> generator has stopped the simulation <<<')
+                break
+            except Exception as e:
+                raise GeneratorError(g, state) from e
+        else:  # no break
+            ty = time.monotonic()
+            log.info('--- yield ---')
+            if yields is None:
+                yield state
+            else:
+                yield state[yields]
+            log.info('>>> yield: %s <<<', timedelta(seconds=time.monotonic()-ty))
+            log.info('»»» iteration %d: %s «««', n, timedelta(seconds=time.monotonic()-ts))
+            continue
+        break
+
+    log.info('=== finalize ===')
+
+    # close all generators
+    for g in generators:
+        _gencall(g, GeneratorExit)
+
+    log.info('>>> done in %s <<<', timedelta(seconds=time.monotonic()-t0))
+
+
+def run(generators):
+    '''Run all generators without yielding.
+
+    This function runs the given generators without yielding the results of each
+    iteration.
+
+    Parameters
+    ----------
+    generators : list of generator
+        The generators to run.
+
+    Notes
+    -----
+    Calling :func:`run` is functionally equivalent to calling :func:`generate`
+    in a trivial loop::
+
+        for _ in generate(generators):
+            pass
+
+    '''
+    for _ in generate(generators):
+        pass
+
+
+def group(name, generators):
+    '''group generators under a common name'''
+
+    # create a generator with the named output
+    @generator(receives='state', yields=name)
+    def g():
+        # prime sub-generators
+        for g in generators:
+            _gencall(g, None)
+
+        # initial yield
+        state = None
+
+        # on every iteration, store sub-generators output in sub-state
+        while True:
+            try:
+                context = yield state
+            except GeneratorExit:
+                break
+
+            state = State(context=context)
+
+            for g in generators:
+                _gencall(g, state)
+
+        # finalise sub-generators
+        for g in generators:
+            _gencall(g, GeneratorExit)
+
+    # also update the name of the group for printing
+    g.__name__ = f'group "{name}"'
+
+    # return the generator just created
+    return g()
+
+
+def save(filename, variables):
+    '''Save variables to file on each iteration.
+
+    This generator receives the nominated variables on each iteration and saves
+    their values to ``filename``.  The variables can subsequently be read from
+    the file and iterated using the :func:`load` generator.
+
+    Parameters
+    ----------
+    filename : str
+        Filename for the stored variables.  If ``filename`` does not end in
+        ``'.glass'``, the suffix will be appended.
+    variables : list of str
+        The variables to be saved.
+
+    Yields
+    ------
+    ---
+
+    Receives
+    --------
+    (variables)
+        All nominated variables.
+
+    Warnings
+    --------
+    No checking at all is perfomed on the given list of variables.  Make sure
+    that it contains everything that may be required for a subsequent run.
+
+    '''
+
+    # make filename with '.glass' extension
+    root, ext = os.path.splitext(filename)
+    if ext != '.glass':
+        filename = root + ext + '.glass'
+
+    # construct generator which receives ITER and all variables
+    @generator(receives=(*variables,))
+    @wraps(save)
+    def g():
+        log.info('filename: %s', filename)
+        log.info('variables:')
+        for v in variables:
+            log.info('- %s', v)
+        with open(filename, 'wb') as f:
+            pickle.dump(variables, f)
+            while True:
+                values = yield
+                pickle.dump(values, f)
+
+    return g()
+
+
+def load(filename):
+    '''Load variables from file on each iteration.
+
+    This generator reads the saved variables from ``filename`` and yields their
+    values on each iteration.  The file would normally be created with the
+    :func:`save` generator.
+
+    Parameters
+    ----------
+    filename : str
+        Filename for the stored variables.  If ``filename`` does not end in
+        ``'.glass'``, the suffix will be appended.
+
+    Yields
+    ------
+    (variables)
+        All saved variables.
+
+    Receives
+    --------
+    ---
+
+    Warnings
+    --------
+    To allow loading of any kind of data, this function uses the :mod:`pickle`
+    module, which can in principle be used to execute arbitrary code on loading.
+    Therefore, you should only load data that you trust.
+
+    '''
+
+    # make filename with '.glass' extension
+    root, ext = os.path.splitext(filename)
+    if ext != '.glass':
+        filename = root + ext + '.glass'
+
+    # read variables from archive
+    with open(filename, 'rb') as f:
+        variables = pickle.load(f)
+
+    # construct generator which yields all variables
+    @generator(yields=(*variables,))
+    @wraps(load)
+    def g():
+        log.info('filename: %s', filename)
+        with open(filename, 'rb') as f:
+            variables = pickle.load(f)
+            log.info('variables:')
+            for v in variables:
+                log.info('- %s', v)
+            values = None
+            while True:
+                yield values
+                try:
+                    values = pickle.load(f)
+                except EOFError:
+                    break
+
+    return g()
---
+++ packages/glass/glass-2022.10.14-py3-none-any.whl/glass/observations.py
@@ -0,0 +1,268 @@
+# author: Nicolas Tessore <n.tessore@ucl.ac.uk>
+# license: MIT
+'''module for observational effects and survey properties'''
+
+import logging
+import numpy as np
+import healpy as hp
+import math
+
+from .generator import generator
+from .util import cumtrapz
+
+
+log = logging.getLogger(__name__)
+
+# variable definitions
+VIS = 'visibility'
+
+
+@generator(yields=VIS)
+def vis_constant(m, nside=None):
+    '''constant visibility map
+
+    Makes a copy of the given :term:`visibility map` and yields it on every
+    iteration.  If a ``nside`` parameter is given, the map is resampled to that
+    resolution.
+
+    Parameters
+    ----------
+    m : array_like
+        A HEALPix :term:`visibility map`.  The input is not checked.
+    nside : int, optional
+        If given, resample map to the resolution of the simulation.
+
+    Yields
+    ------
+    m : array_like
+        A copy of the :term:`visibility map` ``m``, as given.
+
+    '''
+
+    # make a copy of the input at the given resolution
+    if nside is not None and hp.get_nside(m) != nside:
+        log.info('visibility map: NSIDE=%d to %d', hp.get_nside(m), nside)
+        m = hp.ud_grade(m, nside)
+    else:
+        log.info('visibility map: NSIDE=%d', nside)
+        m = np.copy(m)
+
+    # yield on every iteration, or stop on exit
+    while True:
+        try:
+            yield m
+        except GeneratorExit:
+            break
+
+
+def vmap_galactic_ecliptic(nside, galactic=(30, 90), ecliptic=(20, 80)):
+    '''visibility map masking galactic and ecliptic plane
+
+    This function returns a :term:`visibility map` that blocks out stripes for
+    the galactic and ecliptic planes.  The location of the stripes is set with
+    optional parameters.
+
+    Parameters
+    ----------
+    nside : int
+        The NSIDE parameter of the resulting HEALPix map.
+    galactic, ecliptic : (2,) tuple of float
+        The location of the galactic and ecliptic plane in their respective
+        coordinate systems.
+
+    Returns
+    -------
+    vis : array_like
+        A HEALPix :term:`visibility map`.
+
+    Raises
+    ------
+    TypeError
+        If the ``galactic`` or ``ecliptic`` arguments are not pairs of numbers.
+
+    '''
+    if np.ndim(galactic) != 1 or len(galactic) != 2:
+        raise TypeError('galactic stripe must be a pair of numbers')
+    if np.ndim(ecliptic) != 1 or len(ecliptic) != 2:
+        raise TypeError('ecliptic stripe must be a pair of numbers')
+
+    m = np.ones(hp.nside2npix(nside))
+    m[hp.query_strip(nside, *galactic)] = 0
+    m = hp.Rotator(coord='GC').rotate_map_pixel(m)
+    m[hp.query_strip(nside, *ecliptic)] = 0
+    m = hp.Rotator(coord='CE').rotate_map_pixel(m)
+
+    return m
+
+
+def smail_nz(z, z_mode, alpha, beta):
+    r'''redshift distribution following Smail et al. (1994)
+
+    The redshift follows the Smail et al. [1]_ redshift distribution.
+
+    Parameters
+    ----------
+    z : array_like
+        Redshift values of the distribution.
+    z_mode : float or array_like
+        Mode of the redshift distribution, must be positive.
+    alpha : float or array_like
+        Power law exponent (z/z0)^\alpha, must be positive.
+    beta : float or array_like
+        Log-power law exponent exp[-(z/z0)^\beta], must be positive.
+
+    Returns
+    -------
+    pz : array_like
+        Redshift distribution at the given ``z`` values.
+
+    Notes
+    -----
+    The probability distribution function :math:`p(z)` for redshift :math:`z`
+    is given by Amara & Refregier [2]_ as
+
+    .. math::
+
+        p(z) \sim \left(\frac{z}{z_0}\right)^\alpha
+                    \exp\left[-\left(\frac{z}{z_0}\right)^\beta\right] \;,
+
+    where :math:`z_0` is matched to the given mode of the distribution.
+
+    References
+    ----------
+    .. [1] Smail I., Ellis R. S., Fitchett M. J., 1994, MNRAS, 270, 245
+    .. [2] Amara A., Refregier A., 2007, MNRAS, 381, 1018
+
+    '''
+    z_mode = np.asanyarray(z_mode)[..., np.newaxis]
+    alpha = np.asanyarray(alpha)[..., np.newaxis]
+    beta = np.asanyarray(beta)[..., np.newaxis]
+
+    pz = z**alpha*np.exp(-alpha/beta*(z/z_mode)**beta)
+    pz /= np.trapz(pz, z, axis=-1)[..., np.newaxis]
+
+    return pz
+
+
+def fixed_zbins(zmin, zmax, *, nbins=None, dz=None):
+    '''tomographic redshift bins of fixed size
+
+    This function creates contiguous tomographic redshift bins of fixed size.
+    It takes either the number or size of the bins.
+
+    Parameters
+    ----------
+    zmin, zmax : float
+        Extent of the redshift binning.
+    nbins : int, optional
+        Number of redshift bins.  Only one of ``nbins`` and ``dz`` can be given.
+    dz : float, optional
+        Size of redshift bin.  Only one of ``nbins`` and ``dz`` can be given.
+
+    Returns
+    -------
+    zbins : list of tuple of float
+        List of redshift bin edges.
+    '''
+
+    if (nbins is None) == (dz is None):
+        raise ValueError('either nbins or dz must be given')
+
+    if nbins is not None:
+        zbinedges = np.linspace(zmin, zmax, nbins+1)
+    if dz is not None:
+        zbinedges = np.arange(zmin, zmax, dz)
+
+    return list(zip(zbinedges, zbinedges[1:]))
+
+
+def equal_dens_zbins(z, nz, nbins):
+    '''equal density tomographic redshift bins
+
+    This function subdivides a source redshift distribution into ``nbins``
+    tomographic redshift bins with equal density.
+
+    Parameters
+    ----------
+    z, nz : array_like
+        The source redshift distribution. Must be one-dimensional.
+    nbins : int
+        Number of redshift bins.
+
+    Returns
+    -------
+    zbins : list of tuple of float
+        List of redshift bin edges.
+
+    '''
+    # compute the normalised cumulative distribution function
+    # first compute the cumulative integral (by trapezoidal rule)
+    # then normalise: the first z is at CDF = 0, the last z at CDF = 1
+    # interpolate to find the z values at CDF = i/nbins for i = 0, ..., nbins
+    cuml_nz = cumtrapz(nz, z)
+    cuml_nz /= cuml_nz[[-1]]
+    zbinedges = np.interp(np.linspace(0, 1, nbins+1), cuml_nz, z)
+
+    return list(zip(zbinedges, zbinedges[1:]))
+
+
+def tomo_nz_gausserr(z, nz, sigma_0, zbins):
+    '''tomographic redshift bins with a Gaussian redshift error
+
+    This function takes a _true_ overall source redshift distribution ``z``,
+    ``nz`` and returns tomographic source redshift distributions for the
+    tomographic redshift bins given by ``zbins``.  It is assumed that sources
+    are assigned a tomographic redshift bin with a Gaussian error [1]_. The
+    standard deviation of the Gaussian depends on redshift and is given by
+    ``sigma(z) = sigma_0*(1 + z)``.
+
+    Parameters
+    ----------
+    z, nz : array_like
+        The true source redshift distribution. Must be one-dimensional.
+    sigma_0 : float
+        Redshift error in the tomographic binning at zero redshift.
+    zbins : list of tuple of float
+        List of redshift bin edges.
+
+    Returns
+    -------
+    binned_nz : array_like
+        Tomographic redshift bins convolved with a gaussian error.
+        Array has a shape (nbins, len(z))
+
+    See Also
+    --------
+    equal_dens_zbins :
+        produce equal density redshift bins
+    fixed_zbins :
+        produce redshift bins of fixed size
+    glass.galaxies.gal_phz_gausserr :
+        generate photometric galaxy redshifts assuming the same model
+
+    References
+    ----------
+    .. [1] Amara A., Réfrégier A., 2007, MNRAS, 381, 1018.
+           doi:10.1111/j.1365-2966.2007.12271.x
+
+    '''
+    # converting zbins into an array:
+    zbins = np.asanyarray(zbins)
+
+    # bin edges and adds a new axis
+    z_lower = zbins[:, 0, np.newaxis]
+    z_upper = zbins[:, 1, np.newaxis]
+
+    # we need a vectorised version of the error function:
+    erf = np.vectorize(math.erf, otypes=(float,))
+
+    # compute the probabilities that redshifts z end up in each bin
+    # then apply probability as weights to given nz
+    # leading axis corresponds to the different bins
+    sz = 2**0.5*sigma_0*(1 + z)
+    binned_nz = erf((z - z_lower)/sz)
+    binned_nz -= erf((z - z_upper)/sz)
+    binned_nz /= 1 + erf(z/sz)
+    binned_nz *= nz
+
+    return binned_nz
