2afa4a7de5c89f297509588d1c636dde43b39f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:35 +0000

    Add package bhargabhellopkg/0.0.6/bhargabhellopkg-0.0.6.tar.gz

---
+++ packages/bhargabhellopkg/bhargabhellopkg-0.0.6.tar.gz/bhargabhellopkg-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION='0.0.6'
+DESCRIPTION='A basic hello package'
+LONG_DESCRIPTION='A basic hello package long desc'
+
+setup(name='bhargabhellopkg',
+      packages = find_packages(),
+      version=VERSION,
+      description=DESCRIPTION,
+
+      author='Bhargab',
+      author_email='bhargab.ganguli@gmail.com',
+      keywords = ['keyowrd1', 'keyword2', 'keyword3'],
+      license='MIT', #YOUR LICENSE HERE!
+
+      install_requires=['numpy','pandas','scipy'],  #YOUR DEPENDENCIES HERE
+  
+
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License', # Your License Here  
+        'Programming Language :: Python :: 3',    # List Python versions that you support Here  
+        ],
+)
