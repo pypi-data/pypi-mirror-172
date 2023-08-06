c070d020397cfe158facc5eb5af78a6072ac30c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:58 +0000

    Add package cronjob-open-interest/0.2.1/cronjob-open-interest-0.2.1.tar.gz

---
+++ packages/cronjob-open-interest/cronjob-open-interest-0.2.1.tar.gz/cronjob-open-interest-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['app']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-decouple>=3.6,<4.0', 'radar-mongodb>=0.3.0,<0.4.0']
+
+setup_kwargs = {
+    'name': 'cronjob-open-interest',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Ernesto Martinez del Pino',
+    'author_email': 'ernestomar1997@hotmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
