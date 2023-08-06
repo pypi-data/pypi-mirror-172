54cbdaf93340e33d1a9fcca5b923ace8b3bdd90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:55 +0000

    Add package math-basics/0.0.1/math_basics-0.0.1-py3-none-any.whl

---
+++ packages/math-basics/math_basics-0.0.1-py3-none-any.whl/src/math-basics/BasicMath.py
@@ -0,0 +1,11 @@
+def add(number1, number2):
+    return number1 + number2
+
+def sub(number1, number2):
+    return number1 - number2
+
+def mul(number1, number2):
+    return number1 * number2
+
+def div(number1, number2):
+    return number1 / number2
