3a00219e5118a7d3391985d62cc46923955872e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:44 +0000

    Add package saas-co/0.20/saas_co-0.20.tar.gz

---
+++ packages/saas-co/saas_co-0.20.tar.gz/saas_co-0.20/src/saas_co/cur/query.py
@@ -0,0 +1,58 @@
+import os
+import boto3
+import botocore
+
+import pandas as pd
+import awswrangler as wr
+
+from functools import partial, reduce
+
+#####################
+
+def session_from_credentials(creds, region):
+    try:
+        return boto3.session.Session(
+            region_name=region,
+            aws_access_key_id=creds["AccessKeyId"],
+            aws_secret_access_key=creds["SecretAccessKey"],
+            aws_session_token=creds["SessionToken"]
+        )
+    except:
+        return None
+
+#####################
+
+def assume_role(**params):
+
+    region = params.get('region', 'us-east-1')
+    access_key_id = params.get('access_key_id')
+    secret_access_key = params.get('secret_access_key')
+
+    account_id = params.get('accountId')
+    role_name = params.get('roleName')
+    session_name = params.get('sessionName')
+    role_arn = f'arn:aws:iam::{account_id}:role/{role_name}'
+    
+    try:
+        if access_key_id:
+            sts = boto3.client('sts',
+                               region_name=region,
+                               aws_access_key_id=access_key_id,
+                               aws_secret_access_key=secret_access_key)
+        else:
+            sts = boto3.client('sts')
+        assumed_role = sts.assume_role(RoleArn=role_arn,
+                                       RoleSessionName=session_name)
+        credentials = assumed_role.get('Credentials')
+    except:
+        print(account_id, 'Assume error')
+        credentials = None
+
+    return session_from_credentials(credentials, region)
+
+#####################
+
+def athena(session, db, query):
+    return partial(wr.athena.read_sql_query,
+                   database=db,
+                   boto3_session=session)(query)
