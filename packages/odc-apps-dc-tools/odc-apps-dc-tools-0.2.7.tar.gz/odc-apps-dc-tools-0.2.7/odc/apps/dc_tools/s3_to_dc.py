ac627b32f40e869a34e0b84f04b20e44e64402d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:35 +0000

    Add package odc-apps-dc-tools/0.2.7/odc-apps-dc-tools-0.2.7.tar.gz

---
+++ packages/odc-apps-dc-tools/odc-apps-dc-tools-0.2.7.tar.gz/odc-apps-dc-tools-0.2.7/odc/apps/dc_tools/s3_to_dc.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+"""Build S3 iterators using odc-tools
+and index datasets found into RDS
+"""
+import logging
+import sys
+from typing import Tuple
+
+import click
+from datacube import Datacube
+from datacube.index.hl import Doc2Dataset
+
+
+from odc.aio import S3Fetcher, s3_find_glob
+from odc.apps.dc_tools.utils import (IndexingException, SkippedException,
+                                     archive_less_mature, allow_unsafe,
+                                     fail_on_missing_lineage,
+                                     index_update_dataset, no_sign_request,
+                                     request_payer, skip_check, skip_lineage,
+                                     statsd_gauge_reporting, statsd_setting,
+                                     transform_stac, transform_stac_absolute,
+                                     update, update_if_exists, verify_lineage)
+from odc.apps.dc_tools._docs import parse_doc_stream
+from odc.apps.dc_tools._stac import stac_transform, stac_transform_absolute
+
+
+# Grab the URL from the resulting S3 item
+def stream_urls(urls):
+    for url in urls:
+        yield url.url
+
+
+# Parse documents as they stream through from S3
+def stream_docs(documents):
+    for document in documents:
+        yield (document.url, document.data)
+
+
+# Log the internal errors parsing docs
+def doc_error(uri, doc, e):
+    logging.exception(f"Failed to parse doc {uri} with error {e}")
+
+
+def dump_to_odc(
+    document_stream,
+    dc: Datacube,
+    products: list,
+    transform=None,
+    update=False,
+    update_if_exists=False,
+    allow_unsafe=False,
+    archive_less_mature=None,
+    **kwargs,
+) -> Tuple[int, int]:
+    doc2ds = Doc2Dataset(dc.index, products=products, **kwargs)
+
+    ds_added = 0
+    ds_failed = 0
+    ds_skipped = 0
+    uris_docs = parse_doc_stream(stream_docs(document_stream), on_error=doc_error, transform=transform)
+
+    for uri, metadata in uris_docs:
+        try:
+            index_update_dataset(metadata, uri, dc, doc2ds,
+                                 update=update,
+                                 update_if_exists=update_if_exists,
+                                 allow_unsafe=allow_unsafe,
+                                 archive_less_mature=archive_less_mature)
+            ds_added += 1
+        except (IndexingException) as e:
+            logging.exception(f"Failed to index dataset {uri} with error {e}")
+            ds_failed += 1
+        except (SkippedException) as e:
+            ds_skipped +=1
+
+    return ds_added, ds_failed, ds_skipped
+
+
+@click.command("s3-to-dc")
+@skip_lineage
+@fail_on_missing_lineage
+@verify_lineage
+@transform_stac
+@transform_stac_absolute
+@update
+@update_if_exists
+@allow_unsafe
+@skip_check
+@no_sign_request
+@statsd_setting
+@request_payer
+@archive_less_mature
+@click.argument("uri", type=str, nargs=1)
+@click.argument("product", type=str, nargs=1)
+def cli(
+    skip_lineage,
+    fail_on_missing_lineage,
+    verify_lineage,
+    stac,
+    absolute,
+    update,
+    update_if_exists,
+    allow_unsafe,
+    skip_check,
+    no_sign_request,
+    statsd_setting,
+    request_payer,
+    archive_less_mature,
+    uri,
+    product,
+):
+    """ Iterate through files in an S3 bucket and add them to datacube"""
+
+    transform = None
+    if stac:
+        if absolute:
+            transform = stac_transform_absolute
+        else:
+            transform = stac_transform
+
+    candidate_products = product.split()
+
+    opts = {}
+    if request_payer:
+        opts["RequestPayer"] = "requester"
+
+    # Check datacube connection and products
+    dc = Datacube()
+    odc_products = dc.list_products().name.values
+
+    odc_products = set(odc_products)
+    if not set(candidate_products).issubset(odc_products):
+        missing_products = list(set(candidate_products) - odc_products)
+        print(
+            f"Error: Requested Product/s {', '.join(missing_products)} {'is' if len(missing_products) == 1 else 'are'} "
+            "not present in the ODC Database",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    # Get a generator from supplied S3 Uri for candidate documents
+    fetcher = S3Fetcher(aws_unsigned=no_sign_request)
+    document_stream = stream_urls(s3_find_glob(uri, skip_check=skip_check, s3=fetcher, **opts))
+
+    added, failed, skipped = dump_to_odc(
+        fetcher(document_stream),
+        dc,
+        candidate_products,
+        skip_lineage=skip_lineage,
+        fail_on_missing_lineage=fail_on_missing_lineage,
+        verify_lineage=verify_lineage,
+        transform=transform,
+        update=update,
+        update_if_exists=update_if_exists,
+        allow_unsafe=allow_unsafe,
+        archive_less_mature=archive_less_mature
+    )
+
+    print(f"Added {added} datasets, skipped {skipped} datasets and failed {failed} datasets.")
+    if statsd_setting:
+        statsd_gauge_reporting(added, ["app:s3_to_dc", "action:added"], statsd_setting)
+        statsd_gauge_reporting(skipped, ["app:s3_to_dc", "action:skipped"], statsd_setting)
+        statsd_gauge_reporting(failed, ["app:s3_to_dc", "action:failed"], statsd_setting)
+
+    if failed > 0:
+        sys.exit(failed)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/odc-apps-dc-tools/odc-apps-dc-tools-0.2.7.tar.gz/odc-apps-dc-tools-0.2.7/odc/apps/dc_tools/stac_api_to_dc.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python3
+"""Index datasets found from an SQS queue into Postgres
+"""
+import concurrent
+import json
+import logging
+import os
+import sys
+from typing import Any, Dict, Generator, Optional, Tuple
+
+import click
+import pystac
+from datacube import Datacube
+from datacube.index.hl import Doc2Dataset
+from odc.apps.dc_tools._stac import stac_transform, stac_transform_absolute
+from odc.apps.dc_tools.utils import (SkippedException, allow_unsafe,
+                                     archive_less_mature, bbox,
+                                     index_update_dataset, limit,
+                                     statsd_gauge_reporting, statsd_setting,
+                                     update_if_exists)
+from pystac.item import Item
+from pystac_client import Client
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format="%(asctime)s: %(levelname)s: %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S",
+)
+
+
+def _parse_options(options: Optional[str]) -> Dict[str, Any]:
+    parsed_options = {}
+
+    if options is not None:
+        for option in options.split("#"):
+            try:
+                key, value = option.split("=")
+
+                try:
+                    value = json.loads(value)
+                except Exception:
+                    logging.warning(f"Failed to handle value {value} for key {key} as JSON, using str")
+                parsed_options[key] = value
+            except Exception as e:
+                logging.warning(
+                    f"Couldn't parse option {option}, format is key=value, exception was {e}"
+                )
+
+    return parsed_options
+
+
+def _guess_location(
+    item: pystac.Item, rewrite: Optional[Tuple[str, str]] = None
+) -> Tuple[str, bool]:
+    self_link = None
+    asset_link = None
+    relative = True
+
+    for link in item.links:
+        if link.rel == "self":
+            self_link = link.target
+
+        # Override self with canonical
+        if link.rel == "canonical":
+            self_link = link.target
+            break
+
+    for _, asset in item.assets.items():
+        if "geotiff" in asset.media_type:
+            asset_link = os.path.dirname(asset.href)
+            break
+
+    if rewrite is not None:
+        for _, asset in item.assets.items():
+            if "geotiff" in asset.media_type:
+                asset.href = asset.href.replace(rewrite[0], rewrite[1])
+
+    # If the metadata and the document are not on the same path,
+    # we need to use absolute links and not relative ones.
+    if (self_link and asset_link) and os.path.dirname(self_link) != os.path.dirname(
+        asset_link
+    ):
+        relative = False
+
+    return self_link, relative
+
+
+def item_to_meta_uri(
+    item: Item,
+    rewrite: Optional[Tuple[str, str]] = None,
+    rename_product: Optional[str] = None,
+) -> Generator[Tuple[dict, str, bool], None, None]:
+    uri, relative = _guess_location(item, rewrite)
+    metadata = item.to_dict()
+    if rename_product is not None:
+        metadata["properties"]["odc:product"] = rename_product
+
+    if relative:
+        metadata = stac_transform(metadata)
+    else:
+        metadata = stac_transform_absolute(metadata)
+
+    return (metadata, uri)
+
+
+def process_item(
+    item: Item,
+    dc: Datacube,
+    doc2ds: Doc2Dataset,
+    update_if_exists: bool,
+    allow_unsafe: bool,
+    rewrite: Optional[Tuple[str, str]] = None,
+    rename_product: Optional[str] = None,
+    archive_less_mature: bool = False,
+):
+    meta, uri = item_to_meta_uri(item, rewrite, rename_product)
+    index_update_dataset(
+        meta,
+        uri,
+        dc,
+        doc2ds,
+        update_if_exists=update_if_exists,
+        allow_unsafe=allow_unsafe,
+        archive_less_mature=archive_less_mature,
+    )
+
+
+def stac_api_to_odc(
+    dc: Datacube,
+    update_if_exists: bool,
+    config: dict,
+    catalog_href: str,
+    allow_unsafe: bool = True,
+    rewrite: Optional[Tuple[str, str]] = None,
+    rename_product: Optional[str] = None,
+    archive_less_mature: bool = False,
+) -> Tuple[int, int, int]:
+    doc2ds = Doc2Dataset(dc.index)
+    client = Client.open(catalog_href)
+
+    search = client.search(**config)
+    n_items = search.matched()
+    if n_items is not None:
+        logging.info("Found {} items to index".format(n_items))
+        if n_items == 0:
+            logging.warning("Didn't find any items, finishing.")
+            return 0, 0
+    else:
+        logging.warning("API did not return the number of items.")
+
+    # Do the indexing of all the things
+    success = 0
+    failure = 0
+    skipped = 0
+
+    sys.stdout.write("\rIndexing from STAC API...\n")
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        future_to_item = {
+            executor.submit(
+                process_item,
+                item,
+                dc,
+                doc2ds,
+                update_if_exists=update_if_exists,
+                allow_unsafe=allow_unsafe,
+                rewrite=rewrite,
+                rename_product=rename_product,
+                archive_less_mature=archive_less_mature,
+            ): item.id
+            for item in search.get_all_items()
+        }
+        for future in concurrent.futures.as_completed(future_to_item):
+            item = future_to_item[future]
+            try:
+                _ = future.result()
+                success += 1
+                if success % 10 == 0:
+                    sys.stdout.write(f"\rAdded {success} datasets...")
+            except SkippedException:
+                logging.exception(f"{item} Skipped")
+                skipped += 1
+            except Exception as e:
+                logging.exception(f"Failed to handle item {item} with exception {e}")
+                failure += 1
+    sys.stdout.write("\r")
+
+    return success, failure, skipped
+
+
+@click.command("stac-to-dc")
+@limit
+@update_if_exists
+@allow_unsafe
+@click.option(
+    "--catalog-href",
+    type=str,
+    default="https://earth-search.aws.element84.com/v0/",
+    help="URL to the catalog to search",
+)
+@click.option(
+    "--collections",
+    type=str,
+    default=None,
+    help="Comma separated list of collections to search",
+)
+@bbox
+@click.option(
+    "--datetime",
+    type=str,
+    default=None,
+    help="Dates to search, either one day or an inclusive range, e.g. 2020-01-01 or 2020-01-01/2020-01-02",
+)
+@click.option(
+    "--options",
+    type=str,
+    default=None,
+    help="Other search terms, as a # separated list, i.e., --options=cloud_cover=0,100#sky=green",
+)
+@click.option(
+    "--rewrite-assets",
+    type=str,
+    default=None,
+    help=(
+        "Rewrite asset hrefs, for example, to change from "
+        "HTTPS to S3 URIs, --rewrite-assets=https://example.com/,s3://"
+    ),
+)
+@click.option(
+    "--rename-product",
+    type=str,
+    default=None,
+    help=(
+        "Name of product to overwrite collection(s) names, "
+        "only one product name can overwrite, despite multiple collections "
+    ),
+)
+@archive_less_mature
+@statsd_setting
+def cli(
+    limit,
+    update_if_exists,
+    allow_unsafe,
+    catalog_href,
+    collections,
+    bbox,
+    datetime,
+    options,
+    rewrite_assets,
+    rename_product,
+    statsd_setting,
+    archive_less_mature,
+):
+    """
+    Iterate through STAC items from a STAC API and add them to datacube.
+    """
+    config = _parse_options(options)
+    rewrite = None
+
+    # Format the search terms
+    if bbox:
+        config["bbox"] = list(map(float, bbox.split(",")))
+
+    if collections:
+        config["collections"] = collections.split(",")
+
+    if datetime:
+        config["datetime"] = datetime
+
+    # Always set the limit, because some APIs will stop at an arbitrary
+    # number if max_items is not None.
+    config["max_items"] = limit
+
+    if rewrite_assets is not None:
+        rewrite = list(rewrite_assets.split(","))
+        if len(rewrite) != 2:
+            raise ValueError(
+                "Rewrite assets argument needs to be two strings split by ','"
+            )
+
+    # Do the thing
+    dc = Datacube()
+    added, failed, skipped = stac_api_to_odc(
+        dc,
+        update_if_exists,
+        config,
+        catalog_href,
+        allow_unsafe=allow_unsafe,
+        rewrite=rewrite,
+        rename_product=rename_product,
+        archive_less_mature=archive_less_mature,
+    )
+
+    print(
+        f"Added {added} Datasets, failed {failed} Datasets, skipped {skipped} Datasets"
+    )
+    if statsd_setting:
+        statsd_gauge_reporting(
+            added, ["app:stac_api_to_dc", "action:added"], statsd_setting
+        )
+        statsd_gauge_reporting(
+            failed, ["app:stac_api_to_dc", "action:failed"], statsd_setting
+        )
+        statsd_gauge_reporting(
+            skipped, ["app:stac_api_to_dc", "action:skipped"], statsd_setting
+        )
+
+    if failed > 0:
+        sys.exit(failed)
+
+
+if __name__ == "__main__":
+    cli()
