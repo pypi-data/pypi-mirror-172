4bcdaeb145799b1a006fec70ddd695e9e668fc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:22 +0000

    Add package cornflakes/2.4.6/cornflakes-2.4.6-cp38-cp38-manylinux_2_35_x86_64.whl

---
+++ packages/cornflakes/cornflakes-2.4.6-cp38-cp38-manylinux_2_35_x86_64.whl/cornflakes/common/__init__.py
@@ -0,0 +1,17 @@
+"""cornflakes common module.
+__________________________________
+
+.. currentmodule:: cornflakes.common
+
+.. autosummary::
+   :toctree: _generate
+
+    add_slots
+    default_ca_path
+    import_component
+"""  # noqa: RST303 D205
+from cornflakes.common._add_dataclass_slots import add_slots
+from cornflakes.common._import_component import import_component
+from cornflakes.common._default_ca_path import default_ca_path
+
+__all__ = ["add_slots", "import_component", "default_ca_path"]
