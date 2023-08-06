cbe3a5007f1ecd624c9d75cba65da760068d621e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:05 +0000

    Add package geren-juezhide-liliang/2022.10.17.0/geren_juezhide_liliang-2022.10.17.0-py3-none-any.whl

---
+++ packages/geren-juezhide-liliang/geren_juezhide_liliang-2022.10.17.0-py3-none-any.whl/GerenJuezhideLiliang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""geren-juezhide-liliang
+https://github.com/apachecn/geren-juezhide-liliang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.17.0"
