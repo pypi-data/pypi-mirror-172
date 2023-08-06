eae9548db894ba6a3e13cfe7ae22451e150e3056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:01 +0000

    Add package sqlalchemy-repr/0.1.0/sqlalchemy-repr-0.1.0.tar.gz

---
+++ packages/sqlalchemy-repr/sqlalchemy-repr-0.1.0.tar.gz/sqlalchemy-repr-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='sqlalchemy-repr',
+    version='0.1.0',
+    description='Automatically generates pretty repr of a SQLAlchemy model.',
+    long_description=open('README.rst').read(),
+    keywords='pprint pretty print repr sqlalchemy extension',
+    author='Ryosuke Ito',
+    author_email='rito.0305@gmail.com',
+    url='https://github.com/manicmaniac/sqlalchemy-repr',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Database :: Front-Ends',
+    ],
+    py_modules=['sqlalchemy_repr'],
+    install_requires=['SQLAlchemy'],
+    test_suite='tests',
+)
