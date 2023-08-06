237afa3ba4f519d6e8251cefd2c890e6ef8496ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:13 +0000

    Add package SQLAlchemy/2.0.0b2/SQLAlchemy-2.0.0b2-cp38-cp38-win_amd64.whl

---
+++ packages/SQLAlchemy/SQLAlchemy-2.0.0b2-cp38-cp38-win_amd64.whl/sqlalchemy/events.py
@@ -0,0 +1,16 @@
+# sqlalchemy/events.py
+# Copyright (C) 2005-2022 the SQLAlchemy authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of SQLAlchemy and is released under
+# the MIT License: https://www.opensource.org/licenses/mit-license.php
+
+"""Core event interfaces."""
+
+from __future__ import annotations
+
+from .engine.events import ConnectionEvents
+from .engine.events import DialectEvents
+from .pool.events import PoolEvents
+from .sql.base import SchemaEventTarget
+from .sql.events import DDLEvents
