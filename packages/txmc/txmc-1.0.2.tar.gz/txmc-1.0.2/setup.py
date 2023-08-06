5cae3b49a4e601f93016bfada3749064f8c94c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:22 +0000

    Add package txmc/1.0.2/txmc-1.0.2.tar.gz

---
+++ packages/txmc/txmc-1.0.2.tar.gz/txmc-1.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+    name="txmc",
+    version="1.0.2",
+    author="童心少年",
+    author_email="363488350@qq.com",
+    description="我的编程世界Python组件",
+    long_description='通过txmc实现在我的世界中畅游 https://www.yuque.com/makeblock.net.cn/plugin',
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/tongxinblock/txmc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
