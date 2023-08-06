23050e76ed05c10b6e6c5c37e67e3ef41065a28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:19 +0000

    Add package swh.lister/4.0.0/swh.lister-4.0.0.tar.gz

---
+++ packages/swh.lister/swh.lister-4.0.0.tar.gz/swh.lister-4.0.0/swh/lister/gitea/lister.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2018-2021 The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import logging
+
+from ..gogs.lister import GogsLister
+
+logger = logging.getLogger(__name__)
+
+
+class GiteaLister(GogsLister):
+    """List origins from Gitea.
+
+    Gitea API documentation: https://try.gitea.io/api/swagger
+
+    The API does pagination and provides navigation URLs through the 'Link' header.
+    The default value for page size is the maximum value observed on the instances
+    accessible at https://try.gitea.io/api/v1/ and https://codeberg.org/api/v1/."""
+
+    LISTER_NAME = "gitea"
+
+    def on_anonymous_mode(self):
+        logger.warning(
+            "No authentication token set in configuration, using anonymous mode"
+        )
