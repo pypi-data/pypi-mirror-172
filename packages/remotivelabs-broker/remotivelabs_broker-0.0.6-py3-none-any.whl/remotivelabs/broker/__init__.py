cf465945b76181829c0915ea14810380a3fcaa9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:40 +0000

    Add package remotivelabs-broker/0.0.6/remotivelabs_broker-0.0.6-py3-none-any.whl

---
+++ packages/remotivelabs-broker/remotivelabs_broker-0.0.6-py3-none-any.whl/remotivelabs/broker/__init__.py
@@ -0,0 +1,15 @@
+"""
+remotiveLabs Python API for remotiveBroker.
+See `version` below.
+
+Use sub module: `remotivelabs.broker.sync`.
+"""
+
+# SPDX-FileCopyrightText: 2022-present remotiveLabs <support@remotivelabs.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .__about__ import __version__
+
+version = __version__
+"""Library version"""
