680a3a6f7d9231d475b0d25efb762f4f33537084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:35 +0000

    Add package dataclass2PySide6/1.8.4/dataclass2PySide6-1.8.4-py3-none-any.whl

---
+++ packages/dataclass2PySide6/dataclass2PySide6-1.8.4-py3-none-any.whl/dataclass2PySide6/__init__.py
@@ -0,0 +1,36 @@
+from .version import __version__  # noqa
+
+from .datawidgets import (
+    type2Widget,
+    BoolCheckBox,
+    MISSING,
+    EmptyIntValidator,
+    IntLineEdit,
+    EmptyFloatValidator,
+    FloatLineEdit,
+    StrLineEdit,
+    TupleGroupBox,
+    EnumComboBox,
+)
+from .dataclass_widget import (
+    DataclassWidget,
+    StackedDataclassWidget,
+    TabDataclassWidget,
+)
+
+
+__all__ = [
+    "type2Widget",
+    "BoolCheckBox",
+    "MISSING",
+    "EmptyIntValidator",
+    "IntLineEdit",
+    "EmptyFloatValidator",
+    "FloatLineEdit",
+    "StrLineEdit",
+    "TupleGroupBox",
+    "EnumComboBox",
+    "DataclassWidget",
+    "StackedDataclassWidget",
+    "TabDataclassWidget",
+]
