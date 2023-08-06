0569a126c8a3997bfa25efc24f2d9f8fbdc3724e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:42 +0000

    Add package flake8-import-rules/0.1.0/flake8_import_rules-0.1.0.tar.gz

---
+++ packages/flake8-import-rules/flake8_import_rules-0.1.0.tar.gz/flake8_import_rules-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['flake8_import_rules']
+entry_points = \
+{'flake8.extension': ['I013 = flake8_import_rules:ImportRulesChecker']}
+
+setup_kwargs = {
+    'name': 'flake8-import-rules',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'VL',
+    'author_email': '1844144@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
