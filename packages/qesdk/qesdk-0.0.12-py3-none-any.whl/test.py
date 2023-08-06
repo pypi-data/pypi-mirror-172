817df8355e9ec6377d57f3ce2e3ae9028259fc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:02 +0000

    Add package qesdk/0.0.12/qesdk-0.0.12-py3-none-any.whl

---
+++ packages/qesdk/qesdk-0.0.12-py3-none-any.whl/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 23 21:41:10 2022
+
+@author: ScottStation
+"""
+
+
+import qesdk2
+
+qesdk2.login('zys','scott221')
+stratlist=(qesdk2.sm_get_clone_strat_list())
+print(qesdk2.sm_get_clone_strat_position(stratlist))
+#print(qesdk2.get_price('AG2212.SFE','2022-09-01','2022-09-22'))
