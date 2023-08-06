61db8bd3c8cb18fecfbaa73fb03d22bea31e9b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:54 +0000

    Add package PietInterpreter/1.0.3/PietInterpreter-1.0.3-py3-none-any.whl

---
+++ packages/PietInterpreter/PietInterpreter-1.0.3-py3-none-any.whl/PietInterpreterTests/test_Piet.py
@@ -0,0 +1,9 @@
+import unittest
+from ..PietInterpreter.colors import possiblePixels
+
+
+class PietTests(unittest.TestCase):
+
+    def test_dummy(self) -> None:
+        pp = possiblePixels()  # pylint: disable=unused-variable
+        assert 1-1 == 0  # TODO
