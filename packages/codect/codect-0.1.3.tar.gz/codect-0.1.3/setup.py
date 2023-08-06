fb4b336428e73f193572ccc68e43b03190342417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:11 +0000

    Add package codect/0.1.3/codect-0.1.3.tar.gz

---
+++ packages/codect/codect-0.1.3.tar.gz/codect-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='codect',
+    version='0.1.3',    
+    description='Python package to protect your code',
+    url='https://github.com/mategol/codect-python',
+    author='Mateusz Golembowski',
+    author_email='mateusz@golembowski.pl',
+    license='MIT',
+    packages=['codect'],
+    install_requires=['pycryptodome',
+                      'pybase64',
+                      'sys'                     
+                      ],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',      
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+)
