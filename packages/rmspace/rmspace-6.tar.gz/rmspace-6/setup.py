1bf94ee7a43e76a417440b0957d7c998e19cbed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:21 +0000

    Add package rmspace/6/rmspace-6.tar.gz

---
+++ packages/rmspace/rmspace-6.tar.gz/rmspace-6/setup.py
@@ -0,0 +1,11 @@
+"""rmspace installer"""
+
+import setuptools
+
+setuptools.setup(
+    name="rmspace",
+    version="6",
+    description="Remove trailing whitespace from code and other plain-text files.",
+    author="Olle Lindgren",
+    author_email="lindgrenolle@live.se",
+)
