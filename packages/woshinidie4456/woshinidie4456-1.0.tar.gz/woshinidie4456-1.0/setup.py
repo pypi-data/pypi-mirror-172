643881a206d17114f9cbe978facbd1672d0e7edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:44 +0000

    Add package woshinidie4456/1.0/woshinidie4456-1.0.tar.gz

---
+++ packages/woshinidie4456/woshinidie4456-1.0.tar.gz/woshinidie4456-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(
+    name = 'woshinidie4456',
+    version = 'v1.0',
+    description="nothing",
+    author_email='zhangsen2001@gmail.com',
+    author='Sen',
+    url='https://github.com/SengeNB/OliverSen',
+    license='MIT'
+)
