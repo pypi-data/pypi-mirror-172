abc57ed35f0123737ba5556d104e470773918714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:20 +0000

    Add package earthlib/0.2.5/earthlib-0.2.5-py3-none-any.whl

---
+++ packages/earthlib/earthlib-0.2.5-py3-none-any.whl/earthlib/__init__.py
@@ -0,0 +1,23 @@
+from earthlib import (
+    BRDFCorrect,
+    BrightMask,
+    CloudMask,
+    NIRv,
+    Scale,
+    ShadeMask,
+    Unmix,
+    __version__,
+    read,
+)
+from earthlib.config import collections, metadata
+from earthlib.utils import (
+    getBands,
+    getCollection,
+    getScaler,
+    listSensors,
+    listTypes,
+    selectSpectra,
+)
+
+# expose the full spectral library
+library = read.endmembers()
