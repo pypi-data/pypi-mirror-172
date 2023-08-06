59c0505887547faf026068352423254a925079de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:20 +0000

    Add package TestProgramURFU/0.1/TestProgramURFU-0.1.tar.gz

---
+++ packages/TestProgramURFU/TestProgramURFU-0.1.tar.gz/TestProgramURFU-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TestProgramURFU',
+version = '0.1',
+          author = 'Nikolai',
+                   author_email = 'ch_k@mail.com',
+                                  description = 'The Best ever seen',
+                                                long_description = open('README.md').read(),
+                                                                   packages = find_packages()
+)
