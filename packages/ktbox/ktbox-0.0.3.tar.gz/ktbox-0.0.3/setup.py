23043a474707a4c7d163fa62d040b80c62759f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:18 +0000

    Add package ktbox/0.0.3/ktbox-0.0.3.tar.gz

---
+++ packages/ktbox/ktbox-0.0.3.tar.gz/ktbox-0.0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+  
+# reading long description from file
+# with open('DESCRIPTION.txt') as file:
+#     long_description = file.read()
+  
+  
+# specify requirements of your package here
+#REQUIREMENTS = ['requests', 'pandas', 'snowflake.connector']
+  
+# some more details
+# CLASSIFIERS = [
+#     'Development Status :: 4 - Beta',
+#     'Intended Audience :: Developers',
+#     'Topic :: Internet',
+#     'License :: OSI Approved :: MIT License',
+#     'Programming Language :: Python',
+#     'Programming Language :: Python :: 3.10',
+#     ]
+  
+# calling the setup function 
+# setup(name='ktbox',
+#       version='0.0.1',
+#       description='A small wrapper around sfpdil connection',
+#       long_description=long_description,
+#       url='https://gitlab.com/work435/ktbox',
+#       author='Krzysztof',
+#       author_email='my@mail.com',
+#       license='MIT',
+#       packages=['ktbox'],
+#       classifiers=CLASSIFIERS,
+#       install_requires=REQUIREMENTS,
+#       keywords='pdil'
+#       )
+
+setup(
+    version='0.0.3'
+)
