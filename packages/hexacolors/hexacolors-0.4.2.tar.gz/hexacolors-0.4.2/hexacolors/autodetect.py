087dd3d448715f8eb1017348d3e65a00760544b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:10 +0000

    Add package hexacolors/0.4.2/hexacolors-0.4.2.tar.gz

---
+++ packages/hexacolors/hexacolors-0.4.2.tar.gz/hexacolors-0.4.2/hexacolors/autodetect.py
@@ -0,0 +1,40 @@
+import string
+
+from .api import hexacolor, hsl, rgb, cmyk
+from .str import stringcolor
+
+def autodetect(args:str) -> str:
+
+    if args[0] == '#':
+
+        hexacolor(args)
+
+    elif args.count('%') == 2:
+
+        hsl(args)
+
+    elif args.count(',') == 2:
+
+        rgb(args)
+    
+    elif args.count(',') == 3:
+
+        cmyk(args)
+
+    elif args[0] in list(string.digits):
+
+        hexacolor(args)
+    
+    elif args[0] in list(string.ascii_letters):
+
+        try:
+
+            stringcolor(args)
+
+        except:
+
+            hexacolor(args)
+
+    else:
+
+        print('ERROR: Not indentify the type')
