757bd3d071b6c51e250eb2f5e93897907741427b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:02 +0000

    Add package sqlalchemy-easy-softdelete/0.5.0/sqlalchemy_easy_softdelete-0.5.0-py3-none-any.whl

---
+++ packages/sqlalchemy-easy-softdelete/sqlalchemy_easy_softdelete-0.5.0-py3-none-any.whl/sqlalchemy_easy_softdelete/handler/sqlalchemy_easy_softdelete.py
@@ -0,0 +1,20 @@
+from functools import cache
+
+from sqlalchemy.event import listens_for
+from sqlalchemy.orm import ORMExecuteState, Session
+
+from sqlalchemy_easy_softdelete.handler.rewriter import SoftDeleteQueryRewriter
+
+
+@cache
+def activate_soft_delete_hook(deleted_field_name: str, disable_soft_delete_option_name: str):
+    # Enable Soft Delete on all Relationship Loads which implement SoftDeleteMixin
+    @listens_for(Session, "do_orm_execute")
+    def soft_delete_execute(state: ORMExecuteState):
+        if not state.is_select:
+            return
+
+        adapted = SoftDeleteQueryRewriter(deleted_field_name, disable_soft_delete_option_name).rewrite_statement(
+            state.statement
+        )
+        state.statement = adapted
