9c936483801e9ed6f68917bcdc1e0ce88a883727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:42 +0000

    Add package netorca-sdk/0.0.1/netorca_sdk-0.0.1.tar.gz

---
+++ packages/netorca-sdk/netorca_sdk-0.0.1.tar.gz/netorca_sdk-0.0.1/netorca_sdk/__init__.py
@@ -0,0 +1,6 @@
+from netorca_sdk.elements.applications import ApplicationObj
+from netorca_sdk.elements.services import ServiceObj, Services
+from netorca_sdk.elements.cis import ChangeInstanceObj, ChangeInstances
+from netorca_sdk.elements.sis import ServiceItemObj, ServiceItems
+from netorca_sdk.elements.teams import TeamsObj
+from netorca_sdk.elements.declarations import DeclarationObj
