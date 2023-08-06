7bccf5a0c9a6e6f26df63d92782e9e36135a3b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:04 +0000

    Add package math-calc/0.1/math_calc-0.1.tar.gz

---
+++ packages/math-calc/math_calc-0.1.tar.gz/math_calc-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name="math_calc",
+    version="0.1",
+    description="Paquete de ejemplo math_calc",
+    author="SMG",
+    author_email="smaillog@gmail.com",
+    url="http://www.google.es",
+    packages=['math_calc'],
+    scripts=[]
+)
