38d3a25a7aac30ae84be0730aff48a296dd842de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:18 +0000

    Add package mlcvzoo-util/0.1.0/mlcvzoo_util-0.1.0-py3-none-any.whl

---
+++ packages/mlcvzoo-util/mlcvzoo_util-0.1.0-py3-none-any.whl/mlcvzoo_util/model_evaluator/structs.py
@@ -0,0 +1,34 @@
+# Copyright 2022 Open Logistics Foundation
+#
+# Licensed under the Open Logistics License 1.0.
+# For details on the licensing terms, see the LICENSE file.
+
+"""
+Module for defining enums and structs that are used across the
+model_evaluator package.
+"""
+from dataclasses import dataclass
+from enum import auto
+
+try:
+    # python3.11
+    from enum import StrEnum  # type: ignore
+except ImportError:
+    from backports.strenum import StrEnum
+
+
+class CheckpointLoggingModes(StrEnum):
+    BEST = auto()
+    ALL = auto()
+    NONE = auto()
+
+
+@dataclass
+class CheckpointInfo:
+    """
+    Dataclass for storing information about a checkpoint
+    and its overall AP metric
+    """
+
+    path: str
+    score: float
