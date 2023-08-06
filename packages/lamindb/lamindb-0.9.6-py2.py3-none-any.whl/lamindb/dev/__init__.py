5238f6519e5273568977195bcfa3769dc9c5f866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:41 +0000

    Add package lamindb/0.9.6/lamindb-0.9.6-py2.py3-none-any.whl

---
+++ packages/lamindb/lamindb-0.9.6-py2.py3-none-any.whl/lamindb/dev/__init__.py
@@ -0,0 +1,22 @@
+"""Developer API.
+
+.. autosummary::
+   :toctree: .
+
+   db
+   file
+   object
+
+Utilities:
+
+.. autosummary::
+   :toctree: .
+
+   doc_args
+"""
+
+from lnschema_core import id  # noqa
+
+from . import db  # noqa
+from . import file, object  # noqa
+from ._docs import doc_args  # noqa
