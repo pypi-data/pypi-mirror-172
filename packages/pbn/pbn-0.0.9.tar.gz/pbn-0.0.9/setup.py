86331983d887bda5c4009d68d6d40f0d1dffff1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:38 +0000

    Add package pbn/0.0.9/pbn-0.0.9.tar.gz

---
+++ packages/pbn/pbn-0.0.9.tar.gz/pbn-0.0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+VERSION = '0.0.9' 
+DESCRIPTION = 'Paint By Numbers Creator'
+LONG_DESCRIPTION = 'Convert any image into a printable paint by numbers.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="pbn", 
+        version=VERSION,
+        author="Michael Pienaar",
+        author_email="mpienaar9@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=['pbn'],
+        install_requires=['matplotlib', 'sklearn', 'opencv-python', 'python-polylabel', 'numpy'], 
+        keywords=['python', 'art', 'painting']
+)
