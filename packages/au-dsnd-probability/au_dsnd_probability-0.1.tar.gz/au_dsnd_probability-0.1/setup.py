8d1939f2852d154821f664fe0e3f58eb4ff7bb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:22 +0000

    Add package au-dsnd-probability/0.1/au_dsnd_probability-0.1.tar.gz

---
+++ packages/au-dsnd-probability/au_dsnd_probability-0.1.tar.gz/au_dsnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='au_dsnd_probability',
+      version='0.1',
+      description='Gaussian, Binomial and Generic distributions',
+      packages=['au_dsnd_probability'],
+      author = 'Umar Abubakar',
+      author_email = 'uacoolbreeze@gmail.com',
+      zip_safe=False)
