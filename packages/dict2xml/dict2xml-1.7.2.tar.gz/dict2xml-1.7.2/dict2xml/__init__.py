ce309bb8420a18746146697a876055836c709524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:39 +0000

    Add package dict2xml/1.7.2/dict2xml-1.7.2.tar.gz

---
+++ packages/dict2xml/dict2xml-1.7.2.tar.gz/dict2xml-1.7.2/dict2xml/__init__.py
@@ -0,0 +1,11 @@
+from dict2xml.logic import Converter, Node
+
+VERSION = "1.7.2"
+
+
+def dict2xml(data, *args, **kwargs):
+    """Return an XML string of a Python dict object."""
+    return Converter(*args, **kwargs).build(data)
+
+
+__all__ = ["dict2xml", "Converter", "Node"]
---
+++ packages/dict2xml/dict2xml-1.7.2.tar.gz/dict2xml-1.7.2/setup.py
@@ -0,0 +1,40 @@
+from dict2xml import VERSION
+from setuptools import setup
+
+# fmt: off
+
+# Setup the project
+setup(
+      name = "dict2xml"
+    , version = VERSION
+    , packages = ['dict2xml']
+    
+    , python_requires = ">= 3.5"
+
+    , extras_require =
+      { 'tests' :
+        [ "noseOfYeti==2.3.1"
+        , "pytest==7.1.3"
+        ]
+      }
+
+    , classifiers =
+      [ "Development Status :: 5 - Production/Stable"
+      , "License :: OSI Approved :: MIT License"
+      , "Operating System :: OS Independent"
+      , "Programming Language :: Python"
+      , "Programming Language :: Python :: 3"
+      , "Topic :: Software Development :: Libraries :: Python Modules"
+      , "Topic :: Text Processing :: Markup :: XML"
+      ]
+
+    # metadata
+    , url = "http://github.com/delfick/python-dict2xml"
+    , author = "Stephen Moore"
+    , author_email = "stephen@delfick.com"
+    , description = "Small utility to convert a python dictionary into an XML string"
+    , long_description = open("README.rst").read()
+    , license = "MIT"
+    )
+
+# fmt: on
