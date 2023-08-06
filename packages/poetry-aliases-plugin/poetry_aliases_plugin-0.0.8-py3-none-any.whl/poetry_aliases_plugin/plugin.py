461717ae56e362611cf8a07835cb48c0f755a397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:47 +0000

    Add package poetry-aliases-plugin/0.0.8/poetry_aliases_plugin-0.0.8-py3-none-any.whl

---
+++ packages/poetry-aliases-plugin/poetry_aliases_plugin-0.0.8-py3-none-any.whl/poetry_aliases_plugin/plugin.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+from poetry_aliases_plugin.plugin_commands import AliasCommand, PreAliasCommand
+
+if TYPE_CHECKING:
+    from poetry.console.application import Application
+    from poetry.console.commands.command import Command
+
+
+class PoetryAliasesApplicationPlugin(ApplicationPlugin):
+    @property
+    def commands(self) -> list[type[Command]]:
+        return [AliasCommand]
+        return [AliasCommand, PreAliasCommand]
