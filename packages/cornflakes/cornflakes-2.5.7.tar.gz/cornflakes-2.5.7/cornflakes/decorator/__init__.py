97e0976375c9287b1eb598aaf2178568c0c13071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:43 +0000

    Add package cornflakes/2.5.7/cornflakes-2.5.7.tar.gz

---
+++ packages/cornflakes/cornflakes-2.5.7.tar.gz/cornflakes-2.5.7/cornflakes/decorator/__init__.py
@@ -0,0 +1,15 @@
+"""cornflakes decorator module.
+__________________________________
+
+.. currentmodule:: cornflakes.decorator
+
+.. autosummary::
+   :toctree: _generate
+
+    ini_config
+"""  # noqa: RST303 D205
+from cornflakes.decorator._ini_config import ini_config, ini_group
+from cornflakes.decorator._add_dataclass_slots import add_slots
+
+
+__all__ = ["ini_config", "ini_group", "add_slots"]
