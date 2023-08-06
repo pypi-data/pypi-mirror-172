b41ae296a7cc67bd428883ed0549dacd56d5e3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:14 +0000

    Add package robotcode/0.13.24/robotcode-0.13.24.tar.gz

---
+++ packages/robotcode/robotcode-0.13.24.tar.gz/robotcode-0.13.24/robotcode/utils/debugpy.py
@@ -0,0 +1,50 @@
+from .logging import LoggingDescriptor
+from .net import find_free_port
+
+_logger = LoggingDescriptor(name=__name__)
+
+
+def is_debugpy_installed() -> bool:
+    try:
+        __import__("debugpy")
+    except ImportError:
+        _logger.warning("Module debugpy is not installed. If you want to debug python code, please install it.\n")
+        return False
+    return True
+
+
+def wait_for_debugpy_connected() -> bool:
+    if is_debugpy_installed():
+        import debugpy
+
+        _logger.info("wait for debugpy client")
+        debugpy.wait_for_client()
+
+        return True
+    return False
+
+
+def enable_debugpy(port: int) -> bool:
+    if is_debugpy_installed():
+        import debugpy
+
+        debugpy.listen(port)
+
+        return True
+    return False
+
+
+def start_debugpy(port: int, wait_for_client: bool) -> bool:
+    if is_debugpy_installed():
+        import debugpy
+
+        real_port = find_free_port(port)
+        if real_port != port:
+            _logger.warning(f"start debugpy session on port {real_port}")
+        debugpy.listen(real_port)
+
+        if wait_for_client:
+            _logger.info("wait for debugpy client")
+            debugpy.wait_for_client()
+        return True
+    return False
