e42f0597397b3c893272bb2bba525d985e4b521e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:53 +0000

    Add package glidetools/0.0.4.post2/glidetools-0.0.4.post2.tar.gz

---
+++ packages/glidetools/glidetools-0.0.4.post2.tar.gz/glidetools-0.0.4.post2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory /"README.md").read_text()
+setup(
+    name='glidetools',
+    version='0.0.4-2',
+    description='DSD, GLIDE and GLIDER tools',
+    author='Kapil Devkota',
+    license_files = {'LICENSE.txt',},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email='kapil.devkota@tufts.edu',
+    url='https://github.com/kap-devkota/GLIDER',
+    packages=find_packages(),
+    install_requires=["pandas", "numpy", "scipy", "networkx"],
+    entry_points = {
+        'console_scripts': [
+            'glide-compute = glidetools.commands.glide_main:main'
+        ]}
+)
