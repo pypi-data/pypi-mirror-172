fcd28e5ea6552dc3176e5a518c577e2b417c3a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:58:58 +0000

    Add package basecore/0.1.10/basecore-0.1.10-py3-none-any.whl

---
+++ packages/basecore/basecore-0.1.10-py3-none-any.whl/basecore/network/activation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Copyright (c) 2014-2021 Megvii Inc. All rights reserved.
+
+import megengine.module as M
+
+__all__ = ["get_activation", "register_activation"]
+
+
+_ACTIVATION = {
+    "identity": M.Identity,  # identity is different from None
+    "relu": M.ReLU,
+    "prelu": M.PReLU,
+    "sigmoid": M.Sigmoid,
+    "silu": M.SiLU,
+}
+
+
+def get_activation(name, **kwargs):
+    if not name:
+        return None
+
+    act = _ACTIVATION[name](**kwargs)
+    return act
+
+
+def register_activation(name, module):
+    assert name not in _ACTIVATION
+    _ACTIVATION[name] = module
