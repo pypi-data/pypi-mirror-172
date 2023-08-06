32198a1f638979e01ce654e22edd8ec451090ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:27 +0000

    Add package netskopesdk/0.0.20/netskopesdk-0.0.20.tar.gz

---
+++ packages/netskopesdk/netskopesdk-0.0.20.tar.gz/netskopesdk-0.0.20/src/netskope_api/iterator/const.py
@@ -0,0 +1,20 @@
+"""
+Define the const used across all components.
+
+Only put built-in imports in this module to make this really Const.
+Use var.py for variable definitions.
+
+"""
+
+class Const(object):
+
+    """
+    Constant definition for iterator
+    """
+    
+    NSKP_TOKEN = "TOKEN"
+    NSKP_ITERATOR_NAME = "ITERATOR_NAME"
+    NSKP_EVENT_TYPE = "EVENT_TYPE"
+    NSKP_TENANT_HOSTNAME = "TENANT_HOSTNAME"
+    NSKP_PROXIES = "PROXIES_DICT"
+
