721c2ea609f07f6644f96bfc8d56616c74181330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:14 +0000

    Add package LisaPython/1.0.7/LisaPython-1.0.7.tar.gz

---
+++ packages/LisaPython/LisaPython-1.0.7.tar.gz/LisaPython-1.0.7/LisaPython/caculator.py
@@ -0,0 +1,35 @@
+class MyCalculator:
+    def add_numbers(num1, num2):
+        '''
+        Add two given numbers.
+        arguments:
+            num1 (number): first number
+            num2 (number): second number
+        return type: (number)
+        '''
+        return num1 + num2
+
+    def subtract_numbers(num1, num2):
+        '''
+        Substract two given numbers.
+        arguments:
+            num1 (number): first number
+            num2 (number): second number
+        return type: (number)
+        '''
+        return num1 - num2
+
+    def multiply_numbers(num1, num2):
+        '''
+        Multiply two given numbers.
+        arguments:
+            num1 (number): first number
+            num2 (number): second number
+        return type: (number)
+        '''
+        return num1 * num2
+
+    def divide_numbers(num1, num2):
+        return num1 / num2
+
+
