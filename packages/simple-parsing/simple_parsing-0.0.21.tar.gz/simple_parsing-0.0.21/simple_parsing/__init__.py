94473cdc8c349f109aab85e98f25c5c6b55f62da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:33 +0000

    Add package simple-parsing/0.0.21/simple_parsing-0.0.21.tar.gz

---
+++ packages/simple-parsing/simple_parsing-0.0.21.tar.gz/simple_parsing-0.0.21/simple_parsing/__init__.py
@@ -0,0 +1,54 @@
+"""Simple, Elegant Argument parsing.
+@author: Fabrice Normandin
+"""
+from . import helpers, utils, wrappers
+from .conflicts import ConflictResolution
+from .help_formatter import SimpleHelpFormatter
+from .helpers import (
+    MutableField,
+    Serializable,
+    choice,
+    field,
+    flag,
+    list_field,
+    mutable_field,
+    subgroups,
+    subparsers,
+)
+from .parsing import (
+    ArgumentGenerationMode,
+    ArgumentParser,
+    DashVariant,
+    NestedMode,
+    ParsingError,
+    parse,
+)
+from .utils import InconsistentArgumentError
+
+__all__ = [
+    "helpers",
+    "utils",
+    "wrappers",
+    "ConflictResolution",
+    "SimpleHelpFormatter",
+    "MutableField",
+    "Serializable",
+    "choice",
+    "field",
+    "flag",
+    "list_field",
+    "mutable_field",
+    "subgroups",
+    "subparsers",
+    "ArgumentParser",
+    "DashVariant",
+    "ParsingError",
+    "parse",
+    "ArgumentGenerationMode",
+    "NestedMode",
+    "InconsistentArgumentError",
+]
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
