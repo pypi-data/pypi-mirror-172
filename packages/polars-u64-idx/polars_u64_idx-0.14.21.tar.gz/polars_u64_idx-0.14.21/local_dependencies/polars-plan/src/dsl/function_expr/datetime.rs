51d31b2a840f8b5ad670766491325651c7ee3904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:30 +0000

    Add package polars-u64-idx/0.14.21/polars_u64_idx-0.14.21.tar.gz

---
+++ packages/polars-u64-idx/polars_u64_idx-0.14.21.tar.gz/polars_u64_idx-0.14.21/local_dependencies/polars-plan/src/dsl/function_expr/datetime.rs
@@ -0,0 +1,103 @@
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+use super::*;
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Clone, PartialEq, Debug, Eq, Hash)]
+pub enum TemporalFunction {
+    Year,
+    IsoYear,
+    Quarter,
+    Month,
+    Week,
+    WeekDay,
+    Day,
+    OrdinalDay,
+    Hour,
+    Minute,
+    Second,
+    Millisecond,
+    Microsecond,
+    Nanosecond,
+    TimeStamp(TimeUnit),
+    #[cfg(feature = "timezones")]
+    CastTimezone(TimeZone),
+}
+
+impl Display for TemporalFunction {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        use TemporalFunction::*;
+        let s = match self {
+            Year => "year",
+            IsoYear => "iso_year",
+            Quarter => "quarter",
+            Month => "month",
+            Week => "week",
+            WeekDay => "weekday",
+            Day => "day",
+            OrdinalDay => "ordinal_day",
+            Hour => "hour",
+            Minute => "minute",
+            Second => "second",
+            Millisecond => "millisecond",
+            Microsecond => "microsecond",
+            Nanosecond => "nanosecond",
+            TimeStamp(tu) => return write!(f, "dt.timestamp({})", tu),
+            #[cfg(feature = "timezones")]
+            CastTimezone(_) => "cast_timezone",
+        };
+        write!(f, "dt.{}", s)
+    }
+}
+
+pub(super) fn year(s: &Series) -> PolarsResult<Series> {
+    s.year().map(|ca| ca.into_series())
+}
+pub(super) fn iso_year(s: &Series) -> PolarsResult<Series> {
+    s.iso_year().map(|ca| ca.into_series())
+}
+pub(super) fn month(s: &Series) -> PolarsResult<Series> {
+    s.month().map(|ca| ca.into_series())
+}
+pub(super) fn quarter(s: &Series) -> PolarsResult<Series> {
+    s.quarter().map(|ca| ca.into_series())
+}
+pub(super) fn week(s: &Series) -> PolarsResult<Series> {
+    s.week().map(|ca| ca.into_series())
+}
+pub(super) fn weekday(s: &Series) -> PolarsResult<Series> {
+    s.weekday().map(|ca| ca.into_series())
+}
+pub(super) fn day(s: &Series) -> PolarsResult<Series> {
+    s.day().map(|ca| ca.into_series())
+}
+pub(super) fn ordinal_day(s: &Series) -> PolarsResult<Series> {
+    s.ordinal_day().map(|ca| ca.into_series())
+}
+pub(super) fn hour(s: &Series) -> PolarsResult<Series> {
+    s.hour().map(|ca| ca.into_series())
+}
+pub(super) fn minute(s: &Series) -> PolarsResult<Series> {
+    s.minute().map(|ca| ca.into_series())
+}
+pub(super) fn second(s: &Series) -> PolarsResult<Series> {
+    s.second().map(|ca| ca.into_series())
+}
+pub(super) fn millisecond(s: &Series) -> PolarsResult<Series> {
+    s.nanosecond().map(|ca| (ca / 1_000_000).into_series())
+}
+pub(super) fn microsecond(s: &Series) -> PolarsResult<Series> {
+    s.nanosecond().map(|ca| (ca / 1_000).into_series())
+}
+pub(super) fn nanosecond(s: &Series) -> PolarsResult<Series> {
+    s.nanosecond().map(|ca| ca.into_series())
+}
+pub(super) fn timestamp(s: &Series, tu: TimeUnit) -> PolarsResult<Series> {
+    s.timestamp(tu).map(|ca| ca.into_series())
+}
+#[cfg(feature = "timezones")]
+pub(super) fn cast_timezone(s: &Series, tz: &str) -> PolarsResult<Series> {
+    let ca = s.datetime()?;
+    ca.cast_time_zone(tz).map(|ca| ca.into_series())
+}
