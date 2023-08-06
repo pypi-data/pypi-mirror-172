a13168d13e043ac75c93415889c64abea264e7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:01 +0000

    Add package bbrl/0.1.5/bbrl-0.1.5.tar.gz

---
+++ packages/bbrl/bbrl-0.1.5.tar.gz/bbrl-0.1.5/setup.py
@@ -0,0 +1,22 @@
+#
+# Copyright (c) Sorbonne Universite
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+from setuptools import find_packages, setup
+
+setup(
+    name="bbrl",
+    packages=[package for package in find_packages() if package.startswith("bbrl")],
+    version="0.1.5",
+    install_requires=open("requirements.txt", "r").read().splitlines(),
+    tests_require=["pytest==4.4.1"],
+    test_suite="tests",
+    description="RL library inspired from salina",
+    author="Olivier Sigaud",
+    author_email="Olivier.Sigaud@isir.upmc.fr",
+    url="https://github.com/osigaud/bbrl.git",
+    license="MIT",
+)
