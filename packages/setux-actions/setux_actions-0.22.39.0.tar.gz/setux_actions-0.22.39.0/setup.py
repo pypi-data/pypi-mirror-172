426bd2ac6529cd4c697f6d58e77a15a755675da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:46:41 +0000

    Add package setux-actions/0.22.39.0/setux_actions-0.22.39.0.tar.gz

---
+++ packages/setux-actions/setux_actions-0.22.39.0.tar.gz/setux_actions-0.22.39.0/setup.py
@@ -0,0 +1,40 @@
+from os.path import join, dirname, abspath
+
+from setuptools import setup, find_namespace_packages
+
+curdir = abspath(dirname(__file__))
+readme = open(join(curdir, 'README.md')).read()
+
+setup(
+    name             = 'setux_actions',
+    version          = '0.22.39.0',
+    description      = 'System deployment',
+    long_description = readme,
+    long_description_content_type='text/markdown',
+    keywords         = ['utility', ],
+    url              = 'https://notabug.org/dugres/setux_actions',
+    author           = 'Louis RIVIERE',
+    author_email     = 'louis@riviere.xyz',
+    license          = 'MIT',
+    classifiers      = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+    python_requires='>3.9',
+    install_requires = [
+        'pybrary>=0.22.30.0',
+        'setux_core>=0.22.39.0',
+    ],
+    packages = find_namespace_packages(
+        include=['setux.*']
+    ),
+)
