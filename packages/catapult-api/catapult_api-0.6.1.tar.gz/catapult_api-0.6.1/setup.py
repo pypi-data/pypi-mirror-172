aefc0a4cb9df75864972398e718e49cc96794d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:50 +0000

    Add package catapult-api/0.6.1/catapult_api-0.6.1.tar.gz

---
+++ packages/catapult-api/catapult_api-0.6.1.tar.gz/catapult_api-0.6.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.6.1"
+DESCRIPTION = "Python API wrapper for Catapult GPS tracking system"
+
+setup(
+    name="catapult_api",
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=find_packages(),
+)
