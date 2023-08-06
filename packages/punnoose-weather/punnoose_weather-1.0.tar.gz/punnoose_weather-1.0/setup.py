ebe8f23e659a2bb7c62bf131a133613d7e6ad159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:22 +0000

    Add package punnoose-weather/1.0/punnoose_weather-1.0.tar.gz

---
+++ packages/punnoose-weather/punnoose_weather-1.0.tar.gz/punnoose_weather-1.0/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 22 10:20:34 2022
+
+@author: PP
+"""
+from setuptools import setup
+
+setup(name='punnoose_weather',
+      version='1.0',
+      description='Punnoose Weather',
+      author='Punnoose',
+      author_email='punnoose.p@gmail.com',
+      url='',
+      packages=[],
+     )  
