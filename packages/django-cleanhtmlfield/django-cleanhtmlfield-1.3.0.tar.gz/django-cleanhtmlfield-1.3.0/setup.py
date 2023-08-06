e360e7fa0926d78040c36168dfb92be09d01a5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:35 +0000

    Add package django-cleanhtmlfield/1.3.0/django-cleanhtmlfield-1.3.0.tar.gz

---
+++ packages/django-cleanhtmlfield/django-cleanhtmlfield-1.3.0.tar.gz/django-cleanhtmlfield-1.3.0/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-cleanhtmlfield',
+    version=os.getenv('PACKAGE_VERSION', '0.0.0').replace('refs/tags/', ''),
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='A simple Django app that will give you a cleaned HTML field.',
+    long_description=README,
+    long_description_content_type='text/markdown',  # This is important for markdown to work
+    url='https://github.com/anexia/django-cleanhtmlfield',
+    author='Andreas Stocker',
+    author_email='AStocker@anexia-it.com',
+    install_requires=[
+        'django',
+        'beautifulsoup4>=4.6,<5',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
