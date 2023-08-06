17a00975ccc996146e4b59b47fa3a73213658c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:05 +0000

    Add package bnmapi-python/0.0.1/bnmapi_python-0.0.1-py3-none-any.whl

---
+++ packages/bnmapi-python/bnmapi_python-0.0.1-py3-none-any.whl/bnm/__init__.py
@@ -0,0 +1,2 @@
+from .bnm import BNM
+__all__ = ['BNM']
---
+++ packages/bnmapi-python/bnmapi_python-0.0.1-py3-none-any.whl/bnm/constants.py
@@ -0,0 +1,4 @@
+EXCHANGE_SESSION = ['0900', '1130', '1200', '1700']
+BASE_CURRENCY = ['rm', 'fx']
+PRODUCTS = ['money_market_operations', 'interbank', 'overall']
+URL = 'https://api.bnm.gov.my/public'
