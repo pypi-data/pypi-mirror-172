9f2a30608d122a69fd67c5b0f4ba639a71cf3a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:29 +0000

    Add package qsdsan/1.2.1/qsdsan-1.2.1.tar.gz

---
+++ packages/qsdsan/qsdsan-1.2.1.tar.gz/qsdsan-1.2.1/qsdsan/processes/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+QSDsan: Quantitative Sustainable Design for sanitation and resource recovery systems
+
+This module is developed by:
+
+    Yalin Li <mailto.yalin.li@gmail.com>
+
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/QSDsan/blob/main/LICENSE.txt
+for license details.
+'''
+
+from ._aeration import *
+from ._asm1 import *
+from ._asm2d import *
+from ._adm1 import *
+from ._decay import *
+
+from . import (
+    _aeration,
+    _asm1,
+    _asm2d,
+    _adm1,
+    _decay,
+    )
+
+__all__ = (
+    *_aeration.__all__,
+    *_asm1.__all__,
+    *_asm2d.__all__,
+    *_adm1.__all__,
+    *_decay.__all__,
+    )
