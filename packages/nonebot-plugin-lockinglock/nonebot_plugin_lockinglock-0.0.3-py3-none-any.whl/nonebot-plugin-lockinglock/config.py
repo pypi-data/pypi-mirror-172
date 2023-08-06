ed9161f1b86083323404de853968cfdf980fc7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:08 +0000

    Add package nonebot-plugin-lockinglock/0.0.3/nonebot_plugin_lockinglock-0.0.3-py3-none-any.whl

---
+++ packages/nonebot-plugin-lockinglock/nonebot_plugin_lockinglock-0.0.3-py3-none-any.whl/nonebot-plugin-lockinglock/config.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: i2cy(i2cy@outlook.com)
+# Project: NonebotPluginLockingLock
+# Filename: config
+# Created on: 1/10/2022
+
+"""
+config.i2ll_host
+config.i2ll_port
+config.i2ll_psk
+config.i2ll_devices
+"""
+
+from pydantic import BaseSettings, BaseModel, Extra
+from typing import List
+from i2llservice.client import DeviceClient
+
+
+class Devices(BaseModel, extra=Extra.ignore):
+
+    alias: List[str] = []
+    root_topic: str
+    permitted_group: List[int]
+    dev_clt: DeviceClient = None
+
+
+class Config(BaseSettings, extra=Extra.ignore):
+
+    i2ll_host: str = "127.0.0.1"
+    i2ll_port: int = 8421
+    i2ll_psk: str = "i2tcppsk"
+    i2ll_clt_buffer: int = 20
+    i2ll_timeout: int = 15
+    i2ll_devices: List[Devices] = []
+
