2d79825403f448c4a0b431066d66785db810e2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:44 +0000

    Add package cleanlab-cli/0.1.14/cleanlab_cli-0.1.14-py3-none-any.whl

---
+++ packages/cleanlab-cli/cleanlab_cli-0.1.14-py3-none-any.whl/cleanlab_cli/dataset/commands.py
@@ -0,0 +1,12 @@
+import click
+from cleanlab_cli.dataset.schema import schema
+from cleanlab_cli.dataset.upload import upload
+
+
+@click.group(help="upload datasets, generate & validate dataset schema")
+def dataset() -> None:
+    pass
+
+
+dataset.add_command(schema)
+dataset.add_command(upload)
