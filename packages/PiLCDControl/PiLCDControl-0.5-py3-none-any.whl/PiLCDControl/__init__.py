b07bd3d84b2f198100f31391f728485cdeda2104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:05 +0000

    Add package PiLCDControl/0.5/PiLCDControl-0.5-py3-none-any.whl

---
+++ packages/PiLCDControl/PiLCDControl-0.5-py3-none-any.whl/PiLCDControl/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022, Adam Lake
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# ======================================================================================================================
+# Created: 18/10/2022
+# GitHub: https://github.com/adam1lake/Pi-LCD
+# ======================================================================================================================
+
+from PiLCDControl import LCD
