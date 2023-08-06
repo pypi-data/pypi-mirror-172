478dffd31180345eb823619a79f05752681988b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:37 +0000

    Add package dazl/7.9.1/dazl-7.9.1-py3-none-any.whl

---
+++ packages/dazl/dazl-7.9.1-py3-none-any.whl/dazl/_gen/com/daml/ledger/api/v1/admin/__init__.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2017-2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+# fmt: off
+# isort: skip_file
+
+from .config_management_service_pb2 import GetTimeModelRequest, GetTimeModelResponse, SetTimeModelRequest, SetTimeModelResponse, TimeModel
+from .config_management_service_pb2_grpc import ConfigManagementServiceStub
+from .metering_report_service_pb2 import ApplicationMeteringReport, GetMeteringReportRequest, GetMeteringReportResponse, ParticipantMeteringReport
+from .metering_report_service_pb2_grpc import MeteringReportServiceStub
+from .package_management_service_pb2 import ListKnownPackagesRequest, ListKnownPackagesResponse, PackageDetails, UploadDarFileRequest, UploadDarFileResponse
+from .package_management_service_pb2_grpc import PackageManagementServiceStub
+from .participant_pruning_service_pb2 import PruneRequest, PruneResponse
+from .participant_pruning_service_pb2_grpc import ParticipantPruningServiceStub
+from .party_management_service_pb2 import AllocatePartyRequest, AllocatePartyResponse, GetParticipantIdRequest, GetParticipantIdResponse, GetPartiesRequest, GetPartiesResponse, ListKnownPartiesRequest, ListKnownPartiesResponse, PartyDetails
+from .party_management_service_pb2_grpc import PartyManagementServiceStub
+from .user_management_service_pb2 import CreateUserRequest, CreateUserResponse, DeleteUserRequest, DeleteUserResponse, GetUserRequest, GetUserResponse, GrantUserRightsRequest, GrantUserRightsResponse, ListUserRightsRequest, ListUserRightsResponse, ListUsersRequest, ListUsersResponse, RevokeUserRightsRequest, RevokeUserRightsResponse, Right, User
+from .user_management_service_pb2_grpc import UserManagementServiceStub
+
+__all__ = [
+    "AllocatePartyRequest",
+    "AllocatePartyResponse",
+    "ApplicationMeteringReport",
+    "ConfigManagementServiceStub",
+    "CreateUserRequest",
+    "CreateUserResponse",
+    "DeleteUserRequest",
+    "DeleteUserResponse",
+    "GetMeteringReportRequest",
+    "GetMeteringReportResponse",
+    "GetParticipantIdRequest",
+    "GetParticipantIdResponse",
+    "GetPartiesRequest",
+    "GetPartiesResponse",
+    "GetTimeModelRequest",
+    "GetTimeModelResponse",
+    "GetUserRequest",
+    "GetUserResponse",
+    "GrantUserRightsRequest",
+    "GrantUserRightsResponse",
+    "ListKnownPackagesRequest",
+    "ListKnownPackagesResponse",
+    "ListKnownPartiesRequest",
+    "ListKnownPartiesResponse",
+    "ListUserRightsRequest",
+    "ListUserRightsResponse",
+    "ListUsersRequest",
+    "ListUsersResponse",
+    "MeteringReportServiceStub",
+    "PackageDetails",
+    "PackageManagementServiceStub",
+    "ParticipantMeteringReport",
+    "ParticipantPruningServiceStub",
+    "PartyDetails",
+    "PartyManagementServiceStub",
+    "PruneRequest",
+    "PruneResponse",
+    "RevokeUserRightsRequest",
+    "RevokeUserRightsResponse",
+    "Right",
+    "SetTimeModelRequest",
+    "SetTimeModelResponse",
+    "TimeModel",
+    "UploadDarFileRequest",
+    "UploadDarFileResponse",
+    "User",
+    "UserManagementServiceStub",
+]
