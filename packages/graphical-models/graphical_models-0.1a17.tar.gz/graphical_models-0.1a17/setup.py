2cee61f48184c72fb421fd770a071933d7a63ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:54 +0000

    Add package graphical-models/0.1a17/graphical_models-0.1a17.tar.gz

---
+++ packages/graphical-models/graphical_models-0.1a17.tar.gz/graphical_models-0.1a17/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='graphical_models',
+    version='0.1a.017',
+    description='',
+    long_description='',
+    author='',
+    author_email='',
+    packages=setuptools.find_packages(exclude=['tests']),
+    python_requires='>3.5.0',
+    zip_safe=False,
+    classifiers=[
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    install_requires=[
+        'numpy',
+        'einops'
+    ]
+)
+
