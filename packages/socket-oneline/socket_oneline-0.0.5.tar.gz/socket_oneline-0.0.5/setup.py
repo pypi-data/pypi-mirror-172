26421482796b2b402181b9b8895b461411a23fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:57 +0000

    Add package socket-oneline/0.0.5/socket_oneline-0.0.5.tar.gz

---
+++ packages/socket-oneline/socket_oneline-0.0.5.tar.gz/socket_oneline-0.0.5/setup.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+version = '0.0.5'
+
+this_directory = Path(__file__).parent
+long_desc = (this_directory / "README.md").read_text()
+long_version = version.replace('.', '_')
+setup(
+    name='socket_oneline',
+    include_package_data=True,
+    packages=find_packages(include='socket_oneline*', ),
+    version=version,
+    license='MIT',
+    description='Client server base class over socket',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    author='JA',
+    author_email='cppgent0@gmail.com',
+    url='https://github.com/cppgent0/socket-oneline',
+    download_url=f'https://github.com/cppgent0/socket-oneline/archive/refs/tags/v_{long_version}.tar.gz',
+    keywords=['socket', 'client server', 'simple'],
+    install_requires=[
+        'pytest',
+    ],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
