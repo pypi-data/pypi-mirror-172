01a7105ec2dad6d504797f76f3aeab503c003ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:46 +0000

    Add package aws-cdk.lambda-layer-kubectl-v24/0.0.1/aws_cdk.lambda_layer_kubectl_v24-0.0.1-py3-none-any.whl

---
+++ packages/aws-cdk.lambda-layer-kubectl-v24/aws_cdk.lambda_layer_kubectl_v24-0.0.1-py3-none-any.whl/aws_cdk/lambda_layer_kubectl_v24/__init__.py
@@ -0,0 +1,80 @@
+'''
+# Lambda Layer with KubeCtl v1.24
+
+<!--BEGIN STABILITY BANNER-->---
+
+
+![cdk-constructs: Experimental](https://img.shields.io/badge/cdk--constructs-experimental-important.svg?style=for-the-badge)
+
+---
+
+
+> This library is currently under development. Do not use!
+
+<!--END STABILITY BANNER-->
+
+This module exports a single class called `KubectlLayer` which is a `lambda.LayerVersion` that
+bundles the [`kubectl`](https://kubernetes.io/docs/reference/kubectl/kubectl/) and the
+[`helm`](https://helm.sh/) command line.
+
+> * Helm Version: 3.9.4
+> * Kubectl Version: 1.24.0
+
+Usage:
+
+```python
+// KubectlLayer bundles the 'kubectl' and 'helm' command lines
+import { KubectlLayer } from '@aws-cdk/lambda-layer-kubectl-v23';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+declare const fn: lambda.Function;
+const kubectl = new KubectlLayer(this, 'KubectlLayer');
+fn.addLayers(kubectl);
+```
+
+`kubectl` will be installed under `/opt/kubectl/kubectl`, and `helm` will be installed under `/opt/helm/helm`.
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+from ._jsii import *
+
+import aws_cdk.aws_lambda
+import constructs
+
+
+class KubectlLayer(
+    aws_cdk.aws_lambda.LayerVersion,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@aws-cdk/lambda-layer-kubectl-v24.KubectlLayer",
+):
+    '''A CDK Asset construct that contains ``kubectl`` and ``helm``.'''
+
+    def __init__(self, scope: constructs.Construct, id: builtins.str) -> None:
+        '''
+        :param scope: -
+        :param id: -
+        '''
+        if __debug__:
+            def stub(scope: constructs.Construct, id: builtins.str) -> None:
+                ...
+            type_hints = typing.get_type_hints(stub)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        jsii.create(self.__class__, self, [scope, id])
+
+
+__all__ = [
+    "KubectlLayer",
+]
+
+publication.publish()
