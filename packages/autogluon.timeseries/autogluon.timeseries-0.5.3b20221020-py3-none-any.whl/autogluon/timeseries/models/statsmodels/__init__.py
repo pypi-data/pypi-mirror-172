94da0253e5384c1ed5012de526ded8a4c8721dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:59 +0000

    Add package autogluon.timeseries/0.5.3b20221020/autogluon.timeseries-0.5.3b20221020-py3-none-any.whl

---
+++ packages/autogluon.timeseries/autogluon.timeseries-0.5.3b20221020-py3-none-any.whl/autogluon/timeseries/models/statsmodels/__init__.py
@@ -0,0 +1,8 @@
+from .abstract_statsmodels import AbstractStatsmodelsModel
+from .models import ARIMAModel, ETSModel, ThetaModel
+
+__all__ = [
+    "ETSModel",
+    "ARIMAModel",
+    "ThetaModel",
+]
