721480d303c5703981b6a66d1eb1ab5c137b8b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:32 +0000

    Add package esphome-dashboard/20221020.0/esphome-dashboard-20221020.0.tar.gz

---
+++ packages/esphome-dashboard/esphome-dashboard-20221020.0.tar.gz/esphome-dashboard-20221020.0/esphome_dashboard/static/js/esphome/c.7a323ab8.js
@@ -0,0 +1,1 @@
+const e=()=>import("./c.ea1f3aa5.js"),o=(o,a)=>{e();const n=document.createElement("esphome-rename-process-dialog");n.configuration=o,n.newName=a,document.body.append(n)};export{o,e as p};
---
+++ packages/esphome-dashboard/esphome-dashboard-20221020.0.tar.gz/esphome-dashboard-20221020.0/esphome_dashboard/static/js/esphome/c.eb09cc18.js
@@ -0,0 +1,25 @@
+import{b as o,d as t,p as e,n as s,s as i,y as r,U as a}from"./index-99c4f288.js";import"./c.b803e671.js";import{o as n}from"./c.0153deff.js";import"./c.a70a794b.js";import"./c.ea50bec1.js";import"./c.2e012194.js";let c=class extends i{render(){return r`
+      <esphome-process-dialog
+        always-show-close
+        .heading=${`Logs ${this.configuration}`}
+        .type=${"logs"}
+        .spawnParams=${{configuration:this.configuration,port:this.target}}
+        @closed=${this._handleClose}
+        @process-done=${this._handleProcessDone}
+      >
+        <mwc-button
+          slot="secondaryAction"
+          dialogAction="close"
+          label="Edit"
+          @click=${this._openEdit}
+        ></mwc-button>
+        ${void 0===this._result||0===this._result?"":r`
+              <mwc-button
+                slot="secondaryAction"
+                dialogAction="close"
+                label="Retry"
+                @click=${this._handleRetry}
+              ></mwc-button>
+            `}
+      </esphome-process-dialog>
+    `}_openEdit(){a(this.configuration)}_handleProcessDone(o){this._result=o.detail}_handleRetry(){n(this.configuration,this.target)}_handleClose(){this.parentNode.removeChild(this)}};o([t()],c.prototype,"configuration",void 0),o([t()],c.prototype,"target",void 0),o([e()],c.prototype,"_result",void 0),c=o([s("esphome-logs-dialog")],c);
