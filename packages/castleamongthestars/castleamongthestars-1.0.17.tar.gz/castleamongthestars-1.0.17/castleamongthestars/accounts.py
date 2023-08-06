e5bf5b27311e01a29758ca20dbdae3b441e9965f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:28 +0000

    Add package castleamongthestars/1.0.17/castleamongthestars-1.0.17.tar.gz

---
+++ packages/castleamongthestars/castleamongthestars-1.0.17.tar.gz/castleamongthestars-1.0.17/castleamongthestars/accounts.py
@@ -0,0 +1,17 @@
+
+import cloudscraper
+
+scraper = cloudscraper.create_scraper(delay=10, interpreter='nodejs',
+  captcha={'provider': 'return_response'})
+
+
+def account(auth):
+    acc = scraper.get(f'https://api.bloxflip.com/user', headers={'x-auth-token': auth}).json()
+    if acc['sucesss'] == True:
+        id = acc['robloxId']
+        subacc = scraper.get(f'https://api.bloxflip.com/user/lookup/{id}').json()
+        accountsuccess = {'success': True, 'acc': acc , 'subacc' : subacc}
+        return accountsuccess
+    else:
+        error = acc['error']
+        accountsuccess = {'success': False, 'error': error}
