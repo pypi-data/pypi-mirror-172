d73a47b95aa16a87772ad8759d6d0d8f7c1ee7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:46 +0000

    Add package UniversalWrapper/2.1/UniversalWrapper-2.1.tar.gz

---
+++ packages/UniversalWrapper/UniversalWrapper-2.1.tar.gz/UniversalWrapper-2.1/universalwrapper/test_universal_wrapper.py
@@ -0,0 +1,254 @@
+# Copyright 2022 by Bas de Bruijne
+# All rights reserved.
+# Universal Wrapper comes with ABSOLUTELY NO WARRANTY, the writer can not be
+# held responsible for any problems caused by the use of this module.
+#
+# Any changes made to UniversalWrapper must be covered by one of the unit tests
+# below. Expected outputs can not be changed to accommodate for new changes.
+# To run the unittests:
+# $ python3 -m coverage run -m unittest
+# $ python3 -m coverage report --include universal_wrapper.py
+
+import asyncio
+import subprocess
+import unittest
+import universalwrapper
+
+from mock import patch, AsyncMock, ANY
+
+
+class TestUniversalWrapper(unittest.TestCase):
+    @patch("universalwrapper.subprocess.check_output")
+    def test_basic_cases(self, mock_check_output):
+        uw_test = universalwrapper.uw_test
+
+        uw_test("a")
+        mock_check_output.assert_called_with(["uw-test", "a"])
+        uw_test.run("a")
+        mock_check_output.assert_called_with(["uw-test", "run", "a"])
+        uw_test.run("a", b=True)
+        mock_check_output.assert_called_with(["uw-test", "run", "a", "-b"])
+        uw_test.run("a", bar=True)
+        mock_check_output.assert_called_with(["uw-test", "run", "a", "--bar"])
+        uw_test.run("a", bar=[True, True])
+        mock_check_output.assert_called_with(["uw-test", "run", "a", "--bar", "--bar"])
+        uw_test.run("a", b="foo")
+        mock_check_output.assert_called_with(["uw-test", "run", "a", "-b", "foo"])
+        uw_test.run("a", bar="foo")
+        mock_check_output.assert_called_with(["uw-test", "run", "a", "--bar", "foo"])
+        uw_test.run("a", bar=["foo", "bar"])
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "a", "--bar", "foo", "--bar", "bar"]
+        )
+        uw_test.run.runs("a", "b")
+        mock_check_output.assert_called_with(["uw-test", "run", "runs", "a", "b"])
+        uw_test.run.runs("a", "b", root=True)
+        mock_check_output.assert_called_with(
+            ["sudo", "uw-test", "run", "runs", "a", "b"]
+        )
+        uw_test.run.runs("arg with space")
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "runs", "arg with space"]
+        )
+        uw_test.a.b.c.d.e.f.g.h.i.j.k.l.m()
+        mock_check_output.assert_called_with(
+            ["uw-test", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
+        )
+        uw_test.uw_settings.double_dash = False
+        uw_test.run("a", bar=["foo", "bar"])
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "a", "-bar", "foo", "-bar", "bar"]
+        )
+
+    @patch("universalwrapper.subprocess.check_output")
+    def test_input_add(self, mock_check_output):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.input_add = {"bar": -1}
+        uw_test.run.runs("a", "b")
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "runs", "a", "b", "bar"]
+        )
+        uw_test.uw_settings.input_add = {"--bar": -1, "foo": 0}
+        uw_test.run("a", "b")
+        mock_check_output.assert_called_with(
+            ["foo", "uw-test", "run", "a", "b", "--bar"]
+        )
+        uw_test.run("a", "b", bar=False)
+        mock_check_output.assert_called_with(["foo", "uw-test", "run", "a", "b"])
+        uw_test.uw_settings.input_add = {"--bar foo": -1, "--barfoo bar": -1}
+        uw_test.run("a", "b")
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "a", "b", "--bar", "foo", "--barfoo", "bar"]
+        )
+        uw_test.run("a", "b", bar=False)
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "a", "b", "--barfoo", "bar"]
+        )
+        uw_test.run("a", "b", barfoo=False)
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "a", "b", "--bar", "foo"]
+        )
+        uw_test.uw_settings.input_add = {"--bar": -1, "--barfoo bar": -1}
+        uw_test.run("a", "b")
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "a", "b", "--bar", "--barfoo", "bar"]
+        )
+        uw_test.run("a", "b", bar=False)
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "a", "b", "--barfoo", "bar"]
+        )
+        uw_test.run("a", "b", barfoo=False)
+        mock_check_output.assert_called_with(["uw-test", "run", "a", "b", "--bar"])
+
+        uw_test.uw_settings.input_add = {"--bar foo": -1}
+        uw_test.run(bar="bar")
+        mock_check_output.assert_called_with(
+            ["uw-test", "run", "--bar", "bar", "--bar", "foo"]
+        )
+        uw_test.run(bar=[False, "bar"])
+        mock_check_output.assert_called_with(["uw-test", "run", "--bar", "bar"])
+
+    @patch("universalwrapper.subprocess.check_output")
+    def test_input_move(self, mock_check_output):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.input_move = {"runs": 0}
+        uw_test.run.runs("a", "b")
+        mock_check_output.assert_called_with(["runs", "uw-test", "run", "a", "b"])
+        uw_test.uw_settings.input_move = {"uw-test": -1}
+        uw_test.run.runs("a", "b")
+        mock_check_output.assert_called_with(["run", "runs", "a", "b", "uw-test"])
+        uw_test.uw_settings.input_move = {"--bar": 1}
+        uw_test.run.runs("a", bar="foo")
+        mock_check_output.assert_called_with(
+            ["uw-test", "--bar", "foo", "run", "runs", "a"]
+        )
+
+        uw_test.uw_settings.input_move = {}
+        uw_test.uw_settings.input_custom = ["command.reverse()"]
+        uw_test.run.runs("a", "b")
+        mock_check_output.assert_called_with(["b", "a", "runs", "run", "uw-test"])
+
+        universalwrapper.run_command("a")
+        mock_check_output.assert_called_with(["run-command", "a"])
+
+    @patch("universalwrapper.subprocess.check_output")
+    def test_dividers(self, mock_check_output):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.class_divider = "~"
+        uw_test.uw_settings.divider = " "
+        uw_test.uw_settings.flag_divider = "bar"
+        uw_test.run.runs("a", "b", bar_foo=True)
+        mock_check_output.assert_called_with(
+            ["uw", "test~run~runs", "a", "b", "--barbarfoo"]
+        )
+
+    @patch("universalwrapper.print")
+    @patch("universalwrapper.subprocess.check_output")
+    def test_debug(self, mock_check_output, mock_print):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.class_divider = "~"
+        uw_test.uw_settings.divider = " "
+        uw_test.uw_settings.flag_divider = "bar"
+        uw_test.uw_settings.debug = True
+        uw_test.run.runs("a", "b", bar_foo=True)
+        mock_check_output.assert_not_called()
+        mock_print.assert_called_with(
+            "Generated command:\n['uw', 'test~run~runs', 'a', 'b', '--barbarfoo']"
+        )
+
+    def test_change_settings(self):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.debug = True
+        with self.assertRaises(Exception) as context:
+            uw_test.uw_settings.non_existant_setting = True
+
+        self.assertTrue("Valid settings are limited to" in str(context.exception))
+
+    def test_set_settings(self):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.divider = "foo"
+        self.assertEqual(uw_test.uw_settings.divider, "foo")
+        with self.assertRaises(Exception) as context:
+            uw_test = universalwrapper.uw_test
+            uw_test.uw_settings.bar = "foo"
+
+        self.assertTrue("Valid settings are limited to" in str(context.exception))
+
+    @patch("universalwrapper.subprocess.check_output")
+    def test_parse_yaml(self, mock_check_output):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.output_decode = False
+        uw_test.uw_settings.output_yaml = True
+        mock_check_output.return_value = """\
+- foo: bar
+  config:
+    bar: foo"""
+        result = uw_test()
+        self.assertEqual(result, [{"foo": "bar", "config": {"bar": "foo"}}])
+
+    @patch("universalwrapper.subprocess.check_output")
+    def test_parse_json(self, mock_check_output):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.output_decode = False
+        uw_test.uw_settings.output_json = True
+        mock_check_output.return_value = '{"foo": "bar", "config": {"bar": "foo"}}'
+        result = uw_test()
+        self.assertEqual(result, {"foo": "bar", "config": {"bar": "foo"}})
+
+    @patch("universalwrapper.subprocess.check_output")
+    def test_parse_json(self, mock_check_output):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.output_decode = False
+        uw_test.uw_settings.output_splitlines = True
+        mock_check_output.return_value = "a\nb\nc"
+        result = uw_test()
+        self.assertEqual(result, ["a", "b", "c"])
+
+    def test_load_settings(self):
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.divider = " "
+        self.assertEqual(uw_test.uw_settings.divider, " ")
+
+    def test_async(self):
+        asyncio.run(self._test_async())
+
+    @patch("universalwrapper.asyncio.create_subprocess_exec")
+    async def _test_async(self, mock_cse):
+        proc = AsyncMock()
+        proc.returncode = 0
+        proc.communicate.return_value = (b"output", b"")
+        mock_cse.return_value = proc
+
+        uw_test = universalwrapper.uw_test
+        output = await uw_test.async_test()
+        await output
+        mock_cse.assert_called_with("uw-test", "test", stdout=ANY, stderr=ANY)
+
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.enable_async = True
+        output = await uw_test.async_test2()
+        await output
+        mock_cse.assert_called_with("uw-test", "test2", stdout=ANY, stderr=ANY)
+
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.enable_async = True
+        output = await uw_test.test3()
+        await output
+        mock_cse.assert_called_with("uw-test", "test3", stdout=ANY, stderr=ANY)
+
+        uw_test = universalwrapper.uw_test
+        uw_test.uw_settings.enable_async = True
+        output = await uw_test.async_a.b.async_c()
+        await output
+        mock_cse.assert_called_with("uw-test", "a", "b", "c", stdout=ANY, stderr=ANY)
+
+        proc.returncode = 1
+        with self.assertRaises(
+            subprocess.CalledProcessError,
+        ):
+            output = await uw_test.async_a.b.async_c()
+            await output
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/UniversalWrapper/UniversalWrapper-2.1.tar.gz/UniversalWrapper-2.1/universalwrapper/universal_wrapper.py
@@ -0,0 +1,292 @@
+# Copyright 2022 by Bas de Bruijne
+# All rights reserved.
+# Universal Wrapper comes with ABSOLUTELY NO WARRANTY, the writer can not be
+# held responsible for any problems caused by the use of this module.
+
+import asyncio
+import json
+import logging
+import shlex
+import subprocess
+import yaml
+
+from copy import copy
+from typing import Union, List, Dict
+
+
+class UWSettings:
+    __freeze = False
+
+    def __init__(self) -> None:
+        """Loads default uw settings"""
+        self.cmd: str = ""  # Base command
+        self.divider: str = "-"  # String to replace "_" with in commands
+        self.class_divider: str = " "  # String to place in between classes
+        self.flag_divider: str = "-"  # String to replace "_" with in flags
+        self.input_add: Dict[str:int] = {}  # {extra command, index where to add it}
+        self.input_move: Dict[str:int] = {}  # {extra command, index where to move it}
+        self.input_custom: List[str] = []  # custom command: e.g. "command.reverse()"
+        self.output_decode: bool = True  # Decode output to str
+        self.output_yaml: bool = False  # Parse yaml from output
+        self.output_json: bool = False  # Parse json from output
+        self.output_splitlines: bool = False  # Split lines of output
+        self.output_custom: List[str] = []  # custom command: e.g. "output.reverse()"
+        self.debug: bool = False  # Don't run commands but instead print the command
+        self.enable_async: bool = False  # Globally enable asyncio
+        self.double_dash: bool = True  # Use -- instead of - for multi-character flags
+
+        self.cmd_chain: List[str] = []
+        self.__freeze: bool = True
+
+    def __setattr__(self, key: str, value: object) -> None:
+        """Prevents the creating of misspelled uw_settings
+
+        :param key: uw_settings key to change
+        :param value: value to change key to
+        """
+        if self.__freeze and not hasattr(self, key):
+            functions = [item for item in dir(self) if not item.startswith("_")]
+            raise ImportError(f"Valid settings are limited to {functions}")
+        if key == "divider" and hasattr(self, key):
+            self._reset_command(value)
+        object.__setattr__(self, key, value)
+
+    def _reset_command(self, divider: str = None) -> None:
+        """Resets cmd_chain to its original value
+
+        When a command is chained, its chain history is stored in cmd_chain. After that
+        command has been called the chain needs to be reset to the original command.
+        :param divider: The new divider, only applicable if the divider has changed
+        """
+        if divider:
+            self.cmd = self.cmd.replace(self.divider, divider)
+        self.cmd_chain = self.cmd.split(" ")
+
+    def _update_command(self, value: str) -> None:
+        """Add new value to the cmd_chain
+
+        :param value: value to add to the cmd_chain
+        """
+        self.cmd_chain = (
+            (
+                f"{' '.join(self.cmd_chain)}{self.class_divider}"
+                f"{value.replace('_', self.divider)}"
+            )
+            .replace("_", self.divider)
+            .split(" ")
+        )
+
+
+class UniversalWrapper:
+    def __init__(self, cmd: str, uw_settings: UWSettings = None, **kwargs) -> None:
+        """Loads the default settings and changes the settings if requested
+
+        :param cmd: Base command for the class
+        :uw_settings: Pre-configured UWSettings object
+        :kwargs: {key: value} UWSettings to configure
+        """
+        if uw_settings:
+            self.uw_settings = uw_settings
+        else:
+            self.uw_settings = UWSettings()
+            for key, value in kwargs.items():
+                setattr(self.uw_settings, key, value)
+        self.uw_settings.cmd = cmd.replace("_", self.uw_settings.divider)
+        self.uw_settings._reset_command()
+        self._flags_to_remove = []
+
+    def __call__(self, *args: Union[int, str], **kwargs: Union[int, str]) -> str:
+        """Receives the users commands and directs them to the right functions
+
+        :param args: collection of non-keyword arguments for the shell call
+        :param kwargs: collection of keyword arguments for the shell call, can
+        either be `key = value` for `--key value` or `key = True` for `--key`
+        :returns: Response of the shell call
+        """
+        command = self.uw_settings.cmd_chain[:]
+        self.uw_settings._reset_command()
+        command = self._check_async(command)
+        command.extend(self._generate_command(*args, **kwargs))
+        command = self._input_modifier(command)
+        if self._root:
+            command = ["sudo"] + command
+        cmd = shlex.split(" ".join(command), posix=False)
+        if self.uw_settings.debug:
+            print(f"Generated command:\n{cmd}")
+            return
+        logging.debug("Calling shell command '{cmd}'")
+        if self._enable_async:
+            return self._async_run_cmd(cmd)
+        else:
+            return self._run_cmd(cmd)
+
+    def _check_async(self, command: List[str]) -> List[str]:
+        """Checks the command for the "async_" keyword
+
+        :param command: Initial command with possible "async_" keyword
+        :returns: command with keyword removed. Async enablement is stored in
+        self._enable_async
+        """
+        self._enable_async = self.uw_settings.enable_async
+        for i, cmd in enumerate(command):
+            if cmd.startswith(f"async{self.uw_settings.divider}"):
+                logging.debug("Async keyword found")
+                command[i] = cmd.replace(f"async{self.uw_settings.divider}", "")
+                self._enable_async = True
+        return command
+
+    def _generate_command(
+        self, *args: Union[int, str], **kwargs: Union[int, str]
+    ) -> List[str]:
+        """Transforms the args and kwargs to bash arguments and flags
+
+        :param args: collection of non-keyword arguments for the shell call
+        :param kwargs: collection of keyword arguments for the shell call
+        :returns: Shell call
+        """
+        command = []
+        self._root = False
+        for string in args:
+            if " " in str(string):
+                string = f"'{string}'"
+            command.append(str(string))
+        for key, values in kwargs.items():
+            if key == "root" and values is True:
+                self._root = True
+            else:
+                if type(values) != list:
+                    values = [values]
+                for value in values:
+                    if value is False:
+                        self._flags_to_remove.append(self._add_dashes(key))
+                    else:
+                        command.append(self._add_dashes(key))
+                        command[-1] += f" {value}" * (not value is True)
+        return command
+
+    def _add_dashes(self, flag: str) -> str:
+        """Adds the right number of dashes for the bash flags based on the
+        convention that single lettered flags get a single dash and multi-
+        lettered flags get a double dash
+
+        :param flag: flag to add dashes to
+        :returns: flag with dashes
+        """
+        if len(str(flag)) > 1 and self.uw_settings.double_dash:
+            return f"--{flag.replace('_', self.uw_settings.flag_divider)}"
+        else:
+            return f"-{flag}"
+
+    def _input_modifier(self, command: List[str]) -> List[str]:
+        """Handles the input modifiers, e.g. adding and moving commands
+
+        :param command: List of initial commands
+        :returns: Modified list of commands based on uw_settings input
+        modifiers
+        """
+        for input_command, index in self.uw_settings.input_add.items():
+            if not input_command.split(" ")[0] in self._flags_to_remove:
+                command = self._insert_command(command, input_command, index)
+        self._flags_to_remove = []
+        for move_command, index in self.uw_settings.input_move.items():
+            cmd = [cmd.split(" ")[0] for cmd in command]
+            if move_command in cmd:
+                command_index = cmd.index(move_command)
+                popped_command = command.pop(command_index)
+                command = self._insert_command(command, popped_command, index)
+        for cmd in self.uw_settings.input_custom:
+            exec(cmd)
+        return command
+
+    def _insert_command(
+        self, command: List[str], input_command: str, index: int
+    ) -> List[str]:
+        """Combines list.append and list.inset to a continues insert function
+
+        :param command: Initial command to add items to
+        :param input_command: Item to add to command
+        :param index: Index to add command, must be in range(0, len(command))
+        or -1
+        :returns: Modified command
+        """
+        if index == -1:
+            command.append(input_command)
+            return command
+        elif index < 0:
+            index += 1
+        command.insert(index, input_command)
+        return command
+
+    def _run_cmd(self, cmd: List[str]) -> str:
+        """Forwards the generated command to subprocess and handles
+        error displaying
+
+        :param: List of string which combined make the shell command
+        :returns: Output of shell command
+        """
+        try:
+            output = subprocess.check_output(cmd)
+        except subprocess.CalledProcessError as e:
+            print(e.output.decode())
+            raise
+        return self._output_modifier(output)
+
+    async def _async_run_cmd(self, cmd: List[str]) -> str:
+        """Forwards the generated command to async subprocess and
+        handles error displaying
+
+        :param: List of string which combined make the shell command
+        :returns: Output of shell command
+        """
+        proc = await asyncio.create_subprocess_exec(
+            *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+
+        async def _output(proc):
+            stdout, stderr = await proc.communicate()
+            if proc.returncode == 0:
+                return self._output_modifier(stdout)
+            print(stderr.decode())
+            raise subprocess.CalledProcessError(proc.returncode, cmd, stdout, stderr)
+
+        return _output(proc)
+
+    def _output_modifier(self, output: str) -> str:
+        """Modifies the subprocess' output according to uw_settings
+
+        :param output: string to modify, e.g. parse
+        :returns: modified output
+        """
+        if self.uw_settings.output_decode:
+            output = output.decode()
+        if self.uw_settings.output_yaml:
+            try:
+                output = yaml.safe_load(output)
+            except Exception as e:
+                logging.warning("Parse yaml failed")
+                logging.warning(e)
+        if self.uw_settings.output_json:
+            try:
+                output = json.loads(output)
+            except Exception as e:
+                logging.warning("Parse json failed")
+                logging.warning(e)
+        if self.uw_settings.output_splitlines:
+            output = output.splitlines()
+        for cmd in self.uw_settings.output_custom:
+            exec(cmd)
+        return output
+
+    def __getattr__(self, attr: str) -> object:
+        """Handles the creation of (sub)classes
+
+        :param attr: next section of command to construct
+        :returns: universalwrapper class
+        """
+        self.uw_settings._update_command(attr)
+        return self
+
+
+def __getattr__(attr):
+    """Redirects all traffic to UniversalWrapper"""
+    return UniversalWrapper(attr)
