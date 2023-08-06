cfc189be542df7dd17f4ddc855df4d24cc3a3ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:48 +0000

    Add package RelevanceAI-Workflows-Core/0.1.9/RelevanceAI%20Workflows%20Core-0.1.9.tar.gz

---
+++ packages/RelevanceAI-Workflows-Core/RelevanceAI%20Workflows%20Core-0.1.9.tar.gz/RelevanceAI Workflows Core-0.1.9/workflows_core/workflow/abstract_workflow.py
@@ -0,0 +1,71 @@
+import uuid
+import warnings
+from typing import Optional
+from workflows_core.dataset.dataset import Dataset
+
+from workflows_core.engine.abstract_engine import AbstractEngine
+from workflows_core.workflow.context_manager import WorkflowContextManager
+from workflows_core.operator.abstract_operator import AbstractOperator
+
+
+class AbstractWorkflow:
+    def __init__(
+        self,
+        engine: AbstractEngine,
+        job_id: Optional[str] = None,
+        name: Optional[str] = None,
+        **kwargs,
+    ):
+        self._name = "Workflow" if name is None else name
+        self._engine = engine
+
+        if job_id is None:
+            job_id = str(uuid.uuid4())
+            warnings.warn(f"No job id supplied, using {job_id}")
+
+        self._job_id = job_id
+
+        # make it backwards compatible to avoid duplicate parameters being in the
+        # same function call
+        if "job_id" in kwargs:
+            kwargs.pop("job_id")
+
+        if "workflow_id" in kwargs:
+            kwargs.pop("workflow_id")
+
+        self._kwargs = kwargs
+        self._api = engine.dataset.api
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def engine(self) -> AbstractEngine:
+        return self._engine
+
+    @property
+    def dataset(self) -> Dataset:
+        return self.engine.dataset
+
+    @property
+    def operator(self) -> AbstractOperator:
+        return self.engine.operator
+
+    def run(self):
+        with WorkflowContextManager(
+            workflow_name=self._name,
+            job_id=self._job_id,
+            engine=self.engine,
+            dataset=self.dataset,
+            operator=self.operator,
+            **self._kwargs,
+        ):
+            self.engine()
+        return
+
+    def get_status(self):
+        return self._api._get_workflow_status(self._job_id)
+
+
+Workflow = AbstractWorkflow
