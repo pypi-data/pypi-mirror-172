b2cdcf9116d30f9596e4b35afd50372948178be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:04 +0000

    Add package math-calc/0.0.1/math_calc-0.0.1.tar.gz

---
+++ packages/math-calc/math_calc-0.0.1.tar.gz/math_calc-0.0.1/math_calc/calc.py
@@ -0,0 +1,7 @@
+from math_calc.consts import PI
+
+def sumar(num1: float, num2: float):
+	return num1 + num2
+
+def area_circulo(radio: float):
+        return radio * radio * PI
---
+++ packages/math-calc/math_calc-0.0.1.tar.gz/math_calc-0.0.1/test.py
@@ -0,0 +1,6 @@
+from math_calc import consts
+from math_calc.calc import *
+
+print ( str( consts.PI ) )
+print ( str( sumar ( 4,5 )) )
+print ( str( area_circulo( 18) ) )
