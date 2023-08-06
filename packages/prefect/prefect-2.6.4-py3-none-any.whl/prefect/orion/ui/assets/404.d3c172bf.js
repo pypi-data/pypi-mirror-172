243c7527b3369690d9cf4f1e5f01da9d3a486cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:53 +0000

    Add package prefect/2.6.4/prefect-2.6.4-py3-none-any.whl

---
+++ packages/prefect/prefect-2.6.4-py3-none-any.whl/prefect/orion/ui/assets/404.d3c172bf.js
@@ -0,0 +1,1 @@
+import{d as e,e as o,o as a,f as r,g as s,b9 as t}from"./index.dae84138.js";const p=e({__name:"404",setup(n){return o("Page Not Found"),(u,c)=>(a(),r(s(t)))}});export{p as default};
---
+++ packages/prefect/prefect-2.6.4-py3-none-any.whl/prefect/orion/ui/assets/useToast.2b3da93b.js
@@ -0,0 +1,1 @@
+import{ae as a,a4 as n}from"./index.dae84138.js";function u(){const o=[];return a(()=>o.forEach(s=>s.dismiss())),(...s)=>{const t=n(...s);return o.push(t),t}}export{u};
