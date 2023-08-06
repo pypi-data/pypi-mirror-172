a72736c2ed90581d3000b41a582dbe2d826dd825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:10 +0000

    Add package dbfaker-sqlserver-plugin/0.1.0/dbfaker_sqlserver_plugin-0.1.0.tar.gz

---
+++ packages/dbfaker-sqlserver-plugin/dbfaker_sqlserver_plugin-0.1.0.tar.gz/dbfaker_sqlserver_plugin-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dbfaker_sqlserver_plugin',
+    version='0.1.0',
+    description='An project to make fake data dump based on sqlserver database.',
+    long_description='coming soon...',
+    author='lucaslima18',
+    author_email='lucas.ala1999@gmail.com',
+    packages=['sqlServerPlugin'],
+    python_requires='>=3.8,<4.0'
+)
