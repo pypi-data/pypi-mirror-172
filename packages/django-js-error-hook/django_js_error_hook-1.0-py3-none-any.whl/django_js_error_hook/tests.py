f0c04ce08421159f380ff673314d70c914fb4dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:12 +0000

    Add package django-js-error-hook/1.0/django_js_error_hook-1.0-py3-none-any.whl

---
+++ packages/django-js-error-hook/django_js_error_hook-1.0-py3-none-any.whl/django_js_error_hook/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class JSErrorHookTestCase(TestCase):
+    """Test project views."""
+
+    def test_error_handler_view(self):
+        """A POST should log the error"""
+        response = self.client.post(
+            reverse("js-error-handler"),
+            {
+                "context": (
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+                    "(KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+                ),
+                "details": "Description of the error by the browser javascript engine.",
+            },
+        )
+        self.assertEqual(response.status_code, 200)
