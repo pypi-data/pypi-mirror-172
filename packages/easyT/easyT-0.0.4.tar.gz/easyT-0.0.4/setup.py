25e4a76875f3c7365c9980b0693862f420519bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:53 +0000

    Add package easyT/0.0.4/easyT-0.0.4.tar.gz

---
+++ packages/easyT/easyT-0.0.4.tar.gz/easyT-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+import setuptools
+
+long_description = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+with open("requirements.txt", "r") as requirements_file:
+    requirements = requirements_file.readlines()
+
+setuptools.setup(
+    name='easyT',
+    version='0.0.4',
+    license='MIT',
+    author="Joao Paulo Euko",
+    url='https://github.com/Joaopeuko/easyT',
+    keywords=["binance", "spot", "algotrading", "cryptocurrency", "automatic trade", "automated trade"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=[requirement for requirement in requirements],
+    entry_points={
+        "console_scripts": [
+            "easyt = easyT.file_generator:main",
+
+        ],
+    },
+)
