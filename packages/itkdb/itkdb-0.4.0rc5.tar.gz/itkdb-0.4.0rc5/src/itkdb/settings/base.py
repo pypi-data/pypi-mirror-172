c3f8f5deba45496fabe24d0c9aac502ecd552364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:35 +0000

    Add package itkdb/0.4.0rc5/itkdb-0.4.0rc5.tar.gz

---
+++ packages/itkdb/itkdb-0.4.0rc5.tar.gz/itkdb-0.4.0rc5/src/itkdb/settings/base.py
@@ -0,0 +1,14 @@
+SIMPLE_SETTINGS = {"OVERRIDE_BY_ENV": True}
+ITKDB_ACCESS_CODE1 = ""
+ITKDB_ACCESS_CODE2 = ""
+ITKDB_ACCESS_SCOPE = (
+    "openid https://uuapp.plus4u.net/ucl-itkpd-maing01/dcb3f6d1f130482581ba1e7bbe34413c"
+)
+ITKDB_ACCESS_AUDIENCE = (
+    "https://uuapp.plus4u.net/ucl-itkpd-maing01/dcb3f6d1f130482581ba1e7bbe34413c"
+)
+ITKDB_AUTH_URL = "https://uuidentity.plus4u.net/uu-oidc-maing02/bb977a99f4cc4c37a2afce3fd599d0a7/oidc/"
+ITKDB_SITE_URL = (
+    "https://uuapp.plus4u.net/ucl-itkpd-maing01/dcb3f6d1f130482581ba1e7bbe34413c/"
+)
+ITKDB_CASSETTE_LIBRARY_DIR = "tests/integration/cassettes"
