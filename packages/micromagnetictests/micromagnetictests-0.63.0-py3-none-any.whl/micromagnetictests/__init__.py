079d5c2005a895e319e61ea01bc09d3c5b462fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:39 +0000

    Add package micromagnetictests/0.63.0/micromagnetictests-0.63.0-py3-none-any.whl

---
+++ packages/micromagnetictests/micromagnetictests-0.63.0-py3-none-any.whl/micromagnetictests/__init__.py
@@ -0,0 +1,26 @@
+"""Test computational magnetism tools."""
+import pkg_resources
+import pytest
+
+import micromagnetictests.calculatortests
+
+from .get_tests import get_tests
+
+__version__ = pkg_resources.get_distribution(__name__).version
+
+
+def test():
+    """Run all package tests.
+
+    Examples
+    --------
+    1. Run all tests.
+
+    >>> import micromagnetictests as mt
+    ...
+    >>> # mt.test()
+
+    """
+    return pytest.main(
+        ["-v", "--pyargs", "micromagnetictests", "-l"]
+    )  # pragma: no cover
