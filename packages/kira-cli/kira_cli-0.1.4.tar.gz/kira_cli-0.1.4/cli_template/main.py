06fc4d078678ba401982e2c4277cebf504b368df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:02 +0000

    Add package kira-cli/0.1.4/kira_cli-0.1.4.tar.gz

---
+++ packages/kira-cli/kira_cli-0.1.4.tar.gz/kira_cli-0.1.4/cli_template/main.py
@@ -0,0 +1,20 @@
+import typer
+
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    Setup git-leaks automatically
+    """
+
+
+@app.command()
+def initialize(name: str):
+    """
+    Setup Git-Leaks
+    """
+    typer.echo("Git-Leaks setup successfully, we can all go home")
+
