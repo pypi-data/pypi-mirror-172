5815567ec2f31da73cca53c2374fd23c0d0124ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:39 +0000

    Add package servo-serial/0.0.1/servo_serial-0.0.1.tar.gz

---
+++ packages/servo-serial/servo_serial-0.0.1.tar.gz/servo_serial-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='servo_serial',
+    version='0.0.1',
+    packages=['servo_serial'],
+    url='https://github.com/Adam-Software/Servo-serial',
+    license='MIT',
+    author='vertigra',
+    author_email='a@nesterof.com',
+    description='Common class for connect to servo',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_requires=['feetech-servo-sdk'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Other',
+        'Programming Language :: Python :: 3'
+    ]
+)
