0803362fc269779084b954a3ae1bfdbb814e59e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:52 +0000

    Add package pupil-apriltags/1.0.4.post8/pupil_apriltags-1.0.4.post8-cp37-cp37m-win_amd64.whl

---
+++ packages/pupil-apriltags/pupil_apriltags-1.0.4.post8-cp37-cp37m-win_amd64.whl/pupil_apriltags/__init__.py
@@ -0,0 +1,35 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_1_0():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pupil_apriltags.libs'))
+    if sys.version_info[:2] >= (3, 8) and not os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')) or sys.version_info[:2] >= (3, 10):
+        os.add_dll_directory(libs_dir)
+    else:
+        from ctypes import WinDLL
+        with open(os.path.join(libs_dir, '.load-order-pupil_apriltags-1.0.4.post8')) as file:
+            load_order = file.read().split()
+        for lib in load_order:
+            WinDLL(os.path.join(libs_dir, lib))
+
+
+_delvewheel_init_patch_1_1_0()
+del _delvewheel_init_patch_1_1_0
+# end delvewheel patch
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pupil-apriltags")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+from .bindings import Detection, Detector
+
+__all__ = ["Detector", "Detection"]
