9a9b72686d05d855a0e6b3bb57ff6175c92db9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:05 +0000

    Add package tinytable/0.13.0/tinytable-0.13.0.tar.gz

---
+++ packages/tinytable/tinytable-0.13.0.tar.gz/tinytable-0.13.0/tinytable/functional/edit.py
@@ -0,0 +1,3 @@
+from tinytim.edit import drop_row, edit_row_values, edit_row_items, edit_row_items_inplace, edit_row_values_inplace, edit_column
+from tinytim.edit import drop_column_inplace, drop_column, drop_row_inplace, drop_label_inplace, edit_value_inplace, drop_label
+from tinytim.edit import edit_value
