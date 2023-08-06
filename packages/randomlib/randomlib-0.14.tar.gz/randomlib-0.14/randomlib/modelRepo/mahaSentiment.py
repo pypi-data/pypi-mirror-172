56c12cd5dd3de87a980af39aeafdf9242eda3b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:03 +0000

    Add package randomlib/0.14/randomlib-0.14.tar.gz

---
+++ packages/randomlib/randomlib-0.14.tar.gz/randomlib-0.14/randomlib/modelRepo/mahaSentiment.py
@@ -0,0 +1,25 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
+import pandas as pd
+
+class SentimentModel:
+    Models = {
+        'MarathiSentiment': 'l3cube-pune/MarathiSentiment'
+    }
+
+    def __init__(self, modelName='MarathiSentiment'):
+        self.modelName = modelName
+        self.modelRoute = SentimentModel.Models[self.modelName]
+        self.tokenizer = AutoTokenizer.from_pretrained(self.modelRoute)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.modelRoute)
+        self.classifier = pipeline('text-classification',
+                              model=self.model, tokenizer=self.tokenizer)
+    def getPolarityScore(self, text):
+        result = self.classifier(text)
+        df = pd.DataFrame.from_dict(result)
+        return df
+
+    def listModels():
+        modelElements = SentimentModel.Models
+        for i in modelElements:
+            print(i, ": ", modelElements[i], "\n")
