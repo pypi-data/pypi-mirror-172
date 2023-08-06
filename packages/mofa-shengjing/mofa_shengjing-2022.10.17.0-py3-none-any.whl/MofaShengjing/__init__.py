37e7423db9587506a667aab58db0d7ccd104b1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:39 +0000

    Add package mofa-shengjing/2022.10.17.0/mofa_shengjing-2022.10.17.0-py3-none-any.whl

---
+++ packages/mofa-shengjing/mofa_shengjing-2022.10.17.0-py3-none-any.whl/MofaShengjing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mofa-shengjing
+https://github.com/apachecn/mofa-shengjing"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.17.0"
