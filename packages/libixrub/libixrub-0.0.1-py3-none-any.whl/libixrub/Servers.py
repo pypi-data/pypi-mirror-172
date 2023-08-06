6809c47265535d51e61f4947e7df6fb1ada5a1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:59 +0000

    Add package libixrub/0.0.1/libixrub-0.0.1-py3-none-any.whl

---
+++ packages/libixrub/libixrub-0.0.1-py3-none-any.whl/libixrub/Servers.py
@@ -0,0 +1,53 @@
+
+class SetClines:
+	
+	web = {
+		"app_name"    : "Main",
+		"app_version" : "4.1.4",
+		"platform"    : "Web",
+		"package"     : "web.rubika.ir",
+		"lang_code"   : "fa"
+	}
+
+	android = {
+	    "app_name":"Main",
+		"app_version":"3.0.2",
+		"platform":"Android",
+		"package":"ir.resaneh1.iptv",
+		"lang_code":"fa"
+	}
+	
+class Server:
+	
+	matnadress = [
+	        'https://messengerg2c17.iranlms.ir',
+	        'https://messengerg2c1.iranlms.ir',
+	        'https://messengerg2c64.iranlms.ir',
+			'https://messengerg2c13.iranlms.ir',
+			'https://messengerg2c26.iranlms.ir'
+			]
+
+
+	filesadress = [
+			'https://messengerg2c26.iranlms.ir',
+			'https://messengerg2c75.iranlms.ir',
+			'https://messengerg2c21.iranlms.ir',
+			'https://messengerg2c53.iranlms.ir',
+			'https://messengerg2c32.iranlms.ir',
+			'https://messengerg2c54.iranlms.ir',
+			'https://messengerg2c68.iranlms.ir',
+			'https://messengerg2c60.iranlms.ir',
+			'https://messengerg2c1.iranlms.ir',
+			'https://messengerg2c64.iranlms.ir',
+			'https://messengerg2c39.iranlms.ir'
+			]
+			
+	guid = [
+			'c0HGkO0951a2f9159b86470742c0b5d0'
+			'c0Ee9X09008b057804dadf8f941e305a',
+			'c0Btyq095a83abe72ecf41080c6f1c35',
+			'c0Ee9X09008b057804dadf8f941e305a',
+			'c0Btyq095a83abe72ecf41080c6f1c35',
+			'c0wPne0739ac17009a2dae7869e9a0f5',
+			'c0RSKL05e95414cec64d48b54f2e943e'
+			]
