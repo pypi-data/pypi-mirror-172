27364741b531177f3c93616911e84b6b52d32660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:45 +0000

    Add package distributionsJH/0.1/distributionsJH-0.1.tar.gz

---
+++ packages/distributionsJH/distributionsJH-0.1.tar.gz/distributionsJH-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsJH',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsJH'],
+      zip_safe=False)
