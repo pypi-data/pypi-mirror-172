292de8ac766097b69b48abf470abe7b659b2961a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:46 +0000

    Add package 5-Rakoto031-upload-to-pypi/0.1/5_Rakoto031_upload_to_pypi-0.1.tar.gz

---
+++ packages/5-Rakoto031-upload-to-pypi/5_Rakoto031_upload_to_pypi-0.1.tar.gz/5_Rakoto031_upload_to_pypi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='5_Rakoto031_upload_to_pypi',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['5_Rakoto031_upload_to_pypi'],
+      zip_safe=False)
