6e171138c6bdadc9f22f438ef93940d1c0d5e414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:09 +0000

    Add package pciw.py/0.8.2/pciw.py-0.8.2-py3-none-any.whl

---
+++ packages/pciw.py/pciw.py-0.8.2-py3-none-any.whl/pciw/__init__.py
@@ -0,0 +1,13 @@
+from .pciw import \
+    get_cpu_info, \
+    get_cpu_status, \
+    get_ram_info, \
+    get_gpu_info, \
+    get_monitors_info, \
+    get_motherboard_info, \
+    get_bios_info, \
+    get_ngpu_info, \
+    get_sound_device_info
+
+__name__ = "pciw.py"
+__version__ = "0.8.2"
