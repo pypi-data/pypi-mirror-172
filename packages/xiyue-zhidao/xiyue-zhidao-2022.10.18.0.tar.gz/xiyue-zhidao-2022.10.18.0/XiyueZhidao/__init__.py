5946002a09240e6377b153dafbd8001b75bc690f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:22 +0000

    Add package xiyue-zhidao/2022.10.18.0/xiyue-zhidao-2022.10.18.0.tar.gz

---
+++ packages/xiyue-zhidao/xiyue-zhidao-2022.10.18.0.tar.gz/xiyue-zhidao-2022.10.18.0/XiyueZhidao/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiyue-zhidao
+https://github.com/apachecn/xiyue-zhidao"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.18.0"
