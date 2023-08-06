fefbbb6e0c86728a3f306debbf366f9820d47409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:37 +0000

    Add package evdspy/1.0.17.2/evdspy-1.0.17.2.tar.gz

---
+++ packages/evdspy/evdspy-1.0.17.2.tar.gz/evdspy-1.0.17.2/evdspy/EVDSlocal/examples/examples_onload.py
@@ -0,0 +1,7 @@
+from ..common.common_imports import *
+
+# from ..initial.try_loading_initial_commands import *
+
+def examples_get(get_function ):
+    """ It will try to make request using default api key and proxy if set """
+    get_function()
