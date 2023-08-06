71b069c1e4bbc45a1a5887655576680908a88125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:28 +0000

    Add package shapelyM/0.0.5b2/shapelym-0.0.5b2-py3-none-any.whl

---
+++ packages/shapelyM/shapelym-0.0.5b2-py3-none-any.whl/shapelyM/__init__.py
@@ -0,0 +1,9 @@
+from typing import Any, List
+
+__version__ = "0.0.5-beta2"
+
+from shapelyM.linear_reference import LineProjection
+from shapelyM.measureLineString import MeasureLineString
+from shapelyM.measurePoint import MeasurePoint
+
+__all__: List[Any] = ["MeasurePoint", "MeasureLineString", "LineProjection"]
